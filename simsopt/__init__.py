62618148574f4ae8c6069c2ec569b800a93e198e
Matt Landreman <mattland@umd.edu>
2020-10-03 12:16:23 -0400

    Added documentation to README

--- simsopt/__init__.py
+++ simsopt/__init__.py
@@ -4,6 +4,9 @@ from .surface import *
 from .functions import *
 from .dofs import *
 from .least_squares_problem import *
-from .vmec import *
+try:
+    from .vmec import *
+except:
+    print('Unable to load VMEC module, so some functionality will not be available')
 
 #all = ['Parameter']
