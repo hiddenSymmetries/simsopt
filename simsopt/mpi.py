4a170e75e040d39d46d00bf9a86d60753b38bf3b
Matt Landreman <mattland@umd.edu>
2020-10-14 15:18:57 -0400

    Coded up MpiPartition

--- simsopt/mpi.py
+++ simsopt/mpi.py
@@ -1,4 +1,5 @@
 """
+This module contains functions related to MPI parallelization.
 """
 
 import numpy as np
@@ -75,3 +76,64 @@ def worker_loop(dofs):
             raise ValueError('Unexpected data in worker_loop')
         
     logger.debug('worker_loop end')
+
+class MpiPartition():
+    def __init__(self, ngroups=None, comm_world=MPI.COMM_WORLD):
+        self.comm_world = comm_world
+        self.rank_world = comm_world.Get_rank()
+        self.nprocs_world = comm_world.Get_size()
+        self.proc0_world = (self.rank_world == 0)
+        
+        if ngroups is None:
+            ngroups = self.nprocs_world
+        # Force ngroups to be in the range [1, nprocs_world]
+        if ngroups < 1:
+            ngroups = 1
+            logger.info('Raising ngroups to 1')
+        if ngroups > self.nprocs_world:
+            ngroups = self.nprocs_world
+            logger.info('Lowering ngroups to {}'.format(ngroups))
+        self.ngroups = ngroups
+
+        self.group = int(np.floor((self.rank_world * ngroups) / self.nprocs_world))
+
+        # Set up the "groups" communicator:
+        self.comm_groups = self.comm_world.Split(color=self.group, key=self.rank_world)
+        self.rank_groups = self.comm_groups.Get_rank()
+        self.nprocs_groups = self.comm_groups.Get_size()
+        self.proc0_groups = (self.rank_groups == 0)
+
+        # Set up the "leaders" communicator:
+        if self.proc0_groups:
+            color = 0
+        else:
+            color = MPI.UNDEFINED
+        self.comm_leaders = self.comm_world.Split(color=color, key=self.rank_world)
+        if self.proc0_groups:
+            self.rank_leaders = self.comm_leaders.Get_rank()
+            self.nprocs_leaders = self.comm_leaders.Get_size()
+        else:
+            # We are not allowed to query the rank from procs that are
+            # not members of comm_leaders.
+            self.rank_leaders = -1
+            self.nprocs_leaders = -1
+
+    def write(self):
+        """ Dump info about the MPI configuration """
+        columns = ["rank_world","nprocs_world","group","ngroups","rank_groups","nprocs_groups","rank_leaders","nprocs_leaders"]
+        data = [self.rank_world , self.nprocs_world , self.group , self.ngroups , self.rank_groups , self.nprocs_groups , self.rank_leaders , self.nprocs_leaders]
+
+        # Print header row
+        width = max(len(s) for s in columns) + 1
+        print(",".join(s.rjust(width) for s in columns))
+        # Each processor sends their data to proc0_world, and
+        # proc0_world writes the result to the file in order.
+        if self.proc0_world:
+            print(",".join(str(s).rjust(width) for s in data))
+            for tag in range(1, self.nprocs_world):
+                data = self.comm_world.recv(tag=tag)
+                print(",".join(str(s).rjust(width) for s in data))
+        else:
+            tag = self.rank_world
+            self.comm_world.send(data, 0, tag)
+            
