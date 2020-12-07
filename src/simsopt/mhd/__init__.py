623c5066cd142847f019cbe9445060cfb807474a
Matt Landreman <mattland@umd.edu>
2020-12-07 11:49:35 -0500

    Changing way optional vmec is handled

--- src/simsopt/mhd/__init__.py
+++ src/simsopt/mhd/__init__.py
@@ -1,3 +1,5 @@
+from .vmec import vmec_found, Vmec
+
 #try:
 #    import vmec
 #except BaseException as err:
@@ -5,14 +7,14 @@
 #    print('Reason VMEC module was not loaded:')
 #    print(err)
 
-try:
-    from .vmec import Vmec
-    vmec_found = True
-except ImportError as err:
-    vmec_found = False
-    print('Unable to load VMEC module, so some functionality will not be available.')
-    print('Reason VMEC module was not loaded:')
-    print(err)
+#try:
+#    from .vmec import Vmec
+#    vmec_found = True
+#except ImportError as err:
+#    vmec_found = False
+#    print('Unable to load VMEC module, so some functionality will not be available.')
+#    print('Reason VMEC module was not loaded:')
+#    print(err)
 
 #if vmec_found:
 #    from .vmec import Vmec
