621fe2bd09bc611988510890ba1b9e4336069f87
Matt Landreman <mattland@umd.edu>
2020-12-07 10:33:44 -0500

    Now using the standalone VMEC2000 module instead of VMEC from STELLOPT. Tests all pass on Matt's laptop. The CI will need to be updated before it works.

--- src/simsopt/mhd/__init__.py
+++ src/simsopt/mhd/__init__.py
@@ -1,6 +1,18 @@
+#try:
+#    import vmec
+#except BaseException as err:
+#    print('Unable to load VMEC module, so some functionality will not be available.')
+#    print('Reason VMEC module was not loaded:')
+#    print(err)
+
 try:
-    from .vmec import *
-except BaseException as err:
+    from .vmec import Vmec
+    vmec_found = True
+except ImportError as err:
+    vmec_found = False
     print('Unable to load VMEC module, so some functionality will not be available.')
     print('Reason VMEC module was not loaded:')
     print(err)
+
+#if vmec_found:
+#    from .vmec import Vmec
