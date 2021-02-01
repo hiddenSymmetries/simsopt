e983efdb7cd1d3f092f65b1a3bf9096cac8c9881
Matt Landreman <mattland@umd.edu>
2021-02-01 15:16:04 -0500

    Coded up more of Boozer class. Added booz_xform to CI.

--- src/simsopt/mhd/boozer.py
+++ src/simsopt/mhd/boozer.py
@@ -9,6 +9,7 @@ Boozer coordinates, and an optimization target for quasisymmetry.
 
 import logging
 import os.path
+from typing import Union, Iterable
 import numpy as np
 
 booz_xform_found = True
@@ -18,14 +19,37 @@ except:
     booz_xform_found = False
 
 from simsopt.core import Optimizable
+from simsopt.mhd import Vmec
 
 logger = logging.getLogger(__name__)
+
+# This next function can be deleted I think.
+def closest_index(grid: Iterable[float], val: float) -> int:
+    """
+    Given a grid of values, find the grid point that is closest to an
+    abitrary value.
+
+    Args:
+        grid: A list of values.
+        val: We will return the index of the closest grid point to this value.
+    """
+    return np.argmin(np.abs(grid - val))
+
 
 class Boozer(Optimizable):
     """
     This class handles the transformation to Boozer coordinates.
+
+    A Boozer instance maintains a set "s", which is a registry of the
+    surfaces on which other objects want Boozer-coordinate data. When
+    the run() method is called, the Boozer transformation is carried
+    out on all these surfaces. The registry can be cleared at any time
+    by setting the s attribute to {}.
     """
-    def __init__(self, equil, mpol=None, ntor=None):
+    def __init__(self,
+                 equil: Vmec,
+                 mpol: int = None,
+                 ntor: int = None) -> None:
         """
         Constructor
         """
@@ -37,8 +61,126 @@ class Boozer(Optimizable):
         self.mpol = mpol
         self.ntor = ntor
         self.bx = booz_xform.Booz_xform()
+        self.s = set()
         self.need_to_run_code = True
 
+    def register(self, s: Union[float, Iterable[float]]) -> None:
+        """
+        This function is called by objects that depend on this Boozer
+        object, to indicate that they will want Boozer data on the
+        given set of surfaces.
+
+        Args:
+            s: 1 or more surfaces on which Boozer data will be requested.
+        """
+        # Force input surface data to be a set:
+        try:
+            ss = set(s)
+        except:
+            ss = {s}
+
+        for new_s in ss:
+            if new_s < 0 or new_s > 1:
+                raise ValueError("Normalized toroidal flux values s must lie"
+                                 "in the interval [0, 1]")
+        logger.info("Adding entries to Boozer registry: {}".format(ss))
+        self.s = self.s.union(ss)
+        self.need_to_run_code = True
+
+    def run(self):
+        """
+        Run booz_xform on all the surfaces that have been registered.
+        """
+        if not self.need_to_run_code:
+            logger.info("Boozer.run() called but no need to re-run Boozer transformation.")
+            return
+        s = sorted(list(self.s))
+        logger.info("Preparing to run Boozer transformation. Registry:{}".format(s))
+
+        if isinstance(self.equil, Vmec):
+            self.equil.run()
+            wout = self.equil.VMEC.wout # Shorthand
+
+            # Get the half-grid points that are closest to the requested values
+            ns = wout.ns
+            s_full = np.linspace(0, 1, ns)
+            ds = s_full[1] - s_full[0]
+            s_half = s_full[1:] - 0.5 * ds
+
+            compute_surfs = []
+            self.s_to_index = dict()
+            for ss in s:
+                index = np.argmin(np.abs(s_half - ss))
+                compute_surfs.append(index)
+                self.s_to_index[ss] = index
+                
+            # Eliminate any duplicates
+            compute_surfs = sorted(list(set(compute_surfs)))
+            logger.info("compute_surfs={}".format(compute_surfs))
+
+            # Transfer data in memory from VMEC to booz_xform
+            self.bx.asym = bool(wout.lasym)
+            self.bx.nfp = wout.nfp
+
+            self.bx.mpol = wout.mpol
+            self.bx.ntor = wout.ntor
+            self.bx.mnmax = wout.mnmax
+            self.bx.xm = wout.xm
+            self.bx.xn = wout.xn
+            
+            self.bx.mpol_nyq = wout.xm_nyq[-1]
+            self.bx.ntor_nyq = wout.xn_nyq[-1] / nfp
+            self.bx.mnmax_nyq = wout.mnmax_nyq
+            self.bx.xm_nyq = wout.xm_nyq
+            self.bx.xn_nyq = wout.xn_nyq
+
+            if wout.lasym:
+                rmns = wout.rmns
+                zmnc = wout.zmnc
+                lmnc = wout.lmnc
+                bmns = wout.bmns
+                bsubumns = wout.bsubumns
+                bsubvmns = wout.bsubvmns
+            else:
+                # For stellarator-symmetric configs, the asymmetric
+                # arrays have not been initialized.
+                arr = np.array([[]])
+                rmns = arr
+                zmnc = arr
+                lmnc = arr
+                bmns = arr
+                bsubumns = arr
+                bsubvmns = arr
+                
+            # For quantities that depend on radius, booz_xform handles
+            # interpolation and discarding the rows of zeros:
+            self.bx.init_from_vmec(wout.ns,
+                                   wout.iotas,
+                                   wout.rmnc,
+                                   rmns,
+                                   zmnc,
+                                   wout.zmns,
+                                   lmnc,
+                                   wout.lmns,
+                                   wout.bmnc,
+                                   bmns,
+                                   wout.bsubumnc,
+                                   bsubumns,
+                                   wout.bsubvmnc,
+                                   bsubvmns)
+            self.bx.compute_surfs = compute_surfs
+
+        else:
+            # Cases for SPEC, GVEC, etc could be added here.
+            raise ValueError("equil is not an equilibrium type supported by"
+                             "Boozer")
+        
+        logger.info("About to call booz_xform.Booz_xform.run().")
+        self.bx.run()
+        logger.info("Returned from calling booz_xform.Booz_xform.run().")
+        self.need_to_run_code = False
+        
+        
 class Quasisymmetry(Optimizable):
     """
     This class is used to compute the departure from quasisymmetry on
@@ -46,7 +188,7 @@ class Quasisymmetry(Optimizable):
     """
     def __init__(self,
                  boozer: Boozer,
-                 s: float,
+                 s: Union[float, Iterable[float]],
                  m: int,
                  n: int,
                  normalization: str = "B00",
@@ -63,75 +205,89 @@ class Quasisymmetry(Optimizable):
             weight: An option for a m- or n-dependent weight to be applied to the bmnc amplitudes.
         """
         self.boozer = boozer
-        self.s = s
         self.m = m
         self.n = n
         self.normalization = normalization
         self.weight = weight
         self.depends_on = ['boozer']
 
-    def J(self) -> float:
+        # If s is not already iterable, make it so:
+        try:
+            iter(s)
+        except:
+            s = [s]
+        self.s = s
+        boozer.register(s)
+
+
+    def J(self) -> Iterable:
         """
         Carry out the calculation of the quasisymmetry error.
         """
         # The next line is the expensive part of the calculation:
-        bmnc = self.boozer.bmnc(self.s)
-        xm = self.boozer.xm
-        xn = self.boozer.xn / self.boozer.nfp
-
-        if self.m != 0 and self.m != 1:
-            raise ValueError("m for quasisymmetry should be 0 or 1.")
-
-        # Find the indices of the symmetric modes:
-        if self.n == 0:
-            # Quasi-axisymmetry
-            symmetric = (xn == 0)
-            
-        elif self.m == 0:
-            # Quasi-poloidal symmetry
-            symmetric = (xm == 0)
-            
-        else:
-            # Quasi-helical symmetry
-            symmetric = (xm * self.n + xn * self.m == 0)
-            # Stellopt takes the "and" of this with mod(xm, self.m),
-            # which does not seem necessary since self.m must be 1 to
-            # get here.
-        nonsymmetric = np.logical_not(symmetric)
-
-        # Scale all bmnc modes so the average |B| is 1 or close to 1:
+        self.boozer.run()
         
-        if self.normalization == "B00":
-            # Normalize by the (m,n) = (0,0) mode amplitude:
-            assert xm[0] == 0
-            assert xn[0] == 0
-            bnorm = bmnc[0]
+        symmetry_error = []
+        for js, s in enumerate(self.s):
+            index = self.boozer.s_to_index[s]
+            bmnc = self.boozer.bx.bmnc_b[:, index]
+            xm = self.boozer.bx.xm_b
+            xn = self.boozer.bx.xn_b / self.boozer.bx.nfp
 
-        elif self.normalization == "symmetric":
-            # Normalize by sqrt(sum_{symmetric modes} B{m,n}^2)
-            temp = bmnc[symmetric]
-            bnorm = np.sqrt(np.dot(temp, temp))
-            
-        else:
-            raise ValueError("Unrecognized value for normalization in Quasisymmetry")
-        
-        bmnc /= bnorm
+            if self.m != 0 and self.m != 1:
+                raise ValueError("m for quasisymmetry should be 0 or 1.")
 
-        # Apply any weight that depends on m and/or n:
-            
-        if self.weight == "even":
-            # Evenly weight each bmnc mode. Normalize by the m=n=0 mode on that surface.
-            return bmnc[nonsymmetric]
-        
-        elif self.weight == "stellopt":
-            # Stellopt applies a m-dependent radial weight:
-            s_used = self.s # This line may need to be changed
-            rad_sigma = np.full_like(xm, s_used * s_used)
-            rad_sigma[xm < 3] = s_used
-            rad_sigma[xm == 3] = s_used ** 1.5
-            temp = bmnc / rad_sigma
-            return temp[nonsymmetric]
+            # Find the indices of the symmetric modes:
+            if self.n == 0:
+                # Quasi-axisymmetry
+                symmetric = (xn == 0)
 
-        else:
-            raise ValueError("Unrecognized value for weight in Quasisymmetry")
-            
+            elif self.m == 0:
+                # Quasi-poloidal symmetry
+                symmetric = (xm == 0)
+
+            else:
+                # Quasi-helical symmetry
+                symmetric = (xm * self.n + xn * self.m == 0)
+                # Stellopt takes the "and" of this with mod(xm, self.m),
+                # which does not seem necessary since self.m must be 1 to
+                # get here.
+            nonsymmetric = np.logical_not(symmetric)
+
+            # Scale all bmnc modes so the average |B| is 1 or close to 1:
+
+            if self.normalization == "B00":
+                # Normalize by the (m,n) = (0,0) mode amplitude:
+                assert xm[0] == 0
+                assert xn[0] == 0
+                bnorm = bmnc[0]
+
+            elif self.normalization == "symmetric":
+                # Normalize by sqrt(sum_{symmetric modes} B{m,n}^2)
+                temp = bmnc[symmetric]
+                bnorm = np.sqrt(np.dot(temp, temp))
+
+            else:
+                raise ValueError("Unrecognized value for normalization in Quasisymmetry")
+
+            bmnc /= bnorm
+
+            # Apply any weight that depends on m and/or n:
+
+            if self.weight == "even":
+                # Evenly weight each bmnc mode. Normalize by the m=n=0 mode on that surface.
+                symmetry_error.append(bmnc[nonsymmetric])
+
+            elif self.weight == "stellopt":
+                # Stellopt applies a m-dependent radial weight:
+                s_used = self.s # This line may need to be changed
+                rad_sigma = np.full_like(xm, s_used * s_used)
+                rad_sigma[xm < 3] = s_used
+                rad_sigma[xm == 3] = s_used ** 1.5
+                temp = bmnc / rad_sigma
+                symmetry_error.append(temp[nonsymmetric])
+
+            else:
+                raise ValueError("Unrecognized value for weight in Quasisymmetry")
+
+        return np.array(symmetry_error).flatten()
--- tests/simsopt/mhd/test_boozer.py
+++ tests/simsopt/mhd/test_boozer.py
@@ -2,16 +2,16 @@ import unittest
 import numpy as np
 import os
 import logging
-from simsopt.mhd.boozer import Quasisymmetry
+from simsopt.mhd.boozer import Quasisymmetry, booz_xform_found
 from . import TEST_DIR
 
 logger = logging.getLogger(__name__)
 #logging.basicConfig(level=logging.DEBUG)
 
-class MockBoozer():
+class MockBoozXform():
     """
     This class exists only for testing the Quasisymmetry class.  It
-    returns similar data to the real Boozer class, but without doing a
+    returns similar data to the real Booz_xform class, but without doing a
     real calculation.
     """
     def __init__(self, mpol, ntor, nfp):
@@ -23,13 +23,33 @@ class MockBoozer():
             index = ntor + 1 + (ntor * 2 + 1) * (m - 1)
             xm[index : index + (ntor * 2 + 1)] = m
             xn[index : index + (ntor * 2 + 1)] = np.arange(-ntor, ntor + 1)
-        self.xm = xm
-        self.xn = xn * nfp
-        self.mnmax = mnmax
+        self.xm_b = xm
+        self.xn_b = xn * nfp
+        self.mnmax_b = mnmax
         self.nfp = nfp
+        arr1 = np.arange(1.0, mnmax + 1) * 10
+        arr2 = arr1 + 1
+        arr2[0] = 100
+        self.bmnc_b = np.stack((arr1, arr2)).transpose()
+        print('bmnc_b:')
+        print(self.bmnc_b)
+        print('booz_xform_found:', booz_xform_found)
 
-    def bmnc(self, s):
-        return np.arange(1.0, self.mnmax + 1)
+class MockBoozer():
+    """
+    This class exists only for testing the Quasisymmetry class.  It
+    returns similar data to the real Boozer class, but without doing a
+    real calculation.
+    """
+    def __init__(self, mpol, ntor, nfp):
+        self.bx = MockBoozXform(mpol, ntor, nfp)
+        self.s_to_index = {0: 0, 1: 1}
+        
+    def register(self, s):
+        pass
+
+    def run(self):
+        pass
 
 class QuasisymmetryTests(unittest.TestCase):
     def test_quasisymmetry_residuals(self):
@@ -37,16 +57,22 @@ class QuasisymmetryTests(unittest.TestCase):
         Verify that the correct residual vectors are returned for QA, QP, and QH
         """
         b = MockBoozer(3, 2, 4)
-        # xm:   [  0  0  0  1  1  1  1  1  2   2   2   2   2   3   3   3   3   3]
-        # xn:   [  0  4  8 -8 -4  0  4  8 -8  -4   0   4   8  -8  -4   0   4   8]
-        # bmnc: [ 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180]
-        s = 1
+        # xm:    [  0  0  0  1  1  1  1  1  2   2   2   2   2   3   3   3   3   3]
+        # xn:    [  0  4  8 -8 -4  0  4  8 -8  -4   0   4   8  -8  -4   0   4   8]
+        # bmnc: [[ 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180]
+        # bmnc:  [100 21 31 41 51 61 71 81 91 101 111 121 131 141 151 161 171 181]]
         
         # QA
-        q = Quasisymmetry(b, s, 1, 0, "B00", "even")
+        s = 0; q = Quasisymmetry(b, s, 1, 0, "B00", "even")
         np.testing.assert_allclose(q.J(), [2, 3, 4, 5, 7, 8, 9, 10, 12, 13, 14, 15, 17, 18])
+        s = 1; q = Quasisymmetry(b, s, 1, 0, "B00", "even")
+        np.testing.assert_allclose(q.J(), [.21, .31, .41, .51, .71, .81, .91, 1.01, 1.21, 1.31, 1.41, 1.51, 1.71, 1.81])
+        s = (0, 1); q = Quasisymmetry(b, s, 1, 0, "B00", "even")
+        np.testing.assert_allclose(q.J(), [2, 3, 4, 5, 7, 8, 9, 10, 12, 13, 14, 15, 17, 18,
+                                           .21, .31, .41, .51, .71, .81, .91, 1.01, 1.21, 1.31, 1.41, 1.51, 1.71, 1.81])
 
         # QP
+        s = 0
         q = Quasisymmetry(b, s, 0, 1, "B00", "even")
         np.testing.assert_allclose(q.J(), [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
 
@@ -57,6 +83,8 @@ class QuasisymmetryTests(unittest.TestCase):
         # QH, opposite "chirality"
         q = Quasisymmetry(b, s, 1, -1, "B00", "even")
         np.testing.assert_allclose(q.J(), [2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18])
+
+    #@unittest.skipIf(not booz_xform_found, "booz_xform python package not found")
 
 if __name__ == "__main__":
     unittest.main()
