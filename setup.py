e2b03560a639d138f00450295e934e42bb70403c
Bharat Medasani <mbkumar@gmail.com>
2021-03-02 14:03:00 -0500

    Updated setup framework to get simsgeopp extension go to correct location

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import sys
+from setuptools import find_packages
 
 try:
     from skbuild import setup
@@ -7,4 +8,23 @@ except ImportError:
           ' or you need to install the PEP 518 requirements in pyproject.toml yourself', file=sys.stderr)
     raise
 
-setup()
+setup(
+    name="simsopt",
+    version="0.0.3",
+    install_requires=[
+        "numpy >= v1.19.0",
+        "monty >= v4.0.0",
+        "mpi4py >= 3.0.3",
+        "jax >= 0.2.4",
+        "jaxlib >= 0.1.56",
+        "scipy >= 1.5.4",
+        "scikit-build >= 0.11.1"
+    ],
+    package_dir={'': 'src'},
+    packages= find_packages(
+        where='src'),
+    include_package_data=True,
+    package_data = {
+        "": ["input.default"]
+    }
+)
