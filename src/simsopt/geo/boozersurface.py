d24272f435955a8039acda822ba9b4cb037af719
Andrew Giuliani <giuliani@cims.nyu.edu>
2021-03-23 11:06:19 -0400

    surfaces

--- src/simsopt/geo/boozersurface.py
+++ src/simsopt/geo/boozersurface.py
@@ -54,7 +54,7 @@ class BoozerSurface():
             
             val = 0.5 * np.sum(r**2) 
             dval = np.sum(r[:, None]*J, axis=0) 
-            print(val, np.linalg.norm(dval))    
+            #print(val, np.linalg.norm(dval))    
             return val,dval
 
         elif derivatives == 2:
@@ -91,9 +91,9 @@ class BoozerSurface():
 
 
     def BoozerConstrained(self,xl, derivatives = 0):
-        sdofs = xl[:-2]
-        iota = xl[-2]
-        lm = xl[-1]
+        sdofs = xl[:-4]
+        iota = xl[-4]
+        lm = xl[-3:]
         s = self.surface
         bs = self.bs
         s.set_dofs(sdofs)
@@ -101,15 +101,23 @@ class BoozerSurface():
         if derivatives == 0:
             r, Js, Jiota = BoozerSurfaceResidual(s, iota, bs, derivatives = 1)
             J = np.concatenate((Js, Jiota), axis=1)
-            dl  = np.zeros( (xl.shape[0]-1,) )
+            dl  = np.zeros( (xl.shape[0]-3,) )
     
             l            = self.label.J()
             dl[:-1]      = self.label.dJ_by_dsurfacecoefficients()
+            dry  = np.zeros((xl.shape[0]-3,) )
+            drz  = np.zeros((xl.shape[0]-3,) )
             g = [l-self.targetlabel]
+            ry = (s.gamma()[0,0,1] - 0.)
+            rz = (s.gamma()[0,0,2] - 0.)
+            dry[:-1] = s.dgamma_by_dcoeff()[0,0,1,:]
+            drz[:-1] = s.dgamma_by_dcoeff()[0,0,2,:]
     
             res = np.zeros( xl.shape )
-            res[:-1] = np.sum(r[:, None]*J, axis=0) - lm * dl 
-            res[-1]  = g[0]
+            res[:-3] = np.sum(r[:, None]*J, axis=0)  - lm[-3] * dl - lm[-2] * dry - lm[-1] * drz 
+            res[-3]  = g[0]
+            res[-2] = ry
+            res[-1] = rz
             return res
         elif derivatives == 1:
             r, Js, Jiota, Hs, Hsiota, Hiota = BoozerSurfaceResidual(s, iota, bs, derivatives = 2)
@@ -118,29 +126,42 @@ class BoozerSurface():
             col = np.concatenate( (Hsiota, Hiota), axis = 1)
             H = np.concatenate( (Htemp,col[...,None,:]), axis = 1) 
      
-            dl  = np.zeros( (xl.shape[0]-1,) )
-            d2l = np.zeros( (xl.shape[0]-1, xl.shape[0]-1 ) )
+            dl  =  np.zeros((xl.shape[0]-3,) )
+            dry  = np.zeros((xl.shape[0]-3,) )
+            drz  = np.zeros((xl.shape[0]-3,) )
+            d2l =  np.zeros((xl.shape[0]-3, xl.shape[0]-3) )
     
             l            = self.label.J()
             dl[:-1]      = self.label.dJ_by_dsurfacecoefficients()
             d2l[:-1,:-1] = self.label.d2J_by_dsurfacecoefficientsdsurfacecoefficients() 
             g = [l-self.targetlabel]
-    
+            ry = (s.gamma()[0,0,1] - 0.)
+            rz = (s.gamma()[0,0,2] - 0.)
+            dry[:-1] = s.dgamma_by_dcoeff()[0,0,1,:]
+            drz[:-1] = s.dgamma_by_dcoeff()[0,0,2,:]
+            
+
             res = np.zeros( xl.shape )
-            res[:-1] = np.sum(r[:, None]*J, axis=0) - lm * dl 
-            res[-1]  = g[0]
+            res[:-3] = np.sum(r[:, None]*J, axis=0) - lm[-3] * dl - lm[-2] * dry - lm[-1] * drz 
+            res[-3]  = g[0]
+            res[-2]  = ry 
+            res[-1]  = rz
             
             dres = np.zeros( (xl.shape[0], xl.shape[0]) )
-            dres[:-1,:-1] = J.T @ J + np.sum( r[:,None,None] * H, axis = 0) - lm*d2l
-            dres[:-1, -1] = -dl
-            dres[ -1,:-1] =  dl
-            
+            dres[:-3,:-3] = J.T @ J + np.sum( r[:,None,None] * H, axis = 0) - lm[-3]*d2l
+            dres[:-3, -3] = -dl
+            dres[:-3, -2] = -dry
+            dres[:-3, -1] = -drz
+
+            dres[ -3,:-3] =  dl
+            dres[ -2,:-3] =  dry
+            dres[ -1,:-3] =  drz
             return res,dres
  
     def minimizeBoozerScalarizedLBFGS(self,tol = 1e-3, maxiter = 1000, constraint_weight = 1., iota = 0.):
         """
         This function tries to find the surface that approximately solves
-        min || f(x) ||^2_2 + cweight * (label - labeltarget)^2
+        min || f(x) ||^2_2 + 0.5 * constraint_weight * (label - labeltarget)^2
         where || f(x)||^2_2 is the sum of squares of the Boozer residual at
         the quadrature points.  This is done using LBFGS.
         """
@@ -148,12 +169,12 @@ class BoozerSurface():
         s = self.surface
         x = np.concatenate((s.get_dofs(), [iota]))
         res = minimize(lambda x: self.BoozerConstrainedScalarized(x,derivatives = 1, constraint_weight = constraint_weight), \
-                              x, jac=True, method='L-BFGS-B', options={'maxiter': maxiter, 'gtol' : tol})
+                x, jac=True, method='L-BFGS-B', options={'maxiter': maxiter, 'ftol' : tol,'gtol' : tol, 'maxcor' : 50})
         print(res.fun)
         s.set_dofs(res.x[:-1])
-        iota = x[-1]
+        iota = res.x[-1]
         return s,iota
-
+       
 
     def minimizeBoozerScalarizedNewton(self,tol = 1e-12, maxiter = 10,constraint_weight = 1., iota = 0.):
         """
@@ -173,12 +194,11 @@ class BoozerSurface():
             norm = np.linalg.norm(dval) 
             print(norm)
             i = i +1
-        ipdb.set_trace()
         s.set_dofs(x[:-1])
         iota = x[-1]
         return s, iota
 
-    def minimizeBoozerConstrainedNewton(self, tol = 1e-12, maxiter = 10, iota = 0., lm = 0.):
+    def minimizeBoozerConstrainedNewton(self, tol = 1e-12, maxiter = 10, iota = 0., lm = [0.,0.,0.] ):
         """
         This function solves the constrained optimization problem
         min || f(x) ||^2_2
@@ -186,19 +206,24 @@ class BoozerSurface():
         using Lagrange multipliers and Newton's method.
         """
         s = self.surface 
-        xl = np.concatenate( (s.get_dofs(), [iota], [lm]) )
+        xl = np.concatenate( (s.get_dofs(), [iota], lm) )
         val,dval = self.BoozerConstrained(xl, derivatives = 1)
         norm =np.linalg.norm(val) 
         i = 0   
         while i < maxiter and norm > tol:
             val,dval = self.BoozerConstrained(xl, derivatives = 1)
-            dx = np.linalg.solve(dval,val)
-            xl = xl - dx
+            if s.stellsym:
+                dx = np.linalg.solve(dval[:-2,:-2],val[:-2])
+                xl[:-2] = xl[:-2] - dx
+            else:
+                dx = np.linalg.solve(dval,val)
+                xl = xl - dx
             norm = np.linalg.norm(val)
             print( norm )
-        s.set_dofs(xl[:-2])
-        iota = xl[-2]
-        lm = xl[-1]
+            i = i + 1
+        s.set_dofs(xl[:-4])
+        iota = xl[-4]
+        lm = xl[-3:]
         return s, iota, lm
 
 
