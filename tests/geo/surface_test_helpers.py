b21f68be5404595a6ef154c4192e7f76036c7850
Andrew Giuliani <giuliani@cims.nyu.edu>
2021-03-25 15:07:31 -0400

    moved CoilCollection to simsopt.geo

--- tests/geo/surface_test_helpers.py
+++ tests/geo/surface_test_helpers.py
@@ -1,5 +1,6 @@
 from math import pi
 import numpy as np
+from simsopt.geo.coilcollection import CoilCollection
 from simsopt.geo.biotsavart import BiotSavart
 from simsopt.geo.surfacexyzfourier import SurfaceXYZFourier
 from simsopt.geo.surfacerzfourier import SurfaceRZFourier
@@ -10,37 +11,6 @@ from simsopt.geo.surfaceobjectives import ToroidalFlux
 from pathlib import Path
 TEST_DIR = (Path(__file__).parent / ".." / "test_files").resolve()
 
-
-class CoilCollection():
-    """
-    Given some input coils and currents, this performs the reflection and
-    rotation to generate a full set of stellarator coils.
-    """
-
-    def __init__(self, coils, currents, nfp, stellarator_symmetry):
-        self._base_coils = coils
-        self._base_currents = currents
-        self.coils = []
-        self.currents = []
-        flip_list = [False, True] if stellarator_symmetry else [False] 
-        self.map = []
-        self.current_sign = []
-        for k in range(0, nfp):
-            for flip in flip_list:
-                for i in range(len(coils)):
-                    if k == 0 and not flip:
-                        self.coils.append(self._base_coils[i])
-                        self.currents.append(self._base_currents[i])
-                    else:
-                        rotcoil = RotatedCurve(coils[i], 2*pi*k/nfp, flip)
-                        self.coils.append(rotcoil)
-                        self.currents.append(-self._base_currents[i] if flip else currents[i])
-                    self.map.append(i)
-                    self.current_sign.append(-1 if flip else +1)
-        dof_ranges = [(0, len(self._base_coils[0].get_dofs()))]
-        for i in range(1, len(self._base_coils)):
-            dof_ranges.append((dof_ranges[-1][1], dof_ranges[-1][1] + len(self._base_coils[i].get_dofs())))
-        self.dof_ranges = dof_ranges
 
 def get_ncsx_data(Nt_coils=25, Nt_ma=10, ppp=10):
     filename = TEST_DIR / 'NCSX_test_data' / 'NCSX_coil_coeffs.dat'
