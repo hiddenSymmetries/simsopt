f497b083563d951a9ceb77803ba25558df5986c0
Bharat Medasani <mbkumar@gmail.com>
2021-04-04 20:50:14 -0400

    RegisterMeta class added to register return functions

--- src/simsopt/core/util.py
+++ src/simsopt/core/util.py
@@ -73,6 +73,7 @@ class ImmutableId:
     """
     id: Integral
 
+
 class InstanceCounterMeta(type):
     """
     Metaclass to make instance counter not share count with descendants
@@ -84,5 +85,20 @@ class InstanceCounterMeta(type):
         super().__init__(name, bases, attrs)
         cls._ids = itertools.count(1)
 
-class InstanceCounterABCMeta(InstanceCounterMeta, ABCMeta):
+
+class RegisterMeta(type):
+    def __init__(cls, name, bases, attrs):
+        super().__init__(name, bases, attrs)
+        cls.return_fn_map = {}
+
+        def _register_return_fn(name):
+            def inner_register(f):
+                cls.return_fn_map[name] = f
+                return f
+            return inner_register
+
+        cls.register_return_fn = _register_return_fn
+
+
+class OptimizableMeta(InstanceCounterMeta, RegisterMeta, ABCMeta):
     pass
