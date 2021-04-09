6cba415c4642bf66b2fcae6d0e672bce02b6f784
Florian Wechsung <f.wechsung@gmail.com>
2021-04-09 14:44:12 -0400

    format and exception in MagneticField

--- src/simsopt/geo/magneticfield.py
+++ src/simsopt/geo/magneticfield.py
@@ -1,7 +1,12 @@
 import numpy as np
 
+
 class MagneticField():
-    '''Generic class that represents any magnetic field for which each magnetic field class inherits.'''
+
+    '''
+    Generic class that represents any magnetic field for which each magnetic
+    field class inherits.
+    '''
     def clear_cached_properties(self):
         self._B = None
         self._dB_by_dX = None
@@ -52,29 +57,24 @@ class MagneticField():
         return self._d2A_by_dXdX
 
     def compute(self, points, compute_derivatives=0):
-        self._B = None
-        if compute_derivatives >= 1:
-            self._dB_by_dX = None
-        if compute_derivatives >= 2:
-            self._d2B_by_dXdX = None
-        return self
+        raise NotImplementedError('Computation of B field not implemented. Needs to be overwritten by childclass.')
 
     def compute_A(self, points, compute_derivatives=0):
-        self._A = None
-        if compute_derivatives >= 1:
-            self._dA_by_dX = None
-        if compute_derivatives >= 2:
-            self._d2A_by_dXdX = None
-        return self
+        raise NotImplementedError('Computation of potential A not implemented. Needs to be overwritten by childclass.')
 
     def __add__(self, other):
-        return MagneticFieldSum([self,other])
+        return MagneticFieldSum([self, other])
+
 
 class MagneticFieldSum(MagneticField):
-    '''Class used to sum two or more magnetic field together.
-    It can either be called directly with a list of magnetic fields given as input and outputing another magnetic field with B, A and its derivatives added together or it can be called by summing magnetic fields classes as Bfield1 + Bfield1
     '''
-    def __init__(self,Bfields):
+    Class used to sum two or more magnetic field together.  It can either be
+    called directly with a list of magnetic fields given as input and outputing
+    another magnetic field with B, A and its derivatives added together or it
+    can be called by summing magnetic fields classes as Bfield1 + Bfield1
+    '''
+
+    def __init__(self, Bfields):
         self.Bfields = Bfields
         for Bfield in self.Bfields:
             if hasattr(Bfield, 'coils'):
@@ -89,16 +89,16 @@ class MagneticFieldSum(MagneticField):
 
     def compute(self, points, compute_derivatives=0):
         [Bfield.compute(points, compute_derivatives) for Bfield in self.Bfields]
-        self._B             = np.sum([Bfield._B for Bfield in self.Bfields],0)
+        self._B             = np.sum([Bfield._B for Bfield in self.Bfields], 0)
         if compute_derivatives >= 1:
-            self._dB_by_dX  = np.sum([Bfield._dB_by_dX for Bfield in self.Bfields],0)
+            self._dB_by_dX  = np.sum([Bfield._dB_by_dX for Bfield in self.Bfields], 0)
         if compute_derivatives >= 2:
-            self._d2B_by_dX = np.sum([Bfield._d2B_by_dX for Bfield in self.Bfields],0)
+            self._d2B_by_dX = np.sum([Bfield._d2B_by_dX for Bfield in self.Bfields], 0)
 
     def compute_A(self, points, compute_derivatives=0):
         [Bfield.compute_A(points, compute_derivatives) for Bfield in self.Bfields]
-        self._A             = np.sum([Bfield._A for Bfield in self.Bfields],0)
+        self._A             = np.sum([Bfield._A for Bfield in self.Bfields], 0)
         if compute_derivatives >= 1:
-            self._dA_by_dX  = np.sum([Bfield._dA_by_dX for Bfield in self.Bfields],0)
+            self._dA_by_dX  = np.sum([Bfield._dA_by_dX for Bfield in self.Bfields], 0)
         if compute_derivatives >= 2:
-            self._d2A_by_dX = np.sum([Bfield._d2A_by_dX for Bfield in self.Bfields],0)
+            self._d2A_by_dX = np.sum([Bfield._d2A_by_dX for Bfield in self.Bfields], 0)
