a012e407728a4845cb4cbdaed403e7464055e59f
Bharat Medasani <mbkumar@gmail.com>
2021-04-09 08:04:29 -0400

    Bug fixes in least squares for the new multi-fn-return framework

--- tests/core/test_new_least_squares.py
+++ tests/core/test_new_least_squares.py
@@ -10,20 +10,22 @@ from simsopt.core.new_least_squares import LeastSquaresProblem
 class LeastSquaresProblemTests(unittest.TestCase):
     def test_single_value_opt_in(self):
         iden = Identity()
-        lst = LeastSquaresProblem.from_sigma(iden, 3, 0.1)
+        lst = LeastSquaresProblem.from_sigma(3, 0.1, iden)
 
         iden.x = [17]
         correct_value = ((17 - 3) / 0.1) #** 2
-        self.assertAlmostEqual(np.abs(lst()[0]), correct_value, places=11)
+        self.assertAlmostEqual(np.abs(lst.residuals()[0]),
+                               correct_value,
+                               places=11)
 
         iden.x = [0]
-        term1 = LeastSquaresProblem.from_sigma(iden, 3, 2)
-        self.assertAlmostEqual(np.abs(term1()[0]), 1.5)
+        term1 = LeastSquaresProblem.from_sigma(3, 2, iden)
+        self.assertAlmostEqual(np.abs(term1.residuals()[0]), 1.5)
 
         term1.x = [10]
-        self.assertAlmostEqual(np.abs(term1()[0]), 3.5)
-        self.assertAlmostEqual(np.abs(term1(x=[0])), 1.5)
-        self.assertAlmostEqual(np.abs(term1(x=[5])), 1)
+        self.assertAlmostEqual(np.abs(term1.residuals()[0]), 3.5)
+        self.assertAlmostEqual(np.abs(term1.residuals(x=[0])), 1.5)
+        self.assertAlmostEqual(np.abs(term1.residuals(x=[5])), 1)
 
     def test_exceptions(self):
         """
@@ -34,45 +36,45 @@ class LeastSquaresProblemTests(unittest.TestCase):
 
         # sigma cannot be zero
         with self.assertRaises(ValueError):
-            lst = LeastSquaresProblem.from_sigma(iden, 3, 0)
+            lst = LeastSquaresProblem.from_sigma(3, 0, iden)
 
         # Weight cannot be negative
         with self.assertRaises(ValueError):
-            lst = LeastSquaresProblem(iden, 3, -1.0)
+            lst = LeastSquaresProblem(3, -1.0, iden)
 
     def test_multiple_funcs_single_input(self):
         iden1 = Identity(x=10)
         iden2 = Identity()
         # Objective function
         # f(x,y) = ((x - 3) / 2) ** 2 + ((y + 4) / 5) ** 2
-        lsp = LeastSquaresProblem.from_sigma([iden1, iden2], [3, -4], [2, 5])
-        self.assertAlmostEqual(np.abs(lsp()[0]), 3.5)
-        self.assertAlmostEqual(np.abs(lsp()[1]), 0.8)
+        lsp = LeastSquaresProblem.from_sigma([3, -4], [2, 5], [iden1, iden2])
+        self.assertAlmostEqual(np.abs(lsp.residuals()[0]), 3.5)
+        self.assertAlmostEqual(np.abs(lsp.residuals()[1]), 0.8)
         lsp.x = [5, -7]
-        self.assertAlmostEqual(np.abs(lsp()[0]), 1.0)
-        self.assertAlmostEqual(np.abs(lsp()[1]), 0.6)
-        self.assertAlmostEqual(np.abs(lsp([10, 0])[0]), 3.5)
-        self.assertAlmostEqual(np.abs(lsp([10, 0])[1]), 0.8)
-        self.assertAlmostEqual(np.abs(lsp([5, -7])[0]), 1.0)
-        self.assertAlmostEqual(np.abs(lsp([5, -7])[1]), 0.6)
+        self.assertAlmostEqual(np.abs(lsp.residuals()[0]), 1.0)
+        self.assertAlmostEqual(np.abs(lsp.residuals()[1]), 0.6)
+        self.assertAlmostEqual(np.abs(lsp.residuals([10, 0])[0]), 3.5)
+        self.assertAlmostEqual(np.abs(lsp.residuals([10, 0])[1]), 0.8)
+        self.assertAlmostEqual(np.abs(lsp.residuals([5, -7])[0]), 1.0)
+        self.assertAlmostEqual(np.abs(lsp.residuals([5, -7])[1]), 0.6)
 
     def test_parent_dof_transitive_behavior(self):
         iden1 = Identity()
         iden2 = Identity()
-        lsp = LeastSquaresProblem.from_sigma([iden1, iden2], [3, -4], [2, 5])
+        lsp = LeastSquaresProblem.from_sigma([3, -4], [2, 5], [iden1, iden2])
         iden1.x = [10]
-        self.assertAlmostEqual(np.abs(lsp()[0]), 3.5)
-        self.assertAlmostEqual(np.abs(lsp()[1]), 0.8)
+        self.assertAlmostEqual(np.abs(lsp.residuals()[0]), 3.5)
+        self.assertAlmostEqual(np.abs(lsp.residuals()[1]), 0.8)
 
     def test_least_squares_combination(self):
         iden1 = Identity()
         iden2 = Identity()
-        term1 = LeastSquaresProblem.from_sigma(iden1, 3, 2)
-        term2 = LeastSquaresProblem.from_sigma(iden2, -4, 5)
+        term1 = LeastSquaresProblem.from_sigma(3, 2, iden1)
+        term2 = LeastSquaresProblem.from_sigma(-4, 5, iden2)
         lsp = term1 + term2
         iden1.x = [10]
-        self.assertAlmostEqual(np.abs(lsp()[0]), 3.5)
-        self.assertAlmostEqual(np.abs(lsp()[1]), 0.8)
+        self.assertAlmostEqual(np.abs(lsp.residuals()[0]), 3.5)
+        self.assertAlmostEqual(np.abs(lsp.residuals()[1]), 0.8)
 
 
 if __name__ == "__main__":
