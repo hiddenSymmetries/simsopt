71d12b35e4a469411a7a07fd9c66e137f5a0ebf2
Andrew Giuliani <giuliani@cims.nyu.edu>
2021-04-27 11:07:37 -0400

    added another unit test

--- tests/geo/test_surface_objectives.py
+++ tests/geo/test_surface_objectives.py
@@ -59,75 +59,75 @@ def taylor_test2(f, df, d2f, x, epsilons=None, direction1=None, direction2 = Non
         err_old = err
     print("################################################################################")
 
-#class ToroidalFluxTests(unittest.TestCase):
-#    def test_toroidal_flux_is_constant(self):
-#        # this test ensures that the toroidal flux does not change, regardless
-#        # of the cross section (varphi = constant) across which it is computed
-#        s = get_exact_surface()
-#        coils, currents, ma = get_ncsx_data()
-#        stellarator = CoilCollection(coils, currents, 3, True)
-#        bs_tf = BiotSavart(stellarator.coils, stellarator.currents)
-#        
-#        gamma = s.gamma()
-#        num_phi = gamma.shape[0]
-#        
-#        tf_list = np.zeros( (num_phi,) )
-#        for idx in range(num_phi):
-#            tf = ToroidalFlux(s, bs_tf,idx = idx)
-#            tf_list[idx] = tf.J()
-#        mean_tf = np.mean(tf_list)
-#
-#        max_err = np.max( np.abs(mean_tf - tf_list) ) / mean_tf
-#        assert max_err < 1e-2
-#
-#    def test_toroidal_flux_first_derivative(self):
-#        for surfacetype in surfacetypes_list:
-#            for stellsym in stellsym_list:
-#                with self.subTest(surfacetype = surfacetype, stellsym=stellsym):
-#                    self.subtest_toroidal_flux1(surfacetype,stellsym)
-#    def test_toroidal_flux_second_derivative(self):
-#        for surfacetype in surfacetypes_list:
-#            for stellsym in stellsym_list:
-#                with self.subTest(surfacetype = surfacetype, stellsym=stellsym):
-#                    self.subtest_toroidal_flux2(surfacetype,stellsym)
-#
-#    def subtest_toroidal_flux1(self, surfacetype, stellsym):
-#        coils, currents, ma = get_ncsx_data()
-#        stellarator = CoilCollection(coils, currents, 3, True)
-#        bs_tf = BiotSavart(stellarator.coils, stellarator.currents)
-#        s = get_surface(surfacetype, stellsym)
-#        
-#        tf = ToroidalFlux(s, bs_tf)
-#        coeffs = s.get_dofs()
-#        
-#        def f(dofs):
-#            s.set_dofs(dofs)
-#            return tf.J()
-#        def df(dofs):
-#            s.set_dofs(dofs)
-#            return tf.dJ_by_dsurfacecoefficients() 
-#        taylor_test1(f, df, coeffs)
-# 
-#    def subtest_toroidal_flux2(self, surfacetype, stellsym):
-#        coils, currents, ma = get_ncsx_data()
-#        stellarator = CoilCollection(coils, currents, 3, True)
-#        bs = BiotSavart(stellarator.coils, stellarator.currents)
-#        s = get_surface(surfacetype, stellsym)
-#
-#        tf = ToroidalFlux(s, bs)
-#        coeffs = s.get_dofs()
-#        
-#        def f(dofs):
-#            s.set_dofs(dofs)
-#            return tf.J()
-#        def df(dofs):
-#            s.set_dofs(dofs)
-#            return tf.dJ_by_dsurfacecoefficients() 
-#        def d2f(dofs):
-#            s.set_dofs(dofs)
-#            return tf.d2J_by_dsurfacecoefficientsdsurfacecoefficients()
-#
-#        taylor_test2(f, df, d2f, coeffs) 
+class ToroidalFluxTests(unittest.TestCase):
+    def test_toroidal_flux_is_constant(self):
+        # this test ensures that the toroidal flux does not change, regardless
+        # of the cross section (varphi = constant) across which it is computed
+        s = get_exact_surface()
+        coils, currents, ma = get_ncsx_data()
+        stellarator = CoilCollection(coils, currents, 3, True)
+        bs_tf = BiotSavart(stellarator.coils, stellarator.currents)
+        
+        gamma = s.gamma()
+        num_phi = gamma.shape[0]
+        
+        tf_list = np.zeros( (num_phi,) )
+        for idx in range(num_phi):
+            tf = ToroidalFlux(s, bs_tf,idx = idx)
+            tf_list[idx] = tf.J()
+        mean_tf = np.mean(tf_list)
+
+        max_err = np.max( np.abs(mean_tf - tf_list) ) / mean_tf
+        assert max_err < 1e-2
+
+    def test_toroidal_flux_first_derivative(self):
+        for surfacetype in surfacetypes_list:
+            for stellsym in stellsym_list:
+                with self.subTest(surfacetype = surfacetype, stellsym=stellsym):
+                    self.subtest_toroidal_flux1(surfacetype,stellsym)
+    def test_toroidal_flux_second_derivative(self):
+        for surfacetype in surfacetypes_list:
+            for stellsym in stellsym_list:
+                with self.subTest(surfacetype = surfacetype, stellsym=stellsym):
+                    self.subtest_toroidal_flux2(surfacetype,stellsym)
+
+    def subtest_toroidal_flux1(self, surfacetype, stellsym):
+        coils, currents, ma = get_ncsx_data()
+        stellarator = CoilCollection(coils, currents, 3, True)
+        bs_tf = BiotSavart(stellarator.coils, stellarator.currents)
+        s = get_surface(surfacetype, stellsym)
+        
+        tf = ToroidalFlux(s, bs_tf)
+        coeffs = s.get_dofs()
+        
+        def f(dofs):
+            s.set_dofs(dofs)
+            return tf.J()
+        def df(dofs):
+            s.set_dofs(dofs)
+            return tf.dJ_by_dsurfacecoefficients() 
+        taylor_test1(f, df, coeffs)
+ 
+    def subtest_toroidal_flux2(self, surfacetype, stellsym):
+        coils, currents, ma = get_ncsx_data()
+        stellarator = CoilCollection(coils, currents, 3, True)
+        bs = BiotSavart(stellarator.coils, stellarator.currents)
+        s = get_surface(surfacetype, stellsym)
+
+        tf = ToroidalFlux(s, bs)
+        coeffs = s.get_dofs()
+        
+        def f(dofs):
+            s.set_dofs(dofs)
+            return tf.J()
+        def df(dofs):
+            s.set_dofs(dofs)
+            return tf.dJ_by_dsurfacecoefficients() 
+        def d2f(dofs):
+            s.set_dofs(dofs)
+            return tf.d2J_by_dsurfacecoefficientsdsurfacecoefficients()
+
+        taylor_test2(f, df, d2f, coeffs) 
 
 
 class NonQuasiAxiSymmetricComponentPenaltyTests(unittest.TestCase):
@@ -165,69 +165,47 @@ class NonQuasiAxiSymmetricComponentPenaltyTests(unittest.TestCase):
             dJ_by_dcoils = stellarator.reduce_coefficient_derivatives(dJ_by_dcoils)
             return dJ_by_dcoils
         taylor_test1(f, df, coeffs )
-#    def test_NonQuasiSymmetricComponentPenalty_exact(self):
-#        """
-#        This test verifies that the non-QS penalty term returns what we expect it to when
-#        we know what the quasisymmetric and non-quasisymmetric components of the field strength
-#        are.
-#        """
-#        coils, currents, ma = get_ncsx_data()
-#        stellarator = CoilCollection(coils, currents, 3, True)
-#        s = get_exact_surface()
-#        nphi = s.gamma().shape[0] 
-#        ntheta= s.gamma().shape[1] 
-#        quadpoints_phi   = s.quadpoints_phi
-#        quadpoints_theta = s.quadpoints_theta
-#
-#        grid_phi,grid_theta = np.meshgrid(quadpoints_phi, quadpoints_theta)
-#        grid_phi = grid_phi.T
-#        grid_theta = grid_theta.T
-#
-#        modes_phi   = 1 + (nphi-1)/2
-#        modes_theta = 1 + (ntheta-1)/2
-#
-#        bf_phi   = np.ones( (nphi,  nphi,ntheta) )
-#        bf_theta = np.ones( (ntheta,nphi,ntheta) )
-#        
-#        bf_phi[1:(nphi+1),:,:] = np.sin(2*np.pi * (1:(nphi+1)) ) 
-#        bf_phi[(nphi+1):,:,:]  = np.cos(2*np.pi * (1:(nphi+1)) ) 
-#        bf_theta[1:(ntheta+1),:,:] = np.sin(2*np.pi * (1:(ntheta+1)) ) 
-#        bf_theta[(ntheta+1):,:,:]  = np.cos(2*np.pi * (1:(ntheta+1)) ) 
-#       
-#        qs_idx1 = N*np.arange( min(modes_phi,modes_theta) )
-#        qs_idx2 =   np.arange( min(modes_phi,modes_theta) )
-# 
-#        amps = np.random.rand(nphi,ntheta)
-#        amps_non_qs = np.copy(amps)
-#        amps_non_qs[qs_idx1              ,qs_idx2                ] = 0.
-#        
-#        if N>0:
-#            amps_non_qs[qs_idx1[1:]+modes_phi,qs_idx2[1:]            ] = 0.
-#            amps_non_qs[qs_idx1[1:]          ,qs_idx2[1:]+modes_theta] = 0.
-#            amps_non_qs[qs_idx1[1:]+modes_phi,qs_idx2[1:]+modes_theta] = 0.
-#
-#        func = np.sum(amps[:,None,None] * bf_phi[:,None,...] * bf_theta[None,:,...],axis = 0)
-#        func_nonqs = np.sum(amps_non_qs[:,:,None,None] * bf_phi[:,None,...] * bf_theta[None,:,...],axis = (0,1))
-#        class BiotSavartTestField:
-#            def __init__(self,coils, currents):
-#                self.coils = coils
-#                self.currents = currents
-#            def set_points(self,x):
-#                self.points = points
-#                phi = np.linspace(0,1,nphi)
-#                theta = np.linspace(0,1,ntheta)
-#                self.phi,self.theta = np.meshgrid(phi,theta)
-#                self.phi = self.phi.T
-#                self.theta = self.theta.T
-#            def clear_cached_properties(self):
-#                self._B = None
-#            def B(self, compute_derivatives=0):
-#                self._B = 
-#
-#
-#        bs = BiotSavart(stellarator.coils, stellarator.currents)
-#
-#        non_qs = NonQuasiSymmetricComponentPenalty(s, bs, N = 0)
-#        coeffs = stellarator.get_dofs()
-       
+
+    def test_NonQuasiSymmetricComponentPenalty_exact(self):
+        """
+        This test verifies that the non-QS penalty term returns what we expect it to when
+        we know what the non-quasisymmetric component of the field is zero.
+        """
+        coils, currents, ma = get_ncsx_data()
+        stellarator = CoilCollection(coils, currents, 3, True)
+        s = get_exact_surface()
+        nphi = s.gamma().shape[0] 
+        ntheta= s.gamma().shape[1] 
+        quadpoints_phi   = s.quadpoints_phi
+        quadpoints_theta = s.quadpoints_theta
+
+        grid_phi,grid_theta = np.meshgrid(quadpoints_phi, quadpoints_theta)
+        grid_phi = grid_phi.T
+        grid_theta = grid_theta.T
+        class BiotSavartTestField:
+            def __init__(self,coils, currents):
+                self.coils = coils
+                self.currents = currents
+            def set_points(self,x):
+                self.points = x
+                phi = np.linspace(0,1,nphi)
+                theta = np.linspace(0,1,ntheta)
+                self.phi,self.theta = np.meshgrid(phi,theta)
+                self.phi = self.phi.T
+                self.theta = self.theta.T
+                self.clear_cached_properties()
+            def clear_cached_properties(self):
+                self._B = None
+            def B(self, compute_derivatives=0):
+                if self._B is None:
+                    assert compute_derivatives >= 0
+                    self._B = np.cos(self.theta).reshape((-1,1)) * np.ones( self.points.shape )+np.sin(self.theta).reshape((-1,1)) * np.ones( self.points.shape ) 
+                return self._B
+
+
+
+        bs = BiotSavartTestField(coils,currents)
+        non_qs = NonQuasiAxisymmetricComponentPenalty(s, bs)
+        assert np.abs(non_qs.J()) < 1e-14
+
 
