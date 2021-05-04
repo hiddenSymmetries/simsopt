fb2b06513fe7df4c5fb98df894af3bcdb245975a
Bharat Medasani <mbkumar@gmail.com>
2021-05-04 13:46:38 -0400

    MPI solve classes removed from top-level imports

--- src/simsopt/solve/__init__.py
+++ src/simsopt/solve/__init__.py
@@ -1,5 +1,4 @@
 from .serial import least_squares_serial_solve, serial_solve
 from .mpi import least_squares_mpi_solve, fd_jac_mpi
 
-__all__ = ['least_squares_serial_solve', 'serial_solve',
-           'least_squares_mpi_solve', 'fd_jac_mpi']
+__all__ = ['least_squares_serial_solve', 'serial_solve']
