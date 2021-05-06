18c33ccf44bc7e0b06012a9c1cc929e8fd73755d
Bharat Medasani <mbkumar@gmail.com>
2021-05-06 13:52:59 -0400

    Arguments order changes in graph based LeastSquaresProblem

--- tests/objectives/test_graph_least_squares.py
+++ tests/objectives/test_graph_least_squares.py
@@ -11,7 +11,7 @@ from simsopt.objectives.graph_least_squares import LeastSquaresProblem
 class LeastSquaresProblemTests(unittest.TestCase):
     def test_single_value_opt_in(self):
         iden = Identity()
-        lst = LeastSquaresProblem.from_sigma(3, 0.1, iden)
+        lst = LeastSquaresProblem.from_sigma(3, 0.1, opts_in=iden)
 
         iden.x = [17]
         correct_value = ((17 - 3) / 0.1)  # ** 2
@@ -20,7 +20,7 @@ class LeastSquaresProblemTests(unittest.TestCase):
                                places=11)
 
         iden.x = [0]
-        term1 = LeastSquaresProblem.from_sigma(3, 2, iden)
+        term1 = LeastSquaresProblem.from_sigma(3, 2, opts_in=iden)
         self.assertAlmostEqual(np.abs(term1.residuals()[0]), 1.5)
 
         term1.x = [10]
@@ -37,18 +37,18 @@ class LeastSquaresProblemTests(unittest.TestCase):
 
         # sigma cannot be zero
         with self.assertRaises(ValueError):
-            lst = LeastSquaresProblem.from_sigma(3, 0, iden)
+            lst = LeastSquaresProblem.from_sigma(3, 0, opts_in=iden)
 
         # Weight cannot be negative
         with self.assertRaises(ValueError):
-            lst = LeastSquaresProblem(3, -1.0, iden)
+            lst = LeastSquaresProblem(3, -1.0, opts_in=iden)
 
     def test_multiple_funcs_single_input(self):
         iden1 = Identity(x=10)
         iden2 = Identity()
         # Objective function
         # f(x,y) = ((x - 3) / 2) ** 2 + ((y + 4) / 5) ** 2
-        lsp = LeastSquaresProblem.from_sigma([3, -4], [2, 5], [iden1, iden2])
+        lsp = LeastSquaresProblem.from_sigma([3, -4], [2, 5], opts_in=[iden1, iden2])
         self.assertAlmostEqual(np.abs(lsp.residuals()[0]), 3.5)
         self.assertAlmostEqual(np.abs(lsp.residuals()[1]), 0.8)
         lsp.x = [5, -7]
@@ -62,7 +62,7 @@ class LeastSquaresProblemTests(unittest.TestCase):
     def test_parent_dof_transitive_behavior(self):
         iden1 = Identity()
         iden2 = Identity()
-        lsp = LeastSquaresProblem.from_sigma([3, -4], [2, 5], [iden1, iden2])
+        lsp = LeastSquaresProblem.from_sigma([3, -4], [2, 5], opts_in=[iden1, iden2])
         iden1.x = [10]
         self.assertAlmostEqual(np.abs(lsp.residuals()[0]), 3.5)
         self.assertAlmostEqual(np.abs(lsp.residuals()[1]), 0.8)
@@ -70,8 +70,8 @@ class LeastSquaresProblemTests(unittest.TestCase):
     def test_least_squares_combination(self):
         iden1 = Identity()
         iden2 = Identity()
-        term1 = LeastSquaresProblem.from_sigma(3, 2, iden1)
-        term2 = LeastSquaresProblem.from_sigma(-4, 5, iden2)
+        term1 = LeastSquaresProblem.from_sigma(3, 2, opts_in=[iden1])
+        term2 = LeastSquaresProblem.from_sigma(-4, 5, opts_in=[iden2])
         lsp = term1 + term2
         iden1.x = [10]
         self.assertAlmostEqual(np.abs(lsp.residuals()[0]), 3.5)
