f52220ceff604b9ccb9eb8915d264b6a49f27911
Bharat Medasani <mbkumar@gmail.com>
2021-05-06 14:55:09 -0400

    Line breaks added for function and class arguments in sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,6 +61,12 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = [
+    'css/parameter_linebreak.css',
+    'css/equation_numbers.css'
+]
+
+
 # For configuration options of the Read The Docs theme, see
 # https://sphinx-rtd-theme.readthedocs.io/en/latest/configuring.html
 
@@ -72,17 +78,20 @@ html_theme_options = {
 
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
 napoleon_include_special_with_doc = True
 napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_notes = True
 napoleon_use_admonition_for_references = False
 napoleon_use_ivar = False
 napoleon_use_param = True
 napoleon_use_rtype = True
 napoleon_type_aliases = None
 napoleon_attr_annotations = True
+
+# Typehints for autodoc
+#autodoc_typehints = "description"
 
 # This next function is for fixing equation numbers.
 # See links in _static/equation_numbers.css for details.
