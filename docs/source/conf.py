d4e4dca73941f6c221f0572f2d8ab1b568152076
Bharat Medasani <mbkumar@gmail.com>
2021-05-06 13:38:41 -0400

    Mathjax enabled for sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,7 @@ extensions = [
               'sphinx_rtd_theme',
               'sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
+              'sphinx.ext.mathjax',
 ]
 
 autodoc_mock_imports=['mpi4py', 'pyoculus', 'py_spec']
