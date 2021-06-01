a8c7eff92f7f9a9f21f0761b8028ae89ef9898fc
Andrew Giuliani <giuliani@cims.nyu.edu>
2021-06-01 14:19:56 -0400

    curve objectives docstring

--- src/simsopt/geo/curveobjectives.py
+++ src/simsopt/geo/curveobjectives.py
@@ -1,3 +1,4 @@
+import numpy as np
 from jax import grad, vjp
 import jax.numpy as jnp
 from .jit import jit
@@ -7,10 +8,20 @@ from .._core.optimizable import Optimizable
 
 @jit
 def curve_length_pure(l):
+    """
+    A Python+Jax implementation of the formula for the curve length.
+    """
     return jnp.mean(l)
 
 
 class CurveLength(Optimizable):
+    r"""
+    CurveLength is a class that computes the length of a curve, i.e.
+    
+    .. math::
+        J = \int_{\text{curve}}~dl.
+
+    """
 
     def __init__(self, curve):
         self.curve = curve
@@ -18,19 +29,40 @@ class CurveLength(Optimizable):
         self.depends_on = ["curve"]
 
     def J(self):
+        """
+        This function returns the value of this quantity.
+        """
         return curve_length_pure(self.curve.incremental_arclength())
 
     def dJ(self):
+        """
+        This function returns the derivative of this quantity with respect to the curve dofs.
+        """
         return self.curve.dincremental_arclength_by_dcoeff_vjp(self.thisgrad(self.curve.incremental_arclength()))
 
 
 @jit
 def Lp_curvature_pure(kappa, gammadash, p, desired_kappa):
+    """
+    A Python+Jax implementation of the formula for the curvature penalty term.
+    """
     arc_length = jnp.linalg.norm(gammadash, axis=1)
     return (1./p)*jnp.mean(jnp.maximum(kappa-desired_kappa, 0)**p * arc_length)
 
 
 class LpCurveCurvature(Optimizable):
+    r"""
+    LpCurveCurvature is a class that computes a penalty term based on the :math:`L_p` norm
+    of the curve's curvature, and penalizes where the local curve curvature exceeds a threshold
+
+    
+    .. math::
+        J = \frac{1}{p} \int_{\text{curve}} \text{max}(\kappa - \kappa_0, 0)^p ~dl
+    
+    where :math:`\kappa_0` is a threshold curvature.  When ``desired_length`` is
+    provided, :math:`\kappa_0 = 2 \pi / \text{desired_length}`, otherwise :math:`\kappa_0=0`.
+    """
+
 
     def __init__(self, curve, p, desired_length=None):
         self.curve = curve
@@ -38,7 +70,7 @@ class LpCurveCurvature(Optimizable):
         if desired_length is None:
             self.desired_kappa = 0
         else:
-            radius = desired_length/(2*pi)
+            radius = desired_length/(2*np.pi)
             self.desired_kappa = 1/radius
 
         self.J_jax = jit(lambda kappa, gammadash: Lp_curvature_pure(kappa, gammadash, p, self.desired_kappa))
@@ -46,9 +78,15 @@ class LpCurveCurvature(Optimizable):
         self.thisgrad1 = jit(lambda kappa, gammadash: grad(self.J_jax, argnums=1)(kappa, gammadash))
 
     def J(self):
+        """
+        This function returns the value of this quantity.
+        """
         return self.J_jax(self.curve.kappa(), self.curve.gammadash())
 
     def dJ(self):
+        """
+        This function returns the derivative of this quantity with respect to the curve dofs.
+        """
         grad0 = self.thisgrad0(self.curve.kappa(), self.curve.gammadash())
         grad1 = self.thisgrad1(self.curve.kappa(), self.curve.gammadash())
         return self.curve.dkappa_by_dcoeff_vjp(grad0) + self.curve.dgammadash_by_dcoeff_vjp(grad1)
@@ -56,11 +94,24 @@ class LpCurveCurvature(Optimizable):
 
 @jit
 def Lp_torsion_pure(torsion, gammadash, p):
+    """
+    A Python+Jax implementation of the formula for the torsion penalty term.
+    """
     arc_length = jnp.linalg.norm(gammadash, axis=1)
     return (1./p)*jnp.mean(jnp.abs(torsion)**p * arc_length)
 
 
 class LpCurveTorsion(Optimizable):
+    r"""
+    LpCurveTorsion is a class that computes a penalty term based on the :math:`L_p` norm
+    of the curve's torsion:
+
+    
+    .. math::
+        J = \frac{1}{p} \int_{\text{curve}} \tau^p ~dl.
+    
+    """
+
 
     def __init__(self, curve, p):
         self.curve = curve
@@ -71,21 +122,47 @@ class LpCurveTorsion(Optimizable):
         self.thisgrad1 = jit(lambda torsion, gammadash: grad(self.J_jax, argnums=1)(torsion, gammadash))
 
     def J(self):
+        """
+        This function returns the value of this quantity.
+        """
         return self.J_jax(self.curve.torsion(), self.curve.gammadash())
 
     def dJ(self):
+        """
+        This function returns the derivative of this quantity with respect to the curve dofs.
+        """
         grad0 = self.thisgrad0(self.curve.torsion(), self.curve.gammadash())
         grad1 = self.thisgrad1(self.curve.torsion(), self.curve.gammadash())
         return self.curve.dtorsion_by_dcoeff_vjp(grad0) + self.curve.dgammadash_by_dcoeff_vjp(grad1)
 
 
 def distance_pure(gamma1, l1, gamma2, l2, minimum_distance):
+    """
+    A Python+Jax implementation of the distance formula.
+    """
     dists = jnp.sqrt(jnp.sum((gamma1[:, None, :] - gamma2[None, :, :])**2, axis=2))
     alen = jnp.linalg.norm(l1, axis=1) * jnp.linalg.norm(l2, axis=1)
     return jnp.sum(alen * jnp.maximum(minimum_distance-dists, 0)**2)/(gamma1.shape[0]*gamma2.shape[0])
 
 
 class MinimumDistance(Optimizable):
+    r"""
+    MinimumDistance is a class that computes
+    
+    .. math::
+        J = \sum_{i = 1}^{\text{num_coils}} \sum_{j = 1}^{i-1} d_{i,j}
+    
+    where 
+    
+    .. math::
+        d_{i,j} = \int_{\text{curve}_i} \int_{\text{curve}_j} \max(0, d_{\min} - \| \mathbf{r}_i - \mathbf{r}_j \|_2)^2 ~dl_j ~dl_i\\
+    
+    and :math:`\mathbf{r}_i`, :math:`\mathbf{r}_j` are points on coils :math:`i` and :math:`j`, respectively.
+    :math:`d_\min` is a desired threshold minimum intercoil distance.  This penalty term is zero when the points on coil :math:`i` and 
+    coil :math:`j` lie more than :math:`d_\min` away from one another, for :math:`i, j \in \{1, \cdots, \text{num_coils}\}`
+
+    """
+
 
     def __init__(self, curves, minimum_distance):
         self.curves = curves
@@ -99,6 +176,9 @@ class MinimumDistance(Optimizable):
         self.thisgrad3 = jit(lambda gamma1, l1, gamma2, l2: grad(self.J_jax, argnums=3)(gamma1, l1, gamma2, l2))
 
     def J(self):
+        """
+        This function returns the value of this quantity.
+        """
         res = 0
         for i in range(len(self.curves)):
             gamma1 = self.curves[i].gamma()
@@ -110,6 +190,9 @@ class MinimumDistance(Optimizable):
         return res
 
     def dJ(self):
+        """
+        This function returns the derivative of this quantity with respect to the curve dofs.
+        """
         dgamma_by_dcoeff_vjp_vecs = [None for c in self.curves]
         dgammadash_by_dcoeff_vjp_vecs = [None for c in self.curves]
         for i in range(len(self.curves)):
