b302e20a97ddbe6db7d7a14b7b4e70a234ce0a13
Florian Wechsung <f.wechsung@gmail.com>
2021-06-01 13:25:21 +0200

    added field line and guiding center tracing

---
+++ src/simsgeopp/tracing.cpp
@@ -0,0 +1,203 @@
+#include <memory>
+#include <vector>
+#include <functional>
+#include <boost/numeric/odeint.hpp>
+#include "magneticfield.h"
+#include <cassert>
+#include "tracing.h"
+
+
+using namespace boost::numeric::odeint;
+
+using std::shared_ptr;
+using std::vector;
+using std::tuple;
+using std::pair;
+using std::function;
+
+#include "xtensor-python/pyarray.hpp"     // Numpy bindings
+typedef xt::pyarray<double> Array;
+
+
+template<class Array>
+tuple<vector<double>, vector<vector<double>>> particle_guiding_center_tracing(
+        shared_ptr<MagneticField<Array>> field, double xinit, double yinit, double zinit,
+        double m, double q, double vtotal, double vtang, double tmax, double tol, vector<shared_ptr<StoppingCriterion>> stopping_criteria)
+{
+
+
+    Array xyz({{xinit, yinit, zinit}});
+    field->set_points(xyz);
+    double AbsB = field->AbsB_ref()(0);
+    double vperp2 = vtotal*vtotal - vtang*vtang;
+    double mu = vperp2/(2*AbsB);
+
+
+    runge_kutta_dopri5<vector<double>> stepper;
+    //runge_kutta_fehlberg78<vector<double>> stepper;
+    auto rhs_class = GuidingCenterRHS<Array>(field, m, q, mu);
+    GuidingCenterRHS<Array>& rhs_ref = rhs_class; 
+    vector<double> y = {xinit, yinit, zinit, vtang};
+
+    double dtmax = 0.1/vtotal; // can at most move 1cm per step
+    double dt = 0.001 * dtmax; // initial guess for first timestep, will be adjusted by adaptive timestepper
+    auto res_y = vector<vector<double>>();
+    res_y.reserve(2*int(tmax/dtmax));
+    auto res_t = vector<double>();
+    res_t.reserve(2*int(tmax/dtmax));
+    function<void(const vector<double> &, double)> observer =
+        [&res_y, &res_t](const vector<double> &y, double t) {
+            res_y.push_back(y);
+            res_t.push_back(t);
+        };
+    
+    typedef boost::numeric::odeint::result_of::make_dense_output<runge_kutta_dopri5<vector<double>>>::type dense_stepper_type;
+    dense_stepper_type dense = make_dense_output(tol, tol, dtmax, runge_kutta_dopri5<vector<double>>());
+    double t = 0;
+    dense.initialize(y, t, dt);
+    int iter = 0;
+    bool stop = false;
+    do {
+        res_t.push_back(t);
+        res_y.push_back(y);
+        dense.do_step(rhs_ref);
+        iter++;
+        t = dense.current_time();
+        y = dense.current_state();
+        for (int i = 0; i < stopping_criteria.size(); ++i) {
+            if(stopping_criteria[i] && (*stopping_criteria[i])(iter, t, y)){
+                stop = true;
+                break;
+            }
+        }
+    } while(t < tmax && !stop);
+    if(!stop){
+        res_t.push_back(tmax);
+        vector<double> yfinal(4, 0.);
+        dense.calc_state(tmax, yfinal);
+        res_y.push_back(yfinal);
+    }
+
+    return make_tuple(res_t, res_y);
+
+}
+
+template
+tuple<vector<double>, vector<vector<double>>> particle_guiding_center_tracing<Array>(
+        shared_ptr<MagneticField<Array>> field, double xinit, double yinit, double zinit,
+        double m, double q, double vtotal, double vtang, double tmax, double tol, vector<shared_ptr<StoppingCriterion>> stopping_criteria);
+
+double get_phi(const vector<double>& pt, double phi_near){
+    double phi = std::atan2(pt[1], pt[0]);
+    if(phi < 0)
+        phi += 2*M_PI;
+    double phi_near_mod = std::fmod(phi_near, 2*M_PI);
+    double nearest_multiple = std::round(phi_near/(2*M_PI))*2*M_PI;
+    double opt1 = nearest_multiple - 2*M_PI + phi;
+    double opt2 = nearest_multiple + phi;
+    double opt3 = nearest_multiple + 2*M_PI + phi;
+    double dist1 = std::abs(opt1-phi_near);
+    double dist2 = std::abs(opt2-phi_near);
+    double dist3 = std::abs(opt3-phi_near);
+    if(dist1 <= std::min(dist2, dist3))
+        return opt1;
+    else if(dist2 <= std::min(dist1, dist3))
+        return opt2;
+    else
+        return opt3;
+}
+
+#include <boost/math/tools/roots.hpp>
+using boost::math::tools::toms748_solve;
+
+template<class Array>
+tuple<vector<double>, vector<vector<double>>, vector<vector<vector<double>>>> fieldline_tracing(
+        shared_ptr<MagneticField<Array>> field, double xinit, double yinit, double zinit,
+        double tmax, double tol, vector<double> phis, vector<shared_ptr<StoppingCriterion>> stopping_criteria)
+{
+    runge_kutta_dopri5<vector<double>> stepper;
+    //runge_kutta_fehlberg78<vector<double>> stepper;
+    auto rhs_class = FieldlineRHS<Array>(field);
+    vector<double> y = {xinit, yinit, zinit};
+
+    double dtmax = 0.1; // can at most move 1cm per step
+    double dt = 0.001 * dtmax; // initial guess for first timestep, will be adjusted by adaptive timestepper
+
+    auto res_y = vector<vector<double>>();
+    res_y.reserve(2*int(tmax/dtmax));
+    auto res_t = vector<double>();
+    res_t.reserve(2*int(tmax/dtmax));
+    vector<vector<vector<double>>> res_phi_hits(phis.size(), vector<vector<double>>());
+    function<void(const vector<double> &, double)> observer =
+        [&res_y, &res_t](const vector<double> &y, double t) {
+            res_y.push_back(y);
+            res_t.push_back(t);
+        };
+    
+    typedef boost::numeric::odeint::result_of::make_dense_output<runge_kutta_dopri5<vector<double>>>::type dense_stepper_type;
+    dense_stepper_type dense = make_dense_output(tol, tol, dtmax, runge_kutta_dopri5<vector<double>>());
+    double t = 0;
+    dense.initialize(y, t, dt);
+    int iter = 0;
+    bool stop = false;
+    double phi_last = get_phi(y, M_PI);
+    double phi_current;
+    boost::math::tools::eps_tolerance<double> roottol(-int(std::log2(tol)));
+    vector<double> temp = {0., 0., 0.};
+    do {
+        res_t.push_back(t);
+        res_y.push_back(y);
+        tuple<double, double> step = dense.do_step(rhs_class);
+        iter++;
+        t = dense.current_time();
+        y = dense.current_state();
+        phi_current = get_phi(y, phi_last);
+        double tlast = std::get<0>(step);
+        double tcurrent = std::get<1>(step);
+        //fmt::print("tlast={:5f}, tcurrent={:5f}, phi_last={:5f}, phi_current={:5f}\n", tlast, tcurrent, phi_last, phi_current);
+        for (int i = 0; i < phis.size(); ++i) {
+            double phi = phis[i];
+            if(std::floor((phi_last-phi)/(2*M_PI)) != std::floor((phi_current-phi)/(2*M_PI))){ // check whether phi+k*2pi for some k was crossed
+                int fak = std::round(((phi_last+phi_current)/2-phi)/(2*M_PI));
+                double phi_shift = fak*2*M_PI + phi;
+                assert((phi_last <= phi_shift && phi_shift <= phi_current) || (phi_current <= phi_shift && phi_shift <= phi_last));
+
+                std::function<double(double)> rootfun = [&dense, &phi_shift, &temp, &phi_last](double t){
+                    dense.calc_state(t, temp);
+                    return get_phi(temp, phi_last)-phi_shift;
+                };
+
+                uintmax_t maxit = 100;
+                auto root = toms748_solve(rootfun, tlast, tcurrent, phi_last - phi_shift, phi_current-phi_shift, roottol, maxit);
+                auto troot = (std::get<0>(root)+std::get<1>(root))/2;
+                dense.calc_state(troot, temp);
+                double rroot = std::sqrt(temp[0]*temp[0] + temp[1]*temp[1]);
+                double phiroot = std::atan2(temp[1], temp[0]);
+                if(phiroot<0)
+                    phiroot += 2*M_PI;
+                //fmt::print("t={:.5f}, xyz=({:.5f}, {:.5f}, {:.5f}), rphiz=({}, {}, {})\n", troot, temp[0], temp[1], temp[2], rroot, phiroot, temp[2]);
+                res_phi_hits[i].push_back({troot, rroot, phiroot, temp[2]});
+            }
+        }
+        phi_last = phi_current;
+        for (int i = 0; i < stopping_criteria.size(); ++i) {
+            if(stopping_criteria[i] && (*stopping_criteria[i])(iter, t, y)){
+                stop = true;
+                break;
+            }
+        }
+    } while(t < tmax && !stop);
+    if(!stop){
+        res_t.push_back(tmax);
+        vector<double> yfinal(3, 0.);
+        dense.calc_state(tmax, yfinal);
+        res_y.push_back(yfinal);
+    }
+
+    return make_tuple(res_t, res_y, res_phi_hits);
+}
+
+template
+tuple<vector<double>, vector<vector<double>>, vector<vector<vector<double>>>> fieldline_tracing(
+        shared_ptr<MagneticField<Array>> field, double xinit, double yinit, double zinit,
+        double tmax, double tol, vector<double> phis, vector<shared_ptr<StoppingCriterion>> stopping_criteria);
---
+++ src/simsopt/tracing/tracing.py
@@ -0,0 +1,157 @@
+from math import sqrt
+import numpy as np
+import simsgeopp as sgpp
+
+
+def trace_particles_starting_on_axis(axis, field, nparticles, tmax=1e-4, seed=1,
+                                     mass=1.67e-27, charge=1, Ekinev=9000,
+                                     umin=-1, umax=+1, stopping_criteria=[]):
+    e = 1.6e-19
+    Ekin = Ekinev*e
+    m = mass
+    q = charge*e
+
+    vtotal = sqrt(2*Ekin/m)  # Ekin = 0.5 * m * v^2 <=> v = sqrt(2*Ekin/m)
+
+    tol = 1e-9
+
+    np.random.seed(seed)
+    xyz_inits = axis[np.random.randint(0, axis.shape[0], size=(nparticles, )), :]
+
+    us = np.random.uniform(low=umin, high=umax, size=(nparticles, ))
+    vtangs = us*vtotal
+
+    res_ts = []
+    res_ys = []
+    loss_ctr = 0
+    for i in range(nparticles):
+        res_t, res_y = sgpp.particle_guiding_center_tracing(
+            field, xyz_inits[i, 0], xyz_inits[i, 1], xyz_inits[i, 2],
+            m, q, vtotal, vtangs[i], tmax, tol, stopping_criteria)
+        res_ts.append(np.asarray(res_t))
+        res_ys.append(np.asarray(res_y))
+        print(f"{i+1:3d}/{nparticles}, t_final={res_t[-1]}", flush=True)
+        if res_t[-1] < tmax - 1e-15:
+            loss_ctr += 1
+    print(f'Particles lost {loss_ctr}/{nparticles}={loss_ctr/nparticles}', flush=True)
+    return res_ts, res_ys
+
+
+def compute_fieldlines(field, r0, nlines, linestep=0.01, tmax=200, stopping_criteria=[]):
+    xyz_inits = np.zeros((nlines, 3))
+    xyz_inits[:, 0] = np.asarray([r0 + i*linestep for i in range(nlines)])
+    tol = 1e-10
+    res_ts = []
+    res_ys = []
+    phis = [i*2*np.pi/(4*3) for i in range(4)]
+    res_phi_hits = [[] for phi in phis]
+    for i in range(nlines):
+        res_t, res_y, res_phi_hit = sgpp.fieldline_tracing(
+            field, xyz_inits[i, 0], xyz_inits[i, 1], xyz_inits[i, 2],
+            tmax, tol, phis, stopping_criteria=stopping_criteria)
+        res_ts.append(np.asarray(res_t))
+        res_ys.append(np.asarray(res_y))
+        for j in range(len(phis)):
+            res_phi_hits[j].append(np.asarray(res_phi_hit[j]))
+        print(f"{i+1}/{nlines}, t_final={res_t[-1]}", flush=True)
+    return res_ts, res_ys, res_phi_hits
+
+
+def particles_to_vtk(res_ys, filename):
+    from pyevtk.hl import polyLinesToVTK
+    x = np.concatenate([xyz[:, 0] for xyz in res_ys])
+    y = np.concatenate([xyz[:, 1] for xyz in res_ys])
+    z = np.concatenate([xyz[:, 2] for xyz in res_ys])
+    ppl = np.asarray([xyz.shape[0] for xyz in res_ys])
+    data = np.concatenate([i*np.ones((res_ys[i].shape[0], )) for i in range(len(res_ys))])
+    polyLinesToVTK(filename, x, y, z, pointsPerLine=ppl, pointData={'idx': data})
+
+
+def signed_distance_from_surface(xyz, surface):
+    gammas = surface.gamma().reshape((-1, 3))
+    from scipy.spatial.distance import cdist
+    dists = cdist(xyz, gammas)
+    mins = np.argmin(dists, axis=1)
+    n = surface.normal().reshape((-1, 3))
+    nmins = n[mins]
+    gammamins = gammas[mins]
+
+    # Now that we have found the closest node, we approximate the surface with
+    # a plane through that node with the appropriate normal and then compute
+    # the distance from the point to that plane
+    # https://stackoverflow.com/questions/55189333/how-to-get-distance-from-point-to-plane-in-3d
+    mindist = np.sum((xyz-gammamins) * nmins, axis=1)
+
+    a_point_in_the_surface = np.mean(surface.gamma()[0, :, :], axis=0)
+    sign_of_interiorpoint = np.sign(np.sum((a_point_in_the_surface-gammas[0, :])*n[0, :]))
+
+    signed_dists = mindist * sign_of_interiorpoint
+    return signed_dists
+
+
+class SurfaceClassifier():
+
+    def __init__(self, surface, p=1, h=0.05):
+        gammas = surface.gamma()
+        r = np.linalg.norm(gammas[:, :, :2], axis=2)
+        z = gammas[:, :, 2]
+        rmin = max(np.min(r) - 0.1, 0.)
+        rmax = np.max(r) + 0.1
+        zmin = np.min(z) - 0.1
+        zmax = np.max(z) + 0.1
+
+        self.zrange = (zmin, zmax)
+        self.rrange = (rmin, rmax)
+
+        nr = int((self.rrange[1]-self.rrange[0])/h)
+        nphi = int(2*np.pi/h)
+        nz = int((self.zrange[1]-self.zrange[0])/h)
+
+        def fbatch(rs, phis, zs):
+            xyz = np.zeros((len(rs), 3))
+            xyz[:, 0] = rs * np.cos(phis)
+            xyz[:, 1] = rs * np.sin(phis)
+            xyz[:, 2] = zs
+            return list(signed_distance_from_surface(xyz, surface))
+
+        rule = sgpp.UniformInterpolationRule(p)
+        self.dist = sgpp.RegularGridInterpolant3D(
+            rule, [rmin, rmax, nr], [0., 2*np.pi, nphi], [zmin, zmax, nz], 1, True)
+        self.dist.interpolate_batch(fbatch)
+
+    def evaluate(self, xyz):
+        rphiz = np.zeros_like(xyz)
+        rphiz[:, 0] = np.linalg.norm(xyz[:, :2], axis=1)
+        rphiz[:, 1] = np.mod(np.arctan2(xyz[:, 1], xyz[:, 0]), 2*np.pi)
+        rphiz[:, 2] = xyz[:, 2]
+        d = np.zeros((xyz.shape[0], 1))
+        self.dist.evaluate_batch(rphiz, d)
+        return d
+
+    def to_vtk(self, filename, h=0.01):
+        from pyevtk.hl import gridToVTK
+
+        nr = int((self.rrange[1]-self.rrange[0])/h)
+        nphi = int(2*np.pi/h)
+        nz = int((self.zrange[1]-self.zrange[0])/h)
+        rs = np.linspace(self.rrange[0], self.rrange[1], nr)
+        phis = np.linspace(0, 2*np.pi, nphi)
+        zs = np.linspace(self.zrange[0], self.zrange[1], nz)
+
+        R, Phi, Z = np.meshgrid(rs, phis, zs)
+        X = R * np.cos(Phi)
+        Y = R * np.sin(Phi)
+        Z = Z
+
+        RPhiZ = np.zeros((R.size, 3))
+        RPhiZ[:, 0] = R.flatten()
+        RPhiZ[:, 1] = Phi.flatten()
+        RPhiZ[:, 2] = Z.flatten()
+        vals = np.zeros((R.size, 1))
+        self.dist.evaluate_batch(RPhiZ, vals)
+        vals = vals.reshape(R.shape)
+        gridToVTK(filename, X, Y, Z, pointData={"levelset": vals})
+
+
+class LevelsetStoppingCriterion(sgpp.LevelsetStoppingCriterion):
+    pass
