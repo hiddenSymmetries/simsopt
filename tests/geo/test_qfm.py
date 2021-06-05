3a878327ac82a903bb4808e3c403b6bfaaf5ea5d
Elizabeth <ejpaul@umd.edu>
2021-06-05 16:16:40 -0400

    Added SLSQP constrained solver and associated testing/examples.

--- tests/geo/test_qfm.py
+++ tests/geo/test_qfm.py
@@ -39,17 +39,16 @@ class QfmSurfaceTests(unittest.TestCase):
             constraint_weight=weight)
         assert(r0 < 1e-10)
 
-    def test_qfm_constrained_jacobian(self):
+    def test_qfm_objective_gradient(self):
         """
-        Taylor test to verify the Jacobian of the first order optimality
-        conditions of the exactly constrained optimization problem.
+        Taylor test to verify the gradient of the qfm objective.
         """
         for surfacetype in surfacetypes_list:
             for stellsym in stellsym_list:
                 with self.subTest(surfacetype=surfacetype, stellsym=stellsym):
-                    self.subtest_qfm_constrained_jacobian(surfacetype, stellsym)
+                    self.subtest_qfm_objective_gradient(surfacetype, stellsym)
 
-    def subtest_qfm_constrained_jacobian(self, surfacetype, stellsym):
+    def subtest_qfm_objective_gradient(self, surfacetype, stellsym):
         np.random.seed(1)
         coils, currents, ma = get_ncsx_data()
         stellarator = CoilCollection(coils, currents, 3, True)
@@ -65,21 +64,65 @@ class QfmSurfaceTests(unittest.TestCase):
         tf_target = 0.1
         qfm_surface = QfmSurface(bs, s, tf, tf_target)
 
-        lm = [0.]
         x = s.get_dofs()
-        xl = np.concatenate((x, lm))
-        res0, dres0 = qfm_surface.qfm_exact_constraints(xl, derivatives=1)
+        f0, J0 = qfm_surface.qfm_objective(
+            x, derivatives=1)
 
-        h = np.random.uniform(size=xl.shape)-0.5
-        dres_exact = dres0@h
+        h = np.random.uniform(size=x.shape)-0.5
+        Jex = J0@h
 
         err_old = 1e9
         epsilons = np.power(2., -np.asarray(range(7, 20)))
         print("################################################################################")
         for eps in epsilons:
-            res1 = qfm_surface.qfm_exact_constraints(xl + eps*h, derivatives=0)
-            dres_fd = (res1-res0)/eps
-            err = np.linalg.norm(dres_fd-dres_exact)
+            f1 = qfm_surface.qfm_objective(
+                x + eps*h, derivatives=0)
+            Jfd = (f1-f0)/eps
+            err = np.linalg.norm(Jfd-Jex)/np.linalg.norm(Jex)
+            print(err/err_old)
+            assert err < err_old * 0.6
+            err_old = err
+        print("################################################################################")
+
+    def test_qfm_label_constraint_gradient(self):
+        """
+        Taylor test to verify the gradient of the qfm objective.
+        """
+        for surfacetype in surfacetypes_list:
+            for stellsym in stellsym_list:
+                with self.subTest(surfacetype=surfacetype, stellsym=stellsym):
+                    self.subtest_qfm_label_constraint_gradient(surfacetype, stellsym)
+
+    def subtest_qfm_label_constraint_gradient(self, surfacetype, stellsym):
+        np.random.seed(1)
+        coils, currents, ma = get_ncsx_data()
+        stellarator = CoilCollection(coils, currents, 3, True)
+
+        bs = BiotSavart(stellarator.coils, stellarator.currents)
+        bs_tf = BiotSavart(stellarator.coils, stellarator.currents)
+
+        s = get_surface(surfacetype, stellsym)
+        s.fit_to_curve(ma, 0.1)
+
+        tf = ToroidalFlux(s, bs_tf)
+
+        tf_target = 0.1
+        qfm_surface = QfmSurface(bs, s, tf, tf_target)
+
+        x = s.get_dofs()
+        f0, J0 = qfm_surface.qfm_label_constraint(x, derivatives=1)
+
+        h = np.random.uniform(size=x.shape)-0.5
+        Jex = J0@h
+
+        err_old = 1e9
+        epsilons = np.power(2., -np.asarray(range(7, 20)))
+        print("################################################################################")
+        for eps in epsilons:
+            f1 = qfm_surface.qfm_label_constraint(
+                x + eps*h, derivatives=0)
+            Jfd = (f1-f0)/eps
+            err = np.linalg.norm(Jfd-Jex)/np.linalg.norm(Jex)
             print(err/err_old)
             assert err < err_old * 0.6
             err_old = err
@@ -197,6 +240,11 @@ class QfmSurfaceTests(unittest.TestCase):
 
     def subtest_qfm_surface_optimization_convergence(self, surfacetype,
                                                         stellsym):
+        """
+        For each configuration, first reduce penalty objective using LBFGS at
+        fixed volume. Then solve constrained problem using SLSQP. Repeat
+        both steps for fixed area. Check that volume is preserved.
+        """
         coils, currents, ma = get_ncsx_data()
 
         if stellsym:
@@ -235,8 +283,18 @@ class QfmSurfaceTests(unittest.TestCase):
             tol=1e-9, maxiter=1000, constraint_weight=1e3)
 
         assert res['success']
-        assert np.linalg.norm(res['fun']) < 1e-5
-        assert np.abs(vol_target - vol.J()) < 1e-4
+        assert np.linalg.norm(res['gradient']) < 1e-2
+        assert res['fun'] < 1e-5
+        assert np.abs(vol_target - vol.J()) < 1e-5
+
+        # As a second step, optimize with SLSQP
+
+        res = qfm_surface.minimize_qfm_exact_constraints_SLSQP(tol=1e-9, maxiter=1000)
+
+        assert res['success']
+        assert np.linalg.norm(res['gradient']) < 1e-3
+        assert res['fun'] < 1e-5
+        assert np.abs(vol_target - vol.J()) < 1e-6
 
         vol_opt1 = vol.J()
 
@@ -248,13 +306,24 @@ class QfmSurfaceTests(unittest.TestCase):
 
         res = qfm_surface.minimize_qfm_penalty_constraints_LBFGS(
             tol=1e-9, maxiter=1000, constraint_weight=1e3)
+
+        assert res['success']
+        assert res['fun'] < 1e-5
+        assert np.linalg.norm(res['gradient']) < 1e-3
+        assert np.abs(ar_target - ar.J()) < 1e-8
+
+        res = qfm_surface.minimize_qfm_exact_constraints_SLSQP(tol=1e-9, maxiter=1000)
+
+        assert res['success']
+        assert res['fun'] < 1e-5
+        assert np.linalg.norm(res['gradient']) < 1e-3
+        assert np.abs(ar_target - ar.J()) < 1e-4
+
         vol_opt2 = vol.J()
 
         # Check that volume after second opt does not change
 
         assert np.abs(vol_opt2 - vol_opt1) < 1e-4
-        assert res['success']
-        assert np.linalg.norm(res['fun']) < 1e-5
 
 
 if __name__ == "__main__":
