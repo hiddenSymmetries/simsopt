909dd2bf06174a76b2e4af1c2ba2606622bc734c
Elizabeth <ejpaul@umd.edu>
2021-06-05 16:16:40 -0400

    Added SLSQP constrained solver and associated testing/examples.

--- src/simsopt/geo/qfmsurface.py
+++ src/simsopt/geo/qfmsurface.py
@@ -1,4 +1,4 @@
-from scipy.optimize import minimize, least_squares
+from scipy.optimize import minimize, least_squares, NonlinearConstraint
 import numpy as np
 from simsopt.geo.surfaceobjectives import QfmResidual
 
@@ -10,13 +10,73 @@ class QfmSurface():
         self.label = label
         self.targetlabel = targetlabel
 
+    def qfm_label_constraint(self, x, derivatives=0):
+        """
+        Returns the residual
+
+        0.5 * (label - labeltarget)^2
+
+        and optionally the gradient wrt surface params.
+        """
+        assert derivatives in [0, 1]
+
+        s = self.surface
+        sdofs = x
+        nsurfdofs = x.size
+        bs = self.bs
+
+        s.set_dofs(sdofs)
+        l = self.label.J()
+        rl = l-self.targetlabel
+        val = 0.5 * rl ** 2
+
+        if derivatives:
+            dl = self.label.dJ_by_dsurfacecoefficients()
+            dval = rl*dl
+            return val, dval
+        else:
+            return val
+
+    def qfm_objective(self, x, derivatives=0):
+        """
+        Returns the residual
+
+        0.5 * f(x)^2
+
+        and optionally the gradient wrt surface params where f(x) is the QFM
+        residual defined in surfaceobjectives.py.
+        """
+        assert derivatives in [0, 1]
+
+        s = self.surface
+        sdofs = x
+        nsurfdofs = x.size
+        bs = self.bs
+
+        s.set_dofs(sdofs)
+        qfm = QfmResidual(s, bs)
+
+        r = qfm.J()
+
+        l = self.label.J()
+        rl = l-self.targetlabel
+
+        val = 0.5 * r**2
+        if derivatives == 0:
+            return val
+        else:
+            dr = qfm.dJ_by_dsurfacecoefficients()
+            dval = r*dr
+            return val, dval
+
     def qfm_penalty_constraints(self, x, derivatives=0, constraint_weight=1.):
         """
         Returns the residual
 
         0.5 * f(x)^2 + 0.5 * constraint_weight * (label - labeltarget)^2
 
-        and optionally the gradient and Hessian wrt surface params.
+        and optionally the gradient and Hessian wrt surface params where f(x)
+        is the QFM residual defined in surfaceobjectives.py.
         """
 
         assert derivatives in [0, 1, 2]
@@ -48,96 +108,10 @@ class QfmSurface():
 
         d2r = qfm.d2J_by_dsurfacecoefficientsdsurfacecoefficients()
         d2l = self.label.d2J_by_dsurfacecoefficientsdsurfacecoefficients()
-        d2val =  r*d2r + dr[:,None]*dr[None,:] + constraint_weight*rl*d2l + constraint_weight*dl[:,None]*dl[None,:]
+        d2val =  r*d2r + dr[:,None]*dr[None,:] \
+            + constraint_weight*rl*d2l + constraint_weight*dl[:,None]*dl[None,:]
 
         return val, dval, d2val
-
-    def qfm_exact_constraints(self, xl, derivatives=0):
-        """
-        This function returns the optimality conditions corresponding to the
-            minimization problem
-
-            min 0.5 * f(x)^2
-
-            subject to
-
-            label - targetlabel = 0
-
-        as well as optionally the first derivatives for these optimality
-        conditions.
-        """
-        sdofs = xl[:-1]
-        lm = xl[-1]
-        s = self.surface
-        bs = self.bs
-        s.set_dofs(sdofs)
-        nsurfdofs = sdofs.size
-
-        qfm = QfmResidual(s, bs)
-        qfm_res = qfm.J()
-        dqfm_res = qfm.dJ_by_dsurfacecoefficients();
-
-        l = self.label.J()
-        dl = self.label.dJ_by_dsurfacecoefficients()
-        res = np.zeros(xl.shape)
-        res[0:-1] = dqfm_res - lm * dl
-        res[-1] = l - self.targetlabel
-        if derivatives == 0:
-            return res
-
-        dres = np.zeros((xl.shape[0], xl.shape[0]))
-        d2l = self.label.d2J_by_dsurfacecoefficientsdsurfacecoefficients()
-        d2_qfm_res = qfm.d2J_by_dsurfacecoefficientsdsurfacecoefficients()
-
-        dres[0:-1,0:-1] = d2_qfm_res - lm * d2l
-        dres[0:-1, -1] = -dl
-        dres[-1,0:-1] = dl
-
-        return res, dres
-
-    def minimize_qfm_exact_constraints_newton(self, tol=1e-12, maxiter=10,
-        lm=[0.]):
-        """
-        This function solves the constrained optimization problem
-            min 0.5 * f(x)^2
-
-            subject to
-
-            label - targetlabel = 0
-
-        using Lagrange multipliers and Newton's method
-        """
-        s = self.surface
-        xl = np.concatenate((s.get_dofs(), lm))
-
-        val, dval = self.qfm_exact_constraints(xl, derivatives=1)
-        norm = np.linalg.norm(val)
-
-        print('norm: ',norm)
-        print('val: ',val)
-        print('dval: ',dval)
-        print(dval)
-
-        i = 0
-        while i < maxiter and norm > tol:
-            print('norm = ',norm)
-            print('iter = ',i)
-            print('lambda = ',val[-1])
-            dx = np.linalg.solve(dval, val)
-            if norm < 1e-9:  # iterative refinement for higher accuracy. TODO: cache LU factorisation
-                dx += np.linalg.solve(dval, val-dval@dx)
-            xl = xl - dx
-            val, dval = self.qfm_exact_constraints(xl, derivatives=1)
-            norm = np.linalg.norm(val)
-            normgrad = np.linalg.norm(dval)
-
-            i = i + 1
-
-        res = {
-            "residual": val, "jacobian": dval, "iter": i, "success": norm <= tol, "lm": lm,
-        }
-        res['s'] = s
-        return res
 
     def minimize_qfm_penalty_constraints_LBFGS(self, tol=1e-3, maxiter=1000,
         constraint_weight=1.):
@@ -146,7 +120,8 @@ class QfmSurface():
 
         min 0.5 * f(x)^2 + 0.5 * constraint_weight * (label - labeltarget)^2
 
-        where f(x) is the QFM residual. This is done using LBFGS.
+        where f(x) is the QFM residual defined in surfaceobjectives.py. This is
+        done using LBFGS.
         """
 
         s = self.surface
@@ -157,6 +132,40 @@ class QfmSurface():
             fun, x, jac=True, method='L-BFGS-B',
             options={'maxiter': maxiter, 'ftol': tol, 'gtol': tol,
                 'maxcor': 200})
+
+        resdict = {
+            "fun": res.fun, "gradient": res.jac, "iter": res.nit, "info": res,
+                "success": res.success,
+        }
+        s.set_dofs(res.x)
+        resdict['s'] = s
+
+        return resdict
+
+    def minimize_qfm_exact_constraints_SLSQP(self, tol=1e-3, maxiter=1000):
+        """
+        This function tries to find the surface that approximately solves
+
+        min 0.5 * f(x)^2
+
+        subject to
+
+        label = labeltarget
+
+        where f(x) is the QFM residual. This is done using SLSQP.
+        """
+        s = self.surface
+        x = s.get_dofs()
+
+        fun = lambda x: self.qfm_objective(x, derivatives=1)
+        con = lambda x : self.qfm_label_constraint(x, derivatives=1)[0]
+        dcon = lambda x : self.qfm_label_constraint(x, derivatives=1)[1]
+
+        nlc = NonlinearConstraint(con, 0, 0)
+        eq_constraints = [{'type' : 'eq', 'fun': con, 'jac': dcon}]
+        res = minimize(
+            fun, x, jac=True, method='SLSQP',constraints=eq_constraints,
+            options={'maxiter': maxiter, 'ftol': tol})
 
         resdict = {
             "fun": res.fun, "gradient": res.jac, "iter": res.nit, "info": res,
