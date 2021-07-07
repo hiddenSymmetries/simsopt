e3b34d1c383052cff3c815bc9822a8d92f982bc2
Florian Wechsung <f.wechsung@gmail.com>
2021-07-07 17:41:41 -0400

    results on different threads are a little different -> gave up on making sure the path is exactly identical and just checking that the phi hits are the same now

--- tests/field/test_particle.py
+++ tests/field/test_particle.py
@@ -128,7 +128,6 @@ class ParticleTracingTesting(unittest.TestCase):
                 vperp = np.linalg.norm(v - np.sum(v*Bunit)*Bunit)
                 r = compute_gc_radius(m, vperp, q, AbsBs[j, 0])
                 dist = np.linalg.norm(gc_xyzs[j, :] - fo_ty[jdx, 1:4])
-                print("dist", dist)
                 assert dist < 8*r
 
     @unittest.skipIf(not with_boost, "boost not found")
