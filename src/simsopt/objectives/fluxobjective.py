df049e83baa2d0b41bf3db7d2655f61dc287b808
Florian Wechsung <f.wechsung@gmail.com>
2021-09-08 15:47:10 -0400

    make squared flux relative

--- src/simsopt/objectives/fluxobjective.py
+++ src/simsopt/objectives/fluxobjective.py
@@ -23,7 +23,8 @@ class SquaredFlux(Optimizable):
             B_n = (Bcoil_n - self.target)
         else:
             B_n = Bcoil_n
-        return 0.5 * np.mean(B_n**2 * absn)
+        mod_Bcoil = np.linalg.norm(Bcoil, axis=2)
+        return 0.5 * np.mean((B_n/mod_Bcoil)**2 * absn)
 
     def dJ(self):
         n = self.surface.normal()
@@ -35,7 +36,11 @@ class SquaredFlux(Optimizable):
             B_n = (Bcoil_n - self.target)
         else:
             B_n = Bcoil_n
-        dJdB = (B_n[..., None] * unitn * absn[..., None])/absn.size
+        mod_Bcoil = np.linalg.norm(Bcoil, axis=2)
+        dJdB = ((
+            (B_n/mod_Bcoil)[..., None] * (
+                unitn/mod_Bcoil[..., None] - (B_n/mod_Bcoil**3)[..., None] * Bcoil
+            )) * absn[..., None])/absn.size
         dJdB = dJdB.reshape((-1, 3))
         return self.field.B_vjp(dJdB)
 
