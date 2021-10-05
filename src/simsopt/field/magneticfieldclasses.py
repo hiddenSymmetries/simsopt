Small sign change to reflect true spherical coordinates; Hopefully all is fixed now!

src/simsopt/field/magneticfieldclasses.py
@@ -258,6 +258,8 @@ class CircularCoil(MagneticField):
     kind.  As inputs, it takes the radius of the coil (r0), its center, current
     (I) and its normal vector [either spherical angle components
     (normal=[theta,phi]) or (x,y,z) components of a vector (normal=[x,y,z])]).
+    Sign convention: CircularCoil with a positive current produces a magnetic field
+    vector in the same direction as the normal when evaluated at the center of the coil.
 
     Args:
         r0: radius of the coil
@@ -284,11 +286,11 @@ class CircularCoil(MagneticField):
         self.rotMatrix = np.array([
             [np.cos(phi) * np.cos(theta)**2 + np.sin(theta)**2,
              -np.sin(phi / 2)**2 * np.sin(2 * theta), 
-             -np.cos(theta) * np.sin(phi)],
-            [np.sin(phi / 2)**2 * np.sin(2 * theta), 
+             np.cos(theta) * np.sin(phi)],
+            [-np.sin(phi / 2)**2 * np.sin(2 * theta), 
              np.cos(theta)**2 + np.cos(phi) * np.sin(theta)**2, 
              np.sin(phi) * np.sin(theta)],
-            [np.cos(theta) * np.sin(phi),
+            [-np.cos(theta) * np.sin(phi),
              -np.sin(phi) * np.sin(theta), 
              np.cos(phi)]
         ])

tests/field/test_magneticfields.py
@@ -180,7 +180,7 @@ class Testing(unittest.TestCase):
         normal = [0, np.pi/2]
         curve = CurveXYZFourier(300, 1)
         curve.set_dofs([center[0], 0, 0., center[1], radius, 0., center[2], 0., radius])
-        Bcircular = BiotSavart([Coil(curve, Current(current))])
+        Bcircular = BiotSavart([Coil(curve, Current(-current))])
         Bfield = CircularCoil(I=current, r0=radius, normal=normal, center=center)
         Bfield.set_points(points)
         Bcircular.set_points(points)
@@ -192,12 +192,12 @@ class Testing(unittest.TestCase):
         assert np.allclose(dB1_by_dX, transpGradB1)  # symmetry of the gradient
         Bfield.set_points([[0.1, 0.2, 0.3]])
         Afield = Bfield.A()
-        assert np.allclose(Afield, [[0, -5.15785, 2.643056]])
+        assert np.allclose(Afield, [[0, 5.15785, -2.643056]])
         # use normal=[1,0,0]
         normal = [1, 0, 0]
         curve = CurveXYZFourier(300, 1)
         curve.set_dofs([center[0], 0, 0., center[1], radius, 0., center[2], 0., radius])
-        Bcircular = BiotSavart([Coil(curve, Current(current))])
+        Bcircular = BiotSavart([Coil(curve, Current(-current))])
         Bfield = CircularCoil(I=current, r0=radius, normal=normal, center=center)
         Bfield.set_points(points)
         Bcircular.set_points(points)