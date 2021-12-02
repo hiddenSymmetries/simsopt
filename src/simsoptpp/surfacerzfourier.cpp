cb06fe9efd5c1ad9eba9f2129d0b9580c3013b5f
Florian Wechsung <f.wechsung@gmail.com>
2021-12-02 11:19:00 -0500

    try to figure out where the floating point error comes from

--- src/simsoptpp/surfacerzfourier.cpp
+++ src/simsoptpp/surfacerzfourier.cpp
@@ -37,7 +37,11 @@ void SurfaceRZFourier<Array>::gamma_impl(Array& data, Array& quadpoints_phi, Arr
             double cos_nfpphi = cos(-nfp*phi);
             for (int m = 0; m <= mpol; ++m) {
                 simd_t sinterm, costerm;
-                xsimd::sincos(m*theta+ntor*nfp*phi, sinterm, costerm);
+                //xsimd::sincos(m*theta+ntor*nfp*phi, sinterm, costerm);
+                for (int l = 0; l < simd_size; ++l) {
+                    sinterm[l] = sin(m*theta+ntor*nfp*phi);
+                    costerm[l] = cos(m*theta+ntor*nfp*phi);
+                }
                 for (int i = 0; i < 2*ntor+1; ++i) {
                     int n  = i - ntor;
                     //simd_t sinterm, costerm;
