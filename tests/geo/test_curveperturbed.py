dcbd803bcc40936b48cabd5e0101bc02c79cdb5c
Florian Wechsung <f.wechsung@gmail.com>
2021-12-07 11:54:45 -0500

    more testing

--- tests/geo/test_curveperturbed.py
+++ tests/geo/test_curveperturbed.py
@@ -1,10 +1,9 @@
 import unittest
-from simsopt.util.zoo import get_ncsx_data
 from simsopt.geo.curveperturbed import GaussianSampler, PerturbationSample, CurvePerturbed
 from simsopt.geo.curvexyzfourier import CurveXYZFourier
 from randomgen import PCG64
 import numpy as np
-from simsopt.geo.curveobjectives import LpCurveTorsion
+from simsopt.geo.curveobjectives import LpCurveTorsion, MinimumDistance
 
 
 class CurvePerturbationTesting(unittest.TestCase):
@@ -45,7 +44,7 @@ class CurvePerturbationTesting(unittest.TestCase):
         print("periodic_err", np.mean(periodic_err))
         assert np.mean(periodic_err) < 1e-6
 
-    def test_perturbed_objective(self):
+    def test_perturbed_objective_torsion(self):
         # test the torsion objective as that covers all derivatives (up to
         # third) of a curve
         sigma = 1
@@ -81,5 +80,56 @@ class CurvePerturbationTesting(unittest.TestCase):
             deriv_est = (Jh-J0)/eps
             err_new = np.linalg.norm(deriv_est-deriv)
             print("err_new %s" % (err_new))
+            assert err_new < 0.55 * err
+            err = err_new
+
+    def test_perturbed_objective_distance(self):
+        # test the torsion objective as that covers all derivatives (up to
+        # third) of a curve
+        sigma = 1
+        length_scale = 0.2
+        points = np.linspace(0, 1, 200, endpoint=False)
+        sampler = GaussianSampler(points, sigma, length_scale, n_derivs=1)
+        rg = np.random.Generator(PCG64(1))
+        sample1 = PerturbationSample(sampler, randomgen=rg)
+        sample2 = PerturbationSample(sampler, randomgen=rg)
+
+        order = 4
+        nquadpoints = 200
+        curve1 = CurveXYZFourier(nquadpoints, order)
+        dofs = np.zeros((curve1.dof_size, ))
+        dofs[1] = 1.
+        dofs[2*order+3] = 1.
+        dofs[4*order+3] = 1.
+        curve1.x = dofs
+
+        curve2 = CurveXYZFourier(nquadpoints, order)
+        dofs = np.zeros((curve2.dof_size, ))
+        dofs[1] = 2.
+        dofs[2*order+3] = 2.
+        dofs[4*order+3] = 2.
+        curve2.x = dofs
+
+        curve1 = CurvePerturbed(curve1, sample1)
+        curve2 = CurvePerturbed(curve2, sample2)
+
+        J = MinimumDistance([curve1, curve2], 2.0)
+        J0 = J.J()
+        curve1.resample()
+        assert J0 != J.J()
+        J0 = J.J()
+        curve_dofs = J.x
+        h = 1e-3 * np.random.rand(len(curve_dofs)).reshape(curve_dofs.shape)
+        dJ = J.dJ()
+        deriv = np.sum(dJ * h)
+        assert np.abs(deriv) > 1e-10
+        err = 1e6
+        for i in range(2, 10):
+            eps = 0.5**i
+            J.x = curve_dofs + eps * h
+            Jh = J.J()
+            deriv_est = (Jh-J0)/eps
+            err_new = np.linalg.norm(deriv_est-deriv)
+            # print("err_new %s" % (err_new))
             assert err_new < 0.55 * err
             err = err_new
