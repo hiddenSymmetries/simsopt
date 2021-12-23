f67c0f0c80c190700ea54ece100d830f83670167
Bharat Medasani <mbkumar@gmail.com>
2021-12-23 12:52:21 -0500

    unit test added for SimsoptRequires

---
+++ tests/util/test_dev.py
@@ -0,0 +1,31 @@
+import unittest
+
+try:
+    import numpy as np
+except ImportError:
+    np = None
+
+from simsopt.util.dev import SimsoptRequires, deprecated
+from simsopt._core.graph_optimizable import Optimizable
+
+@SimsoptRequires(np is not None, "numpy is not installed.")
+class TestClass(Optimizable):
+    def __init__(self):
+        x = np.array([1.2, 0.9, -0.4])
+        fixed = np.full(3, False)
+        super().__init__(x0=x, fixed=fixed)
+
+    def J(self):
+        return np.exp(self.full_x[0] ** 2 - np.exp(self.full_x[1]) \
+                      + np.sin(self.full_x[2]))
+
+    return_fn_map = {'J': J}
+
+class SimsoptRequiresTest(unittest.TestCase):
+    def test_instance_check(self):
+        tf = TestClass()
+        self.assertIsInstance(tf, TestClass)
+
+
+if __name__ == '__main__':
+    unittest.main()
