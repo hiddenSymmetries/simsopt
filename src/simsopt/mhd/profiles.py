4846c5d3fd26d5eac8b2539d643b860286321c3d
Matt Landreman <mattland@umd.edu>
2021-12-23 16:07:06 -0500

    Coded up expressions in Redl paper

--- src/simsopt/mhd/profiles.py
+++ src/simsopt/mhd/profiles.py
@@ -10,10 +10,6 @@ temperature, pressure, and other quantities that are flux functions.
 import logging
 import numpy as np
 import numpy.polynomial.polynomial as poly
-from scipy.interpolate import RectBivariateSpline
-from scipy.optimize import minimize
-from scipy.integrate import quad
-import matplotlib.pyplot as plt
 from .._core.graph_optimizable import Optimizable
 
 logger = logging.getLogger(__name__)
@@ -28,7 +24,20 @@ class Profile(Optimizable):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def __call__(self, *args, **kwargs):
+        """ Shortcut for calling f(s) """
+        return self.f(*args, **kwargs)
+
     def plot(self, ax=None, show=True, n=100):
+        """
+        Plot the profile using matplotlib.
+
+        Args:
+            ax: The axis object on which to plot. If ``None``, a new figure will be created.
+            show: Whether to call matplotlib's ``show()`` function.
+            n: The number of grid points in s to show.
+        """
+        import matplotlib.pyplot as plt
         if ax is None:
             fig = plt.figure()
             ax = fig.add_subplot()
@@ -65,124 +74,25 @@ class ProfilePolynomial(Profile):
         return poly.polyval(s, poly.polyder(self.local_full_x))
 
 
-def compute_trapped_fraction(modB, sqrtg):
+class ProfileScaled(Profile):
     """
-    Compute the effective fraction of trapped particles, which enters
-    several formulae for neoclassical transport, as well as several
-    quantities that go into its calculation.  The input data can be
-    provided on a uniform grid of arbitrary toroidal and poloidal
-    angles that need not be straight-field-line angles.
-
-    The effective inverse aspect ratio epsilon is defined by
-
-    .. math::
-        \frac{Bmax}{Bmin} = \frac{1 + \epsilon}{1 - \epsilon}
-
-    This definition is motivated by the fact that this formula would
-    be true in the case of circular cross-section surfaces in
-    axisymmetry with :math:`B \propto 1/R` and :math:`R = (1 +
-    \epsilon \cos\theta) R_0`.
+    A Profile which is equivalent to another Profile object but scaled
+    by a constant. This constant is an optimizable dof, which is fixed by default.
 
     Args:
-        modB: 3D array of size (ntheta, nphi, ns) representing |B|.
-        sqrtg: 3D array of size (ntheta, nphi, ns) representing the Jacobian.
-    Returns:
-        3-element tuple containing three 1D arrays, corresponding to radial grid points
-            Bmin: minimum of |B| on each surface
-            Bmax: maximum of |B| on each surface
-            epsilon: A measure of the inverse aspect ratio
-            fsa_B2: <B^2>, where < > denotes a flux surface average.
-            f_t: The effective trapped fraction
+        base: A Profile object to scale
+        scalefac: A number by which the base profile will be scaled.
     """
-    ntheta = modB.shape[0]
-    nphi = modB.shape[1]
-    ns = modB.shape[2]
-    fourpisq = 4 * np.pi * np.pi
-    dVds = np.mean(sqrtg, axis=(0, 1)) / fourpisq
-    fsa_B2 = np.mean(modB * modB * sqrtg, axis=(0, 1)) / (fourpisq * dVds)
 
-    epsilon = np.zeros(ns)
-    f_t = np.zeros(ns)
-    Bmin = np.zeros(ns)
-    Bmax = np.zeros(ns)
+    def __init__(self, base, scalefac):
+        self.base = base
+        super().__init__(x0=np.array([scalefac]), names=['scalefac'])
+        self.fix_all()
 
-    # Make a slightly enlarged version of the input array with the
-    # first row and column appended at the ends, for periodicity.
-    modB_big = np.zeros((ntheta + 1, nphi + 1, ns))
-    modB_big[:ntheta, :nphi, :] = modB
-    modB_big[-1, :nphi, :] = modB[0, :, :]
-    modB_big[:, -1, :] = modB_big[:, 0, :]
+    def f(self, s):
+        """ Return the value of the profile at specified points in s. """
+        return self.local_full_x[0] * self.base.f(s)
 
-    theta = np.arange(ntheta + 1)
-    phi = np.arange(nphi + 1)
-    for js in range(ns):
-        index_of_min = np.unravel_index(np.argmin(modB_big[:, :, js]), modB_big.shape[:2])
-        index_of_max = np.unravel_index(np.argmax(modB_big[:, :, js]), modB_big.shape[:2])
-        modB_spline = RectBivariateSpline(theta, phi, modB_big[:, :, js])
-        soln = minimize(lambda x: np.ravel(modB_spline(x[0], x[1])),
-                        index_of_min,
-                        bounds=((0, ntheta), (0, nphi)))
-        modBmin = soln.fun
-        soln = minimize(lambda x: -np.ravel(modB_spline(x[0], x[1])),
-                        index_of_max,
-                        bounds=((0, ntheta), (0, nphi)))
-        modBmax = -soln.fun
-        Bmin[js] = modBmin
-        Bmax[js] = modBmax
-        w = modBmax / modBmin
-        epsilon[js] = (w - 1) / (w + 1)
-
-        def integrand(lambd):
-            # This function gives lambda / <sqrt(1 - lambda B)>:
-            return lambd / (np.mean(np.sqrt(1 - lambd * modB[:, :, js]) * sqrtg[:, :, js]) \
-                            / (fourpisq * dVds[js]))
-
-        integral = quad(integrand, 0, 1 / modBmax)
-        f_t[js] = 1 - 0.75 * fsa_B2[js] * integral[0]
-
-    return Bmin, Bmax, epsilon, fsa_B2, f_t
-
-
-def quasisymmetry_filtered_trapped_fraction(booz, helicity_m, helicity_n):
-    """
-    Compute quantities needed for the Redl bootstrap current formula.
-
-    Args:
-        booz: An instance of :obj:`simsopt.mhd.boozer.Boozer`
-        helicity_m: The poloidal mode number of the desired symmetry, usually 1.
-        helicity_n: The toroidal mode number of the desired symmetry.
-    Returns:
-        3-element tuple containing three 1D arrays, corresponding to radial grid points
-            epsilon: A measure of the inverse aspect ratio
-            fsa_B2: <B^2>, where < > denotes a flux surface average.
-            f_t: The effective trapped fraction
-    """
-    pass
-
-
-def j_dot_B_Redl(booz, ne, Te, Ti, Zeff, R, iota):
-    """
-    Compute the bootstrap current using the formulae in Redl et al,
-    Physics of Plasmas (2021).
-
-    The quantity <j dot B> is computed at all surfaces s that are
-    available in the booz object.
-
-    The profiles of ne, Te, Ti, and Zeff should all be Profile
-    objects, meaning they have ``f()`` and ``dfds()`` functions. If
-    Zeff==None, a constant 1 is assumed. If Zeff is a float, a
-    constant profile will be assumed.
-
-    Args:
-        booz: An instance of :obj:`simsopt.mhd.boozer.Boozer`
-        ne: A
-    Returns:
-        jdotB: 
-    """
-    if Zeff == None:
-        Zeff = ProfilePolynomial(1.0)
-    if not isinstance(Zeff, Profile):
-        # Zeff is presumably a number. Convert it to a constant profile.
-        Zeff = ProfilePolynomial(Zeff)
-
-    booz.run()
+    def dfds(self, s):
+        """ Return the d/ds derivative of the profile at specified points in s. """
+        return self.local_full_x[0] * self.base.dfds(s)
--- tests/mhd/test_profiles.py
+++ tests/mhd/test_profiles.py
@@ -1,7 +1,7 @@
 import unittest
 import logging
 import numpy as np
-from simsopt.mhd.profiles import ProfilePolynomial, compute_trapped_fraction
+from simsopt.mhd.profiles import ProfilePolynomial, ProfileScaled
 
 logger = logging.getLogger(__name__)
 #logging.basicConfig(level=logging.DEBUG)
@@ -16,6 +16,7 @@ class ProfilesTests(unittest.TestCase):
         prof = ProfilePolynomial([3, 0, 0, -3])
         s = np.linspace(0, 1, 10)
         np.testing.assert_allclose(prof.f(s), 3 * (1 - s ** 3))
+        np.testing.assert_allclose(prof(s), 3 * (1 - s ** 3))
         np.testing.assert_allclose(prof.dfds(s), 3 * (- 3 * s ** 2))
 
     def test_plot(self):
@@ -26,82 +27,18 @@ class ProfilesTests(unittest.TestCase):
         prof = ProfilePolynomial([3, 0, 0, -3])
         prof.plot(show=False)
 
-
-class RedlBootstrapTests(unittest.TestCase):
-    def test_compute_trapped_fraction(self):
-        ns = 2
-        ntheta = 15
-        nphi = 7
-        nfp = 3
-        modB = np.zeros((ntheta, nphi, ns))
-        sqrtg = np.zeros((ntheta, nphi, ns))
-
-        theta1d = np.linspace(0, 2 * np.pi, ntheta, endpoint=False)
-        phi1d = np.linspace(0, 2 * np.pi / nfp, nphi, endpoint=False)
-        phi, theta = np.meshgrid(phi1d, theta1d)
-
-        sqrtg[:, :, 0] = 10.0
-        sqrtg[:, :, 1] = -25.0
-
-        modB[:, :, 0] = 13.0 + 2.6 * np.cos(theta)
-        modB[:, :, 1] = 9.0 + 3.7 * np.sin(theta - nfp * phi)
-
-        Bmin, Bmax, epsilon, fsa_b2, f_t = compute_trapped_fraction(modB, sqrtg)
-        # The average of (b0 + b1 cos(theta))^2 is b0^2 + (1/2) * b1^2
-        np.testing.assert_allclose(fsa_b2, [13.0 ** 2 + 0.5 * 2.6 ** 2, 9.0 ** 2 + 0.5 * 3.7 ** 2])
-        np.testing.assert_allclose(Bmin, [13.0 - 2.6, 9.0 - 3.7], rtol=1e-4)
-        np.testing.assert_allclose(Bmax, [13.0 + 2.6, 9.0 + 3.7], rtol=1e-4)
-        np.testing.assert_allclose(epsilon, [2.6 / 13.0, 3.7 / 9.0], rtol=1e-3)
-        print('f_t:', f_t)
-
-    def test_trapped_fraction_Kim(self):
+    def test_scaled(self):
         """
-        Compare the trapped fraction to eq (C18) in Kim, Diamond, &
-        Groebner, Physics of Fluids B 3, 2050 (1991)
+        Test ProfileScaled
         """
-        ns = 50
-        ntheta = 100
-        nphi = 3
-        B0 = 7.5
-        epsilon_in = np.linspace(0, 1, ns, endpoint=False)  # Avoid divide-by-0 when epsilon=1
-        theta1d = np.linspace(0, 2 * np.pi, ntheta, endpoint=False)
-        nfp = 3
-        phi1d = np.linspace(0, 2 * np.pi / nfp, nphi, endpoint=False)
-        #phi1d = np.array([0])
-        phi, theta = np.meshgrid(phi1d, theta1d)
-        modB = np.zeros((ntheta, nphi, ns))
-        sqrtg = np.zeros((ntheta, nphi, ns))
-        for js in range(ns):
-            # Eq (A6)
-            modB[:, :, js] = B0 / (1 + epsilon_in[js] * np.cos(theta))
-            # For Jacobian, use eq (A7) for the theta dependence,
-            # times an arbitrary overall scale factor
-            sqrtg[:, :, js] = 6.7 * (1 + epsilon_in[js] * np.cos(theta))
-        Bmin, Bmax, epsilon_out, fsa_B2, f_t = compute_trapped_fraction(modB, sqrtg)
-
-        f_t_Kim = 1.46 * np.sqrt(epsilon_in) - 0.46 * epsilon_in  # Eq (C18) in Kim et al
-
-        np.testing.assert_allclose(Bmin, B0 / (1 + epsilon_in))
-        np.testing.assert_allclose(Bmax, B0 / (1 - epsilon_in))
-        np.testing.assert_allclose(epsilon_in, epsilon_out)
-        # Eq (A8):
-        np.testing.assert_allclose(fsa_B2, B0 * B0 / np.sqrt(1 - epsilon_in ** 2), rtol=1e-6)
-        np.testing.assert_allclose(f_t, f_t_Kim, rtol=0.1, atol=0.07)  # We do not expect precise agreement. 
-
-        if False:
-            import matplotlib.pyplot as plt
-            plt.figure(figsize=(14, 5))
-            plt.subplot(1, 2, 1)
-            plt.plot(epsilon_in, f_t, label='simsopt')
-            plt.plot(epsilon_in, f_t_Kim, label='Kim')
-            plt.xlabel('epsilon')
-            plt.title('Trapped fraction $f_t$')
-            plt.legend(loc=0)
-
-            plt.subplot(1, 2, 2)
-            #plt.plot(epsilon_in, (f_t_Kim - f_t) / (1 - f_t))
-            plt.plot(epsilon_in, f_t_Kim - f_t)
-            plt.title('Relative difference in $f_c$')
-            #plt.plot(epsilon_in, epsilon_out, 'r')
-            #plt.plot(epsilon_in, epsilon_in, ':k')
-            plt.show()
+        prof1 = ProfilePolynomial([3, 0, 0, -3])
+        scalefac = 0.6
+        prof2 = ProfileScaled(prof1, scalefac)
+        prof2.unfix_all()
+        s = np.linspace(0, 1, 10)
+        np.testing.assert_allclose(prof2(s), scalefac * prof1(s))
+        np.testing.assert_allclose(prof2.dfds(s), scalefac * prof1.dfds(s))
+        newscalefac = 1.3
+        prof2.x = [newscalefac]
+        np.testing.assert_allclose(prof2(s), newscalefac * prof1(s))
+        np.testing.assert_allclose(prof2.dfds(s), newscalefac * prof1.dfds(s))
