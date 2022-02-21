7c4ef3d6cfb6ba7ebab9c03703f0d4c8eb677cab
Andrew Giuliani <giuliani@cims.nyu.edu>
2022-02-21 16:51:14 -0500

    added vjps

--- src/simsopt/field/biotsavart.py
+++ src/simsopt/field/biotsavart.py
@@ -190,3 +190,95 @@ class BiotSavart(sopp.BiotSavart, MagneticField):
         dB_by_dcoilcurrents = self.dB_by_dcoilcurrents()
         res_current = [np.sum(v * dB_by_dcoilcurrents[i]) for i in range(len(dB_by_dcoilcurrents))]
         return sum([coils[i].vjp(res_gamma[i], res_gammadash[i], np.asarray([res_current[i]])) for i in range(len(coils))])
+
+    def dA_by_dcoilcurrents(self, compute_derivatives=0):
+        points = self.get_points_cart_ref()
+        npoints = len(points)
+        ncoils = len(self.__coils)
+        if any([not self.fieldcache_get_status(f'A_{i}') for i in range(ncoils)]):
+            assert compute_derivatives >= 0
+            self.compute(compute_derivatives)
+        self._dA_by_dcoilcurrents = [self.fieldcache_get_or_create(f'A_{i}', [npoints, 3]) for i in range(ncoils)]
+        return self._dA_by_dcoilcurrents
+
+    def d2A_by_dXdcoilcurrents(self, compute_derivatives=1):
+        points = self.get_points_cart_ref()
+        npoints = len(points)
+        ncoils = len(self.__coils)
+        if any([not self.fieldcache_get_status(f'dA_{i}') for i in range(ncoils)]):
+            assert compute_derivatives >= 1
+            self.compute(compute_derivatives)
+        self._d2A_by_dXdcoilcurrents = [self.fieldcache_get_or_create(f'dA_{i}', [npoints, 3, 3]) for i in range(ncoils)]
+        return self._d2A_by_dXdcoilcurrents
+
+    def d3A_by_dXdXdcoilcurrents(self, compute_derivatives=2):
+        points = self.get_points_cart_ref()
+        npoints = len(points)
+        ncoils = len(self.__coils)
+        if any([not self.fieldcache_get_status(f'ddA_{i}') for i in range(ncoils)]):
+            assert compute_derivatives >= 2
+            self.compute(compute_derivatives)
+        self._d3A_by_dXdXdcoilcurrents = [self.fieldcache_get_or_create(f'ddA_{i}', [npoints, 3, 3, 3]) for i in range(ncoils)]
+        return self._d3A_by_dXdXdcoilcurrents
+
+
+    def A_and_dA_vjp(self, v, vgrad):
+        r"""
+        Same as :obj:`simsopt.geo.biotsavart.BiotSavart.A_vjp` but returns the vector Jacobian product for :math:`A` and :math:`\nabla A`, i.e. it returns
+
+        .. math::
+
+            \{ \sum_{i=1}^{n} \mathbf{v}_i \cdot \partial_{\mathbf{c}_k} \mathbf{A}_i \}_k, \{ \sum_{i=1}^{n} {\mathbf{v}_\mathrm{grad}}_i \cdot \partial_{\mathbf{c}_k} \nabla \mathbf{A}_i \}_k.
+        """
+
+        coils = self.__coils
+        gammas = [coil.curve.gamma() for coil in coils]
+        gammadashs = [coil.curve.gammadash() for coil in coils]
+        currents = [coil.current.get_value() for coil in coils]
+        res_gamma = [np.zeros_like(gamma) for gamma in gammas]
+        res_gammadash = [np.zeros_like(gammadash) for gammadash in gammadashs]
+        res_grad_gamma = [np.zeros_like(gamma) for gamma in gammas]
+        res_grad_gammadash = [np.zeros_like(gammadash) for gammadash in gammadashs]
+
+        points = self.get_points_cart_ref()
+        sopp.biot_savart_vector_potential_vjp_graph(points, gammas, gammadashs, currents, v,
+                                   res_gamma, res_gammadash, vgrad, res_grad_gamma, res_grad_gammadash)
+
+        dA_by_dcoilcurrents = self.dA_by_dcoilcurrents()
+        res_current = [np.sum(v * dA_by_dcoilcurrents[i]) for i in range(len(dA_by_dcoilcurrents))]
+        d2A_by_dXdcoilcurrents = self.d2A_by_dXdcoilcurrents()
+        res_grad_current = [np.sum(vgrad * d2A_by_dXdcoilcurrents[i]) for i in range(len(d2A_by_dXdcoilcurrents))]
+
+        res = (
+            sum([coils[i].vjp(res_gamma[i], res_gammadash[i], np.asarray([res_current[i]])) for i in range(len(coils))]),
+            sum([coils[i].vjp(res_grad_gamma[i], res_grad_gammadash[i], np.asarray([res_grad_current[i]])) for i in range(len(coils))])
+        )
+
+        return res
+
+    def A_vjp(self, v):
+        r"""
+        Assume the field was evaluated at points :math:`\mathbf{x}_i, i\in \{1, \ldots, n\}` and denote the value of the field at those points by
+        :math:`\{\mathbf{A}_i\}_{i=1}^n`.
+        These values depend on the shape of the coils, i.e. on the dofs :math:`\mathbf{c}_k` of each coil.
+        This function returns the vector Jacobian product of this dependency, i.e.
+
+        .. math::
+
+            \{ \sum_{i=1}^{n} \mathbf{v}_i \cdot \partial_{\mathbf{c}_k} \mathbf{A}_i \}_k.
+
+        """
+
+        coils = self.__coils
+        gammas = [coil.curve.gamma() for coil in coils]
+        gammadashs = [coil.curve.gammadash() for coil in coils]
+        currents = [coil.current.get_value() for coil in coils]
+        res_gamma = [np.zeros_like(gamma) for gamma in gammas]
+        res_gammadash = [np.zeros_like(gammadash) for gammadash in gammadashs]
+
+        points = self.get_points_cart_ref()
+        sopp.biot_savart_vector_potential_vjp_graph(points, gammas, gammadashs, currents, v,
+                                   res_gamma, res_gammadash, [], [], [])
+        dA_by_dcoilcurrents = self.dA_by_dcoilcurrents()
+        res_current = [np.sum(v * dA_by_dcoilcurrents[i]) for i in range(len(dA_by_dcoilcurrents))]
+        return sum([coils[i].vjp(res_gamma[i], res_gammadash[i], np.asarray([res_current[i]])) for i in range(len(coils))])
