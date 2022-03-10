4eb41e5c6ab183885e4fa5ee6151b507afb97a8c
Matt Landreman <mattland@umd.edu>
2022-03-10 06:49:58 -0500

    Added documentation and plotting

--- src/simsopt/mhd/vmec_diagnostics.py
+++ src/simsopt/mhd/vmec_diagnostics.py
@@ -721,10 +721,108 @@ def vmec_splines(vmec):
     return results
 
 
-def vmec_fieldlines(vs, s, alpha, theta1d=None, phi1d=None):
-    """
+def vmec_fieldlines(vs, s, alpha, theta1d=None, phi1d=None, plot=False):
+    r"""
     Compute field lines in a vmec configuration, and compute many
-    geometric quantities of interest along the field lines.
+    geometric quantities of interest along the field lines. In
+    particular, this routine computes the geometric quantities that
+    enter the gyrokinetic equation.
+
+    One of the tasks performed by this function is to convert between
+    the poloidal angles :math:`\theta_{vmec}` and
+    :math:`\theta_{pest}`. The latter is the angle in which the field
+    lines are straight when used in combination with the standard
+    toroidal angle :math:`\phi`. Note that all angles in this function
+    have period :math:`2\pi`, not period 1.
+
+    For the inputs and outputs of this function, a field line label
+    coordinate is defined by
+
+    .. math::
+
+        \alpha = \theta_{pest} - \iota \phi.
+
+    Also, wherever the term ``psi`` appears in variable names in this function and the returned arrays,
+    it means :math:`\psi =` the toroidal flux divided by :math:`2\pi`, so
+
+    .. math::
+
+        \vec{B} = \nabla\psi\times\nabla\theta_{pest} + \iota\nabla\phi\times\nabla\psi = \nabla\psi\times\nabla\alpha.
+
+    To specify the parallel extent of the field lines, you can provide
+    either a grid of :math:`\theta_{pest}` values or a grid of
+    :math:`\phi` values. If you specify both or neither, ``ValueError``
+    will be raised.
+
+    Most of the arrays that are computed have shape ``(ns, nalpha,
+    nl)``, where ``ns`` is the number of flux surfaces, ``nalpha`` is the
+    number of field lines on each flux surface, and ``nl`` is the number
+    of grid points along each field line. In other words, ``ns`` is the
+    size of the input ``s`` array, ``nalpha`` is the size of the input
+    ``alpha`` array, and ``nl`` is the size of the input ``theta1d`` or
+    ``phi1d`` array. The output arrays are returned as attributes of the
+    returned object. Many intermediate quantities are included, such
+    as the Cartesian components of the covariant and contravariant
+    basis vectors. Some of the most useful of these output arrays are (all with SI units):
+
+    - ``phi``: The standard toroidal angle :math:`\phi`.
+    - ``theta_vmec``: VMEC's poloidal angle :math:`\theta_{vmec}`.
+    - ``theta_pest``: The straight-field-line angle :math:`\theta_{pest}` associated with :math:`\phi`.
+    - ``modB``: The magnetic field magnitude :math:`|B|`.
+    - ``B_sup_theta_vmec``: :math:`\vec{B}\cdot\nabla\theta_{vmec}`.
+    - ``B_sup_phi``: :math:`\vec{B}\cdot\nabla\phi`.
+    - ``B_cross_grad_B_dot_grad_alpha``: :math:`\vec{B}\times\nabla|B|\cdot\nabla\alpha`.
+    - ``B_cross_grad_B_dot_grad_psi``: :math:`\vec{B}\times\nabla|B|\cdot\nabla\psi`.
+    - ``B_cross_kappa_dot_grad_alpha``: :math:`\vec{B}\times\vec{\kappa}\cdot\nabla\alpha`,
+      where :math:`\vec{\kappa}=\vec{b}\cdot\nabla\vec{b}` is the curvature and :math:`\vec{b}=|B|^{-1}\vec{B}`.
+    - ``B_cross_kappa_dot_grad_psi``: :math:`\vec{B}\times\vec{\kappa}\cdot\nabla\psi`.
+    - ``grad_alpha_dot_grad_alpha``: :math:`|\nabla\alpha|^2 = \nabla\alpha\cdot\nabla\alpha`.
+    - ``grad_alpha_dot_grad_psi``: :math:`\nabla\alpha\cdot\nabla\psi`.
+    - ``grad_psi_dot_grad_psi``: :math:`|\nabla\psi|^2 = \nabla\psi\cdot\nabla\psi`.
+    - ``iota``: The rotational transform :math:`\iota`. This array has shape ``(ns,)``.
+    - ``shat``: The magnetic shear :math:`\hat s= (x/q) (d q / d x)` where 
+      :math:`x = \mathrm{Aminor_p} \, \sqrt{s}` and :math:`q=1/\iota`. This array has shape ``(ns,)``.
+
+    The following normalized versions of these quantities used in the
+    gyrokinetic codes ``stella``, ``gs2``, and ``GX`` are also
+    returned: ``bmag``, ``gbdrift``, ``gbdrift0``, ``cvdrift``,
+    ``cvdrift0``, ``gds2``, ``gds21``, and ``gds22``, along with
+    ``L_reference`` and ``B_reference``.  Instead of ``gradpar``, two
+    variants are returned, ``gradpar_theta`` and ``gradpar_phi``,
+    corresponding to choosing either :math:`\theta_{pest}` or
+    :math:`\phi` as the parallel coordinate.
+
+    The value(s) of ``s`` provided as input need not coincide with the
+    full grid or half grid in VMEC, as spline interpolation will be
+    used radially.
+
+    The implementation in this routine is similar to the one in the
+    gyrokinetic code ``stella``.
+
+    Example usage::
+
+        import numpy as np
+        from simsopt.mhd.vmec import Vmec
+        from simsopt.mhd.vmec_diagnostics import vmec_fieldlines
+
+        v = Vmec('wout_li383_1.4m.nc')
+        theta = np.linspace(-np.pi, np.pi, 50)
+        fl = vmec_fieldlines(v, 0.5, 0, theta1d=theta)
+        print(fl.B_cross_grad_B_dot_grad_alpha)
+
+    Args:
+        vs: Either an instance of :obj:`simsopt.mhd.vmec.Vmec`
+          or the structure returned by :func:`vmec_splines`.
+        s: Values of normalized toroidal flux on which to construct the field lines.
+          You can give a single number, or a list or numpy array.
+        alpha: Values of the field line label :math:`\alpha` on which to construct the field lines.
+          You can give a single number, or a list or numpy array.
+        theta1d: 1D array of :math:`\theta_{pest}` values, setting the grid points
+          along the field line and the parallel extent of the field line.
+        phi1d: 1D array of :math:`\phi` values, setting the grid points along the
+          field line and the parallel extent of the field line.
+        plot: Whether to create a plot of the main geometric quantities. Only one field line will
+          be plotted, corresponding to the leading elements of ``s`` and ``alpha``.
     """
     # If given a Vmec object, convert it to vmec_splines:
     if isinstance(vs, Vmec):
@@ -988,6 +1086,12 @@ def vmec_fieldlines(vs, s, alpha, theta1d=None, phi1d=None):
 
     grad_psi_dot_grad_psi = grad_psi_X * grad_psi_X + grad_psi_Y * grad_psi_Y + grad_psi_Z * grad_psi_Z
 
+    B_cross_grad_B_dot_grad_psi = (B_sub_theta_vmec * d_B_d_phi - B_sub_phi * d_B_d_theta_vmec) / sqrt_g_vmec * edge_toroidal_flux_over_2pi
+
+    # These next 2 lines are WRONG and need correcting!
+    B_cross_kappa_dot_grad_psi = B_cross_grad_B_dot_grad_psi
+    B_cross_kappa_dot_grad_alpha = B_cross_grad_B_dot_grad_alpha
+
     # Package results into a structure to return:
     results = Struct()
     variables = ['ns', 'nalpha', 'nl', 's', 'iota', 'd_iota_d_s', 'd_pressure_d_s', 'shat',
@@ -1003,7 +1107,27 @@ def vmec_fieldlines(vs, s, alpha, theta1d=None, phi1d=None):
                  'B_X', 'B_Y', 'B_Z',
                  'B_cross_grad_s_dot_grad_alpha', 'B_cross_grad_s_dot_grad_alpha_alternate',
                  'B_cross_grad_B_dot_grad_alpha', 'B_cross_grad_B_dot_grad_alpha_alternate',
+                 'B_cross_grad_B_dot_grad_psi', 'B_cross_kappa_dot_grad_psi', 'B_cross_kappa_dot_grad_alpha',
                  'grad_alpha_dot_grad_alpha', 'grad_alpha_dot_grad_psi', 'grad_psi_dot_grad_psi']
+
+    if plot:
+        import matplotlib.pyplot as plt
+        plt.figure(figsize=(13, 7))
+        nrows = 3
+        ncols = 4
+        variables = ['modB', 'B_sup_theta_vmec', 'B_sup_phi', 'B_cross_grad_B_dot_grad_alpha', 'B_cross_grad_B_dot_grad_psi',
+                     'B_cross_kappa_dot_grad_alpha', 'B_cross_kappa_dot_grad_psi',
+                     'grad_alpha_dot_grad_alpha', 'grad_alpha_dot_grad_psi', 'grad_psi_dot_grad_psi']
+        for j, variable in enumerate(variables):
+            plt.subplot(nrows, ncols, j + 1)
+            plt.plot(phi[0, 0, :], eval(variable + '[0, 0, :]'))
+            plt.xlabel('Standard toroidal angle $\phi$')
+            plt.title(variable)
+
+        plt.figtext(0.5, 0.995, f's={s[0]}, alpha={alpha[0]}', ha='center', va='top')
+        plt.tight_layout()
+        plt.show()
+
     for v in variables:
         results.__setattr__(v, eval(v))
 
