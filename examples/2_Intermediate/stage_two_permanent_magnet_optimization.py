Fixed the DipoleField class calculation so that it works with the poincare plotter. However, looks like the computation is too slow currently. Working on speeding this up. Also added option to truncate small elements of the linear least squares matrix in the permanent magnet optimization problem.

examples/2_Intermediate/stage_two_permanent_magnet_optimization.py
@@ -29,6 +29,7 @@ from simsopt.objectives.fluxobjective import SquaredFlux
 from simsopt.objectives.utilities import QuadraticPenalty
 from simsopt.geo.curve import curves_to_vtk, create_equally_spaced_curves
 from simsopt.field.biotsavart import BiotSavart
+from simsopt.field.dipolefield import DipoleField
 from simsopt.field.magneticfieldclasses import InterpolatedField, UniformInterpolationRule
 from simsopt.field.coil import Current, coils_via_symmetries
 from simsopt.geo.curveobjectives import CurveLength, MinimumDistance, \
@@ -59,9 +60,10 @@ R1 = 0.5
 order = 5
 
 # Number of iterations to perform:
-ci = "CI" in os.environ and os.environ['CI'].lower() in ['1', 'true']
+# ci = "CI" in os.environ and os.environ['CI'].lower() in ['1', 'true']
+ci = False
 nfieldlines = 3 if ci else 30
-tmax_fl = 10000 if ci else 40000
+tmax_fl = 5000 if ci else 40000
 degree = 2 if ci else 4
 
 MAXITER = 50 if ci else 400
@@ -95,8 +97,8 @@ coils = coils_via_symmetries(base_curves, base_currents, s.nfp, True)
 base_currents[0].fix_all()
 
 # Uncomment if want to keep the coils circular
-#for i in range(ncoils):
-#    base_curves[i].fix_all()
+for i in range(ncoils):
+    base_curves[i].fix_all()
 
 bs = BiotSavart(coils)
 bs.set_points(s.gamma().reshape((-1, 3)))
@@ -161,8 +163,10 @@ s.to_vtk(OUT_DIR + "surf_opt", extra_data=pointData)
 
 # Basic TF coil currents now optimized, turning to 
 # permanent magnet optimization now. 
-# pm_opt = PermanentMagnetOptimizer(s, coil_offset=0.1, B_plasma_surface=bs.B().reshape((nphi, ntheta, 3)))
-# pm_opt._optimize()
+pm_opt = PermanentMagnetOptimizer(s, coil_offset=0.1, B_plasma_surface=bs.B().reshape((nphi, ntheta, 3)))
+pm_opt._optimize(max_iter_MwPGP=5)
+b_dipole = DipoleField(pm_opt)
+b_dipole.set_points(s.gamma().reshape((-1, 3)))
 
 # Get full surface and get level sets for the Poincare plots below
 s = SurfaceRZFourier.from_vmec_input(filename, range="full torus", nphi=nphi, ntheta=ntheta)
@@ -170,7 +174,7 @@ sc_fieldline = SurfaceClassifier(s, h=0.1, p=2)
 sc_fieldline.to_vtk(OUT_DIR + 'levelset', h=0.02)
 
 
-def trace_fieldlines(bfield, label):
+def trace_fieldlines(bfield, label): 
     t1 = time.time()
     R0 = np.linspace(0.8, 1.3, nfieldlines)
     Z0 = np.zeros(nfieldlines)
@@ -192,15 +196,12 @@ zs = s.gamma()[:, :, 2]
 rrange = (np.min(rs), np.max(rs), n)
 phirange = (0, 2 * np.pi / s.nfp, n * 2)
 zrange = (0, np.max(zs), n // 2)
+#bsh = InterpolatedField(
+#    bs, degree, rrange, phirange, zrange, True, nfp=s.nfp, stellsym=True
+#)
+#trace_fieldlines(bsh, 'bsh_without_PMs')
 bsh = InterpolatedField(
-    bs, degree, rrange, phirange, zrange, True, nfp=s.nfp, stellsym=True
+    bs + b_dipole, degree, rrange, phirange, zrange, True, nfp=s.nfp, stellsym=True
 )
-# print('Error in B', bsh.estimate_error_B(1000), flush=True)
-trace_fieldlines(bsh, 'bsh_pm')
+trace_fieldlines(bsh, 'bsh_with_PMs')
 
-# trace_fieldlines(bs, 'bs')
-
-# Save initial permanent magnet surface
-# curves_to_vtk(curves, OUT_DIR + f"curves_pm")
-# pointData = {"B_N": np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)[:, :, None]}
-# s.to_vtk(OUT_DIR + "surf_pm_init", extra_data=pointData)