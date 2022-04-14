3120908d18f1b48a80943ba365b4e01c41be0ecf
Alan Kaptanoglu <akaptano@uw.edu>
2022-04-14 13:33:53 -0700

    Fixed bug in dipole field calculation with shared variables that should be private. Unfortunately poincare plots still not looking right despite the low Bn errors after optimization.

--- src/simsoptpp/dipole_field.cpp
+++ src/simsoptpp/dipole_field.cpp
@@ -5,7 +5,7 @@ Array dipole_field_B(Array& points, Array& m_points, Array& m) {
     int num_dipoles = m_points.shape(0);
     Array B = xt::zeros<double>({points.shape(0), points.shape(1)});
     double x, y, z, mx, my, mz, mpx, mpy, mpz, rx, ry, rz, rmag, rdotm;
-#pragma omp parallel for
+#pragma omp parallel for private(x, y, z, mx, my, mz, mpx, mpy, mpz, rx, ry, rz, rmag, rdotm)
     for (int i = 0; i < num_points; ++i) {
         x = points(i, 0);
         y = points(i, 1);
