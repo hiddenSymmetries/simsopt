Fixed bug in dipole field calculation with shared variables that should be private. Unfortunately poincare plots still not looking right despite the low Bn errors after optimization.

examples/2_Intermediate/stage_two_permanent_magnet_optimization.py
@@ -63,7 +63,7 @@ order = 5
 # Number of iterations to perform:
 ci = "CI" in os.environ and os.environ['CI'].lower() in ['1', 'true']
 ci = True
-nfieldlines = 20 if ci else 30
+nfieldlines = 30 if ci else 30
 tmax_fl = 30000 if ci else 40000
 degree = 2 if ci else 4
 
@@ -168,9 +168,9 @@ pm_opt = PermanentMagnetOptimizer(
     s, coil_offset=0.1, dr=0.05,
     B_plasma_surface=bs.B().reshape((nphi, ntheta, 3))
 )
-max_iter_MwPGP = 1000
+max_iter_MwPGP = 3000
 print('Done initializing the permanent magnet object')
-MwPGP_history, m_history, RS_history, err, dipoles = pm_opt._optimize(
+MwPGP_history, RS_history, m_history, dipoles = pm_opt._optimize(
     max_iter_MwPGP=max_iter_MwPGP, 
     max_iter_RS=10, reg_l2=0, reg_l0=0,
 )
@@ -233,9 +233,9 @@ def trace_fieldlines(bfield, label):
     Z0 = np.zeros(nfieldlines)
     phis = [(i / 4) * (2 * np.pi / s.nfp) for i in range(4)]
     fieldlines_tys, fieldlines_phi_hits = compute_fieldlines(
-        bfield, R0, Z0, tmax=tmax_fl, tol=1e-7, comm=comm,
+        bfield, R0, Z0, tmax=tmax_fl, tol=1e-10, comm=comm,
         #phis=phis, stopping_criteria=[LevelsetStoppingCriterion(sc_fieldline.dist)])
-        phis=phis, stopping_criteria=[IterationStoppingCriterion(50000)])
+        phis=phis, stopping_criteria=[IterationStoppingCriterion(80000)])
     t2 = time.time()
     print(f"Time for fieldline tracing={t2-t1:.3f}s. Num steps={sum([len(l) for l in fieldlines_tys])//nfieldlines}", flush=True)
     # particles_to_vtk(fieldlines_tys, OUT_DIR + f'fieldlines_{label}')
@@ -251,14 +251,22 @@ zrange = (0, np.max(zs), n // 2)
 bsh = InterpolatedField(
     bs, degree, rrange, phirange, zrange, True, nfp=s.nfp, stellsym=True
 )
+print(bsh.B())
 trace_fieldlines(bsh, 'bsh_without_PMs')
 print('Done with Poincare plots without the permanent magnets')
 t1 = time.time()
 bsh = InterpolatedField(
     b_dipole, degree, rrange, phirange, zrange, True, nfp=s.nfp, stellsym=True
 )
+print(bsh.B())
 t2 = time.time()
-print(f"Time for fieldline setup={t2-t1:.3f}s", flush=True)
 trace_fieldlines(bsh, 'bsh_only_PMs')
+print('Done with Poincare plots with the permanent magnets')
+t1 = time.time()
+bsh = InterpolatedField(
+    bs + b_dipole, degree, rrange, phirange, zrange, True, nfp=s.nfp, stellsym=True
+)
+t2 = time.time()
+trace_fieldlines(bsh, 'bsh_PMs')
 print('Done with Poincare plots with the permanent magnets')
 

src/simsopt/util/permanent_magnet_optimizer.py
@@ -898,13 +898,12 @@ class PermanentMagnetOptimizer:
                 MwPGP_hist, _, m_hist, m = sopp.MwPGP_algorithm(
                     A_obj=self.A_obj_expanded,
                     b_obj=self.b_obj,
-                    ATA=self.ATA_expanded, 
-                    ATb=self.ATb_expanded, 
+                    ATA=np.reshape(ATA, (self.ndipoles, 3, self.ndipoles, 3)),  # self.ATA_expanded, 
+                    ATb=np.reshape(ATb, (self.ndipoles, 3)),  # self.ATb_expanded, 
                     m_proxy=m0.reshape(self.ndipoles, 3),
                     m0=m0.reshape(self.ndipoles, 3),
                     m_maxima=self.m_maxima,
                     alpha=alpha_max,
-                    nu=nu,
                     epsilon=epsilon,
                     max_iter=max_iter_MwPGP,
                     verbose=True,
@@ -924,4 +923,4 @@ class PermanentMagnetOptimizer:
         print('<B * n> with the optimized permanent magnets = {0:.4e}'.format(ave_Bn)) 
         print('<B * n> with the sparsified permanent magnets = {0:.4e}'.format(ave_Bn_proxy)) 
         print('<B * n / |B| > with the permanent magnets = {0:.4e}'.format(ave_BnB)) 
-        return MwPGP_hist, m_hist, err_RS, m_proxy
+        return MwPGP_hist, err_RS, m_hist, m_proxy