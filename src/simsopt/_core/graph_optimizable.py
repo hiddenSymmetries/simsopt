abe9bbaafdfe4281b199e4594fed27cf6a3c05e4
Bharat Medasani <mbkumar@gmail.com>
2022-04-19 19:07:03 -0400

    Modules with graph framework are deprecated

---
+++ src/simsopt/_core/graph_optimizable.py
@@ -0,0 +1,6 @@
+from .optimizable import *
+
+import warnings
+
+warnings.warn("Importing of graph_optimizable module is deprecated. Import optimizable module instead",
+              DeprecationWarning, stacklevel=2)
