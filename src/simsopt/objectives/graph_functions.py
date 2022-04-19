1ff31ea47d499adacbdc97c2fd8772aa34c02272
Bharat Medasani <mbkumar@gmail.com>
2022-04-19 19:07:03 -0400

    Modules with graph framework are deprecated

---
+++ src/simsopt/objectives/graph_functions.py
@@ -0,0 +1,7 @@
+from .functions import *
+
+import warnings
+
+warnings.warn("Importing of simsopt.objectives.functions module is deprecated. "
+              "Instead import simsopt.objectives.functions module",
+              DeprecationWarning, stacklevel=2)
