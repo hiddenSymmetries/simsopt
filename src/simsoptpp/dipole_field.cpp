16a24050e4b335753b3fec8a098a6feeca14b96c
Florian Wechsung <f.wechsung@gmail.com>
2022-04-20 14:13:40 -0400

    formatting

--- src/simsoptpp/dipole_field.cpp
+++ src/simsoptpp/dipole_field.cpp
@@ -6,13 +6,13 @@ Array dipole_field_B(Array& points, Array& m_points, Array& m) {
     int num_dipoles = m_points.shape(0);
     Array B = xt::zeros<double>({points.shape(0), points.shape(1)});
     double x, y, z, mx, my, mz, mpx, mpy, mpz, rx, ry, rz, rmag, rdotm;
-#pragma omp parallel for private(x, y, z, mx, my, mz, mpx, mpy, mpz, rx, ry, rz, rmag, rdotm)
+    #pragma omp parallel for private(x, y, z, mx, my, mz, mpx, mpy, mpz, rx, ry, rz, rmag, rdotm)
     for (int i = 0; i < num_points; ++i) {
         x = points(i, 0);
         y = points(i, 1);
         z = points(i, 2);
         // Need to rotate into cylindrical, apply symmetries, add up, rotate back
-	for (int j = 0; j < num_dipoles; ++j) {
+        for (int j = 0; j < num_dipoles; ++j) {
             mpx = m_points(j, 0);
             mpy = m_points(j, 1);
             mpz = m_points(j, 2);
@@ -20,14 +20,14 @@ Array dipole_field_B(Array& points, Array& m_points, Array& m) {
             my = m(j, 1);
             mz = m(j, 2);
             rx = x - mpx;
-	    ry = y - mpy;
-	    rz = z - mpz;
-	    rmag = sqrt(rx * rx + ry * ry + rz * rz);
+            ry = y - mpy;
+            rz = z - mpz;
+            rmag = sqrt(rx * rx + ry * ry + rz * rz);
             rdotm = rx * mx + ry * my + rz * mz;
-	    B(i, 0) += 3.0 * rdotm * rx / pow(rmag, 5) - mx / pow(rmag, 3);
+            B(i, 0) += 3.0 * rdotm * rx / pow(rmag, 5) - mx / pow(rmag, 3);
             B(i, 1) += 3.0 * rdotm * ry / pow(rmag, 5) - my / pow(rmag, 3);
             B(i, 2) += 3.0 * rdotm * rz / pow(rmag, 5) - mz / pow(rmag, 3);
-	} 
+        } 
     }
     return B * 1e-7;
 }
@@ -38,12 +38,12 @@ Array dipole_field_dB(Array& points, Array& m_points, Array& m) {
     int num_dipoles = m_points.shape(0);
     Array dB = xt::zeros<double>({points.shape(0), points.shape(1), points.shape(1)});
     double x, y, z, mx, my, mz, mpx, mpy, mpz, rx, ry, rz, rmag, rdotm, r5;
-#pragma omp parallel for private(x, y, z, mx, my, mz, mpx, mpy, mpz, rx, ry, rz, rmag, rdotm, r5)
+    #pragma omp parallel for private(x, y, z, mx, my, mz, mpx, mpy, mpz, rx, ry, rz, rmag, rdotm, r5)
     for (int i = 0; i < num_points; ++i) {
         x = points(i, 0);
         y = points(i, 1);
         z = points(i, 2);
-	for (int j = 0; j < num_dipoles; ++j) {
+        for (int j = 0; j < num_dipoles; ++j) {
             mpx = m_points(j, 0);
             mpy = m_points(j, 1);
             mpz = m_points(j, 2);
@@ -51,18 +51,18 @@ Array dipole_field_dB(Array& points, Array& m_points, Array& m) {
             my = m(j, 1);
             mz = m(j, 2);
             rx = x - mpx;
-	    ry = y - mpy;
-	    rz = z - mpz;
-	    rmag = sqrt(rx * rx + ry * ry + rz * rz);
+            ry = y - mpy;
+            rz = z - mpz;
+            rmag = sqrt(rx * rx + ry * ry + rz * rz);
             rdotm = rx * mx + ry * my + rz * mz;
             r5 = 3.0 / pow(rmag, 5);
-	    dB(i, 0, 0) += r5 * ((2 * mx * rx + rdotm) - 5 * rdotm / pow(rmag, 2) * rx * rx); 
-	    dB(i, 0, 1) += r5 * ((mx * ry + my * rx) - 5 * rdotm / pow(rmag, 2) * rx * ry);
-	    dB(i, 0, 2) += r5 * ((mx * rz + mz * rx) - 5 * rdotm / pow(rmag, 2) * rx * rz);
-	    dB(i, 1, 1) += r5 * ((2 * my * ry + rdotm) - 5 * rdotm / pow(rmag, 2) * ry * ry); 
-	    dB(i, 1, 2) += r5 * ((my * rz + mz * ry) - 5 * rdotm / pow(rmag, 2) * ry * rz);
-	    dB(i, 2, 2) += r5 * ((2 * mz * rz + rdotm) - 5 * rdotm / pow(rmag, 2) * rz * rz); 
-	}
+            dB(i, 0, 0) += r5 * ((2 * mx * rx + rdotm) - 5 * rdotm / pow(rmag, 2) * rx * rx); 
+            dB(i, 0, 1) += r5 * ((mx * ry + my * rx) - 5 * rdotm / pow(rmag, 2) * rx * ry);
+            dB(i, 0, 2) += r5 * ((mx * rz + mz * rx) - 5 * rdotm / pow(rmag, 2) * rx * rz);
+            dB(i, 1, 1) += r5 * ((2 * my * ry + rdotm) - 5 * rdotm / pow(rmag, 2) * ry * ry); 
+            dB(i, 1, 2) += r5 * ((my * rz + mz * ry) - 5 * rdotm / pow(rmag, 2) * ry * rz);
+            dB(i, 2, 2) += r5 * ((2 * mz * rz + rdotm) - 5 * rdotm / pow(rmag, 2) * rz * rz); 
+        }
         dB(i, 2, 1) = dB(i, 1, 2);
         dB(i, 2, 0) = dB(i, 0, 2);
         dB(i, 1, 0) = dB(i, 0, 1);
