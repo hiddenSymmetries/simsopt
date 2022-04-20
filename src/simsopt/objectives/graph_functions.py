cd59561ad01acfbc6bd24aebefc0f2722530b680
Bharat Medasani <mbkumar@gmail.com>
2022-04-19 20:18:42 -0400

    Unit tests for unfix_local and unfix_full

--- src/simsopt/objectives/graph_functions.py
+++ src/simsopt/objectives/graph_functions.py
@@ -2,6 +2,6 @@ from .functions import *
 
 import warnings
 
-warnings.warn("Importing of simsopt.objectives.functions module is deprecated. "
+warnings.warn("Importing of simsopt.objectives.graph_functions module is deprecated. "
               "Instead import simsopt.objectives.functions module",
               DeprecationWarning, stacklevel=2)
