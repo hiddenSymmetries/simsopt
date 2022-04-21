fce68dc6e7038ed1fa1aabc4022ad7511f8f9468
Bharat Medasani <mbkumar@gmail.com>
2022-04-21 10:46:54 -0400

    Remove graph_ prefix in imports in the simsopt source code

--- src/simsopt/solve/__init__.py
+++ src/simsopt/solve/__init__.py
@@ -1,3 +1,3 @@
-from .graph_serial import least_squares_serial_solve, serial_solve
+from .serial import least_squares_serial_solve, serial_solve
 
 __all__ = ['least_squares_serial_solve', 'serial_solve']
