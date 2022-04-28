ca060743874c894e17566bba853247f5bbd7db69
Florian Wechsung <f.wechsung@gmail.com>
2022-04-28 11:25:18 -0400

    test convergence order

--- tests/field/test_interpolant.py
+++ tests/field/test_interpolant.py
@@ -31,6 +31,7 @@ class Testing(unittest.TestCase):
         Build a random, vector valued polynomial of a specific degree and check
         that it is interpolated exactly.
         """
+        np.random.seed(0)
         xran = (1.0, 4.0, 20)
         yran = (1.1, 3.9, 10)
         zran = (1.2, 3.8, 15)
@@ -68,6 +69,7 @@ class Testing(unittest.TestCase):
         If created with out_of_bounds_ok=True, then nothing should happen, but if out_of_bounds_ok=False,
         then a runtime error should be raised.
         """
+        np.random.seed(0)
         xran = (1.0, 4.0, 20)
         yran = (1.1, 3.9, 10)
         zran = (1.2, 3.8, 15)
@@ -97,14 +99,87 @@ class Testing(unittest.TestCase):
         with assert_raises(RuntimeError):
             interpolant.evaluate_batch(xyz, fhxyz)
 
-    # def test_skip(self):
+    def test_skip(self):
+        """
+        Check that the interpolant correctly identifies which regions in the
+        domain to skip
+        """
+        np.random.seed(0)
+        xran = (1.0, 4.0, 30)
+        yran = (1.1, 3.9, 30)
+        zran = (1.2, 3.8, 30)
 
-    #     xran = (1.0, 4.0, 20)
-    #     yran = (1.1, 3.9, 10)
-    #     zran = (1.2, 3.8, 15)
+        xkeep = (2.0, 3.0)
+        ykeep = (2.0, 3.0)
+        zkeep = (2.0, 3.0)
+        def skip(xs, ys, zs):
+            xs = np.asarray(xs)
+            ys = np.asarray(ys)
+            zs = np.asarray(zs)
+            keep = (xkeep[0] < xs) * (xs < xkeep[1]) * (ykeep[0] < ys) * (ys < ykeep[1]) * (zkeep[0] < zs) * (zs < zkeep[1])
+            return np.invert(keep)
 
-    #     dim = 3
-    #     degree = 2
-    #     fun = get_random_polynomial(dim, degree)
+        dim = 3
+        degree = 2
+        fun = get_random_polynomial(dim, degree)
 
-    #     rule = sopp.UniformInterpolationRule(degree)
+        rule = sopp.UniformInterpolationRule(degree)
+
+        interpolant = sopp.RegularGridInterpolant3D(rule, xran, yran, zran, dim, True, skip)
+        interpolant.interpolate_batch(fun)
+
+        xyz = np.asarray([
+            [2.4, 2.6, 2.8],  # keep
+            [2.1, 2.1, 2.9],  # keep
+            [2.8, 2.8, 2.1],  # keep
+            [1.3, 1.3, 1.3],  # do not keep
+            [1.3, 2.9, 3.5],  # do not keep
+            [3.5, 1.3, 1.3],  # do not keep
+        ])
+        fhxyz = 100*np.ones((xyz.shape[0], dim))
+
+        interpolant.evaluate_batch(xyz, fhxyz)
+        print("fhxyz %s" % (fhxyz))
+
+        fxyz = fun(xyz[:, 0], xyz[:, 1], xyz[:, 2], flatten=False)
+        assert np.allclose(fhxyz[:3, :], fxyz[:3, :], atol=1e-12, rtol=1e-12)
+        assert np.allclose(fhxyz[3:, :], 100, atol=1e-12, rtol=1e-12)
+
+    def test_convergence_order(self):
+        for dim in [1, 4, 6]:
+            for degree in [1, 3]:
+                with self.subTest(dim=dim, degree=degree):
+                    self.subtest_convergence_order(dim, degree)
+
+    def subtest_convergence_order(self, dim, degree):
+        """
+        Check that the interpolant converges at the correct order
+        """
+        np.random.seed(0)
+
+        fun = get_random_polynomial(dim, degree+1)
+
+        rule = sopp.UniformInterpolationRule(degree)
+        nsamples = 1000
+        xran = [1.0, 4.0, 10]
+        yran = [1.1, 3.9, 10]
+        zran = [1.2, 3.8, 10]
+        xpoints = np.random.uniform(low=xran[0], high=xran[1], size=(nsamples, ))
+        ypoints = np.random.uniform(low=yran[0], high=yran[1], size=(nsamples, ))
+        zpoints = np.random.uniform(low=zran[0], high=zran[1], size=(nsamples, ))
+        xyz = np.asarray([xpoints, ypoints, zpoints]).T.copy()
+        fhxyz = np.zeros((nsamples, dim))
+        fxyz = fun(xyz[:, 0], xyz[:, 1], xyz[:, 2], flatten=False)
+
+        err = 1e6
+        for n in [5, 10, 20, 40]:
+            xran[2] = n
+            yran[2] = n
+            zran[2] = n
+            interpolant = sopp.RegularGridInterpolant3D(rule, xran, yran, zran, dim, True)
+            interpolant.interpolate_batch(fun)
+            interpolant.evaluate_batch(xyz, fhxyz)
+            err_new = np.mean(np.linalg.norm(fxyz-fhxyz, axis=1))
+            print(err_new/err)
+            assert err_new/err < 0.6**(degree+1)
+            err = err_new
