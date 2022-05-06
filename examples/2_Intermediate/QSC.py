9bb32956039686b8628eae67305e8b375ab5f0bc
Bharat Medasani <mbkumar@gmail.com>
2022-05-06 10:03:09 -0400

    Merge inconsistencies removed from docs and examples

--- examples/2_Intermediate/QSC.py
+++ examples/2_Intermediate/QSC.py
@@ -39,7 +39,7 @@ print('Initial dofs: ', stel.get_dofs())
 print('Names of the dofs: ', stel.names)
 
 # Decide which degrees of freedom to optimize
-stel.local_fix_all()
+stel.fix_all()
 stel.unfix('rc(1)')
 stel.unfix('zs(1)')
 stel.unfix('etabar')
