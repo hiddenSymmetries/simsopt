Doing some debugging trying to get the cylindrical grid geometry factor working. Found a bug in the geometry factor calculation that used = instead of += for the cylindrical contributions. Also investigating speeding up the ATA calculation with the SVD of A_obj, since ATA calculation scales badly and gets very expensive around high resolution runs.

src/simsopt/field/magneticfieldclasses.py
@@ -461,29 +461,34 @@ class DipoleField(MagneticField):
         m: Solution for the dipoles using the pm_opt object. 
     """
 
-    def __init__(self, dipole_grid, m, pm_opt=None, stellsym=False, nfp=1):
+    def __init__(self, dipole_grid, m, pm_opt=None, stellsym=False, nfp=1, cylindrical_flag=True):
         MagneticField.__init__(self)
-        self.ndipoles = dipole_grid.shape[0] 
+        self.ndipoles = dipole_grid.shape[0]
+        self.cylindrical_flag = cylindrical_flag
         if pm_opt is not None:
             self.m_maxima = pm_opt.m_maxima
             self.inds = pm_opt.inds
             phi = 2 * np.pi * pm_opt.plasma_boundary.quadpoints_phi
             if stellsym or nfp > 1:
-                self._dipole_fields_from_symmetries(m.reshape(self.ndipoles, 3), dipole_grid[:, 2], pm_opt.final_RZ_grid, phi, stellsym, nfp)
+                self._dipole_fields_from_symmetries(m.reshape(self.ndipoles, 3), dipole_grid, pm_opt.final_RZ_grid, phi, stellsym, nfp)
             else:
                 m = m.reshape(self.ndipoles, 3)
                 dipole_grid_z = dipole_grid[:, 2]
-                dipole_grid_x = np.zeros(len(dipole_grid_z))
-                dipole_grid_y = np.zeros(len(dipole_grid_z))
-                running_tally = 0
-                for i in range(pm_opt.nphi):
-                    if i > 0:
-                        radii = pm_opt.final_RZ_grid[self.inds[i-1]:self.inds[i], i, 0]
-                    else:
-                        radii = pm_opt.final_RZ_grid[:self.inds[i], i, 0]
-                    dipole_grid_x[running_tally:running_tally + len(radii)] = radii * np.cos(phi[i])
-                    dipole_grid_y[running_tally:running_tally + len(radii)] = radii * np.sin(phi[i])
-                    running_tally += len(radii)
+                if self.cylindrical_flag:
+                    dipole_grid_x = dipole_grid[:, 0]
+                    dipole_grid_y = dipole_grid[:, 1]
+                else:
+                    dipole_grid_x = np.zeros(len(dipole_grid_z))
+                    dipole_grid_y = np.zeros(len(dipole_grid_z))
+                    running_tally = 0
+                    for i in range(pm_opt.nphi):
+                        if i > 0:
+                            radii = pm_opt.final_RZ_grid[self.inds[i-1]:self.inds[i], i, 0]
+                        else:
+                            radii = pm_opt.final_RZ_grid[:self.inds[i], i, 0]
+                        dipole_grid_x[running_tally:running_tally + len(radii)] = radii * np.cos(phi[i])
+                        dipole_grid_y[running_tally:running_tally + len(radii)] = radii * np.sin(phi[i])
+                        running_tally += len(radii)
                 self.dipole_grid = np.array([dipole_grid_x, dipole_grid_y, dipole_grid_z]).T
                 self.m_vec = m
         else:
@@ -511,7 +516,9 @@ class DipoleField(MagneticField):
         points = self.get_points_cart_ref()
         dA[:] = sopp.dipole_field_dA(points, self.dipole_grid, self.m_vec) 
 
-    def _dipole_fields_from_symmetries(self, m, dipole_grid_Z, RZ_grid, phi, stellsym, nfp):
+    def _dipole_fields_from_symmetries(self, m, dipole_grid, RZ_grid, phi, stellsym, nfp):
+
+        dipole_grid_Z = dipole_grid[:, 2]
         if stellsym:
             nsym = nfp * 2
         else:
@@ -539,7 +546,11 @@ class DipoleField(MagneticField):
                 dipole_grid_x[running_tally + nr * fp:running_tally + nr * (fp + 1)] = radii * np.cos(phi_sym)
                 dipole_grid_y[running_tally + nr * fp:running_tally + nr * (fp + 1)] = radii * np.sin(phi_sym)
                 dipole_grid_z[running_tally + nr * fp:running_tally + nr * (fp + 1)] = dipole_grid_Z[running_tally_m:running_tally_m + nr] 
-                # For fp symmetry, set mx, my, mz and rotate by phi0
+                # Need to change m to be in cartesian for the call to _B_impl()
+                if self.cylindrical_flag:
+                    m_vec[running_tally + nr * fp:running_tally + nr * (fp + 1), 0] = m[running_tally_m:running_tally_m + nr, 0] * np.cos(phi_sym) - m[running_tally_m:running_tally_m + nr, 1] * np.sin(phi_sym)
+                    m_vec[running_tally + nr * fp:running_tally + nr * (fp + 1), 1] = m[running_tally_m:running_tally_m + nr, 0] * np.sin(phi_sym) + m[running_tally_m:running_tally_m + nr, 1] * np.cos(phi_sym)
+                # For fp symmetry, set mx, my, mz (or mr, mphi, mz) and rotate by phi0
                 m_vec[running_tally + nr * fp:running_tally + nr * (fp + 1), 0] = m[running_tally_m:running_tally_m + nr, 0] * np.cos(phi0) - m[running_tally_m:running_tally_m + nr, 1] * np.sin(phi0)
                 m_vec[running_tally + nr * fp:running_tally + nr * (fp + 1), 1] = m[running_tally_m:running_tally_m + nr, 0] * np.sin(phi0) + m[running_tally_m:running_tally_m + nr, 1] * np.cos(phi0)
                 m_vec[running_tally + nr * fp:running_tally + nr * (fp + 1), 2] = m[running_tally_m:running_tally_m + nr, 2]
@@ -548,8 +559,8 @@ class DipoleField(MagneticField):
             running_tally_m += nr 
 
         if stellsym:
-            # Y and Z coordinates OF THE GRID flip under stellarator symmetry
-            # but X component of the m vector flips under this change
+            # Phi (or Y) and Z coordinates OF THE GRID flip under stellarator symmetry
+            # but R (or X) component of the m vector flips under this change
             dipole_grid_x[offsetm:] = dipole_grid_x[:offsetm]
             dipole_grid_y[offsetm:] = - dipole_grid_y[:offsetm]
             dipole_grid_z[offsetm:] = - dipole_grid_z[:offsetm]

src/simsopt/util/permanent_magnet_optimizer.py
@@ -57,7 +57,8 @@ class PermanentMagnetOptimizer:
         self, plasma_boundary, rz_inner_surface=None, 
         rz_outer_surface=None, plasma_offset=0.1, 
         coil_offset=0.2, B_plasma_surface=None, dr=0.1,
-        filename=None, FOCUS=False, out_dir=''
+        filename=None, FOCUS=False, out_dir='',
+        cylindrical_flag=True,
     ):
         if plasma_offset <= 0 or coil_offset <= 0:
             raise ValueError('permanent magnets must be offset from the plasma')
@@ -69,6 +70,7 @@ class PermanentMagnetOptimizer:
         self.coil_offset = coil_offset
         self.B_plasma_surface = B_plasma_surface
         self.dr = dr
+        self.cylindrical_flag = cylindrical_flag
 
         if not isinstance(plasma_boundary, SurfaceRZFourier):
             raise ValueError(
@@ -164,9 +166,18 @@ class PermanentMagnetOptimizer:
             RPhiZ_grid, self.r_inner, self.r_outer, self.z_inner, self.z_outer
         )
         self.inds = np.array(self.inds, dtype=int)
+        for i in reversed(range(1, len(self.inds))):
+            for j in range(0, i):
+                self.inds[i] += self.inds[j]
         print(self.inds)
         self.ndipoles = self.inds[-1]
-        self.final_RZ_grid = self.final_RZ_grid[:self.ndipoles, :, :]
+        final_grid = []
+        for i in range(self.final_RZ_grid.shape[0]):
+            if not np.allclose(self.final_RZ_grid[i, :, :], 0.0):
+                final_grid.append(self.final_RZ_grid[i, :, :])
+        self.final_RZ_grid = np.array(final_grid)
+        print(self.final_RZ_grid.shape)
+        #self.final_RZ_grid = self.final_RZ_grid[:self.ndipoles, :, :]
         t2 = time.time()
         print("Took t = ", t2 - t1, " s to perform the C++ grid cell eliminations.")
 
@@ -343,7 +354,7 @@ class PermanentMagnetOptimizer:
             self.plasma_boundary.nfp, int(self.plasma_boundary.stellsym), 
             np.ascontiguousarray(self.dipole_grid[:, 1]), 
             np.ascontiguousarray(self.b_obj),
-            False  # If False use Cartesian coords. If True, cylindrical coords
+            self.cylindrical_flag  # If False use Cartesian coords. If True, cylindrical coords
         )
         # Rescale
         self.A_obj_expanded = self.A_obj * grid_fac
@@ -604,6 +615,7 @@ class PermanentMagnetOptimizer:
                 self.m_maxima
             )
             m0 = np.zeros(m0.shape)
+            # m0 = np.random.rand(m0.shape)
 
         self.m0 = m0
 
@@ -746,14 +758,21 @@ class PermanentMagnetOptimizer:
 
         t1 = time.time()
         U, S, Vh = np.linalg.svd(ATA, full_matrices=False, hermitian=True)
+        UA, SA, VhA = np.linalg.svd(self.A_obj, full_matrices=False)
 
         # WARNING: scipy is overwriting ATA for speed here!!
         #U, S, Vh = SVD(ATA, full_matrices=False, check_finite=False, overwrite_a=True, lapack_driver='gesdd') 
         plt.figure()
         plt.semilogy(S)
         plt.savefig(self.out_dir + 'S.png')
-        trunc = U.shape[1]  # np.where(S < S[-10] * 1.2)[0][0] * 2
+        plt.figure()
+        plt.semilogy(SA)
+        plt.savefig(self.out_dir + 'SA.png')
+        trunc = U.shape[1] // 10  # np.where(S < S[-10] * 1.2)[0][0] * 2
         print("Truncation index = ", trunc)
+
+        ATA_svd = VhA.T @ np.diag(SA ** 2) @ VhA
+        print(ATA - ATA_svd, np.allclose(ATA, ATA_svd))
 
         # convert to contiguous arrays for c++ code to work right
         U = np.ascontiguousarray(U[:, :trunc])