44c47fd4752266f4a4ad7e9bbab1d1514bfcc8f6
tmqian <tqian@pppl.gov>
2022-05-18 14:39:33 -0400

    fixing lint test

--- tests/field/test_magneticfields.py
+++ tests/field/test_magneticfields.py
@@ -675,10 +675,10 @@ class Testing(unittest.TestCase):
         bs = BiotSavart(coils)
         bs.to_vtk('/tmp/bfield')
 
-
     # Perhaps the file currently called test_mgrid.py should live here.
     # We can write a function called get_pnas-qa_data()
     #    The to_vtk() test only tests file writing, it does not appear to check the contents of the file.
+
     def test_to_mgrid(self):
         pass
 
