Fixed some more things about the f_B calculation. Everything appearing to work within simsopt (for nfp <= 2) but f_B looks incorrect when loading in famus runs. Need to continue debuggint his.

examples/2_Intermediate/permanent_magnet_initialization.py
@@ -15,7 +15,6 @@ import os
 import sys
 import pickle
 from matplotlib import pyplot as plt
-from simsopt.mhd.vmec import Vmec
 from pathlib import Path
 import numpy as np
 from scipy.optimize import minimize
@@ -34,7 +33,7 @@ import time
 
 t_start = time.time()
 # Determine which plasma equilibrium is being used
-print("Usage requires a configuration flag chosen from: qa(_nonplanar), qh(_nonplanar), muse, ncsx, and a flag specifying high or low resolution")
+print("Usage requires a configuration flag chosen from: qa(_nonplanar), qh(_nonplanar), muse(_famus), ncsx, and a flag specifying high or low resolution")
 if len(sys.argv) < 3:
     print(
         "Error! You must specify at least 1 arguments: "
@@ -42,11 +41,11 @@ if len(sys.argv) < 3:
     )
     exit(1)
 config_flag = str(sys.argv[1])
-if config_flag not in ['qa', 'qa_nonplanar', 'QH', 'qh', 'qh_nonplanar', 'muse', 'ncsx']:
+if config_flag not in ['qa', 'qa_nonplanar', 'QH', 'qh', 'qh_nonplanar', 'muse', 'muse_famus', 'ncsx']:
     print(
         "Error! The configuration flag must specify one of "
         "the pre-set plasma equilibria: qa, qa_nonplanar, "
-        "qh, qh_nonplanar, muse, or ncsx. "
+        "qh, qh_nonplanar, muse, muse_famus, or ncsx. "
     )
     exit(1)
 res_flag = str(sys.argv[2])
@@ -60,8 +59,9 @@ print('Config flag = ', config_flag, ', Resolution flag = ', res_flag)
 
 # Pre-set parameters for each configuration
 surface_flag = 'vmec'
+pms_name = None
 cylindrical_flag = False
-is_premade_ncsx = False
+is_premade_famus_grid = False
 if res_flag == 'high':
     nphi = 64
     ntheta = 64
@@ -73,8 +73,8 @@ else:
     ntheta = 8
 if config_flag == 'muse':
     dr = 0.01
-    coff = 0.05
-    poff = 0.05
+    coff = 0.1
+    poff = 0.02
     surface_flag = 'focus'
     input_name = 'input.' + config_flag 
 elif 'qa' in config_flag or 'qh' in config_flag or 'QH' in config_flag:
@@ -105,6 +105,15 @@ elif config_flag == 'ncsx':
     surface_flag = 'focus'
     input_name = 'input.NCSX_c09r00_halfTeslaTF' 
     coil_name = 'input.NCSX_c09r00_halfTeslaTF_Bn'
+    pms_name = 'init_orient_pm_nonorm_5E4_q4_dp.focus'    
+if config_flag == 'muse_famus':
+    dr = 0.01
+    coff = 0.1
+    poff = 0.02
+    surface_flag = 'focus'
+    input_name = 'input.muse'
+    is_premade_famus_grid = True
+    pms_name = 'zot80.focus'    
 
 # Don't save in home directory on NERSC -- save on SCRATCH
 scratch_path = '/global/cscratch1/sd/akaptano/' 
@@ -121,7 +130,7 @@ surface_filename = TEST_DIR / input_name
 
 # Initialize the boundary magnetic surface:
 t1 = time.time()
-if config_flag == 'muse' or config_flag == 'ncsx':
+if 'muse' in config_flag or config_flag == 'ncsx':
     s = SurfaceRZFourier.from_focus(surface_filename, range="half period", nphi=nphi, ntheta=ntheta) 
 elif 'qh' in config_flag or 'QH' in config_flag:
     s = SurfaceRZFourier.from_wout(surface_filename, range="half period", nphi=nphi, ntheta=ntheta) 
@@ -130,12 +139,16 @@ else:
 t2 = time.time()
 print("Done loading in plasma boundary surface, t = ", t2 - t1)
 
+#dphi = (s.quadpoints_phi[1] - s.quadpoints_phi[0]) * 2 * np.pi
+#dtheta = (s.quadpoints_theta[1] - s.quadpoints_theta[0]) * 2 * np.pi
+#grid_fac = np.sqrt(dphi * dtheta)
+
 # File for the desired TF coils
 t1 = time.time()
-if config_flag == 'muse':
+if 'muse' in config_flag:
     # Load in pre-optimized coils
     TEST_DIR = (Path(__file__).parent / ".." / ".." / "tests" / "test_files").resolve()
-    coils_filename = TEST_DIR / (config_flag + '_tf_coils.focus')
+    coils_filename = TEST_DIR / 'muse_tf_coils.focus'
     base_curves, base_currents, ncoils = read_focus_coils(coils_filename)
     coils = []
     for i in range(ncoils):
@@ -152,8 +165,9 @@ elif config_flag == 'qh':
     R1 = 0.6
     order = 5
 
+    from simsopt.mhd.vmec import Vmec
     vmec_file = 'wout_LandremanPaul2021_QH.nc'
-    total_current = Vmec(vmec_file).external_current() / (2 * s.nfp) / 1.6
+    total_current = Vmec(vmec_file).external_current() / (2 * s.nfp) / 8.5
     base_curves = create_equally_spaced_curves(ncoils, s.nfp, stellsym=True, R0=R0, R1=R1, order=order, numquadpoints=128)
     base_currents = [ScaledCurrent(Current(total_current / ncoils * 1e-5), 1e5) for _ in range(ncoils-1)]
     total_current = Current(total_current)
@@ -170,9 +184,10 @@ elif config_flag == 'qa':
     R0 = 1.0
     R1 = 0.6
     order = 5
-    
+
+    from simsopt.mhd.vmec import Vmec
     vmec_file = 'wout_LandremanPaul2021_QA.nc'
-    total_current = Vmec(vmec_file).external_current() / (2 * s.nfp) / 1.6
+    total_current = Vmec(vmec_file).external_current() / (2 * s.nfp) / 8
     base_curves = create_equally_spaced_curves(ncoils, s.nfp, stellsym=True, R0=R0, R1=R1, order=order, numquadpoints=128)
     base_currents = [ScaledCurrent(Current(total_current / ncoils * 1e-5), 1e5) for _ in range(ncoils-1)]
     total_current = Current(total_current)
@@ -247,7 +262,7 @@ if config_flag != 'ncsx':
 
     quadpoints_phi = np.linspace(0, 1, 2 * nphi, endpoint=True)
     quadpoints_theta = np.linspace(0, 1, ntheta, endpoint=True)
-    if config_flag == 'muse' or config_flag == 'ncsx':
+    if 'muse' in config_flag or config_flag == 'ncsx':
         s_plot = SurfaceRZFourier.from_focus(surface_filename, range="full torus", quadpoints_phi=quadpoints_phi, quadpoints_theta=quadpoints_theta)
     elif 'qh' in config_flag or 'QH' in config_flag:
         s_plot = SurfaceRZFourier.from_wout(surface_filename, range="full torus", quadpoints_phi=quadpoints_phi, quadpoints_theta=quadpoints_theta)
@@ -268,17 +283,19 @@ if config_flag != 'ncsx':
 
     # Save optimized BiotSavart object
     biotsavart_json_str = bs.save(filename=OUT_DIR + 'BiotSavart.json')
+    bs.set_points(s.gamma().reshape((-1, 3)))
     Bnormal = np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
     bs.set_points(s_plot.gamma().reshape((-1, 3)))
-    f_B_sf = SquaredFlux(s_plot, bs).J() 
+    Bnormal_plot = np.sum(bs.B().reshape((2 * nphi, ntheta, 3)) * s_plot.unitnormal(), axis=2)
+    f_B_sf = SquaredFlux(s_plot, bs).J()  # * grid_fac ** 2 
     print('BiotSavart f_B = ', f_B_sf)
 
     # Save Biot Savart fields
-    pointData = {"B_N": Bnormal[:, :, None]}
+    pointData = {"B_N": Bnormal_plot[:, :, None]}
     s_plot.to_vtk(OUT_DIR + "biot_savart_opt", extra_data=pointData)
 else:
     Bnormal = load_ncsx_coil_data(s, coil_name)
-    is_premade_ncsx = True
+    is_premade_famus_grid = True
     quadpoints_phi = np.linspace(0, 1, 2 * nphi, endpoint=True)
     quadpoints_theta = np.linspace(0, 1, ntheta, endpoint=True)
     s_plot = SurfaceRZFourier.from_focus(surface_filename, range="half period", quadpoints_phi=quadpoints_phi, quadpoints_theta=quadpoints_theta)
@@ -287,19 +304,14 @@ else:
     # Save Tf fields 
     pointData = {"B_N": Bnormal_plot[:, :, None]}
     s_plot.to_vtk(OUT_DIR + "biot_savart_init", extra_data=pointData)
-    Btoroidal = ToroidalField(0.5, s_plot.get_rc(0, 0))
-    Btoroidal.set_points(s_plot.gamma().reshape((-1, 3)))
-    Bnormal_toroidal = np.sum(Btoroidal.B().reshape((2 * nphi, ntheta, 3)) * s_plot.unitnormal(), axis=2)
-    pointData = {"B_N": Bnormal_toroidal[:, :, None]}
-    s_plot.to_vtk(OUT_DIR + "biot_savart_init_toroidal", extra_data=pointData)
 
 # permanent magnet setup 
 t1 = time.time()
 pm_opt = PermanentMagnetOptimizer(
-    s, is_premade_ncsx=is_premade_ncsx, coil_offset=coff, dr=dr, plasma_offset=poff,
+    s, is_premade_famus_grid=is_premade_famus_grid, coil_offset=coff, dr=dr, plasma_offset=poff,
     Bn=Bnormal, 
     filename=surface_filename, surface_flag=surface_flag, out_dir=OUT_DIR,
-    cylindrical_flag=cylindrical_flag,
+    cylindrical_flag=cylindrical_flag, pms_name=pms_name
 )
 t2 = time.time()
 print('Done initializing the permanent magnet object')
@@ -313,7 +325,7 @@ b_dipole_initial = DipoleField(pm_opt)
 b_dipole_initial.set_points(s_plot.gamma().reshape((-1, 3)))
 b_dipole_initial._toVTK(OUT_DIR + "Dipole_Fields_initial")
 pm_opt._plot_final_dipoles()
-if is_premade_ncsx:
+if is_premade_famus_grid:
     Bnormal_init = np.sum(b_dipole_initial.B().reshape((2 * nphi, ntheta, 3)) * s_plot.unitnormal(), axis=-1)
     pointData = {"B_N": (Bnormal_init + Bnormal_plot)[:, :, None]}
     s_plot.to_vtk(OUT_DIR + "Bnormal_init_guess", extra_data=pointData)
@@ -321,10 +333,10 @@ t2 = time.time()
 print('Done setting up the Dipole Field class')
 print('Process took t = ', t2 - t1, ' s')
 
-if config_flag == 'ncsx':
+if is_premade_famus_grid:  # config_flag == 'ncsx':
     t1 = time.time()
     # Save FAMUS solution
-    ox, oy, oz, m0, p, mp, mt = np.loadtxt('../../tests/test_files/init_orient_pm_nonorm_5E4_q4_dp.focus', skiprows=3, usecols=[3, 4, 5, 7, 8, 10, 11], delimiter=',', unpack=True)
+    ox, oy, oz, m0, p, mp, mt = np.loadtxt('../../tests/test_files/' + pms_name, skiprows=3, usecols=[3, 4, 5, 7, 8, 10, 11], delimiter=',', unpack=True)
     # r = np.sqrt(ox * ox + oy * oy)
     phi = np.arctan2(oy, ox)
     print('Max and min phi FAMUS = ', np.max(phi), np.min(phi))
@@ -332,7 +344,7 @@ if config_flag == 'ncsx':
     plt.figure()
     plt.hist(rho)
     mm = rho * m0
-    print('FAMUS effective volume = ', np.sum(mm) * 4 * np.pi * 1e-7 / 1.4 * 6) 
+    print('FAMUS effective volume = ', np.sum(mm) * 4 * np.pi * 1e-7 / 1.4 * 2 * s.nfp) 
     mx = mm * np.sin(mt) * np.cos(mp) 
     my = mm * np.sin(mt) * np.sin(mp) 
     mz = mm * np.cos(mt)
@@ -340,30 +352,30 @@ if config_flag == 'ncsx':
     cosphi = np.cos(phi)
     mr = cosphi * mx + sinphi * my
     mphi = -sinphi * mx + cosphi * my
-    m_FAMUS = np.ravel((np.array([mr, mphi, mz]).T)[pm_opt.phi_order, :])
-    print(m_FAMUS.shape)
+    m_FAMUS = np.ravel((np.array([mx, my, mz]).T)[pm_opt.phi_order, :])
+    #m_FAMUS = np.ravel((np.array([mr, mphi, mz]).T)[pm_opt.phi_order, :])
     pm_opt.m = m_FAMUS 
     pm_opt.m_proxy = m_FAMUS
     pm_opt.m_maxima = m0[pm_opt.phi_order]
+    pm_opt.cylindrical_flag = False
     b_dipole_FAMUS = DipoleField(pm_opt)
-    b_dipole_FAMUS.set_points(s_plot.gamma().reshape((-1, 3)))
+    b_dipole_FAMUS.set_points(s.gamma().reshape((-1, 3)))
     b_dipole_FAMUS._toVTK(OUT_DIR + "Dipole_Fields_FAMUS")
     pm_opt._plot_final_dipoles()
-    Bnormal_FAMUS = np.sum(b_dipole_FAMUS.B().reshape((2 * nphi, ntheta, 3)) * s_plot.unitnormal(), axis=-1)
+    Bnormal_FAMUS = np.sum(b_dipole_FAMUS.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=-1)
     pointData = {"B_N": Bnormal_FAMUS[:, :, None]}
-    s_plot.to_vtk(OUT_DIR + "Bnormal_opt_FAMUS", extra_data=pointData)
-    pointData = {"B_N": (Bnormal_FAMUS + Bnormal_plot)[:, :, None]}
-    s_plot.to_vtk(OUT_DIR + "Bnormal_total_FAMUS", extra_data=pointData)
+    s.to_vtk(OUT_DIR + "Bnormal_opt_FAMUS", extra_data=pointData)
+    pointData = {"B_N": (Bnormal_FAMUS + Bnormal)[:, :, None]}
+    #pointData = {"B_N": (Bnormal_FAMUS + Bnormal_plot)[:, :, None]}
+    s.to_vtk(OUT_DIR + "Bnormal_total_FAMUS", extra_data=pointData)
     t2 = time.time()
     print('Saving FAMUS solution took ', t2 - t1, ' s')
-    dphi = (s_plot.quadpoints_phi[1] - s_plot.quadpoints_phi[0]) * 2 * np.pi  # / 6.0
-    dtheta = (s_plot.quadpoints_theta[1] - s_plot.quadpoints_theta[0]) * 2 * np.pi
-    grid_fac = np.sqrt(dphi * dtheta)
-    f_B_famus = SquaredFlux(s_plot, b_dipole_FAMUS).J() 
-    f_B_Bnormal = SquaredFlux(s_plot, Btoroidal).J() 
-    f_B_sf = SquaredFlux(s_plot, b_dipole_FAMUS, Bnormal_plot).J() 
-    print(f_B_famus, f_B_Bnormal, f_B_sf)
-    read_regcoil_pm('../../tests/test_files/regcoil_pm_ncsx.nc', surface_filename, OUT_DIR)
+    f_B_famus = SquaredFlux(s, b_dipole_FAMUS).J()  # * grid_fac ** 2
+    f_B_sf = SquaredFlux(s, b_dipole_FAMUS, Bnormal).J()  # * grid_fac ** 2 
+    #f_B_sf = SquaredFlux(s, b_dipole_FAMUS, Bnormal_plot).J() #* grid_fac ** 2 
+    print(f_B_famus, f_B_sf)
+    if config_flag == 'ncsx':
+        read_regcoil_pm('../../tests/test_files/regcoil_pm_ncsx.nc', surface_filename, OUT_DIR)
 
 # Save PM class object to file for reuse
 t1 = time.time()

src/simsopt/field/coil.py
@@ -102,6 +102,7 @@ class Current(sopp.Current, CurrentBase):
     def from_dict(cls, d):
         return cls(d["current"])
 
+
 class ScaledCurrent(sopp.CurrentBase, CurrentBase):
     """
     Scales :mod:`Current` by a factor. To be used for example to flip currents
@@ -135,16 +136,17 @@ class ScaledCurrent(sopp.CurrentBase, CurrentBase):
         current = decoder.process_decoded(d["current_to_scale"])
         return cls(current, d["scale"])
 
+
 class CurrentSum(sopp.CurrentBase, CurrentBase):
     """
     Take the sum of two :mod:`Current` objects.
     """
 
-    def __init__(self, current_a, current_b):
+    def __init__(self, current_a, current_b, **kwargs):
         self.current_a = current_a
         self.current_b = current_b
         sopp.CurrentBase.__init__(self)
-        CurrentBase.__init__(self, x0=np.asarray([]), depends_on=[current_a, current_b])
+        CurrentBase.__init__(self, x0=np.asarray([]), depends_on=[current_a, current_b], **kwargs)
 
     def vjp(self, v_current):
         return self.current_a.vjp(v_current) + self.current_b.vjp(v_current)
@@ -156,12 +158,16 @@ class CurrentSum(sopp.CurrentBase, CurrentBase):
         d = {}
         d["@module"] = self.__class__.__module__
         d["@class"] = self.__class__.__name__
-        d["current"] = self.get_value()
+        d["current_a"] = self.current_a.as_dict()
+        d["current_b"] = self.current_b.as_dict()
         return d
 
     @classmethod
     def from_dict(cls, d):
-        return cls(Current(d["current"]), 1.0)
+        decoder = MontyDecoder()
+        current_a = decoder.process_decoded(d["current_a"])
+        current_b = decoder.process_decoded(d["current_b"])
+        return cls(current_a, current_b)
 
 
 def apply_symmetries_to_curves(base_curves, nfp, stellsym):

src/simsopt/objectives/fluxobjective.py
@@ -31,15 +31,14 @@ class SquaredFlux(Optimizable):
         self.target = target
         self.field = field
         xyz = self.surface.gamma()
-        #self.field.set_points(xyz.reshape((-1, 3)))
+        self.field.set_points(xyz.reshape((-1, 3)))
         Optimizable.__init__(self, x0=np.asarray([]), depends_on=[field])
 
     def J(self):
         n = self.surface.normal()
         Bcoil = self.field.B().reshape(n.shape)
         if self.target is not None:
-            #Btarget = -self.target.B().reshape(n.shape)
-            Btarget = -self.target
+            Btarget = np.ascontiguousarray(self.target)
         else:
             Btarget = []
         return sopp.integral_BdotN(Bcoil, Btarget, n)