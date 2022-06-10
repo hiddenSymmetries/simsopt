1f483848586f867ab8995763641fdcb9864b14c8
tmqian <tqian@pppl.gov>
2022-06-10 18:55:22 -0400

    adding first draft of unit tests!

--- tests/field/test_mgrid.py
+++ tests/field/test_mgrid.py
@@ -8,22 +8,68 @@ from simsopt.geo.curve import curves_to_vtk
 from simsopt.geo.curvexyzfourier import CurveXYZFourier
 from simsopt.geo.surfacerzfourier import SurfaceRZFourier
 
+from simsopt.field.mgrid import MGrid
+from scipy.io import netcdf as nc
 import unittest
+
+test_file = "mgrid.pnas-qa-test-lowres-standard.nc"
 
 
 class Testing(unittest.TestCase):
 
-    def test_add_field_cylinder(self):
-        pass
-
-    def test_write(self):
-        pass
-
     def test_from_file(self):
-        pass
+        mgrid = MGrid.from_file(test_file)
+
+        assert mgrid.rmin == 0.5
+        assert mgrid.nphi == 6
+        assert mgrid.bvec.shape == (11, 11, 6, 3) 
 
     def test_load_field(self):
-        pass
+        f = nc.netcdf_file(test_file, 'r')
+        mgrid = MGrid()
+        mgrid.load_field(f)
+
+        names = mgrid.coil_names
+        assert len(names) == 1
+        assert names[0] == '________simsopt_coils_________'
+
+        assert mgrid.br_arr.shape == (1, 6, 11, 11)
+        assert mgrid.br[0, 0, 0] == -0.9946816978184079
+
+    def test_add_field_cylinder(self):
+
+        N_points = 5
+        br = np.ones(N_points)
+        bp = br*2
+        bz = br*3
+        name = "test_coil"
+
+        mgrid = MGrid()
+        mgrid.add_field_cylindrical(br, bp, bz, name=name)
+        assert mgrid.n_ext_cur == 1
+        assert mgrid.cur_labels[0] == '__________test_coil___________'
+        assert np.allclose(mgrid.br_arr[0], br)
+
+    def test_write(self):
+        mgrid = MGrid.from_file(test_file)
+
+        filename = '/tmp/mgrid.test'
+        mgrid.write(filename)
+
+        f = nc.netcdf_file(filename)
+        zmin = f.variables['zmin'][:][0]
+        assert zmin == -0.5
+
+        nextcur = f.variables['nextcur'][:][0]
+        assert nextcur == 1
+
+        assert f.variables['br_001'][:].shape == (6, 11, 11)
+        assert f.variables['mgrid_mode'][:][0].decode('ascii') == 'N'
+
+        byte_string = f.variables['coil_group'][:][0]
+        message = "".join([x.decode('ascii') for x in foo])
+        assert message == '________simsopt_coils_________'
+
 
 
 
