570294bae84f62e6fa329df8791276d28f653713
Bharat Medasani <mbkumar@gmail.com>
2022-06-14 11:49:39 -0400

    Bring more classes in _core to second level import

--- src/simsopt/_core/__init__.py
+++ src/simsopt/_core/__init__.py
@@ -1,6 +1,8 @@
 from .derivative import *
 from .optimizable import *
 from .finite_difference import *
+from .util import *
 
-__all__ = (derivative.__all__ + optimizable.__all__ + finite_difference.__all__)
+__all__ = (derivative.__all__ + optimizable.__all__ +
+           finite_difference.__all__ + util.__all__)
 
