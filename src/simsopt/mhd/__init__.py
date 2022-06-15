ceacc68d5fd24d1a25cb2201aef41db2f56691cd
Bharat Medasani <mbkumar@gmail.com>
2022-06-15 09:38:02 -0400

    Allow vmec import even if MPI is not installed at module level

--- src/simsopt/mhd/__init__.py
+++ src/simsopt/mhd/__init__.py
@@ -15,16 +15,17 @@ except ImportError as e:
     logger.debug(str(e))
 
 if MPI is not None:
-    from .vmec import Vmec
+    # from .vmec import Vmec
     from .spec import Spec, Residue
     from .boozer import Boozer, Quasisymmetry
 else:
-    Vmec = None
+    # Vmec = None
     Spec = None
     Residue = None
     Boozer = None
     Quasisymmetry = None
     logger.debug("mpi4py not installed. Not loading Vmec, Spec and other MHD modules.")
 
+from .vmec import *
 from .virtual_casing import *
 from .vmec_diagnostics import *
