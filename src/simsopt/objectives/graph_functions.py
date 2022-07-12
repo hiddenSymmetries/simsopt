94ee03af860107b3e12e01aa10df46766269440c
Bharat Medasani <mbkumar@gmail.com>
2022-07-12 10:16:05 -0400

    Files with graph_ prefix deleted

--- src/simsopt/objectives/graph_functions.py
+++
@@ -1,7 +0,0 @@
-from .functions import *
-
-import warnings
-
-warnings.warn("Importing of simsopt.objectives.graph_functions is deprecated. "
-              "Instead import simsopt.objectives.functions module.", 
-              DeprecationWarning, stacklevel=2)
