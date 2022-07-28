f4f9d11122161ce35ffd4714b90ffe3329770571
Andrew Giuliani <giuliani@cims.nyu.edu>
2022-07-28 13:49:34 -0400

    added back unit tests

--- tests/geo/test_surface_objectives.py
+++ tests/geo/test_surface_objectives.py
@@ -2,9 +2,9 @@ import unittest
 import numpy as np
 from simsopt.field.biotsavart import BiotSavart
 from simsopt.field.coil import coils_via_symmetries
-from simsopt.geo.surfaceobjectives import ToroidalFlux, QfmResidual, parameter_derivatives, Volume, PrincipalCurvature
+from simsopt.geo.surfaceobjectives import ToroidalFlux, QfmResidual, parameter_derivatives, Volume, PrincipalCurvature, MajorRadius, Iotas, NonQuasiAxisymmetricRatio
 from simsopt.configs.zoo import get_ncsx_data
-from .surface_test_helpers import get_surface, get_exact_surface
+from .surface_test_helpers import get_surface, get_exact_surface, get_boozer_surface
 
 
 surfacetypes_list = ["SurfaceXYZFourier", "SurfaceRZFourier",
@@ -224,3 +224,70 @@ class QfmTests(unittest.TestCase):
             return qfm.dJ_by_dsurfacecoefficients()
         taylor_test1(f, df, coeffs,
                      epsilons=np.power(2., -np.asarray(range(13, 22))))
+
+
+class MajorRadiusTests(unittest.TestCase):
+    def test_major_radius_surface_derivative(self):
+        """
+        Taylor test for derivative of surface major radius wrt coil parameters
+        """
+        
+        bs, boozer_surface = get_boozer_surface()
+        coeffs = bs.x
+        mr = MajorRadius(boozer_surface)
+
+        def f(dofs):
+            bs.x = dofs
+            return mr.J()
+
+        def df(dofs):
+            bs.x = dofs
+            return mr.dJ()
+        taylor_test1(f, df, coeffs,
+                     epsilons=np.power(2., -np.asarray(range(13, 19))))
+
+
+class IotasTests(unittest.TestCase):
+    def test_iotas_derivative(self):
+        """
+        Taylor test for derivative of surface rotational transform wrt coil parameters
+        """
+
+        bs, boozer_surface = get_boozer_surface()
+        coeffs = bs.x
+        io = Iotas(boozer_surface)
+
+        def f(dofs):
+            bs.x = dofs
+            return io.J()
+
+        def df(dofs):
+            bs.x = dofs
+            return io.dJ()
+        
+        taylor_test1(f, df, coeffs,
+                     epsilons=np.power(2., -np.asarray(range(13, 19))))
+
+
+class NonQSRatioTests(unittest.TestCase):
+    def test_nonQSratio_derivative(self):
+        """
+        Taylor test for derivative of surface non QS ratio wrt coil parameters
+        """
+
+        bs, boozer_surface = get_boozer_surface()
+        coeffs = bs.x
+        io = NonQuasiAxisymmetricRatio(boozer_surface, bs)
+
+        def f(dofs):
+            bs.x = dofs
+            return io.J()
+
+        def df(dofs):
+            bs.x = dofs
+            return io.dJ()
+        
+        taylor_test1(f, df, coeffs,
+                     epsilons=np.power(2., -np.asarray(range(13, 19))))
+
+
