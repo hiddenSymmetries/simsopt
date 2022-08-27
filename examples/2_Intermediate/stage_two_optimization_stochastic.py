243204cb6fedf49a8bfa62f138ebf2d1a94c25a1
Bharat Medasani <mbkumar@gmail.com>
2022-08-26 21:58:12 -0400

    Update the calls to surfacerzfourier

--- examples/2_Intermediate/stage_two_optimization_stochastic.py
+++ examples/2_Intermediate/stage_two_optimization_stochastic.py
@@ -111,8 +111,7 @@ os.makedirs(OUT_DIR, exist_ok=True)
 # Initialize the boundary magnetic surface; errors break symmetries, so consider the full torus
 nphi = 64
 ntheta = 16
-quadpt_phi, quadpt_theta = Surface.get_quadpoints(range="full torus", nphi=nphi, ntheta=ntheta)
-s = SurfaceRZFourier.from_vmec_input(filename, quadpt_phi, quadpt_theta)
+s = SurfaceRZFourier.from_vmec_input(filename, range="full torus", nphi=nphi, ntheta=ntheta)
 
 # Create the initial coils:
 base_curves = create_equally_spaced_curves(ncoils, s.nfp, stellsym=True, R0=R0, R1=R1, order=order)
