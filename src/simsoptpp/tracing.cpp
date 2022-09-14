16f99ae007ea1a1a542e96318e3537598a8f00d9
Christopher Albert <albert@alumni.tugraz.at>
2022-09-14 17:27:29 +0200

    Sketch for symplectic Euler

--- src/simsoptpp/tracing.cpp
+++ src/simsoptpp/tracing.cpp
@@ -445,6 +445,249 @@ solve(RHS rhs, typename RHS::State y, double tmax, double dt, double dtmax, doub
     return std::make_tuple(res, res_phi_hits);
 }
 
+/*
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
+subroutine f_sympl_euler1(si, f, n, x, fvec, iflag)
+!
+  type(SymplecticIntegrator), intent(inout) :: si
+  type(FieldCan), intent(inout) :: f
+  integer, intent(in) :: n
+  double precision, intent(in) :: x(n)
+  double precision, intent(out) :: fvec(n)
+  integer, intent(in) :: iflag
+
+  call eval_field(f, x(1), si%z(2), si%z(3), 2)
+  call get_derivatives2(f, x(2))
+
+  fvec(1) = f%dpth(1)*(f%pth - si%pthold) + si%dt*(f%dH(2)*f%dpth(1) - f%dH(1)*f%dpth(2))
+  fvec(2) = f%dpth(1)*(x(2) - si%z(4))  + si%dt*(f%dH(3)*f%dpth(1) - f%dH(1)*f%dpth(3))
+
+end subroutine f_sympl_euler1
+
+
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
+subroutine jac_sympl_euler1(si, f, x, jac)
+!
+  type(SymplecticIntegrator), intent(in) :: si
+  type(FieldCan), intent(inout) :: f
+
+  double precision, intent(in)  :: x(2)
+  double precision, intent(out) :: jac(2, 2)
+
+  jac(1,1) = f%d2pth(1)*(f%pth - si%pthold) + f%dpth(1)**2 &
+    + si%dt*(f%d2H(2)*f%dpth(1) + f%dH(2)*f%d2pth(1) - f%d2H(1)*f%dpth(2) - f%dH(1)*f%d2pth(2))
+  jac(1,2) = f%d2pth(7)*(f%pth - si%pthold) + f%dpth(1)*f%dpth(4) &
+    + si%dt*(f%d2H(8)*f%dpth(1) + f%dH(2)*f%d2pth(7) - f%d2H(7)*f%dpth(2) - f%dH(1)*f%d2pth(8))
+  jac(2,1) = f%d2pth(1)*(x(2) - si%z(4)) &
+    + si%dt*(f%d2H(3)*f%dpth(1) + f%dH(3)*f%d2pth(1) - f%d2H(1)*f%dpth(3) - f%dH(1)*f%d2pth(3))
+  jac(2,2) = f%d2pth(7)*(x(2) - si%z(4)) + f%dpth(1) &
+    + si%dt*(f%d2H(9)*f%dpth(1) + f%dH(3)*f%d2pth(7) - f%d2H(7)*f%dpth(3) - f%dH(1)*f%d2pth(9))
+
+end subroutine jac_sympl_euler1
+
+
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
+subroutine newton1(si, f, x, maxit, xlast)
+!
+  type(SymplecticIntegrator), intent(inout) :: si
+  type(FieldCan), intent(inout) :: f
+  integer, parameter :: n = 2
+
+  double precision, intent(inout) :: x(n)
+  integer, intent(in) :: maxit
+  double precision, intent(out) :: xlast(n)
+
+  double precision :: fvec(n), fjac(n,n), ijac(n,n)
+  double precision :: tolref(n)
+  integer :: kit
+
+  tolref(1) = 1d0
+  tolref(2) = dabs(x(2))
+
+  do kit = 1, maxit
+    if(x(1) > 1d0) return
+    if(x(1) < 0d0) x(1) = 0.01d0
+
+    call f_sympl_euler1(si, f, n, x, fvec, 1)
+    call jac_sympl_euler1(si, f, x, fjac)
+    ijac(1,1) = 1d0/(fjac(1,1) - fjac(1,2)*fjac(2,1)/fjac(2,2))
+    ijac(1,2) = -1d0/(fjac(1,1)*fjac(2,2)/fjac(1,2) - fjac(2,1))
+    ijac(2,1) = -1d0/(fjac(1,1)*fjac(2,2)/fjac(2,1) - fjac(1,2))
+    ijac(2,2) = 1d0/(fjac(2,2) - fjac(1,2)*fjac(2,1)/fjac(1,1))
+    xlast = x
+    x = x - matmul(ijac, fvec)
+
+    ! Don't take too small values in pphi as tolerance reference
+    tolref(2) = max(dabs(x(2)), tolref(2))
+    tolref(2) = max(dabs(x(2)), tolref(2))
+
+    if (all(dabs(fvec) < si%atol)) return
+    if (all(dabs(x-xlast) < si%rtol*tolref)) return
+  enddo
+  print *, 'newton1: maximum iterations reached: ', maxit
+  write(6601,*) x(1), x(2)
+  write(6601,*) x-xlast
+  write(6601,*) fvec
+  write(6601,*) ''
+  write(6601,*) fjac(1,1), fjac(1,2)
+  write(6601,*) fjac(2,1), fjac(2,2)
+  write(6601,*) ''
+  write(6601,*) ijac(1,1), ijac(1,2)
+  write(6601,*) ijac(2,1), ijac(2,2)
+  write(6601,*) ''
+  write(6601,*) si%z(2), si%z(3)
+  write(6601,*) ''
+  write(6601,*) ''
+end subroutine
+
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
+subroutine get_val(f, pphi)
+!
+! computes values of H, pth and vpar at z=(r, th, ph, pphi)
+!
+!
+  type(FieldCan), intent(inout) :: f
+  double precision, intent(in) :: pphi
+
+  f%vpar = (pphi - f%Aph/f%ro0)/f%hph
+  f%H = f%vpar**2/2d0 + f%mu*f%Bmod
+  f%pth = f%hth*f%vpar + f%Ath/f%ro0
+
+end subroutine get_val
+
+
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
+subroutine get_derivatives(f, pphi)
+!
+! computes H, pth and vpar at z=(r, th, ph, pphi) and their derivatives
+!
+!
+  type(FieldCan), intent(inout) :: f
+  double precision, intent(in) :: pphi
+
+  call get_val(f, pphi)
+
+  f%dvpar(1:3) = -(f%dAph/f%ro0 + f%dhph*f%vpar)/f%hph
+  f%dvpar(4)   = 1d0/f%hph
+
+  f%dH(1:3) = f%vpar*f%dvpar(1:3) + f%mu*f%dBmod
+  f%dH(4)   = f%vpar/f%hph
+
+  f%dpth(1:3) = f%dvpar(1:3)*f%hth + f%vpar*f%dhth + f%dAth/f%ro0
+
+  f%dpth(4) = f%hth/f%hph
+
+end subroutine get_derivatives
+
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
+subroutine get_derivatives2(f, pphi)
+!
+! computes H, pth and vpar at z=(r, th, ph, pphi) up to 2nd derivatives
+! order of second derivatives:
+! d2dr2, d2drdth, d2drph, d2dth2, d2dthdph, d2dph2,
+! d2dpphdr, d2dpphdth, d2dpphdph, d2dpph2
+!
+  type(FieldCan), intent(inout) :: f
+  double precision, intent(in) :: pphi
+
+  call get_derivatives(f, pphi)
+
+  f%d2vpar(1:6) = -f%d2Aph/f%ro0 - f%d2hph*f%vpar
+  f%d2vpar(1) = f%d2vpar(1) - 2d0*f%dhph(1)*f%dvpar(1)
+  f%d2vpar(2) = f%d2vpar(2) - (f%dhph(1)*f%dvpar(2) + f%dhph(2)*f%dvpar(1))
+  f%d2vpar(3) = f%d2vpar(3) - (f%dhph(1)*f%dvpar(3) + f%dhph(3)*f%dvpar(1))
+  f%d2vpar(4) = f%d2vpar(4) - 2d0*f%dhph(2)*f%dvpar(2)
+  f%d2vpar(5) = f%d2vpar(5) - (f%dhph(2)*f%dvpar(3) + f%dhph(3)*f%dvpar(2))
+  f%d2vpar(6) = f%d2vpar(6) - 2d0*f%dhph(3)*f%dvpar(3)
+  f%d2vpar(1:6) = f%d2vpar(1:6)/f%hph
+
+  f%d2H(1:6) = f%vpar*f%d2vpar(1:6) + f%mu*f%d2Bmod ! + qi*d2Phie
+  f%d2H(1) = f%d2H(1) + f%dvpar(1)**2
+  f%d2H(2) = f%d2H(2) + f%dvpar(1)*f%dvpar(2)
+  f%d2H(3) = f%d2H(3) + f%dvpar(1)*f%dvpar(3)
+  f%d2H(4) = f%d2H(4) + f%dvpar(2)**2
+  f%d2H(5) = f%d2H(5) + f%dvpar(2)*f%dvpar(3)
+  f%d2H(6) = f%d2H(6) + f%dvpar(3)**2
+
+  f%d2pth(1:6) = f%d2vpar(1:6)*f%hth + f%vpar*f%d2hth + f%d2Ath/f%ro0
+  f%d2pth(1) = f%d2pth(1) + 2d0*f%dvpar(1)*f%dhth(1)
+  f%d2pth(2) = f%d2pth(2) + f%dvpar(1)*f%dhth(2) + f%dvpar(2)*f%dhth(1)
+  f%d2pth(3) = f%d2pth(3) + f%dvpar(1)*f%dhth(3) + f%dvpar(3)*f%dhth(1)
+  f%d2pth(4) = f%d2pth(4) + 2d0*f%dvpar(2)*f%dhth(2)
+  f%d2pth(5) = f%d2pth(5) + f%dvpar(2)*f%dhth(3) + f%dvpar(3)*f%dhth(2)
+  f%d2pth(6) = f%d2pth(6) + 2d0*f%dvpar(3)*f%dhth(3)
+
+  f%d2vpar(7:9) = -f%dhph/f%hph**2
+  f%d2H(7:9) = f%dvpar(1:3)/f%hph + f%vpar*f%d2vpar(7:9)
+  f%d2pth(7:9) = f%dhth/f%hph + f%hth*f%d2vpar(7:9)
+
+end subroutine get_derivatives2
+
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!
+subroutine orbit_timestep_sympl_euler1(si, f, ierr)
+!
+  type(SymplecticIntegrator), intent(inout) :: si
+  type(FieldCan), intent(inout) :: f
+  integer, intent(out) :: ierr
+
+  integer, parameter :: n = 2
+  integer, parameter :: maxit = 32
+
+  double precision, dimension(n) :: x, xlast
+  integer :: k, ktau
+
+  ierr = 0
+  ktau = 0
+  do while(ktau .lt. si%ntau)
+    si%pthold = f%pth
+    x(1)=si%z(1)
+    x(2)=si%z(4)
+
+    call newton1(si, f, x, maxit, xlast)
+
+    if (x(1) > 1.0) then
+      ierr = 1
+      return
+    end if
+
+    if (x(1) < 0.0) then
+      print *, 'r<0, z = ', x(1), si%z(2), si%z(3), x(2)
+      x(1) = 0.01
+    end if
+
+    si%z(1) = x(1)
+    si%z(4) = x(2)
+
+    call eval_field(f, si%z(1), si%z(2), si%z(3), 0)
+    call get_derivatives(f, si%z(4))
+
+    si%z(2) = si%z(2) + si%dt*f%dH(1)/f%dpth(1)
+    si%z(3) = si%z(3) + si%dt*(f%vpar - f%dH(1)/f%dpth(1)*f%hth)/f%hph
+
+    si%kt = si%kt+1
+    ktau = ktau+1
+  enddo
+
+end subroutine orbit_timestep_sympl_euler1
+
+
+
+*/
+
+vector<array<double, 5>>
+solve_sympl(array<double, 4> y, double tmax, double dt, double tol, vector<shared_ptr<StoppingCriterion>> stopping_criteria)
+{
+    // TODO
+    return {};
+}
+
 template<template<class, std::size_t, xt::layout_type> class T>
 tuple<vector<array<double, 5>>, vector<array<double, 6>>>
 particle_guiding_center_tracing(
