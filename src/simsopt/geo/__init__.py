155ebb04bd71f1ab4a214a4301cc9df59f6dd092
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-10-07 20:06:45 +0100

    Removed curves import

--- src/simsopt/geo/__init__.py
+++ src/simsopt/geo/__init__.py
@@ -1,15 +1,15 @@
-from jax.config import config
-config.update("jax_enable_x64", True)
+# from jax.config import config
+# config.update("jax_enable_x64", True)
 from .config import *
 
-from .curve import *
-from .curvehelical import *
-from .curverzfourier import *
-from .curvexyzfourier import *
-from .curveperturbed import *
-from .curveobjectives import *
+# from .curve import *
+# from .curvehelical import *
+# from .curverzfourier import *
+# from .curvexyzfourier import *
+# from .curveperturbed import *
+# from .curveobjectives import *
 
-from .finitebuild import *
+# from .finitebuild import *
 from .plotting import *
 
 from .boozersurface import *
@@ -22,9 +22,10 @@ from .surfacerzfourier import *
 from .surfacexyzfourier import *
 from .surfacexyztensorfourier import *
 
-__all__ = (curve.__all__ + curvehelical.__all__ +
-           curverzfourier.__all__ + curvexyzfourier.__all__ +
-           curveperturbed.__all__ + curveobjectives.__all__ +
+__all__ = (
+        #    curve.__all__ + curvehelical.__all__ +
+        #    curverzfourier.__all__ + curvexyzfourier.__all__ +
+        #    curveperturbed.__all__ + curveobjectives.__all__ +
            finitebuild.__all__ + plotting.__all__ +
            boozersurface.__all__ + qfmsurface.__all__ +
            surface.__all__ +
