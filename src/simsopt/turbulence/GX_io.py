259b4e7f8f02333d4977a7e1cb03bbb4fd5375e3
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-10-31 16:51:19 +0000

    convert vmec path

--- src/simsopt/turbulence/GX_io.py
+++ src/simsopt/turbulence/GX_io.py
@@ -385,8 +385,8 @@ class VMEC_GX_geometry_module():
         print('  wrote .ing', out_path+fname)
 
         # run
-        cmd = ['convert_VMEC_to_GX',  out_path+fname]
-        #cmd = ['./{:}convert_VMEC_to_GX'.format(in_path),  out_path+fname]
+        # cmd = ['convert_VMEC_to_GX',  out_path+fname]
+        cmd = ['./{:}convert_VMEC_to_GX'.format(in_path),  out_path+fname]
 
         f_log = out_path + fname + '.log'
         with open(f_log, 'w') as fp:
