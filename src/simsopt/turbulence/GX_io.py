7c933acb1dcd4ec11cd4689067d5e748d105e98a
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-10-31 16:48:43 +0000

    revert

--- src/simsopt/turbulence/GX_io.py
+++ src/simsopt/turbulence/GX_io.py
@@ -57,22 +57,25 @@ class GX_Runner():
         if (os.path.exists(fout) and skip_overwrite):
             print( '  input exists, skipping write', fout )
             return
+
         with open(fout,'w') as f:
+        
             for item in self.inputs.items():
+                 
                 if ( type(item[1]) is not dict ):
-                    # print('  %s = %s ' % item, file=f)  
+                    print('  %s = %s ' % item, file=f)  
                     continue
     
                 header, nest = item
-                # print('\n[%s]' % header, file=f)
+                print('\n[%s]' % header, file=f)
     
                 longest_key =  max( nest.keys(), key=len) 
                 N_space = len(longest_key) 
                 for pair in nest.items():
                     s = '  {:%i}  =  {}' % N_space
-                    # print(s.format(*pair), file=f)
+                    print(s.format(*pair), file=f)
 
-        # print('  wrote input:', fout)
+        print('  wrote input:', fout)
 
 
     def load_slurm(self,f_slurm):
@@ -354,7 +357,7 @@ class VMEC_GX_geometry_module():
             for pair in data.items():
                 s = '  {:%i}  =  {}' % N_space
                 #print(s.format(*pair))   # print to screen for debugging
-                # print(s.format(*pair), file=f)
+                print(s.format(*pair), file=f)
 
 
     def set_vmec(self,wout, vmec_path='./', output_path='./'):
@@ -379,11 +382,11 @@ class VMEC_GX_geometry_module():
         out_path = self.output_path
         fname = self.tag + local_id + '-psi-{:.2f}'.format(s)
         self.write(fname)
-        # print('  wrote .ing', out_path+fname)
+        print('  wrote .ing', out_path+fname)
 
         # run
-        #cmd = ['convert_VMEC_to_GX',  out_path+fname]
-        cmd = ['./{:}convert_VMEC_to_GX'.format(in_path),  out_path+fname]
+        cmd = ['convert_VMEC_to_GX',  out_path+fname]
+        #cmd = ['./{:}convert_VMEC_to_GX'.format(in_path),  out_path+fname]
 
         f_log = out_path + fname + '.log'
         with open(f_log, 'w') as fp:
