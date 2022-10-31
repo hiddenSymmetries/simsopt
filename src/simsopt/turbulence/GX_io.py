b40d499d0c525bb9c7bee5de97641ab83568a4ce
Rogerio Jorge <rogerio.jorge@tecnico.ulisboa.pt>
2022-10-31 17:43:37 +0100

    VMEC file change

--- src/simsopt/turbulence/GX_io.py
+++ src/simsopt/turbulence/GX_io.py
@@ -57,11 +57,8 @@ class GX_Runner():
         if (os.path.exists(fout) and skip_overwrite):
             print( '  input exists, skipping write', fout )
             return
-
         with open(fout,'w') as f:
-        
             for item in self.inputs.items():
-                 
                 if ( type(item[1]) is not dict ):
                     print('  %s = %s ' % item, file=f)  
                     continue
@@ -385,8 +382,8 @@ class VMEC_GX_geometry_module():
         print('  wrote .ing', out_path+fname)
 
         # run
-        cmd = ['convert_VMEC_to_GX',  out_path+fname]
-        #cmd = ['./{:}convert_VMEC_to_GX'.format(in_path),  out_path+fname]
+        #cmd = ['convert_VMEC_to_GX',  out_path+fname]
+        cmd = ['./{:}convert_VMEC_to_GX'.format(in_path),  out_path+fname]
 
         f_log = out_path + fname + '.log'
         with open(f_log, 'w') as fp:
