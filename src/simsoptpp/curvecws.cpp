31461884ca76c744ed03d54f278de72697a18961
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2022-12-05 23:07:36 +0000

    surface part added but not tested

--- src/simsoptpp/curvecws.cpp
+++ src/simsoptpp/curvecws.cpp
@@ -3,12 +3,16 @@
 template <class Array>
 void CurveCWS<Array>::gamma_impl(Array &data, Array &quadpoints)
 {
+    CurveCWS<Array>::set_dofs_surface(res);
     int numquadpoints = quadpoints.size();
 
-    Array pphi;
-    Array ptheta;
-    pphi *= 0;
-    ptheta *= 0;
+    data *= 0;
+
+    double pphi = 0;
+    double ptheta = 0;
+
+    double r = 0;
+    double z = 0;
 
     for (int k = 0; k < numquadpoints; ++k)
     {
@@ -16,20 +20,33 @@ void CurveCWS<Array>::gamma_impl(Array &data, Array &quadpoints)
 
         for (int i = 0; i < order + 1; ++i)
         {
-            pphi(k) += phi_c[i] * cos(i * CWSt);
-            ptheta(k) += theta_c[i] * cos(i * CWSt);
+            pphi += phi_c[i] * cos(i * CWSt);
+            ptheta += theta_c[i] * cos(i * CWSt);
+            data
         }
         for (int i = 1; i < order + 1; ++i)
         {
-            pphi(k) += phi_s[i - 1] * sin(i * CWSt);
-            ptheta(k) += theta_s[i - 1] * sin(i * CWSt);
+            pphi += phi_s[i - 1] * sin(i * CWSt);
+            ptheta += theta_s[i - 1] * sin(i * CWSt);
         }
 
-        pphi(k) += phi_l[0] * CWSt;
-        ptheta(k) += theta_l[0] * CWSt;
+        pphi += phi_l[0] * CWSt;
+        ptheta += theta_l[0] * CWSt;
+
+        // SURFACE
+        for (int m = 0; m <= mpol; ++m)
+        {
+            for (int i = 0; i < 2 * ntor + 1; ++i)
+            {
+                r += rc(m, i) * cos(m * ptheta - nfp * i * pphi) + rs(m, i) * sin(m * ptheta - nfp * i * pphi);
+                z += zc(m, i) * cos(m * ptheta - nfp * i * pphi) + zs(m, i) * sin(m * ptheta - nfp * i * pphi);
+            }
+        }
+
+        data(k, 0) = r * cos(pphi);
+        data(k, 1) = r * sin(pphi);
+        data(k, 2) = z;
     }
-
-
 
     /* data *= 0;
     for (int k = 0; k < numquadpoints; ++k)
--- src/simsoptpp/curvecws.h
+++ src/simsoptpp/curvecws.h
@@ -7,7 +7,7 @@ template <class Array>
 class CurveCWS : public Curve<Array>
 {
 public:
-    const shared_ptr<Surface<Array>> surface;
+    // Curve
     const int order;
     const int nfp;
     const bool stellsym;
@@ -22,7 +22,17 @@ public:
     Array theta_c;
     Array theta_s;
 
-    CurveCWS(shared_ptr<Surface<Array>> surface,
+    // SURFACE
+    // const shared_ptr<Surface<Array>> surface;
+    int mpol;
+    int ntor;
+    vector<double> res = vector<double>(num_dofs(), 0.);
+    Array rc;
+    Array rs;
+    Array zc;
+    Array zs;
+
+    /* CurveCWS(shared_ptr<Surface<Array>> surface,
              int _numquadpoints,
              int _order,
              int _nfp,
@@ -34,6 +44,27 @@ public:
         phi_c = xt::zeros<double>({order + 1});
         theta_s = xt::zeros<double>({order});
         theta_c = xt::zeros<double>({order + 1});
+    } */
+
+    CurveCWS(int _mpol,
+             int _ntor,
+             vector<double> _res,
+             int _numquadpoints,
+             int _order,
+             int _nfp,
+             bool _stellsym) : Curve<Array>(_numquadpoints), order(_order), nfp(_nfp), stellsym(_stellsym), mpol(_mpol), ntor(_ntor), res(_res)
+    {
+        phi_l = xt::zeros<double>({1});
+        theta_l = xt::zeros<double>({1});
+        phi_s = xt::zeros<double>({order});
+        phi_c = xt::zeros<double>({order + 1});
+        theta_s = xt::zeros<double>({order});
+        theta_c = xt::zeros<double>({order + 1});
+
+        rc = xt::zeros<double>({mpol + 1, 2 * ntor + 1});
+        rs = xt::zeros<double>({mpol + 1, 2 * ntor + 1});
+        zc = xt::zeros<double>({mpol + 1, 2 * ntor + 1});
+        zs = xt::zeros<double>({mpol + 1, 2 * ntor + 1});
     }
 
     /*
@@ -60,6 +91,30 @@ public:
             phi_c.data()[i] = dofs[counter++];
         for (int i = 0; i < order; ++i)
             phi_s.data()[i] = dofs[counter++];
+    }
+
+    void set_dofs_surface(vector<double> &res) override
+    {
+        int shift = (mpol + 1) * (2 * ntor + 1);
+        int counter = 0;
+        if (stellsym)
+        {
+            for (int i = ntor; i < shift; ++i)
+                rc.data()[i] = dofs[counter++];
+            for (int i = ntor + 1; i < shift; ++i)
+                zs.data()[i] = dofs[counter++];
+        }
+        else
+        {
+            for (int i = ntor; i < shift; ++i)
+                rc.data()[i] = dofs[counter++];
+            for (int i = ntor + 1; i < shift; ++i)
+                rs.data()[i] = dofs[counter++];
+            for (int i = ntor; i < shift; ++i)
+                zc.data()[i] = dofs[counter++];
+            for (int i = ntor + 1; i < shift; ++i)
+                zs.data()[i] = dofs[counter++];
+        }
     }
 
     void gamma_impl(Array &data, Array &quadpoints) override;
