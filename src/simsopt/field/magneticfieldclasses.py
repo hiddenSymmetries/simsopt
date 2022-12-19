e158ccea1ea436065e4c02750fd5d6ba35952a62
Andrew Giuliani <giuliani@cims.nyu.edu>
2022-12-19 11:25:24 -0500

    fix for failing unit test

--- src/simsopt/field/magneticfieldclasses.py
+++ src/simsopt/field/magneticfieldclasses.py
@@ -481,11 +481,12 @@ class CircularCoil(MagneticField):
         k = np.sqrt(1-np.divide(np.square(alpha), np.square(beta)))
         ellipek2 = ellipe(k**2)
         ellipkk2 = ellipk(k**2)
-
-        A[:] = -self.Inorm/2*np.dot(self.rotMatrix, np.array(
-            (2*self.r0+np.sqrt(points[:, 0]**2+points[:, 1]**2)*ellipek2+(self.r0**2+points[:, 0]**2+points[:, 1]**2+points[:, 2]**2)*(ellipe(k**2)-ellipkk2)) /
-            ((points[:, 0]**2+points[:, 1]**2+1e-31)*np.sqrt(self.r0**2+points[:, 0]**2+points[:, 1]**2+2*self.r0*np.sqrt(points[:, 0]**2+points[:, 1]**2)+points[:, 2]**2+1e-31)) *
-            np.array([-points[:, 1], points[:, 0], 0])).T)
+        
+        num = (2*self.r0+np.sqrt(points[:, 0]**2+points[:, 1]**2)*ellipek2+(self.r0**2+points[:, 0]**2+points[:, 1]**2+points[:, 2]**2)*(ellipe(k**2)-ellipkk2))
+        denom = ((points[:, 0]**2+points[:, 1]**2+1e-31)*np.sqrt(self.r0**2+points[:, 0]**2+points[:, 1]**2+2*self.r0*np.sqrt(points[:, 0]**2+points[:, 1]**2)+points[:, 2]**2+1e-31))
+        fak = num/denom
+        pts = fak[:, None]*np.concatenate((-points[:, 1][:, None], points[:, 0][:, None], np.zeros((points.shape[0], 1))), axis=-1)
+        A[:] = -self.Inorm/2*np.dot(self.rotMatrix, pts.T).T
 
     def as_dict(self, serial_objs_dict):
         d = super().as_dict(serial_objs_dict=serial_objs_dict)
