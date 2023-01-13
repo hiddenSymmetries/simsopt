Gradient descent wasnt working quite right for a while -- got it fixed by finding an error in the B_matrix setup where the integration points were not being accessed correctly (Same with Phi). Now implemented a WindingVolumeField so that I can start plotting the Bnormal solution after optimization, making sure that the optimization is doing what it is intended to do.

examples/2_Intermediate/winding_volume.py
@@ -22,6 +22,7 @@ import simsoptpp as sopp
 from simsopt.geo import SurfaceRZFourier, CurveRZFourier, curves_to_vtk
 from simsopt.objectives import SquaredFlux
 from simsopt.field.biotsavart import BiotSavart
+from simsopt.field import WindingVolumeField
 from simsopt.geo import WindingVolumeGrid
 from simsopt.solve import projected_gradient_descent_Tikhonov 
 from simsopt.util.permanent_magnet_helper_functions import *
@@ -32,11 +33,11 @@ t_start = time.time()
 # Set some parameters
 nphi = 8  # nphi = ntheta >= 64 needed for accurate full-resolution runs
 ntheta = 8
-dx = 0.02
+dx = 0.025
 dy = dx
 dz = dx
 coff = 0.05  # PM grid starts offset ~ 5 cm from the plasma surface
-poff = 0.05  # PM grid end offset ~ 10 cm from the plasma surface
+poff = 0.02  # PM grid end offset ~ 10 cm from the plasma surface
 #input_name = 'input.LandremanPaul2021_QA'
 input_name = 'wout_LandremanPaul_QH_variant.nc'
 
@@ -78,8 +79,6 @@ make_Bnormal_plots(bs, s_plot, OUT_DIR, "biot_savart_initial")
 
 # optimize the currents in the TF coils
 s, bs = coil_optimization(s, bs, base_curves, curves, OUT_DIR, s_plot, 'qa')
-bs.set_points(s.gamma().reshape((-1, 3)))
-Bnormal = np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
 
 # check after-optimization average on-axis magnetic field strength
 calculate_on_axis_B(bs, s)
@@ -118,9 +117,7 @@ wv_grid = WindingVolumeGrid(
 )
 
 wv_grid._toVTK(OUT_DIR + 'grid')
-phis = wv_grid._polynomial_basis(nx=2, ny=2, nz=2)
-print(phis.shape)
-wv_grid._construct_geo_factor()
+
 #print(wv_grid.geo_factor.shape)
 #print(wv_grid.flux_constraint_matrix.shape)
 #for j in range(6): 
@@ -132,10 +129,16 @@ if False:
     t1 = time.time()
     C = wv_grid.flux_constraint_matrix  # matrix is way too big but it is very sparse
     # Need to append Itarget constraint to the flux jump constraints
-    C = vstack([C, wv_grid.Itarget_matrix], format="csc")
+    # C = vstack([C, wv_grid.Itarget_matrix], format="csc")
     CT = C.transpose()
     CCT = C @ CT
+    t2 = time.time()
+    print('Time to make CCT = ', t2 - t1, ' s')
+    t1 = time.time()
     CCT_inv = sparse_inv(CCT)
+    t2 = time.time()
+    print('Time to make CCT_inv = ', t2 - t1, ' s')
+    t1 = time.time()
     CT_CCT_inv = CT @ CCT_inv
     CT_CCT_inv_d = CT_CCT_inv[:, -1] * wv_grid.Itarget_rhs
     projection_onto_constraints = sparse_eye(wv_grid.N_grid * wv_grid.n_functions, format="csc") - CT @ CCT_inv @ C 
@@ -146,7 +149,7 @@ if False:
 nfp = wv_grid.plasma_boundary.nfp
 print('fB initial = ', 0.5 * np.linalg.norm(wv_grid.B_matrix @ np.ravel(wv_grid.alphas) - wv_grid.b_rhs, ord=2) ** 2 * nfp)
 t1 = time.time()
-alpha_opt, fB, fK = projected_gradient_descent_Tikhonov(wv_grid)
+alpha_opt, fB, fK = projected_gradient_descent_Tikhonov(wv_grid, lam=1e-20)
 print('alpha_opt = ', alpha_opt)
 t2 = time.time()
 print('Gradient Descent Tikhonov solve time = ', t2 - t1, ' s')
@@ -158,7 +161,8 @@ plt.grid(True)
 plt.legend()
 wv_grid.alphas = alpha_opt.reshape(wv_grid.N_grid, wv_grid.n_functions)
 wv_grid._toVTK(OUT_DIR + 'grid_after_Tikhonov_solve')
-print('fB after optimization = ', fB) 
+# print('fB after optimization = ', fB) 
+bs_wv = WindingVolumeField(wv_grid)
 
 t_end = time.time()
 print('Total time = ', t_end - t_start)

src/simsopt/field/magneticfieldclasses.py
@@ -17,7 +17,8 @@ from .._core.json import GSONable, GSONDecoder
 logger = logging.getLogger(__name__)
 
 __all__ = ['ToroidalField', 'PoloidalField', 'ScalarPotentialRZMagneticField',
-           'CircularCoil', 'Dommaschk', 'Reiman', 'InterpolatedField', 'DipoleField']
+           'CircularCoil', 'Dommaschk', 'Reiman', 'InterpolatedField', 
+           'DipoleField', 'WindingVolumeField']
 
 
 class ToroidalField(MagneticField):
@@ -501,6 +502,47 @@ class CircularCoil(MagneticField):
         xyz = decoder.process_decoded(d["points"], serial_objs_dict, recon_objs)
         field.set_points_cart(xyz)
         return field
+
+
+def WindingVolumeField(MagneticField):
+    r"""
+    Computes the MagneticField induced by N grid cells, each with spatially varying and
+    locally divergence-free current, for the Winding Volume method. This is done by
+    calling the normal BiotSavart field for each of the cells, and summing the result.
+
+    Args:
+        winding_volume: WindingVolume grid class, containing the grid cell locations,
+                        integration points in each cell, etc.
+    """
+
+    def __init__(self, winding_volume):
+        MagneticField.__init__(self)
+        self.winding_volume = winding_volume
+        self.integration_points = winding_volume.integration_points
+        self.num_cells = winding_volume.N_grid
+        Phi = winding_volume.Phi
+        Phi = Phi.reshape(winding_volume.n_functions, n, Phi.shape[2] * Phi.shape[3] * Phi.shape[4], 3)
+        # Compute Jvec as average J over the integration points in a cell
+        Jvec = np.zeros((self.num_cells, Phi.shape[2], 3))
+        for i in range(3):
+            Jvec[:, :, i] = np.sum(winding_volume.alphas.T * Phi[:, :, :, i], axis=0)
+        self.J = winding_volume.Jvec
+
+    def _B_impl(self, B):
+        points = self.get_points_cart_ref()
+        B[:] = sopp.winding_volume_field_B(points, self.integration_points, self.J)
+
+    def _dB_by_dX_impl(self, dB):
+        points = self.get_points_cart_ref()
+        dB[:] = sopp.dipole_field_dB(points, self.dipole_grid, self.m_vec)
+
+    def _A_impl(self, A):
+        points = self.get_points_cart_ref()
+        A[:] = sopp.dipole_field_A(points, self.dipole_grid, self.m_vec)
+
+    def _dA_by_dX_impl(self, dA):
+        points = self.get_points_cart_ref()
+        dA[:] = sopp.dipole_field_dA(points, self.dipole_grid, self.m_vec)
 
 
 class DipoleField(MagneticField):

src/simsopt/geo/winding_volume_grid.py
@@ -58,7 +58,7 @@ class WindingVolumeGrid:
         dx=0.02, dy=0.02, dz=0.02,
         filename=None, surface_flag='vmec',
         famus_filename=None, 
-        OUT_DIR='',
+        OUT_DIR='', nx=2, ny=2, nz=2,
     ):
         if plasma_offset <= 0 or coil_offset <= 0:
             raise ValueError('permanent magnets must be offset from the plasma')
@@ -209,6 +209,12 @@ class WindingVolumeGrid:
         self.N_grid = self.XYZ_flat.shape[0]
         self.alphas = np.random.rand(self.N_grid, self.n_functions)
 
+        # Initialize Phi
+        self._setup_polynomial_basis(nx=nx, ny=ny, nz=nz)
+
+        # Initialize the factors for optimization
+        self._construct_geo_factor()
+
     def _setup_uniform_grid(self):
         """
             Initializes a uniform grid in cartesian coordinates and sets
@@ -328,9 +334,11 @@ class WindingVolumeGrid:
         oz = self.XYZ_flat[:, 2]
 
         Jvec = np.zeros((n, 3))
-        Phi = self._polynomial_basis().reshape(self.n_functions, n, 3)
+        Phi = self.Phi
+        Phi = Phi.reshape(self.n_functions, n, Phi.shape[2] * Phi.shape[3] * Phi.shape[4], 3)
+        # Compute Jvec as average J over the integration points in a cell
         for i in range(3):
-            Jvec[:, i] = np.sum(self.alphas.T * Phi[:, :, i], axis=0) 
+            Jvec[:, i] = np.mean(np.sum(self.alphas.T * Phi[:, :, :, i], axis=0), axis=-1)
         Jx = Jvec[:, 0]
         Jy = Jvec[:, 1]
         Jz = Jvec[:, 2]
@@ -358,7 +366,7 @@ class WindingVolumeGrid:
             contig(self.XYZ_uniform[:, 2])
         )
 
-    def _polynomial_basis(self, nx=1, ny=1, nz=1):
+    def _setup_polynomial_basis(self, nx=1, ny=1, nz=1):
         """
         Evaluate the basis of divergence-free polynomials
         at a given set of points. For now,
@@ -436,7 +444,13 @@ class WindingVolumeGrid:
         plasma_unitnormal = contig(self.plasma_boundary.unitnormal().reshape(-1, 3))
         coil_points = contig(self.XYZ_flat)
         integration_points = contig(self.XYZ_integration)
-        self.geo_factor = sopp.winding_volume_geo_factors(points, coil_points, integration_points, plasma_unitnormal, self.Phi)
+        print(points.shape, coil_points.shape, integration_points.shape, plasma_unitnormal.shape, self.Phi.shape)
+        self.geo_factor = sopp.winding_volume_geo_factors(
+            points, 
+            integration_points, 
+            plasma_unitnormal, 
+            contig(self.Phi)
+        )
         dphi = self.plasma_boundary.quadpoints_phi[1]
         dtheta = self.plasma_boundary.quadpoints_theta[1]
         plasma_normal = self.plasma_boundary.normal().reshape(-1, 3)
@@ -447,14 +461,17 @@ class WindingVolumeGrid:
             B_matrix[i, :] *= np.sqrt(normN[i])
             b_rhs[i] *= np.sqrt(normN[i])
 
-        # So far B_matrix has only the contributions from the
-        # 1 / 2 * nfp part of the winding volume
         self.B_matrix = B_matrix
-        self.b_rhs = b_rhs
+        self.b_rhs = -b_rhs  # minus sign because ||B_{coil,N} + B_{0,N}||_2^2 -> ||A * alpha - b||_2^2
 
         curve_dl = self.Itarget_curve.gammadash().reshape(-1, 3)
         dphi_loop = np.sqrt(self.plasma_boundary.quadpoints_phi[1])
-        self.Itarget_matrix = sopp.winding_volume_geo_factors(points, coil_points, integration_points, curve_dl, self.Phi).reshape(B_matrix.shape[0], self.N_grid * self.n_functions) * dphi_loop
+        self.Itarget_matrix = sopp.winding_volume_geo_factors(
+            points, 
+            integration_points, 
+            contig(curve_dl), 
+            contig(self.Phi)
+        ).reshape(B_matrix.shape[0], self.N_grid * self.n_functions) * dphi_loop
         self.Itarget_rhs = self.Bn_Itarget * dphi_loop
         self.Itarget_matrix = np.sum(self.Itarget_matrix, axis=0)
         self.Itarget_rhs = np.sum(self.Itarget_rhs) + 4 * np.pi * 1e-7 * self.Itarget

src/simsopt/solve/winding_volume_optimization.py
@@ -22,29 +22,38 @@ def projected_gradient_descent_Tikhonov(winding_volume, lam=0.0, alpha0=None, ma
                 P = I - C.T @ (C * C.T)^{-1} @ C. P is very large but very sparse
                 so is assumed to be stored as a scipy csc_matrix. 
     """
+    # B = np.random.rand(winding_volume.B_matrix.shape[0], winding_volume.B_matrix.shape[1]) # winding_volume.B_matrix
     B = winding_volume.B_matrix
-    L = np.linalg.svd(B, compute_uv=False)[0] ** 2
+    BT = B.T
+    L = np.linalg.svd(B @ BT + lam * np.eye(B.shape[0]), compute_uv=False)[0]
     step_size = 1.0 / L  # largest step size suitable for gradient descent
     print('L, step_size = ', L, step_size)
-    BT = B.T
+    # b = np.random.rand(winding_volume.b_rhs.shape[0])
     b = winding_volume.b_rhs
-    BTb = step_size * (BT @ b)
-    BT = step_size * BT
-    lam = step_size * lam
+    BTb = BT @ b
     n = winding_volume.N_grid
     nfp = winding_volume.plasma_boundary.nfp
     num_basis = winding_volume.n_functions
+    #print('B = ', B)
+    #print('b = ', b)
 
-    alpha_opt = np.random.rand(n * num_basis) - 0.5  # set some initial guess
+    alpha_opt = (np.random.rand(n * num_basis) - 0.5) * 1e4  # set some initial guess
+    #print('alpha initial = ', alpha_opt)
+    B_inv = np.linalg.pinv(B, rcond=1e-30)
+    #print('B_inv @ b = ', B_inv @ b)
+    #print('B @ B_inv @ b - b = ', B @ (B_inv @ b) - b)
+    #print('B @ BT = ', B @ BT)
     f_B = []
     f_K = []
+    #print(np.mean(abs(BTb)), np.mean(abs(BT @ (B @ alpha_opt))))
     if P is None:
         for i in range(max_iter):
-            alpha_opt = alpha_opt + BTb - BT @ (B @ alpha_opt) - lam * alpha_opt
+            alpha_opt = alpha_opt + step_size * (BTb - BT @ (B @ alpha_opt) - lam * alpha_opt)
             f_B.append(np.linalg.norm(B @ alpha_opt - b, ord=2))
             f_K.append(np.linalg.norm(alpha_opt, ord=2))
-            if (i % 1000) == 0.0:
-                print(i, f_B[i])
+            if (i % 100) == 0.0:
+                print(i, f_B[i] ** 2 * nfp * 0.5, alpha_opt[:10])
+                print(np.mean(abs(BTb)), np.mean(abs(BT @ (B @ alpha_opt))))
     else:
         for i in range(max_iter):
             alpha_opt = P.dot(alpha_opt + BTb - BT @ (B @ alpha_opt) - lam * alpha_opt)