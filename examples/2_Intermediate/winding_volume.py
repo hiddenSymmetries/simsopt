Gotten very close to getting the bnormal part of the optimization working. the least squares calculation of Bnormal actually appears to be correct, as far as I can tell, but for some reason the Bnormal computed with the WindingVolumeField object is off. The calculation is just Biot Savart, so not sure what is going wrong here.

examples/2_Intermediate/winding_volume.py
@@ -31,9 +31,9 @@ import time
 t_start = time.time()
 
 # Set some parameters
-nphi = 16  # nphi = ntheta >= 64 needed for accurate full-resolution runs
-ntheta = 16
-dx = 0.025
+nphi = 8  # nphi = ntheta >= 64 needed for accurate full-resolution runs
+ntheta = 8
+dx = 0.035
 dy = dx
 dz = dx
 coff = 0.05  # PM grid starts offset ~ 5 cm from the plasma surface
@@ -61,24 +61,25 @@ bs = BiotSavart(coils)
 calculate_on_axis_B(bs, s)
 
 # Make higher resolution surface for plotting Bnormal
-qphi = 2 * nphi
-quadpoints_phi = np.linspace(0, 1, qphi, endpoint=True)
-quadpoints_theta = np.linspace(0, 1, ntheta, endpoint=True)
+#qphi = 2 * nphi
+#quadpoints_phi = np.linspace(0, 1, qphi, endpoint=True)
+#quadpoints_theta = np.linspace(0, 1, ntheta, endpoint=True)
 
 #s_plot = SurfaceRZFourier.from_vmec_input(
 #    surface_filename, range="full torus",
 #    quadpoints_phi=quadpoints_phi, quadpoints_theta=quadpoints_theta
 #)
-s_plot = SurfaceRZFourier.from_wout(
-    surface_filename, range="full torus",
-    quadpoints_phi=quadpoints_phi, quadpoints_theta=quadpoints_theta
-)
+#s_plot = SurfaceRZFourier.from_wout(
+#    surface_filename, range="full torus",
+#    quadpoints_phi=quadpoints_phi, quadpoints_theta=quadpoints_theta
+#)
 
 # Plot initial Bnormal on plasma surface from un-optimized BiotSavart coils
-make_Bnormal_plots(bs, s_plot, OUT_DIR, "biot_savart_initial")
+make_Bnormal_plots(bs, s, OUT_DIR, "biot_savart_initial")
+#make_Bnormal_plots(bs, s_plot, OUT_DIR, "biot_savart_initial")
 
 # optimize the currents in the TF coils
-s, bs = coil_optimization(s, bs, base_curves, curves, OUT_DIR, s_plot, 'qa')
+s, bs = coil_optimization(s, bs, base_curves, curves, OUT_DIR, 'qa')
 
 # check after-optimization average on-axis magnetic field strength
 calculate_on_axis_B(bs, s)
@@ -103,7 +104,7 @@ Itarget = 1e5  # 0.1 MA
 bs.set_points(curve.gamma().reshape((-1, 3)))
 Bnormal_Itarget = np.sum((bs.B() * curve.gammadash()).reshape(-1, 3), axis=-1)
 
-nx = 10
+nx = 8
 # Finally, initialize the winding volume 
 wv_grid = WindingVolumeGrid(
     s, Itarget_curve=curve, Itarget=Itarget, 
@@ -149,7 +150,7 @@ if False:
     print('Time to make projection operator and project alpha = ', t2 - t1, ' s')
     wv_grid._toVTK(OUT_DIR + 'grid_with_flux_jump_constraints')
 nfp = wv_grid.plasma_boundary.nfp
-print('fB initial = ', 0.5 * np.linalg.norm(wv_grid.B_matrix @ np.ravel(wv_grid.alphas) - wv_grid.b_rhs, ord=2) ** 2 * nfp)
+print('fB initial = ', 0.5 * np.linalg.norm(wv_grid.B_matrix @ wv_grid.alphas - wv_grid.b_rhs, ord=2) ** 2 * nfp)
 t1 = time.time()
 alpha_opt, fB, fK = projected_gradient_descent_Tikhonov(wv_grid, lam=1e-20)
 print('alpha_opt = ', alpha_opt)
@@ -161,22 +162,24 @@ plt.semilogy(fK, label='fK')
 plt.semilogy(fB + fK, label='total')
 plt.grid(True)
 plt.legend()
-wv_grid.alphas = alpha_opt.reshape(wv_grid.N_grid, wv_grid.n_functions)
+wv_grid.alphas = alpha_opt
 wv_grid._toVTK(OUT_DIR + 'grid_after_Tikhonov_solve')
 # print('fB after optimization = ', fB) 
 
 # set up WindingVolume Bfield
 bs_wv = WindingVolumeField(wv_grid)
-bs_wv.set_points(s_plot.gamma().reshape((-1, 3)))
-
-# Set up correct Bnormal from TF coils 
-bs.set_points(s_plot.gamma().reshape((-1, 3)))
-Bnormal = np.sum(bs.B().reshape((qphi, ntheta, 3)) * s_plot.unitnormal(), axis=2)
-fB_direct = SquaredFlux(s_plot, bs_wv, -Bnormal).J()
+bs_wv.set_points(s.gamma().reshape((-1, 3)))
+Bnormal_wv = np.sum(bs_wv.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
+normN = np.linalg.norm(s.normal().reshape(-1, 3), axis=-1)
+print('Bnormal direct = ', Bnormal_wv)
+print('Bnormal lstsq = ', wv_grid.B_matrix @ alpha_opt * np.sqrt(nphi * ntheta) / np.sqrt(normN))
+print('Bnormal coils = ', Bnormal)
+print('fB direct = ', np.sum(normN * np.ravel(Bnormal_wv + Bnormal) ** 2) * 0.5 / (nphi * ntheta))
+fB_direct = SquaredFlux(s, bs_wv, -Bnormal).J()
 print('fB_direct = ', fB_direct)
 
-make_Bnormal_plots(bs_wv, s_plot, OUT_DIR, "biot_savart_only_winding_volume")
-make_Bnormal_plots(bs + bs_wv, s_plot, OUT_DIR, "biot_savart_total")
+make_Bnormal_plots(bs_wv, s, OUT_DIR, "biot_savart_only_winding_volume")
+make_Bnormal_plots(bs + bs_wv, s, OUT_DIR, "biot_savart_total")
 
 t_end = time.time()
 print('Total time = ', t_end - t_start)

src/simsopt/field/magneticfieldclasses.py
@@ -517,18 +517,21 @@ class WindingVolumeField(MagneticField):
 
     def __init__(self, winding_volume):
         MagneticField.__init__(self)
+        contig = np.ascontiguousarray
         self.winding_volume = winding_volume
-        self.integration_points = winding_volume.XYZ_integration
+        self.integration_points = contig(winding_volume.XYZ_integration)
         self.num_cells = winding_volume.N_grid
+        # self.Phi = contig(winding_volume.Phi)
+        # self.alphas = contig(winding_volume.alphas)
         Phi = winding_volume.Phi
         Phi = Phi.reshape(winding_volume.n_functions, self.num_cells, Phi.shape[2] * Phi.shape[3] * Phi.shape[4], 3)
-        # Compute Jvec as average J over the integration points in a cell
         Jvec = np.zeros((self.num_cells, Phi.shape[2], 3))
+        alphas = winding_volume.alphas.reshape(self.num_cells, winding_volume.n_functions)
         for i in range(3):
             for j in range(Phi.shape[2]):
-                Jvec[:, j, i] = np.sum(winding_volume.alphas.T * Phi[:, :, j, i], axis=0)
-        self.J = Jvec
-        self.grid_scaling = winding_volume.dx * winding_volume.dy * winding_volume.dz
+                Jvec[:, j, i] = np.sum(Phi[:, :, j, i].T * alphas, axis=1)
+        self.J = contig(Jvec)
+        self.grid_scaling = winding_volume.dx * winding_volume.dy * winding_volume.dz / (winding_volume.nx * winding_volume.ny * winding_volume.nz)
 
     def _B_impl(self, B):
         points = self.get_points_cart_ref()

src/simsopt/geo/winding_volume_grid.py
@@ -207,10 +207,13 @@ class WindingVolumeGrid:
             self.XYZ_flat = np.array([ox, oy, oz]).T
             self.N_grid = self.XYZ_flat.shape[0]
         self.N_grid = self.XYZ_flat.shape[0]
-        self.alphas = np.random.rand(self.N_grid, self.n_functions)
+        self.alphas = np.random.rand(self.n_functions * self.N_grid)
+        self.nx = nx
+        self.ny = ny
+        self.nz = nz
 
         # Initialize Phi
-        self._setup_polynomial_basis(nx=nx, ny=ny, nz=nz)
+        self._setup_polynomial_basis()
 
         # Initialize the factors for optimization
         self._construct_geo_factor()
@@ -337,10 +340,11 @@ class WindingVolumeGrid:
         n_interp = Phi.shape[2] * Phi.shape[3] * Phi.shape[4]
         Jvec = np.zeros((n, n_interp, 3))
         Phi = Phi.reshape(self.n_functions, n, n_interp, 3)
+        alphas = self.alphas.reshape(n, self.n_functions).T
         # Compute Jvec as average J over the integration points in a cell
         for i in range(3):
             for j in range(n_interp):
-                Jvec[:, j, i] = np.sum(self.alphas.T * Phi[:, :, j, i], axis=0)
+                Jvec[:, j, i] = np.sum(alphas * Phi[:, :, j, i], axis=0)
         Jvec = np.mean(Jvec, axis=-1)
         Jx = Jvec[:, 0]
         Jy = Jvec[:, 1]
@@ -369,7 +373,7 @@ class WindingVolumeGrid:
             contig(self.XYZ_uniform[:, 2])
         )
 
-    def _setup_polynomial_basis(self, nx=1, ny=1, nz=1):
+    def _setup_polynomial_basis(self):
         """
         Evaluate the basis of divergence-free polynomials
         at a given set of points. For now,
@@ -385,6 +389,9 @@ class WindingVolumeGrid:
         dx = self.dx
         dy = self.dy
         dz = self.dz
+        nx = self.nx
+        ny = self.ny
+        nz = self.nz
         n = self.N_grid
         x_leftpoints = self.XYZ_flat[:, 0]
         y_leftpoints = self.XYZ_flat[:, 1]
@@ -447,19 +454,23 @@ class WindingVolumeGrid:
         plasma_unitnormal = contig(self.plasma_boundary.unitnormal().reshape(-1, 3))
         coil_points = contig(self.XYZ_flat)
         integration_points = contig(self.XYZ_integration)
-        print(points.shape, coil_points.shape, integration_points.shape, plasma_unitnormal.shape, self.Phi.shape)
         self.geo_factor = sopp.winding_volume_geo_factors(
             points, 
             integration_points, 
             plasma_unitnormal, 
             contig(self.Phi)
         )
-        dphi = self.plasma_boundary.quadpoints_phi[1]
-        dtheta = self.plasma_boundary.quadpoints_theta[1]
+        nphi = len(self.plasma_boundary.quadpoints_phi)
+        ntheta = len(self.plasma_boundary.quadpoints_theta)
+        N_quadrature_inv = 1.0 / (nphi * ntheta)
         plasma_normal = self.plasma_boundary.normal().reshape(-1, 3)
         normN = np.linalg.norm(plasma_normal, ord=2, axis=-1)
-        B_matrix = (self.geo_factor * 1e-7 * np.sqrt(dphi * dtheta) * self.dx * self.dy * self.dz).reshape(self.geo_factor.shape[0], self.N_grid * self.n_functions)
-        b_rhs = np.ravel(self.Bn * np.sqrt(dphi * dtheta))
+
+        # Delta x from intra-cell integration is self.dx (uniform grid spacing) divided 
+        # by self.nx (number of points within a cell)
+        coil_integration_factor = self.dx * self.dy * self.dz / (self.nx * self.ny * self.nz)
+        B_matrix = (self.geo_factor * 1e-7 * np.sqrt(N_quadrature_inv) * coil_integration_factor).reshape(self.geo_factor.shape[0], self.N_grid * self.n_functions)
+        b_rhs = np.ravel(self.Bn * np.sqrt(N_quadrature_inv))
         for i in range(B_matrix.shape[0]):
             B_matrix[i, :] *= np.sqrt(normN[i])
             b_rhs[i] *= np.sqrt(normN[i])
@@ -468,20 +479,19 @@ class WindingVolumeGrid:
         self.b_rhs = -b_rhs  # minus sign because ||B_{coil,N} + B_{0,N}||_2^2 -> ||A * alpha - b||_2^2
 
         curve_dl = self.Itarget_curve.gammadash().reshape(-1, 3)
-        dphi_loop = np.sqrt(self.plasma_boundary.quadpoints_phi[1])
+        nphi_loop_sqrt_inv = np.sqrt(1.0 / nphi) 
         self.Itarget_matrix = sopp.winding_volume_geo_factors(
             points, 
             integration_points, 
             contig(curve_dl), 
             contig(self.Phi)
-        ).reshape(B_matrix.shape[0], self.N_grid * self.n_functions) * dphi_loop
-        self.Itarget_rhs = self.Bn_Itarget * dphi_loop
+        ).reshape(B_matrix.shape[0], self.N_grid * self.n_functions)
+        self.Itarget_matrix *= 1e-7 * nphi_loop_sqrt_inv * coil_integration_factor
         self.Itarget_matrix = np.sum(self.Itarget_matrix, axis=0)
+
+        self.Itarget_rhs = self.Bn_Itarget * nphi_loop_sqrt_inv
         self.Itarget_rhs = np.sum(self.Itarget_rhs) + 4 * np.pi * 1e-7 * self.Itarget
 
-        nx = self.Phi.shape[2]
-        ny = self.Phi.shape[3]
-        nz = self.Phi.shape[4]
         flux_factor, self.connection_list = sopp.winding_volume_flux_jumps(
             coil_points, 
             self.Phi, 

src/simsopt/solve/winding_volume_optimization.py
@@ -35,18 +35,11 @@ def projected_gradient_descent_Tikhonov(winding_volume, lam=0.0, alpha0=None, ma
     n = winding_volume.N_grid
     nfp = winding_volume.plasma_boundary.nfp
     num_basis = winding_volume.n_functions
-    #print('B = ', B)
-    #print('b = ', b)
-
+    B_reshaped = B.reshape(B.shape[0], n, num_basis)
     alpha_opt = (np.random.rand(n * num_basis) - 0.5) * 1e3  # set some initial guess
-    #print('alpha initial = ', alpha_opt)
     B_inv = np.linalg.pinv(B, rcond=1e-30)
-    #print('B_inv @ b = ', B_inv @ b)
-    #print('B @ B_inv @ b - b = ', B @ (B_inv @ b) - b)
-    #print('B @ BT = ', B @ BT)
     f_B = []
     f_K = []
-    #print(np.mean(abs(BTb)), np.mean(abs(BT @ (B @ alpha_opt))))
     if P is None:
         if acceleration:  # Nesterov acceleration 
             # first iteration do regular GD 
@@ -60,7 +53,7 @@ def projected_gradient_descent_Tikhonov(winding_volume, lam=0.0, alpha0=None, ma
                 alpha_opt_prev = alpha_opt
                 alpha_opt = vi + step_size_i * (BTb - BT @ (B @ vi) - lam * vi)
                 step_size_i = (1 + np.sqrt(1 + 4 * step_size_i ** 2)) / 2.0
-                f_B.append(np.linalg.norm(B @ alpha_opt - b, ord=2))
+                f_B.append(np.linalg.norm(np.tensordot(B_reshaped, alpha_opt.reshape(n, num_basis), ((1, 2), (0, 1))) - b, ord=2))
                 f_K.append(np.linalg.norm(alpha_opt, ord=2))
                 if (i % 100) == 0.0:
                     print(i, f_B[i] ** 2 * nfp * 0.5, alpha_opt[:10])