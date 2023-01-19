ee532cb9bbe8d6e909f34602c393c05c78a0632d
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2023-01-19 16:32:26 +0000

    added plot superimposing curve and surface

--- tests/geo/test_CurveCWS.py
+++ tests/geo/test_CurveCWS.py
@@ -3,8 +3,9 @@ from simsopt.geo import SurfaceRZFourier
 import matplotlib.pyplot as plt
 import numpy as np
 
-filename = "../test_files/wout_circular_tokamak_reference.nc"
+# filename = "../test_files/wout_circular_tokamak_reference.nc"
 #filename = "../test_files/wout_10x10.nc"
+filename = "../test_files/wout_W7-X_without_coil_ripple_beta0p05_d23p4_tm_reference.nc"
 
 s = SurfaceRZFourier.from_wout(
     filename, range="full torus", ntheta=32, nphi=32
@@ -21,22 +22,31 @@ cws = CurveCWS(
 )
 # cws.set_dofs([1, 0, 0, 0])
 
-phi_array = np.linspace(0, 2 * np.pi, 20)
+phi_array = np.linspace(0., 2 * np.pi/s.nfp/2, 7)
 fig = plt.figure()
-ax = fig.add_subplot(projection="3d")
-ax.set_xlim3d(-7.5, 7.5)
-ax.set_ylim3d(-7.5, 7.5)
-ax.set_zlim3d(-7.5, 7.5)
+# ax = fig.add_subplot(projection="3d")
+ax = fig.add_subplot(111)
+# ax.set_xlim3d(-7.5, 7.5)
+# ax.set_ylim3d(-7.5, 7.5)
+# ax.set_zlim3d(-7.5, 7.5)
 for phi in phi_array:
     # print(phi)
-    cws.set_dofs([1, 0, 1, 1, 0, phi, 0, 0])
+    cws.set_dofs([1, 0, 0, 0, 0, phi, 0, 0])
     gamma = cws.gamma()
     x = gamma[:, 0]
     y = gamma[:, 1]
     z = gamma[:, 2]
-    ax.plot(x, y, z)
+    r = np.sqrt(x*x+y*y)
+    ax.plot(r, z, label=f'coil at phi={phi}')
 
-#s.plot()
+    surface_gamma = s.cross_section(phi)
+    x = surface_gamma[:,0]
+    y = surface_gamma[:,1]
+    z = surface_gamma[:,2]
+    r = np.sqrt(x*x+y*y)
+    ax.plot(r, z, '--', label=f'surface at phi={phi}')
+plt.legend()
+# s.plot(show=False, ax=ax, alpha=0.2)
 # cws.plot()
 # print(cws.gamma())
 # plt.plot(cws.gamma())
