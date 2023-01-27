Fixed some grid issues. Added XSIMD of the B and Bext calculations, which seems to mildly speed things up (factor of 2 ish). Tried coding up accelerated prox gradient descent in c++ but not seeing any speedup.

examples/2_Intermediate/winding_volume_QA.py
@@ -35,10 +35,7 @@ t1 = time.time()
 # Set some parameters
 nphi = 32  # nphi = ntheta >= 64 needed for accurate full-resolution runs
 ntheta = 16
-#dx = 0.1
-#dy = dx
-#dz = dx
-Nx = 30
+Nx = 10
 Ny = Nx
 Nz = Nx  # - 1
 poff = 0.3  # PM grid end offset ~ 10 cm from the plasma surface
@@ -157,7 +154,7 @@ print('fB initial = ', 0.5 * np.linalg.norm(wv_grid.B_matrix @ wv_grid.alphas - 
 t1 = time.time()
 lam = 1e-20
 acceleration = True
-max_iter = 20000
+max_iter = 50000
 alpha_opt, fB, fK, fI = projected_gradient_descent_Tikhonov(wv_grid, lam=lam, P=projection_onto_constraints, acceleration=acceleration, max_iter=max_iter)
 # print('alpha_opt = ', alpha_opt)
 if projection_onto_constraints is not None:

src/simsopt/field/magneticfieldclasses.py
@@ -559,7 +559,7 @@ class WindingVolumeField(MagneticField):
                 J_temp[:, :, 2] = Jz
 
                 int_points = np.transpose(np.array([ox_temp, oy_temp, oz_temp]), [1, 2, 0])
-                B[:] += sopp.winding_volume_field_B(
+                B[:] += sopp.winding_volume_field_B_SIMD(
                     contig(points), 
                     contig(int_points), 
                     contig(J_temp)

src/simsopt/geo/winding_volume_grid.py
@@ -351,12 +351,7 @@ class WindingVolumeGrid:
         Phi = self.Phi
         n_interp = Phi.shape[2]
         Jvec = self.J
-        # Jvec = np.zeros((n, n_interp, 3))
         alphas = self.alphas.reshape(n, self.n_functions).T
-        # # Compute Jvec as average J over the integration points in a cell
-        # for i in range(3):
-        #     for j in range(n_interp):
-        #         Jvec[:, j, i] = np.sum(alphas * Phi[:, :, j, i], axis=0)
         Jvec_avg = np.mean(Jvec, axis=1)
         Jvec = Jvec.reshape(n, self.nx, self.ny, self.nz, 3)
 
@@ -585,7 +580,9 @@ class WindingVolumeGrid:
         t1 = time.time()
         num_points = points.shape[0]
         num_points_curve = points_curve.shape[0]
-        B_factor = np.zeros((num_points + num_points_curve, 3, n, num_basis))
+        # B_factor = np.zeros((num_points + num_points_curve, 3, n, num_basis))
+        geo_factor = np.zeros((num_points, n, num_basis))
+        Itarget_matrix = np.zeros((num_points_curve, n, num_basis))
         points_total = np.vstack((points, points_curve))
         for stell in stell_list:
             for fp in range(nfp):
@@ -600,15 +597,33 @@ class WindingVolumeGrid:
                 Phivec_full[:, index:index + n, :, 0] = Phi[:, :, :, 0] * np.cos(phi0) * stell - Phi[:, :, :, 1] * np.sin(phi0)
                 Phivec_full[:, index:index + n, :, 1] = Phi[:, :, :, 0] * np.sin(phi0) * stell + Phi[:, :, :, 1] * np.cos(phi0)
                 Phivec_full[:, index:index + n, :, 2] = Phi[:, :, :, 2]
-                Phivec_transpose = np.transpose(Phivec_full[:, index:index + n, :, :], [1, 2, 0, 3])
 
+                Phivec_transpose = np.transpose(Phivec_full[:, index:index + n, :, :], [1, 2, 0, 3])
                 int_points = np.transpose(np.array([ox_full[index:index + n, :], oy_full[index:index + n, :], oz_full[index:index + n, :]]), [1, 2, 0])
-                B_factor += sopp.winding_volume_field_Bext(
-                    points_total, 
+                geo_factor += sopp.winding_volume_field_Bext_SIMD(
+                    points, 
                     contig(int_points), 
-                    contig(Phivec_transpose)
+                    #contig(Phivec_full[:, index:index + n, :, :])
+                    contig(Phivec_transpose),
+                    plasma_unitnormal
                 )
+                Itarget_matrix += sopp.winding_volume_field_Bext_SIMD(
+                    points_curve, 
+                    contig(int_points), 
+                    #contig(Phivec_full[:, index:index + n, :, :])
+                    contig(Phivec_transpose),
+                    curve_dl
+                )
+
                 index += n
+
+        self.geo_factor = geo_factor
+        self.Itarget_matrix = Itarget_matrix
+        t2 = time.time()
+        print('Computing full coil surface grid took t = ', t2 - t1, ' s')
+        t1 = time.time()
+        Phivec_transpose = np.transpose(Phivec_full, [1, 2, 0, 3])
+        int_points = np.transpose(np.array([ox_full, oy_full, oz_full]), [1, 2, 0])
         t2 = time.time()
         print('Computing B factor and I factor took t = ', t2 - t1, ' s')
 
@@ -620,15 +635,19 @@ class WindingVolumeGrid:
             for j in range(n_interp):
                 for k in range(self.n_functions):
                     self.J[:, j, i] += alphas[:, k] * Phi[k, :, j, i]
-        self.integration_points_full = contig(np.transpose(np.array([ox_full, oy_full, oz_full]), [1, 2, 0]))
-        self.geo_factor = np.zeros((points.shape[0], n, num_basis))
-        self.Itarget_matrix = np.zeros((points_curve.shape[0], n, num_basis))
-        for i in range(B_factor.shape[2]):
-            for j in range(B_factor.shape[3]):
-                self.geo_factor[:, i, j] = np.sum(B_factor[:num_points, :, i, j] * plasma_unitnormal, axis=-1)
-                self.Itarget_matrix[:, i, j] = np.sum(B_factor[num_points:, :, i, j] * curve_dl, axis=-1)
+        self.integration_points_full = int_points  # contig(np.transpose(np.array([ox_full, oy_full, oz_full]), [1, 2, 0]))
         t2 = time.time()
-        print('Finishing the B and I matrices took t = ', t2 - t1, ' s')
+        print('Computing J took t = ', t2 - t1, ' s')
+        #t1 = time.time()
+        #self.geo_factor = np.zeros((points.shape[0], n, num_basis))
+        #self.Itarget_matrix = np.zeros((points_curve.shape[0], n, num_basis))
+        #self.geo_factor = np.tensordot(B_factor[:num_points, :, :, :], plasma_unitnormal, axes=[
+        #for i in range(B_factor.shape[2]):
+        #    for j in range(B_factor.shape[3]):
+        #        self.geo_factor[:, i, j] = np.sum(B_factor[:num_points, :, i, j] * plasma_unitnormal, axis=-1)
+        #        self.Itarget_matrix[:, i, j] = np.sum(B_factor[num_points:, :, i, j] * curve_dl, axis=-1)
+        #t2 = time.time()
+        #print('Finishing the B and I matrices took t = ', t2 - t1, ' s')
         t1 = time.time()
 
         # Delta x from intra-cell integration is self.dx (uniform grid spacing) divided 
@@ -925,17 +944,10 @@ class WindingVolumeGrid:
         n_interp = Phi.shape[2]
         alpha_opt = self.alphas.reshape(n, self.n_functions)
         # integration_points = self.integration_points
-        Jvec = self.J  # np.zeros((n, n_interp, 3))
         Nx = self.nx
         Ny = self.ny
         Nz = self.nz
-
-        # Compute Jvec as average J over the integration points in a cell
-        # for i in range(3):
-        #     for j in range(n_interp):
-        #         for k in range(self.n_functions):
-        #             Jvec[:, j, i] += alpha_opt[:, k] * Phi[k, :, j, i]
-        Jvec = Jvec.reshape(n, Nx, Ny, Nz, 3)
+        Jvec = self.J.reshape(n, Nx, Ny, Nz, 3)
 
         # Compute all the fluxes (unnormalized!)
         flux = np.zeros((n, 6))
@@ -981,20 +993,13 @@ class WindingVolumeGrid:
         q = 0
         qq = 0
         for i in range(n):
-            # ind_zero = np.ravel(np.where(self.connection_list[i, :, 0] >= 0))
-            # ind_two = np.ravel(np.where(self.connection_list[i, :, 2] >= 0))
             for j in range(6):
-                # print(i, j, flux[i, j])
-                if (i in self.x_inds) and (j == 1) and (self.range != 'full torus'):  # and (len(ind_zero) > 0):
-                    #ind = ind_zero[0]
-                    #k_ind = self.connection_list[i, ind, 0]
+                #print(i, j, flux[i, j])
+                if (i in self.x_inds) and (j == 1) and (self.range != 'full torus'):
                     k_ind = self.z_flip_x[q]
-                    # print(i, k_ind, flux[i, 1], flux[k_ind, 1])
                     assert np.isclose(flux[i, 1], flux[k_ind, 1], atol=flux_max, rtol=1e-3)
                     q += 1
-                elif (i in self.y_inds) and (j == 3) and (self.range != 'full torus'):  # and (len(ind_two) > 0):
-                    #ind = ind_two[0]
-                    #k_ind = self.connection_list[i, ind, 2]
+                elif (i in self.y_inds) and (j == 3) and (self.range != 'full torus'):
                     k_ind = self.z_flip_y[qq]
                     assert np.isclose(flux[i, 3], flux[k_ind, 3], atol=flux_max, rtol=1e-3)
                     qq += 1
@@ -1006,7 +1011,7 @@ class WindingVolumeGrid:
                             j_ind = j + 1
                         else:
                             j_ind = j - 1
-                        # print(i, j, k_ind, j_ind, ind, flux[i, j], flux[k_ind, j_ind])
+                        #print(i, j, k_ind, j_ind, ind, flux[i, j], flux[k_ind, j_ind])
                         assert np.isclose(flux[i, j], -flux[k_ind, j_ind], atol=flux_max, rtol=1e-3)
                 else:
                     assert np.isclose(flux[i, j], 0.0, atol=flux_max)

src/simsopt/solve/winding_volume_optimization.py
@@ -1,5 +1,6 @@
 import numpy as np
 import simsoptpp as sopp
+import time
 
 __all__ = ['projected_gradient_descent_Tikhonov']
 
@@ -23,14 +24,19 @@ def projected_gradient_descent_Tikhonov(winding_volume, lam=0.0, alpha0=None, ma
                 P = I - C.T @ (C * C.T)^{-1} @ C. P is very large but very sparse
                 so is assumed to be stored as a scipy csc_matrix. 
     """
+    t1 = time.time()
     B = winding_volume.B_matrix
     I = winding_volume.Itarget_matrix
     BT = B.T
+    # BTB = BT @ B
     IT = I.T
-    L = np.linalg.svd(B @ BT + I @ IT + lam * np.eye(B.shape[0]), compute_uv=False)[0]
-    cond_num = np.linalg.cond(B @ BT + I @ IT + lam * np.eye(B.shape[0]))
+    # ITI = IT @ I
+    #L = np.linalg.svd(BTB + ITI + lam * np.eye(B.shape[1]), compute_uv=False)[0]
+    L = np.linalg.svd(BT @ B + IT @ I + lam * np.eye(B.shape[1]), compute_uv=False)[0]
+    #cond_num = np.linalg.cond(BTB + ITI + lam * np.eye(B.shape[1]))
+    # cond_num = np.linalg.cond(BT @ B + IT @ I + lam * np.eye(B.shape[1]))
     step_size = 1.0 / L  # largest step size suitable for gradient descent
-    print('L, step_size, condition number = ', L, step_size, cond_num)
+    print('L, step_size, condition number = ', L, step_size)  # , cond_num)
     b = winding_volume.b_rhs
     b_I = winding_volume.Itarget_rhs
     BTb = BT @ b
@@ -44,9 +50,21 @@ def projected_gradient_descent_Tikhonov(winding_volume, lam=0.0, alpha0=None, ma
     # feasible region (the area allowed by the equality constraints)
     if P is not None:
         alpha_opt = P.dot(alpha_opt)
+    t2 = time.time()
+    print('Time to setup the algo = ', t2 - t1, ' s')
+    contig = np.ascontiguousarray
+    #t1 = time.time()
+    #Pdense = P.todense()
+    #print(Pdense.shape, B.shape, I.shape, BTb.shape, ITbI.shape)
+    #alpha_opt_cpp = sopp.acc_prox_grad_descent(contig(Pdense), contig(B), contig(I), contig(BTb), contig(ITbI), lam, step_size, max_iter)
+    #t2 = time.time()
+    #print('Time to run algo in C++ = ', t2 - t1, ' s')
+    #print('f_B from c++ calculation = ', 0.5 * nfp * np.linalg.norm(B @ alpha_opt_cpp - b, ord=2) ** 2)
     f_B = []
     f_I = []
     f_K = []
+    print_iter = 200
+    t1 = time.time()
     if P is None:
         if acceleration:  # Nesterov acceleration 
             # first iteration do regular GD 
@@ -66,7 +84,7 @@ def projected_gradient_descent_Tikhonov(winding_volume, lam=0.0, alpha0=None, ma
                 f_B.append(np.linalg.norm(B @ alpha_opt - b, ord=2))
                 f_I.append((I @ alpha_opt - b_I) ** 2)
                 f_K.append(np.linalg.norm(alpha_opt, ord=2))
-                if (i % 100) == 0.0:
+                if (i % print_iter) == 0.0:
                     print(i, f_B[i] ** 2 * nfp * 0.5, f_I[i], f_K[i])
         else:
             for i in range(max_iter):
@@ -74,7 +92,7 @@ def projected_gradient_descent_Tikhonov(winding_volume, lam=0.0, alpha0=None, ma
                 f_B.append(np.linalg.norm(B @ alpha_opt - b, ord=2))
                 f_I.append((I @ alpha_opt - b_I) ** 2)
                 f_K.append(np.linalg.norm(alpha_opt, ord=2))
-                if (i % 100) == 0.0:
+                if (i % print_iter) == 0.0:
                     print(i, f_B[i] ** 2 * nfp * 0.5, f_I[i], f_K[i])
     else:
         if acceleration:  # Nesterov acceleration 
@@ -90,26 +108,28 @@ def projected_gradient_descent_Tikhonov(winding_volume, lam=0.0, alpha0=None, ma
             f_K.append(np.linalg.norm(alpha_opt, ord=2))
             print('0', f_B[0] ** 2 * nfp * 0.5, f_I[0], f_K[0])
             for i in range(1, max_iter):
-                # This step can be unstable if P.dot not used here, since then vi can leave the feasible region!
                 vi = alpha_opt + (i - 1) / (i + 2) * (alpha_opt - alpha_opt_prev)
-                # vi = P.dot(alpha_opt + (i - 1) / (i + 2) * (alpha_opt - alpha_opt_prev))
-                alpha_opt_prev = np.copy(alpha_opt)
+                alpha_opt_prev = alpha_opt
+                #alpha_opt = P.dot(vi + step_size_i * (BTb + ITbI - (BTB + ITI + lam * ident) @ vi))
                 alpha_opt = P.dot(vi + step_size_i * (BTb + ITbI - BT @ (B @ vi) - IT * (I @ vi) - lam * vi))
 
                 step_size_i = (1 + np.sqrt(1 + 4 * step_size_i ** 2)) / 2.0
-                if (i % 100) == 0.0:
+                if (i % print_iter) == 0.0:
                     f_B.append(np.linalg.norm(B @ alpha_opt - b, ord=2))
                     f_I.append((I @ alpha_opt - b_I) ** 2)
                     f_K.append(np.linalg.norm(alpha_opt, ord=2))
-                    print(i, f_B[i // 100] ** 2 * nfp * 0.5, f_I[i // 100], f_K[i // 100])
+                    print(i, f_B[i // print_iter] ** 2 * nfp * 0.5, f_I[i // print_iter], f_K[i // print_iter])
         else:
             for i in range(max_iter):
                 alpha_opt = P.dot(alpha_opt + step_size * (BTb + ITbI - BT @ (B @ alpha_opt) - IT * (I @ alpha_opt) - lam * alpha_opt))
-                if (i % 100) == 0.0:
+                if (i % print_iter) == 0.0:
                     f_B.append(np.linalg.norm(B @ alpha_opt - b, ord=2))
                     f_I.append((I @ alpha_opt - b_I) ** 2)
                     f_K.append(np.linalg.norm(alpha_opt, ord=2))
-                    print(i, f_B[i // 100] ** 2 * nfp * 0.5, f_I[i // 100], f_K[i // 100])
+                    print(i, f_B[i // print_iter] ** 2 * nfp * 0.5, f_I[i // print_iter], f_K[i // print_iter])
+    t2 = time.time()
+    print('Time to run algo = ', t2 - t1, ' s')
+    t1 = time.time()
     winding_volume.alphas = alpha_opt 
     winding_volume.J = np.zeros((n, winding_volume.Phi.shape[2], 3))
     alphas = alpha_opt.reshape(n, num_basis)
@@ -117,4 +137,6 @@ def projected_gradient_descent_Tikhonov(winding_volume, lam=0.0, alpha0=None, ma
         for j in range(winding_volume.Phi.shape[2]):
             for k in range(num_basis):
                 winding_volume.J[:, j, i] += alphas[:, k] * winding_volume.Phi[k, :, j, i]
+    t2 = time.time()
+    print('Time to compute J = ', t2 - t1, ' s')
     return alpha_opt, 0.5 * np.array(f_B) ** 2 * nfp, 0.5 * np.array(f_K) ** 2, 0.5 * np.array(f_I)