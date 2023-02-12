61ade22c0658d6e2b8c96b249cddcab67f6281eb
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-02-12 02:47:51 +0000

    completed gamma###

--- src/simsoptpp/curvecws.cpp
+++ src/simsoptpp/curvecws.cpp
@@ -37,8 +37,8 @@ void CurveCWS<Array>::gamma_impl(Array &data, Array &quadpoints)
         {
             for (int i = 0; i < 2 * ntor + 1; ++i)
             {
-                r += rc(m, i) * cos(m * ptheta - nfp * (i-ntor) * pphi);
-                z += zs(m, i) * sin(m * ptheta - nfp * (i-ntor) * pphi);
+                r += rc(m, i) * cos(m * ptheta - nfp * (i - ntor) * pphi);
+                z += zs(m, i) * sin(m * ptheta - nfp * (i - ntor) * pphi);
             }
         }
         if (!stellsym)
@@ -47,8 +47,8 @@ void CurveCWS<Array>::gamma_impl(Array &data, Array &quadpoints)
             {
                 for (int i = 0; i < 2 * ntor + 1; ++i)
                 {
-                    r += rs(m, i) * sin(m * ptheta - nfp * (i-ntor) * pphi);
-                    z += zc(m, i) * cos(m * ptheta - nfp * (i-ntor) * pphi);
+                    r += rs(m, i) * sin(m * ptheta - nfp * (i - ntor) * pphi);
+                    z += zc(m, i) * cos(m * ptheta - nfp * (i - ntor) * pphi);
                 }
             }
         }
@@ -231,6 +231,117 @@ void CurveCWS<Array>::gammadashdash_impl(Array &data)
         data(k, 0) = ddr * cos(pphi) - 2 * (dr * sin(pphi) * dpphi) - r * cos(pphi) * pow(dpphi, 2) - r * sin(pphi) * ddpphi;
         data(k, 1) = ddr * sin(pphi) + 2 * (dr * cos(pphi) * dpphi) - r * sin(pphi) * pow(dpphi, 2) + r * cos(pphi) * ddpphi;
         data(k, 2) = ddz;
+    }
+};
+
+template <class Array>
+void CurveCWS<Array>::gammadashdashdash_impl(Array &data)
+{
+    CurveCWS<Array>::set_dofs_surface(idofs);
+
+    data *= 0;
+
+#pragma omp parallel for
+    for (int k = 0; k < numquadpoints; ++k)
+    {
+        double pphi = 0;
+        double ptheta = 0;
+        double dpphi = 0;
+        double dptheta = 0;
+        double ddpphi = 0;
+        double ddptheta = 0;
+        double ddd_pphi = 0;
+        double ddd_ptheta = 0;
+
+        double r = 0;
+        double z = 0;
+        double dr = 0;
+        double dz = 0;
+        double ddr = 0;
+        double ddz = 0;
+        double ddd_r = 0;
+        double ddd_z = 0;
+
+        double CWSt = 2 * M_PI * quadpoints[k];
+
+        // Termos com Cossenos e as suas derivas
+        for (int i = 0; i < order + 1; ++i)
+        {
+            pphi += phi_c[i] * cos(i * CWSt);
+            ptheta += theta_c[i] * cos(i * CWSt);
+
+            dpphi += -phi_c[i] * i * sin(i * CWSt);
+            dptheta += -theta_c[i] * i * sin(i * CWSt);
+
+            ddpphi += -phi_c[i] * pow(i, 2) * cos(i * CWSt);
+            ddptheta += -theta_c[i] * pow(i, 2) * cos(i * CWSt);
+
+            ddd_pphi += phi_c[i] * pow(i, 3) * sin(i * CWSt);
+            ddd_ptheta += theta_c[i] * pow(i, 3) * sin(i * CWSt);
+        }
+        // Termos com Senos e as suas derivas
+        for (int i = 1; i < order + 1; ++i)
+        {
+            pphi += phi_s[i - 1] * sin(i * CWSt);
+            ptheta += theta_s[i - 1] * sin(i * CWSt);
+
+            dpphi += phi_s[i - 1] * i * cos(i * CWSt);
+            dptheta += theta_s[i - 1] * i * cos(i * CWSt);
+
+            ddpphi += -phi_s[i - 1] * pow(i, 2) * sin(i * CWSt);
+            ddptheta += -theta_s[i - 1] * pow(i, 2) * sin(i * CWSt);
+
+            ddd_pphi += -phi_s[i - 1] * pow(i, 3) * cos(i * CWSt);
+            ddd_ptheta += -theta_s[i - 1] * pow(i, 3) * cos(i * CWSt);
+        }
+
+        pphi += phi_l * CWSt;
+        ptheta += theta_l * CWSt;
+        dpphi += phi_l;
+        dptheta += theta_l;
+
+        // SURFACE
+        for (int m = 0; m <= mpol; ++m)
+        {
+            for (int i = 0; i < 2 * ntor + 1; ++i)
+            {
+                r += rc(m, i) * cos(m * ptheta - nfp * i * pphi);
+                z += zs(m, i) * sin(m * ptheta - nfp * i * pphi);
+
+                dr += -rc(m, i) * sin(m * ptheta - nfp * i * pphi) * (m * dptheta - nfp * i * dpphi);
+                dz += zs(m, i) * cos(m * ptheta - nfp * i * pphi) * (m * dptheta - nfp * i * dpphi);
+
+                ddr += -rc(m, i) * cos(m * ptheta - nfp * i * pphi) * pow((m * dptheta - nfp * i * dpphi), 2) - rc(m, i) * sin(m * ptheta - nfp * i * pphi) * (m * ddptheta - nfp * i * ddpphi);
+                ddz += -zs(m, i) * sin(m * ptheta - nfp * i * pphi) * pow((m * dptheta - nfp * i * dpphi), 2) + zs(m, i) * cos(m * ptheta - nfp * i * pphi) * (m * ddptheta - nfp * i * ddpphi);
+
+                ddd_r += rc(m, i) * sin(m * ptheta - nfp * i * pphi) * pow((m * dptheta - nfp * i * dpphi), 3) - rc(m, i) * cos(m * ptheta - nfp * i * pphi) * 2 * (m * dptheta - nfp * i * dpphi) * (m * ddptheta - nfp * i * ddpphi) - rc(m, i) * cos(m * ptheta - nfp * i * pphi) * (m * dptheta - nfp * i * dpphi) * (m * ddptheta - nfp * i * ddpphi) - rc(m, i) * sin(m * ptheta - nfp * i * pphi) * (m * ddd_ptheta - nfp * i * ddd_pphi);
+                ddd_z += -zs(m, i) * cos(m * ptheta - nfp * i * pphi) * pow((m * dptheta - nfp * i * dpphi), 3) - zs(m, i) * sin(m * ptheta - nfp * i * pphi) * 2 * (m * dptheta - nfp * i * dpphi) * (m * ddptheta - nfp * i * ddpphi) - zs(m, i) * sin(m * ptheta - nfp * i * pphi) * (m * dptheta - nfp * i * dpphi) * (m * ddptheta - nfp * i * ddpphi) + zs(m, i) * cos(m * ptheta - nfp * i * pphi) * (m * ddd_ptheta - nfp * i * ddd_pphi);
+            }
+        }
+        if (!stellsym)
+        {
+            for (int m = 0; m <= mpol; ++m)
+            {
+                for (int i = 0; i < 2 * ntor + 1; ++i)
+                {
+                    r += rs(m, i) * sin(m * ptheta - nfp * i * pphi);
+                    z += zc(m, i) * cos(m * ptheta - nfp * i * pphi);
+
+                    dr += rs(m, i) * cos(m * ptheta - nfp * i * pphi) * (m * dptheta - nfp * i * dpphi);
+                    dz += -zc(m, i) * sin(m * ptheta - nfp * i * pphi) * (m * dptheta - nfp * i * dpphi);
+
+                    ddr += -rs(m, i) * sin(m * ptheta - nfp * i * pphi) * pow((m * dptheta - nfp * i * dpphi), 2) + rs(m, i) * cos(m * ptheta - nfp * i * pphi) * (m * ddptheta - nfp * i * ddpphi);
+                    ddz += -zc(m, i) * cos(m * ptheta - nfp * i * pphi) * pow((m * dptheta - nfp * i * dpphi), 2) - zc(m, i) * sin(m * ptheta - nfp * i * pphi) * (m * ddptheta - nfp * i * ddpphi);
+
+                    ddd_r += -rs(m, i) * cos(m * ptheta - nfp * i * pphi) * pow((m * dptheta - nfp * i * dpphi), 3) - rs(m, i) * sin(m * ptheta - nfp * i * pphi) * 2 * (m * dptheta - nfp * i * dpphi) * (m * ddptheta - nfp * i * ddpphi) - rs(m, i) * sin(m * ptheta - nfp * i * pphi) * (m * dptheta - nfp * i * dpphi) * (m * ddptheta - nfp * i * ddpphi) + rs(m, i) * cos(m * ptheta - nfp * i * pphi) * (m * ddd_ptheta - nfp * i * ddd_pphi);
+                    ddd_z += zc(m, i) * sin(m * ptheta - nfp * i * pphi) * pow((m * dptheta - nfp * i * dpphi), 3) - zc(m, i) * cos(m * ptheta - nfp * i * pphi) * 2 * (m * dptheta - nfp * i * dpphi) * (m * ddptheta - nfp * i * ddpphi) - zc(m, i) * cos(m * ptheta - nfp * i * pphi) * (m * dptheta - nfp * i * dpphi) * (m * ddptheta - nfp * i * ddpphi) - zc(m, i) * sin(m * ptheta - nfp * i * pphi) * (m * ddd_ptheta - nfp * i * ddd_pphi);
+                }
+            }
+        }
+
+        data(k, 0) = ddd_r * cos(pphi) - ddr * sin(pphi) * dpphi - 2 * (ddr * sin(pphi) * dpphi) - 2 * (dr * cos(pphi) * pow(dpphi, 2)) - 2 * (dr * sin(pphi) * ddpphi) - dr * cos(pphi) * pow(dpphi, 2) + r * sin(pphi) * pow(dpphi, 3) - r * cos(pphi) * 2 * dpphi * ddpphi - dr * sin(pphi) * ddpphi - r * cos(pphi) * dpphi * ddpphi - r * sin(pphi) * ddd_pphi;
+        data(k, 1) = ddd_r * sin(pphi) + ddr * cos(pphi) * dpphi + 2 * (ddr * cos(pphi) * dpphi) - 2 * (dr * sin(pphi) * pow(dpphi, 2)) + 2 * (dr * cos(pphi) * ddpphi) - dr * sin(pphi) * pow(dpphi, 2) - r * cos(pphi) * pow(dpphi, 3) - r * sin(pphi) * 2 * dpphi * ddpphi + dr * cos(pphi) * ddpphi - r * sin(pphi) * dpphi * ddpphi + r * cos(pphi) * ddd_pphi;
+        data(k, 2) = ddd_z;
     }
 };
 
--- tests/geo/test_CurveCWS.py
+++ tests/geo/test_CurveCWS.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # filename = "../test_files/wout_circular_tokamak_reference.nc"
-# filename = "../test_files/wout_W7-X_without_coil_ripple_beta0p05_d23p4_tm_reference.nc"
+#filename = "../test_files/wout_W7-X_without_coil_ripple_beta0p05_d23p4_tm_reference.nc"
 filename = "../test_files/wout_n3are_R7.75B5.7.nc"
 
 
 s = SurfaceRZFourier.from_wout(
-    filename, range="full torus", ntheta=32, nphi=32
+    filename, range="full torus", ntheta=64, nphi=64
 )  # range = 'full torus', 'field period', 'half period'
 
 sdofs = s.get_dofs()
@@ -18,7 +18,7 @@ cws = CurveCWS(
     mpol=s.mpol,
     ntor=s.ntor,
     idofs=sdofs,
-    numquadpoints=200,
+    numquadpoints=250,
     order=1,
     nfp=s.nfp,
     stellsym=s.stellsym,
@@ -30,7 +30,7 @@ fig = plt.figure()
 #Create multiple cws curves and plot them with the surface
 ax = fig.add_subplot(projection="3d")
 for phi in phi_array:
-    cws.set_dofs([1, 0, 0, 0, 0, phi, 0, 0])
+    cws.set_dofs([0, phi, 0, 0, 1, 0, 0, 0])
     gamma = cws.gamma()
     x = gamma[:, 0]
     y = gamma[:, 1]
