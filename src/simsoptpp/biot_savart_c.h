a4f4dcbe0798a813619fc2c4a39cc061f7a1ee1a
Bharat Medasani <mbkumar@gmail.com>
2023-02-14 17:21:49 -0500

    Update to biot_savart

--- src/simsoptpp/biot_savart_c.h
+++ src/simsoptpp/biot_savart_c.h
@@ -5,4 +5,4 @@ template<class T, int derivs>
 void biot_savart_kernel(AlignedPaddedVec& pointsx, AlignedPaddedVec& pointsy, AlignedPaddedVec& pointsz, T& gamma, T& dgamma_by_dphi, T& B, T& dB_by_dX, T& d2B_by_dXdX);
 
 template<class T, int derivs>
-void biot_savart_kernel_plain(AlignedPaddedVecPortable& pointsx, AlignedPaddedVecPortable& pointsy, AlignedPaddedVecPortable& pointsz, T& gamma, T& dgamma_by_dphi, T& B, T& dB_by_dX, T& d2B_by_dXdX);
+void biot_savart_kernel(AlignedPaddedVecPortable& pointsx, AlignedPaddedVecPortable& pointsy, AlignedPaddedVecPortable& pointsz, T& gamma, T& dgamma_by_dphi, T& B, T& dB_by_dX, T& d2B_by_dXdX);
