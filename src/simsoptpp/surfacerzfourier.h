5dad414628628e07c2ec59d7fadba07cf9a14dc3
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-02-15 19:11:53 +0000

    all ready for pull request

--- src/simsoptpp/surfacerzfourier.h
+++ src/simsoptpp/surfacerzfourier.h
@@ -2,9 +2,8 @@
 
 #include "surface.h"
 
-template <class Array>
-class SurfaceRZFourier : public Surface<Array>
-{
+template<class Array>
+class SurfaceRZFourier : public Surface<Array> {
     /*
        SurfaceRZFourier is a surface that is represented in cylindrical
        coordinates using the following Fourier series:
@@ -25,107 +24,97 @@ class SurfaceRZFourier : public Surface<Array>
        the cos terms for z.
        */
 
-public:
-    using Surface<Array>::quadpoints_phi;
-    using Surface<Array>::quadpoints_theta;
-    using Surface<Array>::numquadpoints_phi;
-    using Surface<Array>::numquadpoints_theta;
-    Array rc;
-    Array rs;
-    Array zc;
-    Array zs;
-    int nfp;
-    int mpol;
-    int ntor;
-    bool stellsym;
+    public:
+        using Surface<Array>::quadpoints_phi;
+        using Surface<Array>::quadpoints_theta;
+        using Surface<Array>::numquadpoints_phi;
+        using Surface<Array>::numquadpoints_theta;
+        Array rc;
+        Array rs;
+        Array zc;
+        Array zs;
+        int nfp;
+        int mpol;
+        int ntor;
+        bool stellsym;
 
-    SurfaceRZFourier(int _mpol, int _ntor, int _nfp, bool _stellsym, vector<double> _quadpoints_phi, vector<double> _quadpoints_theta)
-        : Surface<Array>(_quadpoints_phi, _quadpoints_theta), mpol(_mpol), ntor(_ntor), nfp(_nfp), stellsym(_stellsym)
-    {
-        this->allocate();
-    }
+        SurfaceRZFourier(int _mpol, int _ntor, int _nfp, bool _stellsym, vector<double> _quadpoints_phi, vector<double> _quadpoints_theta)
+            : Surface<Array>(_quadpoints_phi, _quadpoints_theta), mpol(_mpol), ntor(_ntor), nfp(_nfp), stellsym(_stellsym) {
+                this->allocate();
+            }
 
-    void allocate()
-    {
-        rc = xt::zeros<double>({mpol + 1, 2 * ntor + 1});
-        rs = xt::zeros<double>({mpol + 1, 2 * ntor + 1});
-        zc = xt::zeros<double>({mpol + 1, 2 * ntor + 1});
-        zs = xt::zeros<double>({mpol + 1, 2 * ntor + 1});
-    }
-
-    int num_dofs() override
-    {
-        if (stellsym)
-            return 2 * (mpol + 1) * (2 * ntor + 1) - ntor - (ntor + 1);
-        else
-            return 4 * (mpol + 1) * (2 * ntor + 1) - 2 * ntor - 2 * (ntor + 1);
-    }
-
-    void set_dofs_impl(const vector<double> &dofs) override
-    {
-        int shift = (mpol + 1) * (2 * ntor + 1);
-        int counter = 0;
-        if (stellsym)
-        {
-            for (int i = ntor; i < shift; ++i)
-                rc.data()[i] = dofs[counter++];
-            for (int i = ntor + 1; i < shift; ++i)
-                zs.data()[i] = dofs[counter++];
+        void allocate() {
+            rc = xt::zeros<double>({mpol+1, 2*ntor+1});
+            rs = xt::zeros<double>({mpol+1, 2*ntor+1});
+            zc = xt::zeros<double>({mpol+1, 2*ntor+1});
+            zs = xt::zeros<double>({mpol+1, 2*ntor+1});
         }
-        else
-        {
-            for (int i = ntor; i < shift; ++i)
-                rc.data()[i] = dofs[counter++];
-            for (int i = ntor + 1; i < shift; ++i)
-                rs.data()[i] = dofs[counter++];
-            for (int i = ntor; i < shift; ++i)
-                zc.data()[i] = dofs[counter++];
-            for (int i = ntor + 1; i < shift; ++i)
-                zs.data()[i] = dofs[counter++];
-        }
-    }
 
-    vector<double> get_dofs() override
-    {
-        auto res = vector<double>(num_dofs(), 0.);
-        int shift = (mpol + 1) * (2 * ntor + 1);
-        int counter = 0;
-        if (stellsym)
-        {
-            for (int i = ntor; i < shift; ++i)
-                res[counter++] = rc.data()[i];
-            for (int i = ntor + 1; i < shift; ++i)
-                res[counter++] = zs.data()[i];
+        int num_dofs() override {
+            if(stellsym)
+                return 2*(mpol+1)*(2*ntor+1) - ntor - (ntor+1);
+            else
+                return 4*(mpol+1)*(2*ntor+1) - 2*ntor - 2*(ntor+1);
         }
-        else
-        {
-            for (int i = ntor; i < shift; ++i)
-                res[counter++] = rc.data()[i];
-            for (int i = ntor + 1; i < shift; ++i)
-                res[counter++] = rs.data()[i];
-            for (int i = ntor; i < shift; ++i)
-                res[counter++] = zc.data()[i];
-            for (int i = ntor + 1; i < shift; ++i)
-                res[counter++] = zs.data()[i];
+
+        void set_dofs_impl(const vector<double>& dofs) override {
+            int shift = (mpol+1)*(2*ntor+1);
+            int counter = 0;
+            if(stellsym) {
+                for (int i = ntor; i < shift; ++i)
+                    rc.data()[i] = dofs[counter++];
+                for (int i = ntor+1; i < shift; ++i)
+                    zs.data()[i] = dofs[counter++];
+
+            } else {
+                for (int i = ntor; i < shift; ++i)
+                    rc.data()[i] = dofs[counter++];
+                for (int i = ntor+1; i < shift; ++i)
+                    rs.data()[i] = dofs[counter++];
+                for (int i = ntor; i < shift; ++i)
+                    zc.data()[i] = dofs[counter++];
+                for (int i = ntor+1; i < shift; ++i)
+                    zs.data()[i] = dofs[counter++];
+            }
         }
-        return res;
-    }
 
-    void gamma_impl(Array &data, Array &quadpoints_phi, Array &quadpoints_theta) override;
-    void gamma_lin(Array &data, Array &quadpoints_phi, Array &quadpoints_theta) override;
-    void gammadash1_impl(Array &data) override;
-    void gammadash2_impl(Array &data) override;
-    void gammadash1dash1_impl(Array &data) override;
-    void gammadash2dash2_impl(Array &data) override;
-    void gammadash1dash2_impl(Array &data) override;
+        vector<double> get_dofs() override {
+            auto res = vector<double>(num_dofs(), 0.);
+            int shift = (mpol+1)*(2*ntor+1);
+            int counter = 0;
+            if(stellsym) {
+                for (int i = ntor; i < shift; ++i)
+                    res[counter++] = rc.data()[i];
+                for (int i = ntor+1; i < shift; ++i)
+                    res[counter++] = zs.data()[i];
+            } else {
+                for (int i = ntor; i < shift; ++i)
+                    res[counter++] = rc.data()[i];
+                for (int i = ntor+1; i < shift; ++i)
+                    res[counter++] = rs.data()[i];
+                for (int i = ntor; i < shift; ++i)
+                    res[counter++] = zc.data()[i];
+                for (int i = ntor+1; i < shift; ++i)
+                    res[counter++] = zs.data()[i];
+            }
+            return res;
+        }
 
-    void dgamma_by_dcoeff_impl(Array &data) override;
-    void dgammadash1_by_dcoeff_impl(Array &data) override;
-    void dgammadash2_by_dcoeff_impl(Array &data) override;
-    void dgammadash1dash1_by_dcoeff_impl(Array &data) override;
-    void dgammadash1dash2_by_dcoeff_impl(Array &data) override;
-    void dgammadash2dash2_by_dcoeff_impl(Array &data) override;
-    Array dgamma_by_dcoeff_vjp(Array &v) override;
-    Array dgammadash1_by_dcoeff_vjp(Array &v) override;
-    Array dgammadash2_by_dcoeff_vjp(Array &v) override;
+        void gamma_impl(Array& data, Array& quadpoints_phi, Array& quadpoints_theta) override;
+        void gamma_lin(Array& data, Array& quadpoints_phi, Array& quadpoints_theta) override;
+        void gammadash1_impl(Array& data) override;
+        void gammadash2_impl(Array& data) override;
+        void gammadash1dash1_impl(Array& data) override;
+        void gammadash2dash2_impl(Array& data) override;
+        void gammadash1dash2_impl(Array& data) override;
+
+        void dgamma_by_dcoeff_impl(Array& data) override;
+        void dgammadash1_by_dcoeff_impl(Array& data) override;
+        void dgammadash2_by_dcoeff_impl(Array& data) override;
+        void dgammadash1dash1_by_dcoeff_impl(Array& data) override;
+        void dgammadash1dash2_by_dcoeff_impl(Array& data) override;
+        void dgammadash2dash2_by_dcoeff_impl(Array& data) override;
+        Array dgamma_by_dcoeff_vjp(Array& v) override;
+        Array dgammadash1_by_dcoeff_vjp(Array& v) override;
+        Array dgammadash2_by_dcoeff_vjp(Array& v) override;
 };
