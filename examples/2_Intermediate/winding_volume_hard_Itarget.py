Working on getting the analytic and projected gradient descent methods both correct and comparable. Pseudoinverse for the constraint matrix inversion for the projection operator appears to work okay. Inverse appears crucial to use instead of pseudoinverse for the inversion in the H matrix -- otherwise doesnt give me the correct solution with respect to the constraints. Another minor mystery was that I tried to shift the definition of the polynomial basis to be grid-centered and normalized, and this immediately broke the constraints, such that it would ignore the current target constraint. Will sideline this issue for now and focus on getting the two algorithms to agree (at least at finite tikhonov regularization and no l0 included). Unclear if can get away with a pseudoinverse during calculation of CHCT_inv.

examples/2_Intermediate/winding_volume_hard_Itarget.py
@@ -14,10 +14,6 @@ import os
 #from matplotlib import pyplot as plt
 from pathlib import Path
 import numpy as np
-from scipy.sparse import csc_matrix
-from scipy.sparse import vstack, hstack
-from scipy.sparse import eye as sparse_eye
-from scipy.sparse.linalg import inv as sparse_inv
 # from sksparse.cholmod import cholesky
 import simsoptpp as sopp
 from simsopt.geo import SurfaceRZFourier, Curve, CurveRZFourier, curves_to_vtk
@@ -40,9 +36,9 @@ ntheta = 8
 poff = 0.3  # grid end offset ~ 10 cm from the plasma surface
 coff = 0.1  # grid starts offset ~ 5 cm from the plasma surface
 input_name = 'input.LandremanPaul2021_QA'
-lam = 1e-22
+lam = 1e-20
 l0_threshold = 0.0
-nu = 1e10
+nu = 1e100
 
 # Read in the plasma equilibrium file
 TEST_DIR = (Path(__file__).parent / ".." / ".." / "tests" / "test_files").resolve()
@@ -69,190 +65,153 @@ print('First setup took time = ', t2 - t1, ' s')
 fB_all = []
 fI_all = []
 fK_all = []
-nx_scan = [6]  # np.arange(1, 6, 1)
-Nx_scan = [6]
-for nx_i, nx in enumerate(nx_scan):
-    print('Iteration = ', nx_i, ', nx = ', nx)
-    for Nx_i, Nx in enumerate(Nx_scan):
-        print('Iteration = ', Nx_i, ', Nx = ', Nx)
-        Ny = Nx
-        Nz = Nx  # - 1
-        # Define a curve to define a Itarget loss term
-        # Make circle at Z = 0
-        t1 = time.time()
-        numquadpoints = nphi * s.nfp * 2  # * 5
-        order = 20
-        curve = CurveRZFourier(numquadpoints, order, nfp=1, stellsym=False)
-        for m in range(s.mpol + 1):
-            if m == 0:
-                nmin = 0
-            else: 
-                nmin = -s.ntor
-            for n in range(nmin, s.ntor + 1):
-                curve.rc[s.nfp * int(abs(n))] += s.get_rc(m, n)
-                curve.zs[s.nfp * int(abs(n))] += s.get_zs(m, n) * np.sign(n)
+nx = 6
+Nx = 8
+Ny = Nx
+Nz = Nx  # - 1
+# Define a curve to define a Itarget loss term
+# Make circle at Z = 0
+t1 = time.time()
+numquadpoints = nphi * s.nfp * 2  # * 5
+order = 20
+curve = CurveRZFourier(numquadpoints, order, nfp=1, stellsym=False)
+for m in range(s.mpol + 1):
+    if m == 0:
+        nmin = 0
+    else: 
+        nmin = -s.ntor
+    for n in range(nmin, s.ntor + 1):
+        curve.rc[s.nfp * int(abs(n))] += s.get_rc(m, n)
+        curve.zs[s.nfp * int(abs(n))] += s.get_zs(m, n) * np.sign(n)
 
-        curve.x = curve.get_dofs()
-        curve.x = curve.x  # need to do this to transfer data to C++
-        # curves_to_vtk([curve], OUT_DIR + f"Itarget_curve")
-        Itarget = 0.5e6
-        t2 = time.time()
-        print('Curve initialization took time = ', t2 - t1, ' s')
+curve.x = curve.get_dofs()
+curve.x = curve.x  # need to do this to transfer data to C++
+# curves_to_vtk([curve], OUT_DIR + f"Itarget_curve")
+Itarget = 0.5e6
+t2 = time.time()
+print('Curve initialization took time = ', t2 - t1, ' s')
 
-        # Finally, initialize the winding volume 
-        t1 = time.time()
-        wv_grid = WindingVolumeGrid(
-            s, Itarget_curve=curve, Itarget=Itarget, 
-            coil_offset=coff, 
-            Nx=Nx, Ny=Ny, Nz=Nz, 
-            plasma_offset=poff,
-            Bn=Bnormal,
-            Bn_Itarget=np.zeros(curve.gammadash().reshape(-1, 3).shape[0]),
-            filename=surface_filename,
-            surface_flag='vmec',
-            OUT_DIR=OUT_DIR,
-            # coil_range="full torus",
-            nx=nx, ny=nx, nz=nx
-        )
-        t2 = time.time()
-        print('WV grid initialization took time = ', t2 - t1, ' s')
+# Finally, initialize the winding volume 
+t1 = time.time()
+wv_grid = WindingVolumeGrid(
+    s, Itarget_curve=curve, Itarget=Itarget, 
+    coil_offset=coff, 
+    Nx=Nx, Ny=Ny, Nz=Nz, 
+    plasma_offset=poff,
+    Bn=Bnormal,
+    Bn_Itarget=np.zeros(curve.gammadash().reshape(-1, 3).shape[0]),
+    filename=surface_filename,
+    surface_flag='vmec',
+    OUT_DIR=OUT_DIR,
+    # coil_range="full torus",
+    nx=nx, ny=nx, nz=nx,
+    sparse_constraint_matrix=False,
+)
+t2 = time.time()
+print('WV grid initialization took time = ', t2 - t1, ' s')
 
-        t1 = time.time()
-        C = wv_grid.flux_constraint_matrix  # matrix is way too big but it is very sparse
-        # Need to append Itarget constraint to the flux jump constraints
-        print(C.shape, wv_grid.Itarget_matrix.shape)
-        # C = vstack([C, np.copy(wv_grid.Itarget_matrix)], format="csc")
-        # print(C.shape, wv_grid.Itarget_rhs.shape)
-        # d = np.hstack([np.zeros(C.shape[0] - 1), np.copy(wv_grid.Itarget_rhs)])  # , format="csc")
-        # print(C.todense(), d)
+# acceleration = True
+max_iter = 100
+# cpp = True
+rs_max_iter = 100
 
-        # CT = C.transpose()
-        # C_inv = np.linalg.pinv(C.todense())
-        # CCT = C @ CT
-        # for i in range(C.shape[0]):
-        #     print(C[i, :])
-        q = 0
-        for i in [0]:  # range(C.shape[1]):
-            for j in range(i, C.shape[1]):
-                if i != j:
-                    inner_product = np.inner(
-                        C[:, i],
-                        C[:, j]
-                    )
-                    norm_i = np.linalg.norm(C[:, i])
-                    norm_j = np.linalg.norm(C[:, j])
+# alpha_opt, fB, fK, fI, fRS, f0 = relax_and_split(wv_grid, lam=lam, nu=nu, P=projection_onto_constraints, max_iter=max_iter, l0_threshold=l0_threshold, rs_max_iter=rs_max_iter)
+l0_thresholds = [l0_threshold]  # np.linspace(l0_threshold, 10 * l0_threshold, 10)
+alpha_opt, fB, fK, fI, fRS, f0 = relax_and_split_analytic(
+    wv_grid, lam=lam, nu=nu, 
+    max_iter=max_iter, 
+    l0_thresholds=l0_thresholds, 
+    rs_max_iter=rs_max_iter
+)
 
-                    if inner_product != 0.0 and np.abs(inner_product - norm_j * norm_i) < 1E-5:
-                        print(i, j)
-                        print('I: ', C[:, i])
-                        print('J: ', C[:, j])
-                        print('Prod: ', inner_product)
-                        print('Norm i: ', norm_i)
-                        print('Norm j: ', norm_j)
-                        print('norm_i * norm_j = ', norm_i * norm_j)
-                        print(np.abs(inner_product - norm_j * norm_i))
-                    if np.abs(inner_product - norm_j * norm_i) < 1E-5:
+# print('alpha_opt = ', alpha_opt)
+if wv_grid.P is not None:
+    # print('P * alpha_opt - alpha_opt = ', projection_onto_constraints.dot(alpha_opt) - alpha_opt)
+    # print('||P * alpha_opt - alpha_opt|| / ||alpha_opt|| = ', np.linalg.norm(wv_grid.P.dot(alpha_opt) - alpha_opt) / np.linalg.norm(alpha_opt))
+    print('C * alpha - d = ', np.linalg.norm(wv_grid.C.dot(alpha_opt) - wv_grid.d))
+    print('C * alpha = ', np.linalg.norm(wv_grid.C.dot(alpha_opt)))
+t2 = time.time()
+print('Gradient Descent Tikhonov solve time = ', t2 - t1, ' s')
+plt.figure()
+plt.semilogy(fB, label=r'$f_B$')
+plt.semilogy(lam * fK, label=r'$\lambda \|\alpha\|^2$')
+plt.semilogy(fI, label=r'$f_I$')
+if l0_threshold > 0:
+    plt.semilogy(fRS, label=r'$\nu^{-1} \|\alpha - w\|^2$')
+    # plt.semilogy(f0, label=r'$\|\alpha\|_0^G$')
+plt.semilogy(fB + fI + lam * fK + fRS, label='Total objective (not incl. l0)')
+plt.grid(True)
+plt.legend()
+plt.savefig(OUT_DIR + 'optimization_progress.jpg')
+t1 = time.time()
+wv_grid._toVTK(OUT_DIR + 'grid_after_Tikhonov_solve')
+t2 = time.time()
+print('Time to plot the optimized grid = ', t2 - t1, ' s')
+print('fB after optimization = ', fB[-1]) 
+print('fB check = ', 0.5 * np.linalg.norm(wv_grid.B_matrix @ alpha_opt - wv_grid.b_rhs) ** 2 * s.nfp * 2)
 
-                        print('Dependent')
-                        print(q)
-                        q += 1
-                    # else:
-                    #     print('Independent')
-        print(C[0:6, :])
-        S = np.linalg.svd(C.T, compute_uv=False)
-        plt.semilogy(S, 'ro')
-        plt.show()
+# set up WindingVolume Bfield
+bs_wv = WindingVolumeField(wv_grid.J, wv_grid.XYZ_integration, wv_grid.grid_scaling, wv_grid.coil_range)
+t1 = time.time()
+bs_wv.set_points(s.gamma().reshape((-1, 3)))
+Bnormal_wv = np.sum(bs_wv.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
+normN = np.linalg.norm(s.normal().reshape(-1, 3), axis=-1)
+contig = np.ascontiguousarray
+print('fB direct = ', np.sum(normN * np.ravel(Bnormal_wv + Bnormal) ** 2) * 0.5 / (nphi * ntheta) * s.nfp * 2)
+print(nphi, ntheta, wv_grid.dx, wv_grid.nx)
 
-        # CCT += 1e-16 * sparse_eye(CCT.shape[0], format="csc")
-        CCT_inv = sparse_inv(CCT)
-        projection_onto_constraints = sparse_eye(wv_grid.N_grid * wv_grid.n_functions, format="csc", dtype="double") - CT @ CCT_inv @ C 
-        t2 = time.time()
-        print('Time to make CCT_inv = ', t2 - t1, ' s')
+t2 = time.time()
+print('Time to compute Bnormal_wv = ', t2 - t1, ' s')
+fB_direct = SquaredFlux(s, bs_wv, -Bnormal).J() * 2 * s.nfp
+print('fB_direct = ', fB_direct)
 
-        nfp = wv_grid.plasma_boundary.nfp
-        print('fB initial = ', 0.5 * np.linalg.norm(wv_grid.B_matrix @ wv_grid.alphas - wv_grid.b_rhs) ** 2 * nfp)
-        t1 = time.time()
-        # acceleration = True
-        max_iter = 100
-        # cpp = True
-        rs_max_iter = 100
-        alpha0 = (C_inv @ d).T
-        print(C.dot(alpha0)[-1])
-        wv_grid.C = C
-        wv_grid.d = d
+t1 = time.time()
+make_Bnormal_plots(bs_wv, s_plot, OUT_DIR, "biot_savart_winding_volume")
+t2 = time.time()
+print('Time to plot Bnormal_wv = ', t2 - t1, ' s')
+# make_Bnormal_plots(bs + bs_wv, s, OUT_DIR, "biot_savart_total")
 
-        # alpha_opt, fB, fK, fI, fRS, f0 = relax_and_split(wv_grid, lam=lam, nu=nu, P=projection_onto_constraints, max_iter=max_iter, l0_threshold=l0_threshold, rs_max_iter=rs_max_iter)
-        l0_thresholds = [l0_threshold]  # np.linspace(l0_threshold, 10 * l0_threshold, 10)
-        alpha_opt, fB, fK, fI, fRS, f0 = relax_and_split_analytic(
-            wv_grid, lam=lam, nu=nu, 
-            alpha0=alpha0,
-            P=projection_onto_constraints, 
-            max_iter=max_iter, 
-            l0_thresholds=l0_thresholds, 
-            rs_max_iter=rs_max_iter
-        )
+# t1 = time.time()
+# wv_grid.check_fluxes()
+# t2 = time.time()
+# print('Time to check all the flux constraints = ', t2 - t1, ' s')
 
-        # print('alpha_opt = ', alpha_opt)
-        if projection_onto_constraints is not None:
-            # print('P * alpha_opt - alpha_opt = ', projection_onto_constraints.dot(alpha_opt) - alpha_opt)
-            print('||P * alpha_opt - alpha_opt|| / ||alpha_opt|| = ', np.linalg.norm(projection_onto_constraints.dot(alpha_opt) - alpha_opt) / np.linalg.norm(alpha_opt))
-        t2 = time.time()
-        print('Gradient Descent Tikhonov solve time = ', t2 - t1, ' s')
-        plt.figure()
-        plt.semilogy(fB, label=r'$f_B$')
-        plt.semilogy(lam * fK, label=r'$\lambda \|\alpha\|^2$')
-        plt.semilogy(fI, label=r'$f_I$')
-        if l0_threshold > 0:
-            plt.semilogy(fRS, label=r'$\nu^{-1} \|\alpha - w\|^2$')
-            # plt.semilogy(f0, label=r'$\|\alpha\|_0^G$')
-        plt.semilogy(fB + fI + lam * fK + fRS, label='Total objective (not incl. l0)')
-        plt.grid(True)
-        plt.legend()
-        plt.savefig(OUT_DIR + 'optimization_progress.jpg')
-        t1 = time.time()
-        wv_grid._toVTK(OUT_DIR + 'grid_after_Tikhonov_solve')
-        t2 = time.time()
-        print('Time to plot the optimized grid = ', t2 - t1, ' s')
-        print('fB after optimization = ', fB[-1]) 
-        print('fB check = ', 0.5 * np.linalg.norm(wv_grid.B_matrix @ alpha_opt - wv_grid.b_rhs) ** 2 * nfp * 2)
+t1 = time.time()
+# biotsavart_json_str = bs_wv.save(filename=OUT_DIR + 'BiotSavart.json')
+# bs_wv.set_points(s.gamma().reshape((-1, 3)))
+# trace_fieldlines(bs_wv, 'poincare_qa', 'qa', s_plot, comm, OUT_DIR)
+t2 = time.time()
+print(OUT_DIR)
 
-        # set up WindingVolume Bfield
-        bs_wv = WindingVolumeField(wv_grid.J, wv_grid.XYZ_integration, wv_grid.grid_scaling, wv_grid.coil_range)
-        t1 = time.time()
-        bs_wv.set_points(s.gamma().reshape((-1, 3)))
-        Bnormal_wv = np.sum(bs_wv.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
-        normN = np.linalg.norm(s.normal().reshape(-1, 3), axis=-1)
-        contig = np.ascontiguousarray
-        print('fB direct = ', np.sum(normN * np.ravel(Bnormal_wv + Bnormal) ** 2) * 0.5 / (nphi * ntheta) * s.nfp * 2)
-        print(nphi, ntheta, wv_grid.dx, wv_grid.nx)
+alpha_opt, fB, fK, fI, fRS, f0 = relax_and_split_increasingl0(
+    wv_grid, lam=lam, nu=nu, 
+    max_iter=max_iter, 
+    l0_thresholds=l0_thresholds, 
+    rs_max_iter=1
+)
 
-        t2 = time.time()
-        print('Time to compute Bnormal_wv = ', t2 - t1, ' s')
-        fB_direct = SquaredFlux(s, bs_wv, -Bnormal).J() * 2 * s.nfp
-        print('fB_direct = ', fB_direct)
+if wv_grid.P is not None:
+    # print('P * alpha_opt - alpha_opt = ', projection_onto_constraints.dot(alpha_opt) - alpha_opt)
+    # print('||P * alpha_opt - alpha_opt|| / ||alpha_opt|| = ', np.linalg.norm(wv_grid.P.dot(alpha_opt) - alpha_opt) / np.linalg.norm(alpha_opt))
+    print('C * alpha - d = ', np.linalg.norm(wv_grid.C.dot(alpha_opt) - wv_grid.d))
+t2 = time.time()
+print('Gradient Descent Tikhonov solve time = ', t2 - t1, ' s')
+plt.figure(10)
+plt.semilogy(fB, label=r'$f_B$')
+plt.semilogy(lam * fK, label=r'$\lambda \|\alpha\|^2$')
+plt.semilogy(fI, label=r'$f_I$')
+if l0_threshold > 0:
+    plt.semilogy(fRS, label=r'$\nu^{-1} \|\alpha - w\|^2$')
+    # plt.semilogy(f0, label=r'$\|\alpha\|_0^G$')
+plt.semilogy(fB + fI + lam * fK + fRS, label='Total objective (not incl. l0)')
+plt.grid(True)
+plt.legend()
+plt.savefig(OUT_DIR + 'optimization_progress_proj_grad_descent.jpg')
+print('fB after optimization = ', fB[-1]) 
+print('fB check = ', 0.5 * np.linalg.norm(wv_grid.B_matrix @ alpha_opt - wv_grid.b_rhs) ** 2 * s.nfp * 2)
 
-        t1 = time.time()
-        make_Bnormal_plots(bs_wv, s_plot, OUT_DIR, "biot_savart_winding_volume")
-        t2 = time.time()
-        print('Time to plot Bnormal_wv = ', t2 - t1, ' s')
-        # make_Bnormal_plots(bs + bs_wv, s, OUT_DIR, "biot_savart_total")
+t_end = time.time()
+print('Total time = ', t_end - t_start)
 
-        # t1 = time.time()
-        # wv_grid.check_fluxes()
-        # t2 = time.time()
-        # print('Time to check all the flux constraints = ', t2 - t1, ' s')
 
-        t1 = time.time()
-        # biotsavart_json_str = bs_wv.save(filename=OUT_DIR + 'BiotSavart.json')
-        # bs_wv.set_points(s.gamma().reshape((-1, 3)))
-        # trace_fieldlines(bs_wv, 'poincare_qa', 'qa', s_plot, comm, OUT_DIR)
-        t2 = time.time()
-        print(OUT_DIR)
-        fB_all.append(fB_direct)
-        fI_all.append(fI[-1])
-        fK_all.append(lam * fK[-1])
-
-        t_end = time.time()
-        print('Total time = ', t_end - t_start)
 plt.show()
-print(fB_all, fI_all, fK_all)

src/simsopt/geo/winding_volume_grid.py
@@ -5,6 +5,10 @@ import simsoptpp as sopp
 import time
 import warnings
 from scipy.sparse import lil_matrix
+from scipy.sparse import csc_matrix
+from scipy.sparse import vstack, hstack
+from scipy.sparse import eye as sparse_eye
+from scipy.sparse.linalg import inv as sparse_inv
 from matplotlib import pyplot as plt
 from .._core.json import GSONDecoder, GSONable
 # from .._core.optimizable import Optimizable
@@ -65,20 +69,8 @@ class WindingVolumeGrid:
         famus_filename=None, 
         coil_range='half period',
         OUT_DIR='', nx=2, ny=2, nz=2,
+        sparse_constraint_matrix=True,
     ):
-        # super().__init__(
-        #         # depends_on=[
-        #         #     plasma_boundary, Itarget_curve, Bn_Itarget, Itarget,
-        #         #     rz_inner_surface,
-        #         #     rz_outer_surface, plasma_offset,
-        #         #     coil_offset, Bn,
-        #         #     Nx, Ny, Nz,
-        #         #     filename, surface_flag,
-        #         #     famus_filename, 
-        #         #     coil_range,
-        #         #     OUT_DIR, nx, ny, nz
-        #         # ]
-        #     )
         if plasma_offset <= 0 or coil_offset <= 0:
             raise ValueError('permanent magnets must be offset from the plasma')
 
@@ -103,6 +95,7 @@ class WindingVolumeGrid:
         self.Nz = Nz
         self.OUT_DIR = OUT_DIR
         self.n_functions = 11  # hard-coded for linear basis
+        self.sparse_constraint_matrix = sparse_constraint_matrix
 
         if not isinstance(plasma_boundary, SurfaceRZFourier):
             raise ValueError(
@@ -497,21 +490,24 @@ class WindingVolumeGrid:
         yrange = np.zeros((n, ny))
         zrange = np.zeros((n, nz))
         for i in range(n):
-            xrange[i, :] = np.linspace(
+            x_midpoint = (x_leftpoints[i] + dx / 2.0)
+            xrange[i, :] = (np.linspace(
                 x_leftpoints[i], 
                 x_leftpoints[i] + dx,
                 nx
-            )
-            yrange[i, :] = np.linspace(
+            ))  # - x_midpoint) / np.sqrt(dx * dy * dz)
+            y_midpoint = (y_leftpoints[i] + dy / 2.0)
+            yrange[i, :] = (np.linspace(
                 y_leftpoints[i], 
                 y_leftpoints[i] + dy,
                 ny
-            )
-            zrange[i, :] = np.linspace(
+            ))  # - y_midpoint) / np.sqrt(dx * dy * dz)
+            z_midpoint = (z_leftpoints[i] + dz / 2.0)
+            zrange[i, :] = (np.linspace(
                 z_leftpoints[i], 
                 z_leftpoints[i] + dz,
                 nz
-            )
+            ))  # - z_midpoint) / np.sqrt(dx * dy * dz)
         Phi = np.zeros((self.n_functions, n, nx, ny, nz, 3)) 
         zeros = np.zeros(n)
         ones = np.ones(n)
@@ -536,7 +532,6 @@ class WindingVolumeGrid:
             dJz_dz = -(Phi[i, :, :, :, 1:, 2] - Phi[i, :, :, :, :-1, 2]) / dz
             divJ = dJx_dx[:, :, :-1, :-1] + dJy_dy[:, :-1, :, :-1] + dJz_dz[:, :-1, :-1, :]
             divJ = np.sum(np.sum(np.sum(divJ, axis=1), axis=1), axis=1)
-            print(i, divJ)
             assert np.allclose(divJ, 0.0)
         self.Phi = Phi.reshape(self.n_functions, n, nx * ny * nz, 3)
 
@@ -669,7 +664,7 @@ class WindingVolumeGrid:
         B_matrix = (self.geo_factor * np.sqrt(N_quadrature_inv) * coil_integration_factor).reshape(
             self.geo_factor.shape[0], self.N_grid * self.n_functions
         )
-        print(B_matrix.shape, self.geo_factor.shape)
+        # print(B_matrix.shape, self.geo_factor.shape)
         b_rhs = np.ravel(self.Bn * np.sqrt(N_quadrature_inv))
         for i in range(B_matrix.shape[0]):
             B_matrix[i, :] *= np.sqrt(normN[i])
@@ -772,35 +767,31 @@ class WindingVolumeGrid:
         print('Number of constraints = ', n_constraints, ', 6N = ', 6 * self.N_grid)
 
         num_basis = self.n_functions
-        flux_constraint_matrix = np.zeros((n_constraints, self.N_grid * num_basis))
+        N = self.N_grid * num_basis
 
-        # flux_constraint_matrix = lil_matrix((n_constraints, self.N_grid * num_basis), dtype="double")
+        if self.sparse_constraint_matrix:
+            flux_constraint_matrix = lil_matrix((n_constraints, N), dtype="double")
+        else:
+            flux_constraint_matrix = np.zeros((n_constraints, N))
+
         i_constraint = 0
         q = 0
         qq = 0
         print('Ngrid = ', self.N_grid)
         for i in range(self.N_grid):        
-            print(self.connection_list[i, :, :])
             ind = np.ravel(np.where(self.connection_list[i, :, 0] >= 0))
             k_ind = self.connection_list[i, ind, 0]
-            # print(coil_points[i, :], coil_points[k_ind, :], self.connection_list[i, :, 0])
             ind = np.ravel(np.where(self.connection_list[i, :, 1] >= 0))
             k_ind = self.connection_list[i, ind, 1]
-            # print(coil_points[i, :], coil_points[k_ind, :], self.connection_list[i, :, 1])
             ind = np.ravel(np.where(self.connection_list[i, :, 2] >= 0))
             k_ind = self.connection_list[i, ind, 2]
-            # print(coil_points[i, :], coil_points[k_ind, :], self.connection_list[i, :, 2], k_ind)
             ind = np.ravel(np.where(self.connection_list[i, :, 3] >= 0))
             k_ind = self.connection_list[i, ind, 3]
-            # print(coil_points[i, :], coil_points[k_ind, :], self.connection_list[i, :, 3])
             ind = np.ravel(np.where(self.connection_list[i, :, 4] >= 0))
             k_ind = self.connection_list[i, ind, 4]
-            # print(coil_points[i, :], coil_points[k_ind, :], self.connection_list[i, :, 4])
             ind = np.ravel(np.where(self.connection_list[i, :, 5] >= 0))
             k_ind = self.connection_list[i, ind, 5]
-            # print(coil_points[i, :], coil_points[k_ind, :], self.connection_list[i, :, 5])
             # Loop through every cell and check the cell in + nx, + ny, or + nz direction
-            print(i, i_constraint)
             if np.any(self.connection_list[i, :, 0] >= 0):
                 ind = np.ravel(np.where(self.connection_list[i, :, 0] >= 0))[0]
                 k_ind = self.connection_list[i, ind, 0]
@@ -811,7 +802,6 @@ class WindingVolumeGrid:
                                        k_ind * num_basis:(k_ind + 1) * num_basis
                                        ] = flux_factor[1, k_ind, :]
                 i_constraint += 1
-            print(i, i_constraint)
             if np.any(self.connection_list[i, :, 2] >= 0):
                 ind = np.ravel(np.where(self.connection_list[i, :, 2] >= 0))[0]
                 k_ind = self.connection_list[i, ind, 2]
@@ -822,7 +812,6 @@ class WindingVolumeGrid:
                                        k_ind * num_basis:(k_ind + 1) * num_basis
                                        ] = flux_factor[3, k_ind, :]
                 i_constraint += 1
-            print(i, i_constraint)
             if np.any(self.connection_list[i, :, 4] >= 0):
                 ind = np.ravel(np.where(self.connection_list[i, :, 4] >= 0))[0]
                 k_ind = self.connection_list[i, ind, 4]
@@ -833,7 +822,6 @@ class WindingVolumeGrid:
                                        k_ind * num_basis:(k_ind + 1) * num_basis
                                        ] = flux_factor[5, k_ind, :]
                 i_constraint += 1
-            print(i, i_constraint)
 
             # Loop through cells and check if does not have a cell in the - nx, - ny, or - nz direction 
             # Special case for Nfp = 2, -x direction and -y direction 
@@ -857,7 +845,6 @@ class WindingVolumeGrid:
                                            i * num_basis:(i + 1) * num_basis
                                            ] = flux_factor[1, i, :]
                 i_constraint += 1
-            print(i, i_constraint)
             if np.all(self.connection_list[i, :, 3] < 0):
                 # ind = np.ravel(np.where(self.connection_list[i, :, 2] >= 0))
                 if (i in y_inds) and (self.coil_range != 'full torus'):
@@ -878,32 +865,27 @@ class WindingVolumeGrid:
                                            i * num_basis:(i + 1) * num_basis
                                            ] = flux_factor[3, i, :]
                 i_constraint += 1
-            print(i, i_constraint)
             if np.all(self.connection_list[i, :, 5] < 0):
                 flux_constraint_matrix[i_constraint, 
                                        i * num_basis:(i + 1) * num_basis
                                        ] = flux_factor[5, i, :]
                 i_constraint += 1
-            print(i, i_constraint)
             # Loop through cells and check if does not have a cell in the + nx, + ny, or + nz direction 
             if np.all(self.connection_list[i, :, 0] < 0):
                 flux_constraint_matrix[i_constraint, 
                                        i * num_basis:(i + 1) * num_basis
                                        ] = flux_factor[0, i, :]
                 i_constraint += 1
-            print(i, i_constraint)
             if np.all(self.connection_list[i, :, 2] < 0):
                 flux_constraint_matrix[i_constraint, 
                                        i * num_basis:(i + 1) * num_basis
                                        ] = flux_factor[2, i, :]
                 i_constraint += 1
-            print(i, i_constraint)
             if np.all(self.connection_list[i, :, 4] < 0):
                 flux_constraint_matrix[i_constraint, 
                                        i * num_basis:(i + 1) * num_basis
                                        ] = flux_factor[4, i, :]
                 i_constraint += 1
-            print(i, i_constraint)
 
         connect_list_zeros = np.copy(self.connection_list)
         connect_list_zeros[connect_list_zeros >= 0] = 1
@@ -912,76 +894,47 @@ class WindingVolumeGrid:
         # print(self.num_constraints_per_cell)
         self.flux_factor = flux_factor
 
-        # Once matrix elements are set, convert to CSC for quicker matrix ops
-        # self.flux_constraint_matrix = flux_constraint_matrix.tocsc()
-        #flux_constraint_matrix = flux_constraint_matrix.todense()
-        self.flux_constraint_matrix = flux_constraint_matrix
-        # CCT = flux_constraint_matrix @ flux_constraint_matrix.T
-        # print(CCT)
-        # S_C = np.linalg.svd(flux_constraint_matrix, compute_uv=False)
-        # S_CCT = np.linalg.svd(CCT, compute_uv=False)
-        # plt.semilogy(S_C)
-        # plt.semilogy(S_CCT)
-        # plt.grid(True)
-        # plt.show()
-        # print(CCT.shape[0], np.linalg.matrix_rank(CCT))
         t2 = time.time()
         print('Time to make the flux jump constraint matrix = ', t2 - t1, ' s')
+        t1 = time.time()
 
-    # def as_dict(self, serial_objs_dict) -> dict:
-    #     d = super().as_dict(serial_objs_dict=serial_objs_dict)
-    #     d["plasma_boundary"] = self.plasma_boundary
-    #     d["Bn_Itarget"] = self.Bn_Itarget
-    #     d["Itarget"] = self.Itarget
-    #     d["Itarget_curve"] = self.Itarget_curve
-    #     d["rz_inner_surface"] = self.rz_inner_surface
-    #     d["rz_outer_surface"] = self.rz_outer_surface
-    #     d["plasma_offset"] = self.plasma_offset
-    #     d["coil_offset"] = self.coil_offset
-    #     d["Bn"] = self.Bn
-    #     d["Nx"] = self.Nx
-    #     d["Ny"] = self.Ny
-    #     d["Nz"] = self.Nz
-    #     d["filename"] = self.filename
-    #     d["surface_flag"] = self.surface_flag
-    #     d["famus_filename"] = self.famus_filename
-    #     d["coil_range"] = self.coil_range
-    #     d["OUT_DIR"] = self.OUT_DIR
-    #     d["nx"] = self.nx
-    #     d["ny"] = self.ny
-    #     d["nz"] = self.nz
-    #     return d
+        print(self.Itarget_matrix, self.Itarget_matrix.shape)
+        if self.sparse_constraint_matrix:
+            C = flux_constraint_matrix.tocsc()
+            C = vstack([C, np.copy(self.Itarget_matrix)], format="csc")
+            CT = C.transpose()
+            C_inv = np.linalg.pinv(C.todense())
+            d = hstack([np.zeros(C.shape[0] - 1), np.copy(self.Itarget_rhs)], format="csc")
+            CCT = C @ CT
+            CCT_inv = np.linalg.pinv(CCT.todense(), rcond=1e-8)
+            projection_onto_constraints = sparse_eye(N, format="csc", dtype="double") - CT @ CCT_inv @ C 
+        else:
+            C = flux_constraint_matrix
+            C = np.vstack((C, np.copy(self.Itarget_matrix)))
+            CT = C.transpose()
+            C_inv = np.linalg.pinv(C, rcond=1e-8)
+            d = np.hstack([np.zeros(C.shape[0] - 1), np.copy(self.Itarget_rhs)])
+            CCT = C @ CT
+            CCT_inv = np.linalg.pinv(CCT, rcond=1e-8)
+            projection_onto_constraints = sparse_eye(N, format="csc", dtype="double") - CT @ CCT_inv @ C 
+        # print(C.shape, d.shape)
+        # print('C = ', C)
 
-    # @classmethod
-    # def from_dict(cls, d, serial_objs_dict, recon_objs):
-    #     decoder = GSONDecoder()
-    #     plasma_surface = decoder.process_decoded(d["plasma_boundary"], serial_objs_dict, recon_objs)
-    #     rz_inner_surface = decoder.process_decoded(d["rz_inner_surface"], serial_objs_dict, recon_objs)
-    #     rz_outer_surface = decoder.process_decoded(d["rz_outer_surface"], serial_objs_dict, recon_objs)
-    #     Itarget_curve = decoder.process_decoded(d["Itarget_curve"], serial_objs_dict, recon_objs)
-    #     field = cls(
-    #         plasma_surface,
-    #         Itarget_curve,
-    #         d["Bn_Itarget"],
-    #         d["Itarget"],
-    #         rz_inner_surface,
-    #         rz_outer_surface,
-    #         d["plasma_offset"],
-    #         d["coil_offset"],
-    #         d["Bn"],
-    #         d["Nx"],
-    #         d["Ny"],
-    #         d["Nz"],
-    #         d["filename"],
-    #         d["surface_flag"],
-    #         d["famus_filename"],
-    #         d["coil_range"],
-    #         d["OUT_DIR"],
-    #         d["nx"],
-    #         d["ny"],
-    #         d["nz"],
-    #     )
-    #     return field
+        t2 = time.time()
+        # print('d = ', d)
+        print('Time to make CCT_inv = ', t2 - t1, ' s')
+
+        S = np.linalg.svd(C, compute_uv=False)
+        plt.semilogy(S, 'ro')
+        S2 = np.linalg.svd(CCT, compute_uv=False)
+        plt.semilogy(S2, 'bo')
+        plt.show()
+
+        self.C = C
+        self.d = d
+        self.alpha0 = (C_inv @ d)
+        print('alpha0 = ', self.alpha0, self.alpha0.shape, C @ self.alpha0)
+        self.P = projection_onto_constraints
 
     def check_fluxes(self):
         """

src/simsopt/solve/winding_volume_optimization.py
@@ -1,6 +1,7 @@
 import numpy as np
 from simsoptpp import acc_prox_grad_descent, matmult, matmult_sparseA
 import time
+from matplotlib import pyplot as plt
 # from scipy.linalg import svd
 
 __all__ = ['projected_gradient_descent_Tikhonov', 'relax_and_split', 'relax_and_split_increasingl0', 'relax_and_split_analytic']
@@ -351,8 +352,7 @@ def relax_and_split(winding_volume, lam=0.0, nu=1e20, alpha0=None, max_iter=5000
 
 
 def relax_and_split_increasingl0(
-        winding_volume, lam=0.0, nu=1e20, 
-        alpha0=None, max_iter=5000, P=None, 
+        winding_volume, lam=0.0, nu=1e20, max_iter=5000,
         rs_max_iter=1, l0_thresholds=[0.0], print_iter=1):
     """
         This function performs a relax-and-split algorithm for solving the 
@@ -368,6 +368,9 @@ def relax_and_split_increasingl0(
                 so is assumed to be stored as a scipy csc_matrix. 
     """
     t1 = time.time()
+    P = winding_volume.P
+    # alpha0 = winding_volume.alpha0
+    alpha0 = None
     n = winding_volume.N_grid
     nfp = winding_volume.plasma_boundary.nfp
     num_basis = winding_volume.n_functions
@@ -436,7 +439,7 @@ def relax_and_split_increasingl0(
     f_0 = []
     f_0w = []
     t1 = time.time()
-    print('C @ alpha  (initial) =', C @ alpha_opt)
+    # print('C @ alpha  (initial) =', C @ alpha_opt)
     # w_opt = np.copy(alpha_opt)  # (np.random.rand(len(alpha_opt), 1) - 0.5) * 1e5
     w_opt = prox_group_l0(alpha_opt, l0_thresholds[0], n, num_basis)
     for j, threshold in enumerate(l0_thresholds):
@@ -461,7 +464,7 @@ def relax_and_split_increasingl0(
                 # alpha_opt = P.dot(alpha_opt + step_size * (BTB_ITbI_nuw - matmult(BT, matmult(B, contig(alpha_opt))) - matmult(IT, matmult(I, contig(alpha_opt))) - lam_nu * alpha_opt))
                 # alpha_opt = P.dot(alpha_opt + step_size * (BTB_ITbI_nuw - BT @ (B @ alpha_opt) - IT * (I @ alpha_opt) - lam_nu * alpha_opt))
                 alpha_opt = alpha_opt + step_size * P.dot(BTB_ITbI_nuw - BT @ (B @ alpha_opt) - lam_nu * alpha_opt)
-                print(C @ (step_size * P.dot(BTB_ITbI_nuw - BT @ (B @ alpha_opt) - lam_nu * alpha_opt)))
+                # print(C @ (step_size * P.dot(BTB_ITbI_nuw - BT @ (B @ alpha_opt) - lam_nu * alpha_opt)))
 
                 # print metrics
                 # if ((i % print_iter) == 0.0 or i == max_iter - 1):
@@ -478,7 +481,7 @@ def relax_and_split_increasingl0(
                       f_0[ind], 
                       f_0w[j * rs_max_iter + k], 
                       step_size_i,
-                      winding_volume.C @ alpha_opt
+                      # winding_volume.C @ alpha_opt
                       )  
 
             # now do the prox step
@@ -508,9 +511,8 @@ def relax_and_split_increasingl0(
 
 
 def relax_and_split_analytic(
-        winding_volume, lam=0.0, nu=1e20, 
-        alpha0=None, max_iter=5000, P=None, 
-        rs_max_iter=1, l0_thresholds=[0.0], print_iter=100):
+        winding_volume, lam=0.0, nu=1e20, max_iter=5000,
+        rs_max_iter=1, l0_thresholds=[0.0], print_iter=10):
     """
         This function performs a relax-and-split algorithm for solving the 
         current voxel problem. The convex part of the solve is done with
@@ -525,6 +527,8 @@ def relax_and_split_analytic(
                 so is assumed to be stored as a scipy csc_matrix. 
     """
     t1 = time.time()
+    P = winding_volume.P
+    alpha0 = winding_volume.alpha0
     n = winding_volume.N_grid
     nfp = winding_volume.plasma_boundary.nfp
     num_basis = winding_volume.n_functions
@@ -534,11 +538,12 @@ def relax_and_split_analytic(
     else:
         alpha_opt = (np.random.rand(N) - 0.5) * 1e3  
 
-    print(alpha_opt.shape, P.shape)
+    # print(alpha_opt.shape, P.shape)
     C = winding_volume.C
+    CT = C.T
     d = winding_volume.d
-    print(C.dot(alpha_opt)[-1])
-    print(np.linalg.norm(winding_volume.C.dot(alpha_opt) - winding_volume.d))
+    # print(C.dot(alpha_opt)[-1])
+    # print(np.linalg.norm(winding_volume.C.dot(alpha_opt) - winding_volume.d))
     # if P is not None:
     #     alpha_opt = P.dot(alpha_opt)
     print(C.dot(alpha_opt)[-1])
@@ -586,11 +591,14 @@ def relax_and_split_analytic(
 
     print('Time to setup step size = ', t2 - t1, ' s')
 
-    C = winding_volume.C 
-    CT = C.T
-    d = winding_volume.d 
     H = np.linalg.inv(BT @ B + np.eye(N) * lam_nu)
-    CHCT_inv = np.linalg.pinv(C @ H @ CT)
+    CHCT = C @ H @ CT  # Tends to be not full rank!!!!
+    S = np.linalg.svd(CHCT, compute_uv=False)
+    S2 = np.linalg.svd(H, compute_uv=False)
+    plt.figure(22)
+    plt.semilogy(S)
+    plt.semilogy(S2)
+    CHCT_inv = np.linalg.pinv(CHCT, rcond=1e-30)
     analytic_mat = H @ (np.eye(N) - CT @ CHCT_inv @ C @ H)
     analytic_vec = (H @ CT @ CHCT_inv @ d).reshape(N, 1)
 
@@ -601,7 +609,7 @@ def relax_and_split_analytic(
     f_0 = []
     f_0w = []
     t1 = time.time()
-    print('C @ alpha  (initial) =', C @ alpha_opt)
+    # print('C @ alpha  (initial) =', C @ alpha_opt)
     # w_opt = np.copy(alpha_opt)  # (np.random.rand(len(alpha_opt), 1) - 0.5) * 1e5
     w_opt = prox_group_l0(alpha_opt, l0_thresholds[0], n, num_basis)
     for j, threshold in enumerate(l0_thresholds):
@@ -609,39 +617,31 @@ def relax_and_split_analytic(
         # w_opt = prox_group_l0(np.copy(alpha0), l0_threshold, n, num_basis)
         print('threshold iteration = ', j + 1, ' / ', len(l0_thresholds), ', threshold = ', threshold)
         for k in range(rs_max_iter):
-            f_0w.append(np.count_nonzero(np.linalg.norm(w_opt.reshape(n, num_basis), axis=-1) > threshold))
+            if (k % print_iter) == 0.0 or k == 0:
+                ind = (j * rs_max_iter + k) // print_iter
+                f_B.append(np.linalg.norm(np.ravel(B @ alpha_opt - b), ord=2) ** 2)
+                f_I.append(np.linalg.norm(np.ravel(I @ alpha_opt - b_I)) ** 2)
+                f_K.append(np.linalg.norm(np.ravel(alpha_opt)) ** 2) 
+                f_RS.append(np.linalg.norm(np.ravel(alpha_opt - w_opt) ** 2))
+                f_0.append(np.count_nonzero(np.linalg.norm(alpha_opt.reshape(n, num_basis), axis=-1) > threshold))
+                f_0w.append(np.count_nonzero(np.linalg.norm(w_opt.reshape(n, num_basis), axis=-1) > threshold))
+                print(k, f_B[ind], 
+                      f_I[ind], 
+                      lam * f_K[ind], 
+                      f_RS[ind] / nu, 
+                      f_0[ind], 
+                      f_0w[ind], 
+                      # winding_volume.C @ alpha_opt
+                      )  
             BTB_nuw = BTb + w_opt / nu
-
             alpha_opt = analytic_mat.dot(BTB_nuw) + analytic_vec
             w_opt = prox_group_l0(alpha_opt, threshold, n, num_basis)
-
-            f_B.append(np.linalg.norm(np.ravel(B @ alpha_opt - b), ord=2) ** 2)
-            f_I.append(np.linalg.norm(np.ravel(I @ alpha_opt - b_I)) ** 2)
-            f_K.append(np.linalg.norm(np.ravel(alpha_opt)) ** 2) 
-            f_RS.append(np.linalg.norm(np.ravel(alpha_opt - w_opt) ** 2))
-            f_0.append(np.count_nonzero(np.linalg.norm(alpha_opt.reshape(n, num_basis), axis=-1) > threshold))
-            ind = (j * rs_max_iter + k) // print_iter
-            print(k, f_B[ind], 
-                  f_I[ind], 
-                  lam * f_K[ind], 
-                  f_RS[ind] / nu, 
-                  f_0[ind], 
-                  f_0w[j * rs_max_iter + k], 
-                  # winding_volume.C @ alpha_opt
-                  )  
-
-            # now do the prox step
-            # print(np.mean(np.linalg.norm(alpha_opt.reshape(n, num_basis), axis=-1)))
-            # print('Number of cells below the threshold = ', 
-            #       np.count_nonzero(np.linalg.norm(alpha_opt.reshape(n, num_basis), axis=-1) < threshold), 
-            #       ' / ', n
-            #       )
-            # print('Relax-and-split iteration = ', (k + 1), ' / ', rs_max_iter)
 
     t2 = time.time()
     print('Time to run algo = ', t2 - t1, ' s')
     t1 = time.time()
     alpha_opt = np.ravel(alpha_opt)
+    # print('alpha_opt = ', alpha_opt)
     winding_volume.alphas = alpha_opt
     winding_volume.w = np.ravel(w_opt)
     winding_volume.J = np.zeros((n, winding_volume.Phi.shape[2], 3))