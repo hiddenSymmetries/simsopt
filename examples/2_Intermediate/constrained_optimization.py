fixed error in using mpi gradients to compute objective and cosntraint gradients... two finite difference objects cannot be used simultaneously. made the constrained optimization example dynamic resolution

examples/2_Intermediate/constrained_optimization.py
@@ -7,14 +7,14 @@ from simsopt.solve.mpi import constrained_mpi_solve
 import os
 
 """
-Optimize a VMEC equilibrium for quasi-axis symmetry (M=1, N=0)
+Optimize a VMEC equilibrium for quasi-helical symmetry (M=1, N=1)
 throughout the volume.
 
 Solve as a constrained opt problem
-min quasi-axis symmetry error
+min QH symmetry error
 s.t. 
-  aspect ratio <= 6
-  0.41 <= iota <= 0.44
+  aspect ratio <= 8
+  -1.00 <= iota <= -1.05
 
 Run with 
   mpiexec -n 9 constrained_optimization.py
@@ -23,49 +23,79 @@ Run with
 # This problem has 8 degrees of freedom, so we can use 8 + 1 = 9
 # concurrent function evaluations for 1-sided finite difference
 # gradients.
-mpi = MpiPartition()
+mpi = MpiPartition(9)
 
 if mpi.proc0_world:
     print("Running 2_Intermediate/constrained_optimization.py")
     print("=============================================")
 
 
-vmec_input = os.path.join(os.path.dirname(__file__), 'inputs', 'input.nfp2_QA')
+vmec_input = os.path.join(os.path.dirname(__file__), 'inputs', 'input.nfp4_QH_warm_start')
 vmec = Vmec(vmec_input, mpi=mpi,verbose=False)
-
-# Define parameter space:
 surf = vmec.boundary
-surf.fix_all()
-max_mode = 1
-surf.fixed_range(mmin=0, mmax=max_mode,
-                 nmin=-max_mode, nmax=max_mode, fixed=False)
-surf.fix("rc(0,0)") # Major radius
-
-x0 = surf.x
 
 # Configure quasisymmetry objective:
 qs = QuasisymmetryRatioResidual(vmec,
                                 np.arange(0, 1.01, 0.1),  # Radii to target
-                                helicity_m=1, helicity_n=0)  # (M, N) you want in |B|
+                                helicity_m=1, helicity_n=1)  # (M, N) you want in |B|
 # nonlinear constraints
-tuples_nlc = [(vmec.aspect,-np.inf,6),(vmec.mean_iota,0.41,0.44)]
+tuples_nlc = [(vmec.aspect,-np.inf,8),(vmec.mean_iota,-1.05,-1.0)]
 
 # define problem
 prob = ConstrainedProblem(qs.total,tuples_nlc=tuples_nlc)
 
-# solve the problem
-constrained_mpi_solve(prob,mpi,grad=True, rel_step=1e-5, abs_step=1e-7)
-xopt = prob.x
-
-# evaluate the solution
-surf.x = xopt
 vmec.run()
+if mpi.proc0_world:
+    print("Initial Quasisymmetry:", qs.total())
+    print("Initial aspect ratio:", vmec.aspect())
+    print("Initial rotational transform:", vmec.mean_iota())
+
+
+# Fourier modes of the boundary with m <= max_mode and |n| <= max_mode
+# will be varied in the optimization. A larger range of modes are
+# included in the VMEC and booz_xform calculations.
+for step in range(3):
+    max_mode = step + 1
+
+    # VMEC's mpol & ntor will be 3, 4, 5:
+    vmec.indata.mpol = 3 + step
+    vmec.indata.ntor = vmec.indata.mpol
+
+    if mpi.proc0_world:
+        print("Beginning optimization with max_mode =", max_mode, \
+              ", vmec mpol=ntor=", vmec.indata.mpol, \
+              ". Previous vmec iteration = ", vmec.iter)
+
+    # Define parameter space:
+    surf.fix_all()
+    surf.fixed_range(mmin=0, mmax=max_mode, 
+                     nmin=-max_mode, nmax=max_mode, fixed=False)
+    surf.fix("rc(0,0)")  # Major radius
+
+    # solver options
+    options = {'disp':True, 'ftol':1e-7, 'maxiter':300}
+    # solve the problem
+    constrained_mpi_solve(prob,mpi,grad=True, rel_step=1e-5, abs_step=1e-7,options=options)
+    xopt = prob.x
+
+    # Preserve the output file from the last iteration, so it is not
+    # deleted when vmec runs again:
+    vmec.files_to_delete = []
+
+    # evaluate the solution
+    surf.x = xopt
+    vmec.run()
+    if mpi.proc0_world:
+        print("")
+        print(f"Complted optimization with max_mode ={max_mode}. ")
+        print(f"Final vmec iteration = {vmec.iter}")
+        print("Quasisymmetry:", qs.total())
+        print("aspect ratio:", vmec.aspect())
+        print("rotational transform:", vmec.mean_iota())
+
 
 if mpi.proc0_world:
-    print("Quasisymmetry objective after optimization:", qs.total())
-    print("Final aspect ratio:", vmec.aspect())
-    print("Final rotational transform:", vmec.mean_iota())
-    
+    print("")
     print("End of 2_Intermediate/constrained_optimization.py")
     print("============================================")
 

src/simsopt/solve/mpi.py
@@ -245,7 +245,7 @@ def constrained_mpi_solve(prob: ConstrainedProblem,
                             rel_step: float = 0.0,
                             diff_method: str = "forward",
                             opt_method: str = "SLSQP",
-                            **kwargs):
+                            options: dict = None):
     """
     Solve a constrained minimization problem using
     MPI. All MPI processes (including group leaders and workers)
@@ -267,12 +267,8 @@ def constrained_mpi_solve(prob: ConstrainedProblem,
              "forward". If ``centered``, centered finite differences will
              be used. If ``forward``, one-sided finite differences will
              be used. Else, error is raised.
-        kwargs: Any arguments to pass to
-                `scipy.optimize.least_squares <https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.least_squares.html>`_.
-                For instance, you can supply ``max_nfev=100`` to set
-                the maximum number of function evaluations (not counting
-                finite-difference gradient evaluations) to 100. Or, you
-                can supply ``method`` to choose the optimization algorithm.
+        options: dict, Any arguments to pass to
+                `scipy.optimize.minimize <https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html#scipy.optimize.minimize>`_.
     """
     if MPI is None:
         raise RuntimeError(
@@ -306,6 +302,9 @@ def constrained_mpi_solve(prob: ConstrainedProblem,
         except:
             objective_val = prob.fail
             logger.info("Exception caught during function evaluation.")
+
+        # TODO: remove
+        print(objective_val)
 
         nonlocal objective_datalog_started, objective_file, n_objective_evals
 
@@ -355,6 +354,9 @@ def constrained_mpi_solve(prob: ConstrainedProblem,
             constraint_val = np.full(prob.nvals, 1.0e12)
             logger.info("Exception caught during function evaluation.")
 
+        # TODO: remove
+        print(constraint_val)
+
         nonlocal constraint_datalog_started, constraint_file, n_constraint_evals
 
         # Since the number of terms is not known until the first
@@ -401,25 +403,28 @@ def constrained_mpi_solve(prob: ConstrainedProblem,
     # For MPI finite difference gradient, get the worker and leader action from
     # MPIFiniteDifference
     if grad:
-        with MPIFiniteDifference(prob.objective, mpi, abs_step=abs_step,
-                                 rel_step=rel_step, diff_method=diff_method) as fd_obj,\
-             MPIFiniteDifference(prob.nonlinear_constraints, mpi, abs_step=abs_step,
-                                 rel_step=rel_step, diff_method=diff_method) as fd_nlc:
+        with MPIFiniteDifference(prob.all_funcs, mpi, abs_step=abs_step,
+                                 rel_step=rel_step, diff_method=diff_method) as fd:
+
+            def obj_jac(x):
+                # dummy wrapper for batch finite difference
+                return fd.jac(x)[0]
  
             if mpi.proc0_world:
                 if prob.has_nlc:
-                    nlc = NonlinearConstraint(_nlc_proc0,lb=prob.lhs_nlc,ub=prob.rhs_nlc,jac =fd_nlc.jac)
+                    def nlc_jac(x):
+                        # dummy wrapper for batch finite difference
+                        return fd.jac(x)[1:]
+                    nlc = NonlinearConstraint(_nlc_proc0,lb=prob.lhs_nlc,ub=prob.rhs_nlc,jac =nlc_jac)
                     constraints.append(nlc)
+                
                 # proc0_world does this block, running the optimization.
                 x0 = np.copy(prob.x)
                 logger.info("Using finite difference method implemented in "
                             "SIMSOPT for evaluating gradient")
-                # TODO: the print statements are not functional
-                # TODO: **kwargs may not work as options here
-                result = minimize(_f_proc0, x0, jac=fd_obj.jac,
+                result = minimize(_f_proc0, x0, jac=obj_jac,
                                   bounds=bounds, constraints = constraints,
-                                  method=opt_method, **kwargs,
-                                  options={'disp':True})
+                                  method=opt_method, options=options)
 
     else:
         raise NotImplementedError
@@ -439,7 +444,7 @@ def constrained_mpi_solve(prob: ConstrainedProblem,
             logger.info("Using derivative-free method")
             result = minimize(_f_proc0, x0,
                               bounds=bounds, constraints = constraints,
-                              method=opt_method, verbose=2, **kwargs)
+                              method=opt_method, verbose=2, options=options)
 
         # Stop loops for workers and group leaders:
         mpi.together()