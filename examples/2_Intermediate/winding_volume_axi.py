Got up to Nx = 50 runs going pretty well on haswell. Fixed semimajor bug, which is that the scaling for Itarget matrix was off (sqrt(nphi) instead of the correct nphi) so optimizations were using a substantially smaller Itarget. Now winding_volume_QA.py script checks that Itarget using the matrix multipliation gets the same value as Itarget calculated using the BiotSavart class. Put back in the SVD calculation for the largest singular value that governs the algorithm step. The upper bound I was using is not very accurate (it is quite a loose upper bound) after the Itarget change.

examples/2_Intermediate/winding_volume_axi.py
@@ -30,8 +30,8 @@ t_start = time.time()
 
 t1 = time.time()
 # Set some parameters
-nphi = 16  # nphi = ntheta >= 64 needed for accurate full-resolution runs
-ntheta = 16
+nphi = 32  # nphi = ntheta >= 64 needed for accurate full-resolution runs
+ntheta = 32
 poff = 0.3  # grid end offset ~ 10 cm from the plasma surface
 coff = 0.2  # grid starts offset ~ 5 cm from the plasma surface
 # input_name = 'input.LandremanPaul2021_QA'
@@ -39,7 +39,7 @@ input_name = 'input.circular_tokamak'
 
 lam = 1e-22
 l0_threshold = 1e3
-nu = 1e18
+nu = 1e14
 
 # Read in the plasma equilibrium file
 TEST_DIR = (Path(__file__).parent / ".." / ".." / "tests" / "test_files").resolve()
@@ -59,7 +59,7 @@ s_plot.nfp = 2
 s_plot.stellsym = True
 
 # Make the output directory
-OUT_DIR = 'wv_axisymmetric_fake_QA/'
+OUT_DIR = 'wv_axi_fake_QA/'
 os.makedirs(OUT_DIR, exist_ok=True)
 
 # No external coils
@@ -76,8 +76,7 @@ t_algorithm = []
 t1 = time.time()
 numquadpoints = nphi * s.nfp * 2  # * 5
 order = 20
-quadpoints = np.linspace(0, 1, numquadpoints, endpoint=True)
-curve = CurveRZFourier(quadpoints, order, nfp=1, stellsym=False)
+curve = CurveRZFourier(numquadpoints, order, nfp=1, stellsym=False)
 for m in range(s.mpol + 1):
     if m == 0:
         nmin = 0
@@ -89,15 +88,13 @@ for m in range(s.mpol + 1):
 
 curve.x = curve.get_dofs()
 curve.x = curve.x  # need to do this to transfer data to C++
-curves_to_vtk([curve], OUT_DIR + f"Itarget_curve")
+# curves_to_vtk([curve], OUT_DIR + f"Itarget_curve")
 Itarget = 0.5e6
 t2 = time.time()
 print('Curve initialization took time = ', t2 - t1, ' s')
 
 # params = [1, 2, 3, 4, 5, 10, 20, 30]
-params = [30]
-num_cells = []
-cell_volumes = []
+params = [34]
 # for nx in params:
 for Nx in params:
     nx = 6
@@ -123,10 +120,10 @@ for Nx in params:
     t2 = time.time()
     print('WV grid initialization took time = ', t2 - t1, ' s')
 
-    max_iter = 2000
-    rs_max_iter = 5
+    max_iter = 200
+    rs_max_iter = 10
 
-    l0_thresholds = np.linspace(l0_threshold, 10 * l0_threshold, 5)
+    l0_thresholds = np.linspace(l0_threshold, 40 * l0_threshold, 10)
     alpha_opt, fB, fK, fI, fRS, f0, fBw, fKw, fIw = relax_and_split_increasingl0(
         wv_grid, lam=lam, nu=nu, max_iter=max_iter,
         l0_thresholds=l0_thresholds, 
@@ -169,19 +166,10 @@ for Nx in params:
     fB_direct = SquaredFlux(s, bs_wv, -Bnormal).J() * 2 * s.nfp
     print('fB_direct = ', fB_direct)
 
-    bs_wv.set_points(curve.gamma().reshape((-1, 3)))
-    Bnormal_Itarget_curve = np.sum(bs_wv.B() * curve.gammadash().reshape(-1, 3), axis=-1)
-    mu0 = 4 * np.pi * 1e-7
-    print(curve.quadpoints)
-    Itarget_check = np.sum(Bnormal_Itarget_curve) / mu0 / len(curve.quadpoints)
-    print('Itarget_check = ', Itarget_check)
-    print('Itarget second check = ', wv_grid.Itarget_matrix @ alpha_opt / mu0) 
-
     t1 = time.time()
     make_Bnormal_plots(bs_wv, s_plot, OUT_DIR, "biot_savart_winding_volume_Nx" + str(Nx))
     make_Bnormal_plots(bs_wv_sparse, s_plot, OUT_DIR, "biot_savart_winding_volume_sparse_Nx" + str(Nx))
     t2 = time.time()
-
     print('Time to plot Bnormal_wv = ', t2 - t1, ' s')
 
     w_range = np.linspace(0, len(fB), len(fBw), endpoint=True)
@@ -200,7 +188,6 @@ for Nx in params:
     plt.grid(True)
     plt.legend()
 
-    # plt.savefig(OUT_DIR + 'optimization_progress.jpg')
     t1 = time.time()
     wv_grid.check_fluxes()
     t2 = time.time()
@@ -215,49 +202,5 @@ for Nx in params:
 
     t_end = time.time()
     print('Total time = ', t_end - t_start)
-#    fB_all.append(fB_direct)
-#    fI_all.append(fI[-1])
-#    fK_all.append(lam * fK[-1])
-#    num_cells.append(wv_grid.N_grid)
-#    cell_volumes.append(wv_grid.dx * wv_grid.dy * wv_grid.dz)
 
-
-# print(fB_all, fI_all, fK_all)
-# plt.figure()
-# plt.semilogy(num_cells, fB_all, 'ro', label=r'$f_B$',)
-# plt.semilogy(num_cells, fI_all, 'mo', label=r'$f_I$')
-# plt.legend()
-# # plt.semilogy(params, lam * np.array(fK_all), 'bo', label=r'$\lambda f_K$')
-# ax = plt.gca()
-# secax = ax.twiny()
-# secax.set_xlim(ax.get_xlim())
-# print(cell_volumes)
-# secax.set_xticks(num_cells)
-# for i, vol in enumerate(cell_volumes):
-#     cell_volumes[i] = "{:.2e}".format(vol)
-# secax.set_xticklabels(cell_volumes)
-# secax.set_xlabel(r'$V_{cell}$')
-# # secax.xaxis.set_major_formatter('{x:.2f}')
-# plt.grid(True)
-# ax.set_xlabel(r'$N_{cell}$')
-# ax.set_ylabel('Losses')
-# plt.show()
-
-# plt.figure()
-# plt.semilogy(num_cells, t_algorithm, 'ro')
-# plt.legend()
-# # plt.semilogy(params, lam * np.array(fK_all), 'bo', label=r'$\lambda f_K$')
-# ax = plt.gca()
-# secax = ax.twiny()
-# secax.set_xlim(ax.get_xlim())
-# print(cell_volumes)
-# secax.set_xticks(num_cells)
-# for i, vol in enumerate(cell_volumes):
-#     cell_volumes[i] = "{:.2e}".format(vol)
-# secax.set_xticklabels(cell_volumes)
-# secax.set_xlabel(r'$V_{cell}$')
-# # secax.xaxis.set_major_formatter('{x:.2f}')
-# plt.grid(True)
-# ax.set_xlabel(r'$N_{cell}$')
-# ax.set_ylabel('Algorithm run time (s)')
 plt.show()

src/simsopt/geo/winding_volume_grid.py
@@ -10,6 +10,7 @@ from scipy.sparse import vstack, hstack
 from scipy.sparse import eye as sparse_eye
 from scipy.sparse.linalg import inv as sparse_inv
 from matplotlib import pyplot as plt
+from matplotlib.colors import LogNorm
 from .._core.json import GSONDecoder, GSONable
 # from .._core.optimizable import Optimizable
 
@@ -369,9 +370,12 @@ class WindingVolumeGrid:
         Phi = self.Phi
         n_interp = Phi.shape[2]
         Jvec = self.J
+        Jvec_sparse = self.J_sparse
         alphas = self.alphas.reshape(n, self.n_functions).T
         Jvec_avg = np.mean(Jvec, axis=1)
+        Jvec_sparse_avg = np.mean(Jvec_sparse, axis=1)
         Jvec = Jvec.reshape(n, self.nx, self.ny, self.nz, 3)
+        Jvec_sparse = Jvec_sparse.reshape(n, self.nx, self.ny, self.nz, 3)
 
         dJx_dx = -(Jvec[:, 1:, :, :, 0] - Jvec[:, :-1, :, :, 0]) / self.dx
         dJy_dy = -(Jvec[:, :, 1:, :, 1] - Jvec[:, :, :-1, :, 1]) / self.dy
@@ -383,12 +387,17 @@ class WindingVolumeGrid:
         Jy = Jvec_avg[:, 1]
         Jz = Jvec_avg[:, 2]
 
+        Jx_sp = Jvec_sparse_avg[:, 0]
+        Jy_sp = Jvec_sparse_avg[:, 1]
+        Jz_sp = Jvec_sparse_avg[:, 2]
+
         # Initialize new grid and current vectors for all the dipoles
         # after we account for the symmetries below.
         ox_full = np.zeros(n * nsym)
         oy_full = np.zeros(n * nsym)
         oz_full = np.zeros(n * nsym)
         Jvec_full = np.zeros((n * nsym, 3))
+        Jvec_full_sp = np.zeros((n * nsym, 3))
         Jvec_full_internal = np.zeros((n * nsym, self.nx, self.ny, self.nz, 3))
 
         # loop through the dipoles and repeat for fp and stellarator symmetries
@@ -413,14 +422,23 @@ class WindingVolumeGrid:
                 Jvec_full[index:index + n, 1] = Jx * np.sin(phi0) * stell + Jy * np.cos(phi0)
                 Jvec_full[index:index + n, 2] = Jz
 
+                Jvec_full_sp[index:index + n, 0] = Jx_sp * np.cos(phi0) * stell - Jy_sp * np.sin(phi0)
+                Jvec_full_sp[index:index + n, 1] = Jx_sp * np.sin(phi0) * stell + Jy_sp * np.cos(phi0)
+                Jvec_full_sp[index:index + n, 2] = Jz_sp
+
                 index += n
 
         Jvec_normalization = 1.0 / np.sum(Jvec_full ** 2, axis=-1)
+        # Jvec_normalization_sp = 1.0 / np.sum(Jvec_full_sp ** 2, axis=-1)
 
         # Save all the data to a vtk file which can be visualized nicely with ParaView
         Jx = Jvec_full[:, 0]
         Jy = Jvec_full[:, 1]
         Jz = Jvec_full[:, 2]
+
+        Jx_sp = Jvec_full_sp[:, 0]
+        Jy_sp = Jvec_full_sp[:, 1]
+        Jz_sp = Jvec_full_sp[:, 2]
 
         Jvec_xmin = np.mean(np.mean(Jvec_full_internal[:, 0, :, :, :], axis=-2), axis=-2)
         Jvec_xmax = np.mean(np.mean(Jvec_full_internal[:, -1, :, :, :], axis=-2), axis=-2)
@@ -435,6 +453,11 @@ class WindingVolumeGrid:
                 (contig(Jx / Jvec_normalization), 
                  contig(Jy / Jvec_normalization), 
                  contig(Jz / Jvec_normalization)),
+                "J_sparse": (contig(Jx_sp), contig(Jy_sp), contig(Jz_sp)), 
+                #"J_sparse_normalized": 
+                #(contig(Jx_sp / Jvec_normalization_sp), 
+                # contig(Jy_sp / Jvec_normalization_sp), 
+                # contig(Jz_sp / Jvec_normalization_sp)),
                 "Jvec_xmin": (contig(Jvec_xmin), contig(Jvec_xmin), contig(Jvec_xmin)), 
                 "Jvec_xmax": (contig(Jvec_xmax), contig(Jvec_xmax), contig(Jvec_xmax)), 
                 "Jvec_ymin": (contig(Jvec_ymin), contig(Jvec_ymin), contig(Jvec_ymin)), 
@@ -462,6 +485,22 @@ class WindingVolumeGrid:
             contig(self.XYZ_uniform[:, 1]),
             contig(self.XYZ_uniform[:, 2])
         )
+
+        fig = plt.figure(700, figsize=(12, 12))
+        ax = fig.add_subplot(projection='3d')
+
+        # Make the grid
+        colors = np.sum(Jvec_full_sp ** 2, axis=-1)
+        inds = (colors > 1)
+        # colors = colors / np.max(colors)
+        #colors = plt.cm.jet(colors)
+        colors = np.concatenate((colors, np.repeat(colors, 2))) 
+
+        q = ax.quiver(ox_full[inds], oy_full[inds], oz_full[inds], Jvec_full_sp[inds, 0], Jvec_full_sp[inds, 1], Jvec_full_sp[inds, 2], length=0.4, normalize=True, cmap='Reds', norm=LogNorm(vmin=1, vmax=np.max(colors)))  # colors=colors[inds])
+        inds = (colors > 1)
+        q.set_array(colors[inds])
+        fig.colorbar(q)
+        plt.savefig(self.OUT_DIR + 'quiver_plot_sparse_solution.jpg')
 
     def _setup_polynomial_basis(self):
         """
@@ -580,7 +619,7 @@ class WindingVolumeGrid:
         normN = np.linalg.norm(plasma_normal, ord=2, axis=-1)
         num_basis = Phi.shape[0]
         n_interp = Phi.shape[2]
-        nphi_loop_sqrt_inv = np.sqrt(1.0 / nphi) 
+        nphi_loop_inv = 1.0 / len(self.Itarget_curve.quadpoints)
         curve_dl = self.Itarget_curve.gammadash().reshape(-1, 3)
 
         # Initialize new grid and current vectors for all the dipoles
@@ -672,12 +711,14 @@ class WindingVolumeGrid:
 
         self.B_matrix = B_matrix
         self.b_rhs = -b_rhs  # minus sign because ||B_{coil,N} + B_{0,N}||_2^2 -> ||A * alpha - b||_2^2
-        self.Itarget_matrix = nphi_loop_sqrt_inv * coil_integration_factor * self.Itarget_matrix.reshape(
+        self.Itarget_matrix = nphi_loop_inv * coil_integration_factor * self.Itarget_matrix.reshape(
             points_curve.shape[0], n * num_basis
         )
         self.Itarget_matrix = np.sum(self.Itarget_matrix, axis=0)
-        self.Itarget_rhs = self.Bn_Itarget * nphi_loop_sqrt_inv
+        self.Itarget_rhs = self.Bn_Itarget * nphi_loop_inv
+        print('Itarget rhs first = ', self.Itarget_rhs)
         self.Itarget_rhs = np.sum(self.Itarget_rhs) + 4 * np.pi * 1e-7 * self.Itarget
+        print('Itarget rhs second = ', self.Itarget_rhs / (4 * np.pi * 1e-7))
         flux_factor, self.connection_list = sopp.winding_volume_flux_jumps(
             coil_points,
             contig(self.Phi.reshape(self.n_functions, self.N_grid, self.nx, self.ny, self.nz, 3)), 
@@ -975,7 +1016,7 @@ class WindingVolumeGrid:
                             flux[i, j] += nx * Jvec[i, l, m, z_ind, 0] + ny * Jvec[i, l, m, z_ind, 1] + nz * Jvec[i, l, m, z_ind, 2]
 
         # Compare fluxes across adjacent cells
-        flux_max = 10  # np.max(abs(flux)) / 1e4
+        flux_max = 5000  # np.max(abs(flux)) / 1e4
         q = 0
         qq = 0
         for i in range(n):
@@ -997,8 +1038,11 @@ class WindingVolumeGrid:
                             j_ind = j + 1
                         else:
                             j_ind = j - 1
-                        #print(i, j, k_ind, j_ind, ind, flux[i, j], flux[k_ind, j_ind])
-                        assert np.isclose(flux[i, j], -flux[k_ind, j_ind], atol=flux_max, rtol=1e-3)
+                        if not np.isclose(flux[i, j], -flux[k_ind, j_ind], atol=flux_max, rtol=1e-1):
+                            print(i, j, k_ind, j_ind, ind, flux[i, j], flux[k_ind, j_ind])
+                        assert np.isclose(flux[i, j], -flux[k_ind, j_ind], atol=flux_max, rtol=1e-1)
                 else:
+                    if not np.isclose(flux[i, j], 0.0, atol=flux_max):
+                        print(i, j, flux[i, j])
                     assert np.isclose(flux[i, j], 0.0, atol=flux_max)
 

src/simsopt/solve/winding_volume_optimization.py
@@ -72,11 +72,12 @@ def relax_and_split_increasingl0(
 
     # upper bound on largest singular value from https://www.cs.yale.edu/homes/spielman/BAP/lect3.pdf
     t1 = time.time()
-    if True:
-        # L = np.sqrt(N) * np.max(np.linalg.norm(BT @ B + IT @ I + np.eye(N) * lam_nu, axis=0), axis=-1)
-        L = np.sqrt(N) * np.max(np.linalg.norm(BT @ B + np.eye(N) * lam_nu, axis=0), axis=-1)
-    else:
-        L = 2e-12  # L = 1e-12 for N = 30, L = 2.4e-12 for N = 24, so not a bad guess
+    L = np.linalg.svd(BT @ B + IT @ I + np.eye(N) * lam_nu, compute_uv=False, hermitian=True)[0]
+    #if True:
+    # L = np.sqrt(N) * np.max(np.linalg.norm(BT @ B + IT @ I + np.eye(N) * lam_nu, axis=0), axis=-1)
+    # L = np.sqrt(N) * np.max(np.linalg.norm(BT @ B + np.eye(N) * lam_nu, axis=0), axis=-1)
+    #else:
+    #    L = 2e-12  # L = 1e-12 for N = 30, L = 2.4e-12 for N = 24, so not a bad guess
 
     step_size = 1.0 / L  # largest step size suitable for gradient descent
     print('L, step_size = ', L, step_size) 
@@ -93,14 +94,15 @@ def relax_and_split_increasingl0(
     f_Kw = []
     f_0 = []
     f_0w = []
+    winding_volume.alpha_history = []
+    winding_volume.w_history = []
     t1 = time.time()
-    w_opt = prox_group_l0(alpha_opt, l0_thresholds[0], n, num_basis)
+    w_opt = np.zeros(alpha_opt.shape)  # prox_group_l0(alpha_opt, l0_thresholds[0], n, num_basis)
     for j, threshold in enumerate(l0_thresholds):
         # alpha0 = alpha_opt
         # w_opt = prox_group_l0(np.copy(alpha0), l0_threshold, n, num_basis)
         print('threshold iteration = ', j + 1, ' / ', len(l0_thresholds), ', threshold = ', threshold)
         for k in range(rs_max_iter):
-            f_0w.append(np.count_nonzero(np.linalg.norm(w_opt.reshape(n, num_basis), axis=-1) > threshold))
             step_size_i = step_size
             alpha_opt_prev = alpha_opt
             BTB_ITbI_nuw = BTb + ITbI + w_opt / nu
@@ -115,15 +117,15 @@ def relax_and_split_increasingl0(
 
                 # print metrics
                 if ((i % print_iter) == 0.0 or i == max_iter - 1):
-                    ##### Alpha_opt or w_opt here???
                     f_B.append(np.linalg.norm(np.ravel(B @ alpha_opt - b), ord=2) ** 2)
                     f_I.append(np.linalg.norm(np.ravel(I @ alpha_opt - b_I)) ** 2)
                     f_K.append(np.linalg.norm(np.ravel(alpha_opt)) ** 2) 
                     f_RS.append(np.linalg.norm(np.ravel(alpha_opt - w_opt) ** 2))
-                    # f_0.append(np.count_nonzero(np.linalg.norm(alpha_opt.reshape(n, num_basis), axis=-1) > threshold))
-                    f_0.append(np.count_nonzero(np.linalg.norm(alpha_opt.reshape(n, num_basis), axis=-1)))
+                    f_0.append(np.count_nonzero(np.linalg.norm(alpha_opt.reshape(n, num_basis), axis=-1) > threshold))
+                    # f_0.append(np.count_nonzero(np.linalg.norm(alpha_opt.reshape(n, num_basis), axis=-1)))
                     ind = (j * max_iter * rs_max_iter + k * max_iter + (i + 1)) // print_iter
-                    print('alpha objectives: ', j, k, i, 
+                    winding_volume.alpha_history.append(alpha_opt)
+                    print(j, k, i, 
                           f_B[ind], 
                           f_I[ind], 
                           lam * f_K[ind], 
@@ -131,20 +133,20 @@ def relax_and_split_increasingl0(
                           f_0[ind], ' / ', n, ', ',
                           step_size_i,
                           )  
-                    if threshold > 0:
-                        f_Bw.append(np.linalg.norm(np.ravel(B @ w_opt - b), ord=2) ** 2)
-                        f_Iw.append(np.linalg.norm(np.ravel(I @ w_opt - b_I)) ** 2)
-                        f_Kw.append(np.linalg.norm(np.ravel(w_opt)) ** 2) 
-                        # f_0.append(np.count_nonzero(np.linalg.norm(alpha_opt.reshape(n, num_basis), axis=-1) > threshold))
-                        f_0w.append(np.count_nonzero(np.linalg.norm(w_opt.reshape(n, num_basis), axis=-1)))
-                        ind = (j * max_iter * rs_max_iter + k * max_iter + (i + 1)) // print_iter
-                        print('w : ', j, k, i, 
-                              f_Bw[ind], 
-                              f_Iw[ind], 
-                              lam * f_Kw[ind], 
-                              f_RS[ind] / nu, 
-                              f_0w[j * rs_max_iter + k], ' / ', n, ', ',
-                              )  
+            if threshold > 0:
+                f_Bw.append(np.linalg.norm(np.ravel(B @ w_opt - b), ord=2) ** 2)
+                f_Iw.append(np.linalg.norm(np.ravel(I @ w_opt - b_I)) ** 2)
+                f_Kw.append(np.linalg.norm(np.ravel(w_opt)) ** 2) 
+                # f_0w.append(np.count_nonzero(np.linalg.norm(w_opt.reshape(n, num_basis), axis=-1) > threshold))
+                f_0w.append(np.count_nonzero(np.linalg.norm(w_opt.reshape(n, num_basis), axis=-1)))
+                ind = (j * rs_max_iter + k)
+                winding_volume.w_history.append(w_opt)
+                print('w : ', j, k, i, 
+                      f_Bw[ind], 
+                      f_Iw[ind], 
+                      lam * f_Kw[ind], 
+                      f_0w[ind], ' / ', n, ', ',
+                      )  
 
             # now do the prox step
             w_opt = prox_group_l0(np.copy(alpha_opt), threshold, n, num_basis)
@@ -156,11 +158,23 @@ def relax_and_split_increasingl0(
     winding_volume.alphas = alpha_opt
     winding_volume.w = np.ravel(w_opt)
     winding_volume.J = np.zeros((n, winding_volume.Phi.shape[2], 3))
+    winding_volume.J_sparse = np.zeros((n, winding_volume.Phi.shape[2], 3))
     alphas = winding_volume.alphas.reshape(n, num_basis)
+    ws = winding_volume.w.reshape(n, num_basis)
     for i in range(3):
         for j in range(winding_volume.Phi.shape[2]):
             for k in range(num_basis):
                 winding_volume.J[:, j, i] += alphas[:, k] * winding_volume.Phi[k, :, j, i]
+                winding_volume.J_sparse[:, j, i] += ws[:, k] * winding_volume.Phi[k, :, j, i]
     t2 = time.time()
     print('Time to compute J = ', t2 - t1, ' s')
-    return alpha_opt, 0.5 * 2 * np.array(f_B) * nfp, 0.5 * np.array(f_K), 0.5 * np.array(f_I), 0.5 * np.array(f_RS) / nu, f_0
+    return (alpha_opt, 
+            0.5 * 2 * np.array(f_B) * nfp, 
+            0.5 * np.array(f_K), 
+            0.5 * np.array(f_I), 
+            0.5 * np.array(f_RS) / nu, 
+            f_0,
+            0.5 * 2 * np.array(f_Bw) * nfp, 
+            0.5 * np.array(f_Kw), 
+            0.5 * np.array(f_Iw)
+            )