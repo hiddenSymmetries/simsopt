Bug at mpol > 16 appears to be because Ak matrix gets large enough that it is no longer really full rank, so the pseudoinverse calculation was including a bunch of junk singular values. Fixed by just setting rcond=1e-10, which seems like it should work for most cases.

examples/2_Intermediate/winding_surface.py
@@ -295,8 +295,8 @@ def run_target():
     """
 
     fB_target = 5e-5
-    mpol = 16
-    ntor = 16
+    mpol = 20
+    ntor = 20
 
     for file in files:
         filename = TEST_DIR / file
@@ -318,7 +318,7 @@ def run_target():
         contig = np.ascontiguousarray
 
         # Loop through wide range of regularization values
-        lambdas = np.flip(np.logspace(-19, -9, 20))
+        lambdas = np.flip(np.logspace(-24, -12, 20))
         for i, lambda_reg in enumerate(lambdas):
             # Solve the REGCOIL problem that uses Tikhonov regularization (L2 norm)
             optimized_phi_mn, f_B, _ = cpst.solve_tikhonov(lam=lambda_reg)
@@ -346,7 +346,7 @@ def run_target():
         print('Now repeating for Lasso: ')
         for i, lambda_reg in enumerate(lambdas):
             # Solve the REGCOIL problem with the Lasso 
-            optimized_phi_mn, f_B, _, fB_history, _ = cpst.solve_lasso(lam=lambda_reg, max_iter=10000, acceleration=True)
+            optimized_phi_mn, f_B, _, fB_history, _ = cpst.solve_lasso(lam=lambda_reg, max_iter=1000, acceleration=True)
             print(i, lambda_reg, f_B)
             cp_opt = cpst.current_potential
 
@@ -378,8 +378,8 @@ def run_target():
 
 # Run one of the functions and time it
 t1 = time.time()
-run_scan()
-# run_target()
+# run_scan()
+run_target()
 t2 = time.time()
 print('Total run time = ', t2 - t1)
 plt.show()

src/simsopt/field/currentpotentialsolve.py
@@ -453,9 +453,10 @@ class CurrentPotentialSolve:
         nfp = self.plasma_surface.nfp
 
         # Ak is non-square so pinv required. Careful with rcond parameter
-        Ak_inv = np.linalg.pinv(Ak_matrix, rcond=1e-30)
+        Ak_inv = np.linalg.pinv(Ak_matrix, rcond=1e-10)
         A_new = A_matrix @ Ak_inv
         b_new = b_e - A_new @ d
+        # print('Checking Ak_inv computed with pinv = ', Ak_inv)
 
         # rescale the l1 regularization
         l1_reg = lam
@@ -516,7 +517,7 @@ class CurrentPotentialSolve:
         # An upper bound on the
         # Lipshitz constant L of the least-squares loss term
         # can be computed easily as largest eigenvalue of ATA
-        L = np.linalg.svd(ATA, compute_uv=False)[0]
+        L = np.linalg.svd(ATA, compute_uv=False, hermitian=True)[0]
 
         # SVD is slow for large problems so can use
         # the upper bound on largest singular value from https://www.cs.yale.edu/homes/spielman/BAP/lect3.pdf
@@ -533,14 +534,17 @@ class CurrentPotentialSolve:
         x_history = [xi0]
         if acceleration:  # FISTA algorithm
             # first iteration do ISTA
-            x_history.append(prox(xi0 + ti * (ATb - ATA @ xi0), ti * alpha))
+            x_prev = xi0
+            x = prox(xi0 + ti * (ATb - ATA @ xi0), ti * alpha)
             for i in range(1, max_iter):
-                vi = x_history[i] + (i - 1) / (i + 2) * (x_history[i] - x_history[i - 1])
+                vi = x + i / (i + 3) * (x - x_prev)
+                x_prev = x
                 # note l1 'threshold' is rescaled here
-                x_history.append(prox(vi + ti * (ATb - ATA @ vi), ti * alpha))
+                x = prox(vi + ti * (ATb - ATA @ vi), ti * alpha)
                 ti = (1 + np.sqrt(1 + 4 * ti ** 2)) / 2.0
                 if (i % 100) == 0:
-                    if np.all(abs(x_history[i + 1] - x_history[i]) < tol):
+                    x_history.append(x)
+                    if np.all(abs(x_history[-1] - x_history[-2]) < tol):
                         break
         else:  # ISTA algorithm
             alpha = ti * alpha  # ti does not vary in ISTA algorithm