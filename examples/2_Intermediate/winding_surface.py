ae449bf0b2a776784ce553573dabbdc56aba25b9
Alan Kaptanoglu <akaptano@uw.edu>
2023-03-09 08:09:09 -0800

    Small change to the example script that Im committing to switch to other branches

--- examples/2_Intermediate/winding_surface.py
+++ examples/2_Intermediate/winding_surface.py
@@ -334,7 +334,7 @@ def run_target():
         contig = np.ascontiguousarray
 
         # Loop through wide range of regularization values
-        lambdas = np.flip(np.logspace(-24, -12, 20))
+        lambdas = np.flip(np.logspace(-24, -12, 14))
         for i, lambda_reg in enumerate(lambdas):
             # Solve the REGCOIL problem that uses Tikhonov regularization (L2 norm)
             optimized_phi_mn, f_B, _ = cpst.solve_tikhonov(lam=lambda_reg)
