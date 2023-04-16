1a5aa0f00e2832d96d9f2dd51cca966f1d185905
Matt Landreman <mattland@umd.edu>
2023-04-16 13:52:04 -0400

    Tidy up imports in permanent magnet examples

--- examples/2_Intermediate/permanent_magnet_NCSX.py
+++ examples/2_Intermediate/permanent_magnet_NCSX.py
@@ -22,16 +22,17 @@ https://github.com/akaptano/simsopt_permanent_magnet_advanced_scripts.git
 """
 
 import os
-from matplotlib import pyplot as plt
-from pathlib import Path
-import numpy as np
-from simsopt.geo import SurfaceRZFourier
-from simsopt.objectives import SquaredFlux
-from simsopt.field import DipoleField, ToroidalField
-from simsopt.geo import PermanentMagnetGrid
-from simsopt.solve import GPMO 
 import pickle
 import time
+from pathlib import Path
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from simsopt.field import DipoleField, ToroidalField
+from simsopt.geo import PermanentMagnetGrid, SurfaceRZFourier
+from simsopt.objectives import SquaredFlux
+from simsopt.solve import GPMO
 from simsopt.util.permanent_magnet_helper_functions import *
 
 t_start = time.time()
@@ -39,9 +40,9 @@ t_start = time.time()
 # Set some parameters
 nphi = 8  # change to 64 for a real run
 ntheta = 8  # same as above
-dr = 0.02
-coff = 0.02
-poff = 0.1
+dr = 0.02  # Radial extent in meters of the cylindrical permanent magnet bricks
+coff = 0.02  # Offset from the plasma surface of the start of the permanent magnet grid, in meters
+poff = 0.1  # Offset from the plasma surface of the end of the permanent magnet grid, in meters
 surface_flag = 'wout'
 input_name = 'wout_c09r00_fixedBoundary_0.5T_vacuum_ns201.nc'
 famus_filename = 'init_orient_pm_nonorm_5E4_q4_dp.focus'
@@ -169,12 +170,12 @@ write_pm_optimizer_to_famus(OUT_DIR, pm_opt)
 
 # Optionally make a QFM and pass it to VMEC
 # This is worthless unless plasma
-# surface is 64 x 64 resolution.
+# surface is at least 64 x 64 resolution.
 vmec_flag = False
 if vmec_flag:
     from mpi4py import MPI
-    from simsopt.util.mpi import MpiPartition
     from simsopt.mhd.vmec import Vmec
+    from simsopt.util.mpi import MpiPartition
     mpi = MpiPartition(ngroups=4)
     comm = MPI.COMM_WORLD
 
