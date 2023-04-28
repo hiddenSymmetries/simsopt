37d518e3dd826e605a5b13b2cf1475329725247d
Bharat Medasani <mbkumar@gmail.com>
2023-04-27 21:16:11 -0400

    Update macro used for XSIMD

--- src/simsoptpp/biot_savart_vjp_c.h
+++ src/simsoptpp/biot_savart_vjp_c.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "simdhelpers.h"
 
-#if __x86_64__ || __aarch64__
+#if defined(USE_XSIMD)
 
 template<class T, int derivs>
 void biot_savart_vjp_kernel(AlignedPaddedVec& pointsx, AlignedPaddedVec& pointsy, AlignedPaddedVec& pointsz, T& gamma, T& dgamma_by_dphi, T& v, T& res_gamma, T& res_dgamma_by_dphi, T& vgrad, T& res_grad_gamma, T& res_grad_dgamma_by_dphi);
