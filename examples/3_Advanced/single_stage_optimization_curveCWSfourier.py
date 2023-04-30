0e4f49a3f36881380a906f35d2696b38336d51f9
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2023-04-30 08:23:57 +0100

    finally some good parameters for single stage cws optimization

--- examples/3_Advanced/single_stage_optimization_curveCWSfourier.py
+++ examples/3_Advanced/single_stage_optimization_curveCWSfourier.py
@@ -38,35 +38,35 @@ start = time.time()
 ##########################################################################################
 ############## Input parameters
 ##########################################################################################
-max_mode = 1
-MAXITER_stage_2 = 40
-MAXITER_single_stage = 20
+max_modes = [1,1,1,1,2,2,2,3,3]
+MAXITER_stage_2 = 200
+MAXITER_single_stage = 100
 vmec_input_filename = os.path.join(parent_path, 'inputs', 'input.nfp2_QA')
-ncoils = 5
+ncoils = 6
 nmodes_coils = 10
-quadpoints = 13 * nmodes_coils
+quadpoints = 150
 aspect_ratio_target = 7.0
 CC_THRESHOLD = 0.1
-LENGTH_THRESHOLD = 6.5
-CURVATURE_THRESHOLD = 15
-MSC_THRESHOLD = 15
+LENGTH_THRESHOLD = 4.5
+CURVATURE_THRESHOLD = 14
+MSC_THRESHOLD = 14
 nphi_VMEC = 32
 ntheta_VMEC = 32
 coils_objective_weight = 1e+3
-aspect_ratio_weight = 1
+aspect_ratio_weight = 1e-1
+mean_iota_target = 0.41
+mean_iota_weight = 1e+1
 diff_method = "forward"
-R0 = 1.0
-R1 = 0.6
-minor_radius_factor_cws = 2.5
+minor_radius_factor_cws = 4
 quasisymmetry_target_surfaces = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-finite_difference_abs_step = 1e-4
+finite_difference_abs_step = 1e-5
 finite_difference_rel_step = 1e-3
 JACOBIAN_THRESHOLD = 10000
 LENGTH_CON_WEIGHT = 0.1  # Weight on the quadratic penalty for the curve length
 LENGTH_WEIGHT = 1e-8  # Weight on the curve lengths in the objective function
 CC_WEIGHT = 5e3  # Weight for the coil-to-coil distance penalty in the objective function
-CURVATURE_WEIGHT = 1e-3  # Weight for the curvature penalty in the objective function
-MSC_WEIGHT = 1e-3 # Weight for the mean squared curvature penalty in the objective function
+CURVATURE_WEIGHT = 1e-8  # Weight for the curvature penalty in the objective function
+MSC_WEIGHT = 1e-8 # Weight for the mean squared curvature penalty in the objective function
 ARCLENGTH_WEIGHT = 1e-9  # Weight for the arclength variation penalty in the objective function
 ##########################################################################################
 ##########################################################################################
@@ -84,6 +84,8 @@ if comm.rank == 0:
 ##########################################################################################
 ##########################################################################################
 # Stage 1
+if os.path.exists(os.path.join(this_path, "input.final")):
+    vmec_input_filename = os.path.join(this_path, "input.final")
 pprint(f' Using vmec input file {vmec_input_filename}')
 vmec = Vmec(vmec_input_filename, mpi=mpi, verbose=vmec_verbose, nphi=nphi_VMEC, ntheta=ntheta_VMEC, range_surface='half period')
 surf = vmec.boundary
@@ -147,10 +149,10 @@ Jals = [ArclengthVariation(c) for c in base_curves]
 J_LENGTH = LENGTH_WEIGHT * sum(Jls)
 J_CC = CC_WEIGHT * Jccdist
 J_CURVATURE = CURVATURE_WEIGHT * sum(Jcs)
-J_MSC = MSC_WEIGHT * sum(QuadraticPenalty(J, MSC_THRESHOLD) for i, J in enumerate(Jmscs))
+J_MSC = MSC_WEIGHT * sum(QuadraticPenalty(J, MSC_THRESHOLD, f="max") for i, J in enumerate(Jmscs))
 J_ALS = ARCLENGTH_WEIGHT * sum(Jals)
-J_LENGTH_PENALTY = LENGTH_CON_WEIGHT * sum([QuadraticPenalty(Jls[i], LENGTH_THRESHOLD) for i in range(len(base_curves))])
-JF = Jf + J_CC + J_LENGTH_PENALTY + J_LENGTH + J_CURVATURE + J_MSC
+J_LENGTH_PENALTY = LENGTH_CON_WEIGHT * sum([QuadraticPenalty(Jls[i], LENGTH_THRESHOLD, f="max") for i in range(len(base_curves))])
+JF = Jf + J_CC + J_LENGTH_PENALTY + J_CURVATURE + J_ALS + J_MSC# + J_LENGTH
 ##########################################################################################
 pprint(f'  Starting optimization')
 ##########################################################################################
@@ -212,7 +214,6 @@ def fun(dofs, prob_jacobian=None, info={'Nfeval': 0}):
         B_n = Bcoil_n
         B_diff = Bcoil
         B_N = np.sum(Bcoil * n, axis=2)
-        assert Jf.local
         dJdx = (B_n/mod_Bcoil**2)[:, :, None] * (np.sum(dB_by_dX*(n-B*(B_N/mod_Bcoil**2)[:, :, None])[:, :, None, :], axis=3))
         dJdN = (B_n/mod_Bcoil**2)[:, :, None] * B_diff - 0.5 * (B_N**2/absn**3/mod_Bcoil**2)[:, :, None] * n
         deriv = surf.dnormal_by_dcoeff_vjp(dJdN/(nphi_VMEC*ntheta_VMEC)) + surf.dgamma_by_dcoeff_vjp(dJdx/(nphi_VMEC*ntheta_VMEC))
@@ -237,57 +238,61 @@ def fun(dofs, prob_jacobian=None, info={'Nfeval': 0}):
 ## Perform optimization
 #############################################################
 ##########################################################################################
-surf.fix_all()
-surf.fixed_range(mmin=0, mmax=max_mode, nmin=-max_mode, nmax=max_mode, fixed=False)
-surf.fix("rc(0,0)")
-number_vmec_dofs = int(len(surf.x))
-qs = QuasisymmetryRatioResidual(vmec, quasisymmetry_target_surfaces, helicity_m=1, helicity_n=0)
-objective_tuple = [(vmec.aspect, aspect_ratio_target, aspect_ratio_weight), (qs.residuals, 0, 1)]
-prob = LeastSquaresProblem.from_tuples(objective_tuple)
-dofs = np.concatenate((JF.x, vmec.x))
-pprint(f"  Aspect ratio before stage 2 optimization: {vmec.aspect()}")
-pprint(f"  Mean iota before stage 2 optimization: {vmec.mean_iota()}")
-pprint(f"  Quasisymmetry objective stage 2 before optimization: {qs.total()}")
-pprint(f"  Squared flux before stage 2 optimization: {Jf.J()}")
-pprint(f'Performing stage 2 optimization with ~{MAXITER_stage_2} iterations')
-res = minimize(fun_coils, dofs[:-number_vmec_dofs], jac=True, args=({'Nfeval': 0}), method='L-BFGS-B', options={'maxiter': MAXITER_stage_2, 'maxcor': 300}, tol=1e-7)
-bs.set_points(surf.gamma().reshape((-1, 3)))
-Bbs = bs.B().reshape((nphi_VMEC, ntheta_VMEC, 3))
-BdotN_surf = np.sum(Bbs * surf.unitnormal(), axis=2)
-if comm.rank == 0:
-    curves_to_vtk(curves, os.path.join(coils_results_path, "curves_after_stage2"))
-    pointData = {"B_N": BdotN_surf[:, :, None]}
-    surf.to_vtk(os.path.join(coils_results_path, "surf_after_stage2"), extra_data=pointData)
-pprint(f'Performing single stage optimization with ~{MAXITER_single_stage} iterations')
-pprint(f"  Aspect ratio before single stage optimization: {vmec.aspect()}")
-pprint(f"  Mean iota before single stage optimization: {vmec.mean_iota()}")
-pprint(f"  Quasisymmetry objective before single stage optimization: {qs.total()}")
-pprint(f"  Squared flux before single stage optimization: {Jf.J()}")
-BdotN_surf = np.sum(Bbs * surf.unitnormal(), axis=2)
-BdotN = np.mean(np.abs(BdotN_surf))
-BdotNmax = np.max(np.abs(BdotN_surf))
-outstr = f"  Coil parameters: ⟨B·n⟩={BdotN:.1e}, B·n max={BdotNmax:.1e}"
-outstr += f", ║∇J coils║={np.linalg.norm(JF.dJ()):.1e}, C-C-Sep={Jccdist.shortest_distance():.2f}"
-cl_string = ", ".join([f"{j.J():.1f}" for j in Jls])
-kap_string = ", ".join(f"{np.max(c.kappa()):.1f}" for c in base_curves)
-msc_string = ", ".join(f"{j.J():.1f}" for j in Jmscs)
-outstr += f" lengths=sum([{cl_string}])={sum(j.J() for j in Jls):.1f}, curv=[{kap_string}], msc=[{msc_string}]"
-pprint(outstr)
-x0 = np.copy(np.concatenate((JF.x, vmec.x)))
-dofs = np.concatenate((JF.x, vmec.x))
-with MPIFiniteDifference(prob.objective, mpi, diff_method=diff_method, abs_step=finite_difference_abs_step, rel_step=finite_difference_rel_step) as prob_jacobian:
-    if mpi.proc0_world:
-        res = minimize(fun, dofs, args=(prob_jacobian, {'Nfeval': 0}), jac=True, method='BFGS', options={'maxiter': MAXITER_single_stage}, tol=1e-7)
-mpi.comm_world.Bcast(dofs, root=0)
-Bbs = bs.B().reshape((nphi_VMEC, ntheta_VMEC, 3))
-BdotN_surf = np.sum(Bbs * surf.unitnormal(), axis=2)
-if comm.rank == 0:
-    curves_to_vtk(curves, os.path.join(coils_results_path, "curves_opt"))
-    pointData = {"B_N": BdotN_surf[:, :, None]}
-    surf.to_vtk(os.path.join(coils_results_path, "surf_opt"), extra_data=pointData)
-    cws.to_vtk(os.path.join(coils_results_path, "cws_opt"))
-bs.save(os.path.join(coils_results_path, "biot_savart_opt.json"))
-vmec.write_input(os.path.join(this_path, f'input.final'))
+for max_mode in max_modes:
+    surf.fix_all()
+    surf.fixed_range(mmin=0, mmax=max_mode, nmin=-max_mode, nmax=max_mode, fixed=False)
+    surf.fix("rc(0,0)")
+    number_vmec_dofs = int(len(surf.x))
+    qs = QuasisymmetryRatioResidual(vmec, quasisymmetry_target_surfaces, helicity_m=1, helicity_n=0)
+    objective_tuple = [(vmec.aspect, aspect_ratio_target, aspect_ratio_weight),
+                       (vmec.mean_iota, mean_iota_target, mean_iota_weight),
+                       (qs.residuals, 0, 1),
+    ]
+    prob = LeastSquaresProblem.from_tuples(objective_tuple)
+    dofs = np.concatenate((JF.x, vmec.x))
+    pprint(f"  Aspect ratio before stage 2 optimization: {vmec.aspect()}")
+    pprint(f"  Mean iota before stage 2 optimization: {vmec.mean_iota()}")
+    pprint(f"  Quasisymmetry objective stage 2 before optimization: {qs.total()}")
+    pprint(f"  Squared flux before stage 2 optimization: {Jf.J()}")
+    pprint(f'Performing stage 2 optimization with ~{MAXITER_stage_2} iterations')
+    res = minimize(fun_coils, dofs[:-number_vmec_dofs], jac=True, args=({'Nfeval': 0}), method='L-BFGS-B', options={'maxiter': MAXITER_stage_2, 'maxcor': 300}, tol=1e-7)
+    bs.set_points(surf.gamma().reshape((-1, 3)))
+    Bbs = bs.B().reshape((nphi_VMEC, ntheta_VMEC, 3))
+    BdotN_surf = np.sum(Bbs * surf.unitnormal(), axis=2)
+    if comm.rank == 0:
+        curves_to_vtk(curves, os.path.join(coils_results_path, "curves_after_stage2"))
+        pointData = {"B_N": BdotN_surf[:, :, None]}
+        surf.to_vtk(os.path.join(coils_results_path, "surf_after_stage2"), extra_data=pointData)
+    pprint(f'Performing single stage optimization with ~{MAXITER_single_stage} iterations')
+    pprint(f"  Aspect ratio before single stage optimization: {vmec.aspect()}")
+    pprint(f"  Mean iota before single stage optimization: {vmec.mean_iota()}")
+    pprint(f"  Quasisymmetry objective before single stage optimization: {qs.total()}")
+    pprint(f"  Squared flux before single stage optimization: {Jf.J()}")
+    BdotN_surf = np.sum(Bbs * surf.unitnormal(), axis=2)
+    BdotN = np.mean(np.abs(BdotN_surf))
+    BdotNmax = np.max(np.abs(BdotN_surf))
+    outstr = f"  Coil parameters: ⟨B·n⟩={BdotN:.1e}, B·n max={BdotNmax:.1e}"
+    outstr += f", ║∇J coils║={np.linalg.norm(JF.dJ()):.1e}, C-C-Sep={Jccdist.shortest_distance():.2f}"
+    cl_string = ", ".join([f"{j.J():.1f}" for j in Jls])
+    kap_string = ", ".join(f"{np.max(c.kappa()):.1f}" for c in base_curves)
+    msc_string = ", ".join(f"{j.J():.1f}" for j in Jmscs)
+    outstr += f" lengths=sum([{cl_string}])={sum(j.J() for j in Jls):.1f}, curv=[{kap_string}], msc=[{msc_string}]"
+    pprint(outstr)
+    x0 = np.copy(np.concatenate((JF.x, vmec.x)))
+    dofs = np.concatenate((JF.x, vmec.x))
+    with MPIFiniteDifference(prob.objective, mpi, diff_method=diff_method, abs_step=finite_difference_abs_step, rel_step=finite_difference_rel_step) as prob_jacobian:
+        if mpi.proc0_world:
+            res = minimize(fun, dofs, args=(prob_jacobian, {'Nfeval': 0}), jac=True, method='BFGS', options={'maxiter': MAXITER_single_stage}, tol=1e-4)
+    mpi.comm_world.Bcast(dofs, root=0)
+    Bbs = bs.B().reshape((nphi_VMEC, ntheta_VMEC, 3))
+    BdotN_surf = np.sum(Bbs * surf.unitnormal(), axis=2)
+    if comm.rank == 0:
+        curves_to_vtk(curves, os.path.join(coils_results_path, "curves_opt"))
+        pointData = {"B_N": BdotN_surf[:, :, None]}
+        surf.to_vtk(os.path.join(coils_results_path, "surf_opt"), extra_data=pointData)
+        cws.to_vtk(os.path.join(coils_results_path, "cws_opt"))
+    bs.save(os.path.join(coils_results_path, "biot_savart_opt.json"))
+    vmec.write_input(os.path.join(this_path, f'input.final'))
 pprint(f"  Aspect ratio after optimization: {vmec.aspect()}")
 pprint(f"  Mean iota after optimization: {vmec.mean_iota()}")
 pprint(f"  Quasisymmetry objective after optimization: {qs.total()}")
