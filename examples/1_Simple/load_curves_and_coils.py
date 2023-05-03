81fd2228a94d4f1ded736cf939119a70066e02f1
MigMadeira <miguel.madeira@tecnico.ulisboa.pt>
2023-05-03 15:13:10 +0100

    mixed my function with load_curves_from_file

--- examples/1_Simple/load_curves_and_coils.py
+++ examples/1_Simple/load_curves_and_coils.py
@@ -2,10 +2,10 @@ from simsopt.geo import CurveXYZFourier
 from simsopt.field import coils_via_file, Coil
 from simsopt.util.coil_util import *
 
-file = "inputs/coils.tester"
+#file = "inputs/coils.tester"
 #file = "inputs/coils.w7x_std_mc"
 #file = "inputs/coils.bigtest"
-
+file = "inputs/tester.dat"
 #------------------------------------------------------------------------------
 #curves = CurveXYZFourier.load_curves_from_coils_file(file, 19,ppp=5)
 #coils  = coils_via_file(file, 19,5)
@@ -22,4 +22,20 @@ file = "inputs/coils.tester"
 #print(import_current(file))
 ##print(importCoils_and_current(file))
 
-#-------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+#curves_old = CurveXYZFourier.load_curves_from_coils_file(file, 19,ppp=5)
+#curves_new = CurveXYZFourier.load_curves_from_file_new(file, 19,ppp=5,Cartesian=True)
+#currents = import_current(file)
+#
+#Coil.export_coils_in_cartesian("output_new", curves_new,currents, 1)
+#Coil.export_coils_in_cartesian("output_old", curves_old,currents, 1)
+#print(importCurves("output_new") == importCurves("output_old"))
+#---------------------------------------------------------------------------------------
+#curves_old = CurveXYZFourier.load_curves_from_file(file, 19,ppp=5)
+#curves_new = CurveXYZFourier.load_curves_from_file_new(file, 19,ppp=5)
+#print(len(curves_old))
+#currents = [1]*len(curves_old)
+#print(currents)
+#Coil.export_coils_in_cartesian("output_new", curves_new,currents, 1)
+#Coil.export_coils_in_cartesian("output_old", curves_old,currents, 1)
+#print(importCurves("output_new") == importCurves("output_old"))
