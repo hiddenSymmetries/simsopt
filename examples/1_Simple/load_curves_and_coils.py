3d8519da87218a7d36d3e9f54fde0bd136e5bce1
MigMadeira <miguel.madeira@tecnico.ulisboa.pt>
2023-05-04 16:25:11 +0100

    Added the maximum interations to control accuracy and did some adittional testing

--- examples/1_Simple/load_curves_and_coils.py
+++ examples/1_Simple/load_curves_and_coils.py
@@ -1,59 +1,156 @@
-from simsopt.geo import CurveXYZFourier
-from simsopt.field import  Coil, Current
-from simsopt.field.coil import coils_via_file , coils_to_makegrid
-from simsopt.util.coil_util import *
+#!/usr/bin/env python
+r"""In this example we solve a FOCUS like Stage II coil optimisation problem: the
+goal is to find coils that generate a specific target normal field on a given
+surface.  In this particular case we consider a vacuum field, so the target is
+just zero.
 
-file = "inputs/coils.tester"
-#file = "inputs/coils.w7x_std_mc"
-#file = "inputs/coils.bigtest"
-#file = "inputs/tester.dat"
-#------------------------------------------------------------------------------
-#curves = CurveXYZFourier.load_curves_from_coils_file(file, 19,ppp=5)
-#coils  = coils_via_file(file, 19,5)
-#
-#currents = [i.current.get_value() for i in coils]
-#print(len(curves))
-#print(currents)
-#curves_from_coil  = [i.curve for i in coils]
-#print(len(curves_from_coil))
-##Coil.export_coils_in_cartesian("output_with_coil", coils, NFP=1)
-#Coil.export_coils_in_cartesian("output_with_curves_from_coil&current", curves_from_coil,currents, 1)
-#Coil.export_coils("output_with_curves_from_coil&current2", coils, 1)
-#Coil.export_coils_in_cartesian("output_with_curve&current", curves,currents, 1)
-#print(import_current(file))
-##print(importCoils_and_current(file))
+The objective is given by
 
-#---------------------------------------------------------------------------------------
-#curves_old = CurveXYZFourier.load_curves_from_coils_file(file, 19,ppp=5)
-#curves_new = CurveXYZFourier.load_curves_from_file_new(file, 19,ppp=5,Cartesian=True)
-#currents = import_current(file)
-#
-#Coil.export_coils_in_cartesian("output_new", curves_new,currents, 1)
-#Coil.export_coils_in_cartesian("output_old", curves_old,currents, 1)
-#print(importCurves("output_new") == importCurves("output_old"))
-#---------------------------------------------------------------------------------------
-#curves_old = CurveXYZFourier.load_curves_from_file(file, 19,ppp=5)
-#curves_new = CurveXYZFourier.load_curves_from_file_new(file, 19,ppp=5)
-#print(len(curves_old))
-#currents = [1]*len(curves_old)
-#print(currents)
-#Coil.export_coils_in_cartesian("output_new", curves_new,currents, 1)
-#Coil.export_coils_in_cartesian("output_old", curves_old,currents, 1)
-#print(importCurves("output_new") == importCurves("output_old"))
-#---------------------------------------------------------------------------------------
-#coils_old = coils_via_file(file,19,ppp=5)
-#coils_new = coils_via_file_new(file, 19,ppp=5)
-#
-#Coil.export_coils("output_new", coils_new, 1)
-#Coil.export_coils("output_old", coils_old, 1)
-#print(importCurves("output_new") == importCurves("output_old"))
-#--------------------------------------------------------------------------------------
-curves_old = CurveXYZFourier.load_curves_from_file(file, 19,ppp=5)
-#curves_new = CurveXYZFourier.load_curves_from_file_new(file, 19,ppp=5,Cartesian=True)
+    J = (1/2) \int |B dot n|^2 ds
+        + LENGTH_WEIGHT * (LENGTH_TARGET - sum CurveLengths)^2
 
-currents = [Current(1)]*len(curves_old)
+If LENGTH_TARGET is increased, the coils
+become more complicated, but the target normal field may be
+achieved more accurately.
 
-coils_to_makegrid("output_old", curves_old,currents)
-#coils_to_makegrid("output_new", curves_new,currents)
+The target equilibrium is the QA configuration of arXiv:2108.03711.
 
-print(importCurves("output_new") == importCurves("output_old"))
+Better coils can be obtained by including more terms in the objective
+function to control the coil curvature and coil-to-coil distance. For
+an example of this more sophisticated coil optimization see
+examples/2_Intermediate/stage_two_optimization.py.
+
+"""
+
+import os
+from pathlib import Path
+import numpy as np
+from scipy.optimize import minimize
+
+from simsopt.geo import SurfaceRZFourier, create_equally_spaced_curves, \
+    CurveLength, curves_to_vtk, CurveXYZFourier
+from simsopt.field import Current, coils_via_symmetries, BiotSavart, coils_to_makegrid
+from simsopt.objectives import SquaredFlux, QuadraticPenalty
+
+# Number of unique coil shapes, i.e. the number of coils per half field period:
+# (Since the configuration has nfp = 2, multiply by 4 to get the total number of coils.)
+ncoils = 4
+
+# Major radius for the initial circular coils:
+R0 = 1.0
+
+# Minor radius for the initial circular coils:
+R1 = 0.5
+
+# Number of Fourier modes describing each Cartesian component of each coil:
+order = 5
+
+# Weight on the curve lengths in the objective function.
+LENGTH_WEIGHT = 1.0
+
+# Target value for the sum of the lengths of the base coils:
+LENGTH_TARGET = 18.0
+
+# Number of iterations to perform:
+ci = "CI" in os.environ and os.environ['CI'].lower() in ['1', 'true']
+MAXITER = 50 if ci else 300
+
+# File for the desired boundary magnetic surface:
+TEST_DIR = (Path(__file__).parent / ".." / ".." / "tests" / "test_files").resolve()
+filename = TEST_DIR / 'input.LandremanPaul2021_QA'
+
+# Directory for output
+OUT_DIR = "./output/"
+os.makedirs(OUT_DIR, exist_ok=True)
+
+#######################################################
+# End of input parameters.
+#######################################################
+
+# Initialize the boundary magnetic surface:
+nphi = 32
+ntheta = 32
+s = SurfaceRZFourier.from_vmec_input(filename, range="half period", nphi=nphi, ntheta=ntheta)
+
+# Create the initial coils:
+base_curves = create_equally_spaced_curves(ncoils, s.nfp, stellsym=True, R0=R0, R1=R1, order=order)
+base_currents = [Current(1.0) * 1e5 for i in range(ncoils)]
+# Since the target field is zero, one possible solution is just to set all
+# currents to 0. To avoid the minimizer finding that solution, we fix one
+# of the currents:
+base_currents[0].fix_all()
+
+coils = coils_via_symmetries(base_curves, base_currents, s.nfp, True)
+bs = BiotSavart(coils)
+bs.set_points(s.gamma().reshape((-1, 3)))
+
+curves = [c.curve for c in coils]
+curves_to_vtk(curves, OUT_DIR + "curves_init", close=True)
+pointData = {"B_N": np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)[:, :, None]}
+s.to_vtk(OUT_DIR + "surf_init", extra_data=pointData)
+
+# Define the individual terms objective function:
+Jf = SquaredFlux(s, bs)
+Jls = [CurveLength(c) for c in base_curves]
+
+# Form the total objective function. To do this, we can exploit the
+# fact that Optimizable objects with J() and dJ() functions can be
+# multiplied by scalars and added:
+JF = Jf + LENGTH_WEIGHT * QuadraticPenalty(sum(Jls), LENGTH_TARGET, "max")
+
+B_dot_n = np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
+print('Initial max|B dot n|:', np.max(np.abs(B_dot_n)))
+print('Names of the dofs:', JF.dof_names)
+
+# We don't have a general interface in SIMSOPT for optimisation problems that
+# are not in least-squares form, so we write a little wrapper function that we
+# pass directly to scipy.optimize.minimize
+
+
+def fun(dofs):
+    JF.x = dofs
+    return JF.J(), JF.dJ()
+
+
+print("""
+################################################################################
+### Perform a Taylor test ######################################################
+################################################################################
+""")
+f = fun
+dofs = JF.x
+np.random.seed(1)
+h = np.random.uniform(size=dofs.shape)
+J0, dJ0 = f(dofs)
+dJh = sum(dJ0 * h)
+for eps in [1e-3, 1e-4, 1e-5, 1e-6, 1e-7]:
+    J1, _ = f(dofs + eps*h)
+    J2, _ = f(dofs - eps*h)
+    print("err", (J1-J2)/(2*eps) - dJh)
+
+print("""
+################################################################################
+### Run the optimisation #######################################################
+################################################################################
+""")
+res = minimize(fun, dofs, jac=True, method='L-BFGS-B',
+               options={'maxiter': MAXITER, 'maxcor': 300, 'iprint': 5}, tol=1e-15)
+curves_to_vtk(curves, OUT_DIR + f"curves_opt", close=True)
+pointData = {"B_N": np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)[:, :, None]}
+s.to_vtk(OUT_DIR + "surf_opt", extra_data=pointData)
+
+B_dot_n = np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
+print('Final max|B dot n|:', np.max(np.abs(B_dot_n)))
+
+total_curve_length = sum(func.J() for func in Jls)
+print("Sum of lengths of base curves:", total_curve_length)
+assert total_curve_length < 1.1 * LENGTH_TARGET
+
+# Save the optimized coil shapes and currents so they can be loaded into other scripts for analysis:
+#bs.save(OUT_DIR + "biot_savart_opt.json")
+currents = [coil.current for coil in coils]
+curves_to_vtk(curves, OUT_DIR + "curves_opt", close=True)
+coils_to_makegrid("coils.test", curves, currents, groups=None, nfp=1, stellsym=False)
+curves_to_vtk(CurveXYZFourier.load_curves_from_file("coils.test",50,76),OUT_DIR + "curves_opt_after_loaded", close=True)
+coils_to_makegrid("coils.results", CurveXYZFourier.load_curves_from_file("coils.test",5,76), currents, groups=None, nfp=1, stellsym=False)
+
