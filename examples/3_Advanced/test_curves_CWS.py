29965d2cbe98360377c612f6622f7fc35449c783
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2023-06-07 11:03:37 +0100

    ran autopep

--- examples/3_Advanced/test_curves_CWS.py
+++ examples/3_Advanced/test_curves_CWS.py
@@ -9,16 +9,16 @@ this_path = str(Path(__file__).parent.resolve())
 
 nphi = 128
 ntheta = 64
-nfp_array = [2,3]
+nfp_array = [2, 3]
 
 ## This assumes that the script is run from the examples/3_Advanced/ folder
 for nfp in nfp_array:
-    if nfp==2:
-        coils_file = os.path.join(this_path,'optimization_cws_singlestage_nfp2_QA_ncoils3_axiTorus','coils','biot_savart_opt_maxmode3.json')
-        vmec_file  = os.path.join(this_path, 'optimization_cws_singlestage_nfp2_QA_ncoils3_axiTorus','input.maxmode3')
-    elif nfp==3:
-        coils_file = os.path.join(this_path,'optimization_cws_singlestage_nfp3_QA_ncoils4_axiTorus','coils','biot_savart_opt_maxmode4.json')
-        vmec_file  = os.path.join(this_path, 'optimization_cws_singlestage_nfp3_QA_ncoils4_axiTorus','input.maxmode4')
+    if nfp == 2:
+        coils_file = os.path.join(this_path, 'optimization_cws_singlestage_nfp2_QA_ncoils3_axiTorus', 'coils', 'biot_savart_opt_maxmode3.json')
+        vmec_file = os.path.join(this_path, 'optimization_cws_singlestage_nfp2_QA_ncoils3_axiTorus', 'input.maxmode3')
+    elif nfp == 3:
+        coils_file = os.path.join(this_path, 'optimization_cws_singlestage_nfp3_QA_ncoils4_axiTorus', 'coils', 'biot_savart_opt_maxmode4.json')
+        vmec_file = os.path.join(this_path, 'optimization_cws_singlestage_nfp3_QA_ncoils4_axiTorus', 'input.maxmode4')
     else:
         raise ValueError("nfp not supported")
 
@@ -40,10 +40,10 @@ for nfp in nfp_array:
     bs.set_points(surf.gamma().reshape((-1, 3)))
     BdotN = np.sum(bs.B().reshape((nphi, ntheta, 3)) * surf.unitnormal(), axis=2)
     pointData = {"B_N": BdotN[:, :, None]}
-    surf.to_vtk(os.path.join(this_path,f'surface_nfp{nfp}'), extra_data=pointData)
-    curves_to_vtk(curves, os.path.join(this_path,f'coils_nfp{nfp}'))
+    surf.to_vtk(os.path.join(this_path, f'surface_nfp{nfp}'), extra_data=pointData)
+    curves_to_vtk(curves, os.path.join(this_path, f'coils_nfp{nfp}'))
 
     BdotNmean = np.mean(np.abs(BdotN))
-    BdotNmax  = np.max(np.abs(BdotN))
+    BdotNmax = np.max(np.abs(BdotN))
     outstr = f"nfp={nfp}: Jf={Jf.J():.1e}, ⟨B·n⟩={BdotNmean:.1e}, B·n max={BdotNmax:.1e}"
     print(outstr)
