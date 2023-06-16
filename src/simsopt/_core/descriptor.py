62929b5a16cdead18f3e49e96958041eb0f1bdad
Bharat Medasani <mbkumar@gmail.com>
2023-06-16 09:05:58 -0400

    Add docstrings to descriptor classes

--- src/simsopt/_core/descriptor.py
+++ src/simsopt/_core/descriptor.py
@@ -4,11 +4,17 @@ from dataclasses import dataclass
 
 import numbers
 
-__all__ = ['OneofStrings', 'OneofIntegers', 'String', 'Integer', 'PositiveInteger',
-           'Float', 'PositiveFloat']
+__all__ = ['OneofStrings', 'OneofIntegers', 'String', 'Integer', 'Float',
+           'PositiveInteger', 'PositiveFloat']
 
 
 class Validator(ABC):
+    """
+    Validator is the abstract base class implementing the descriptor protocol.
+    It implements the ``__get__`` and ``__set__`` methods as well as the
+    ``__set_name__`` method of the descriptor protocol. The implementation
+    follows the strategy outlined in the official python documentation.
+    """
     def __set_name__(self, owner, name):
         self.public_name = name
         self.private_name = f"_{name}"
@@ -22,10 +28,20 @@ class Validator(ABC):
 
     @abstractmethod
     def validate(self, value):
+        """
+        All subclasses implement validate method to check for various constraints
+        """
         pass
 
 
 class OneOf(Validator):
+    """
+    OneOf is used to define descriptors which take only specific values
+    as inputs.
+
+    Args:
+        args: All the specific values that the descriptor can accept
+    """
     def __init__(self, *args):
         self.options = set(args)
 
@@ -38,6 +54,16 @@ class OneOf(Validator):
 
 @dataclass
 class String(Validator):
+    """
+    Validates that the input is a string and implements some other checks
+    for string object.
+
+    Args:
+        minsize: Minimum size of the input string
+        maxsize: Maximum size of the input string
+        predicate: Callable function that is used to do any other checks
+            on the string
+    """
     minsize: int = None
     maxsize: int = None
     predicate: Callable[[str], bool] = None
@@ -57,7 +83,10 @@ class String(Validator):
         super().validate(value)
 
 
-class Real(Validator):
+class _Real(Validator):
+    """
+    Validates that the input is a real number.
+    """
     def validate(self, value):
         if not isinstance(value, numbers.Real):
             raise TypeError(
@@ -65,7 +94,10 @@ class Real(Validator):
         super().validate(value)
 
 
-class Integral(Validator):
+class _Integral(Validator):
+    """
+    Validates that the input is an integer.
+    """
     def validate(self, value):
         if not isinstance(value, numbers.Integral):
             raise TypeError(
@@ -75,6 +107,13 @@ class Integral(Validator):
 
 @dataclass
 class RangeChecker(Validator):
+    """
+    Validates that the input number is within specific bounds.
+
+    Args:
+        min_value: Minimum acceptable value
+        max_value: Maximum acceptable value
+    """
     min_value: numbers.Real = None
     max_value: numbers.Real = None
 
@@ -89,11 +128,18 @@ class RangeChecker(Validator):
 
 
 class PositiveChecker(RangeChecker):
+    """
+    Validates that the input number is a positive number.
+    """
     def __init__(self):
         super().__init__(min_value=0)
 
 
-class Integer(Integral, RangeChecker):
+class Integer(_Integral, RangeChecker):
+    """
+    Validates that the input number is an integer. Optionally the user can specify
+    the bounds for the number.
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -102,11 +148,18 @@ class Integer(Integral, RangeChecker):
 
 
 class PositiveInteger(Integer, PositiveChecker):
+    """
+    Validates that the input number is a positive integer number.
+    """
     def validate(self, value):
         super().validate(value)
 
 
-class Float(Real, RangeChecker):
+class Float(_Real, RangeChecker):
+    """
+    Validates that the input number is a real number. Optionally the user can specify
+    the bounds for the number
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -114,12 +167,21 @@ class Float(Real, RangeChecker):
         super().validate(value)
 
 
-class PositiveFloat(Real, PositiveChecker):
+class PositiveFloat(_Real, PositiveChecker):
+    """
+    Validates that the input number is a positive real number.
+    """
     def validate(self, value):
         super().validate(value)
 
 
 class OneofStrings(String, OneOf):
+    """
+    Validates that the input is among the specified strings.
+
+    Args:
+        *args: All the acceptable values for the string
+    """
     def __init__(self, *args):
         OneOf.__init__(self, *args)
 
@@ -127,7 +189,13 @@ class OneofStrings(String, OneOf):
         super().validate(value)
 
 
-class OneofIntegers(Integral, OneOf):
+class OneofIntegers(_Integral, OneOf):
+    """
+    Validates that the input is among the specified integers.
+
+    Args:
+        *args: All the acceptable values for the integer
+    """
     def __init__(self, *args):
         OneOf.__init__(self, *args)
 
