2aa45139e16322863b27ac0f27afade915b81046
Matt Landreman <mattland@umd.edu>
2023-07-18 15:55:46 -0400

    Updated free-boundary vmec example

--- examples/2_Intermediate/free_boundary_vmec.py
+++ examples/2_Intermediate/free_boundary_vmec.py
@@ -1,39 +1,70 @@
 #!/usr/bin/env python
 
+"""
+This example shows how to take coils in simsopt, write an mgrid file, and then
+run free-boundary vmec. No optimization of the plasma or coil shapes is
+performed in this example.
+"""
+
 from pathlib import Path
 import numpy as np
-from simsopt.field.biotsavart import BiotSavart
-from simsopt.field.coil import Current, coils_via_symmetries, ScaledCurrent
-from simsopt.geo.curve import curves_to_vtk
-from simsopt.geo.curvexyzfourier import CurveXYZFourier
-from simsopt.geo.surfacerzfourier import SurfaceRZFourier
+from simsopt.configs import get_w7x_data
+from simsopt.field import BiotSavart, coils_via_symmetries
+from simsopt.mhd import Vmec
+from simsopt.util import MpiPartition
 
-# File for the desired boundary magnetic surface:
-TEST_DIR = (Path(__file__).parent / ".." / ".." / "tests" / "test_files").resolve()
-filename = TEST_DIR / 'input.LandremanPaul2021_QA'
+nfp = 5
 
-nphi = 32
-ntheta = 32
-s = SurfaceRZFourier.from_vmec_input(filename, range="half period", nphi=nphi, ntheta=ntheta)
-
-ncoils = 4
-nfp = 2
-base_curves = [CurveXYZFourier(200, 16) for i in range(ncoils)]
-base_currents = []
-for i in range(ncoils):
-    curr = Current(1.)
-    if i == 0:
-        curr.fix_all()
-    base_currents.append(ScaledCurrent(curr, 1e5))
-
-coils = coils_via_symmetries(base_curves, base_currents, nfp, True)
+# Load in some coils
+curves, currents, magnetic_axis = get_w7x_data()
+coils = coils_via_symmetries(curves, currents, nfp, True)
 bs = BiotSavart(coils)
-bs.x = np.loadtxt(TEST_DIR / "coils_wechsung_pnas_qa24.txt")
 
-bs.set_points(s.gamma().reshape((-1, 3)))
-np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
+# Number of grid points in the toroidal angle:
+nphi = 24
 
-curves = [c.curve for c in coils]
-curves_to_vtk(curves, "/tmp/curves_init")
-pointData = {"B_N": np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)[:, :, None]}
-s.to_vtk("/tmp/surf_init", extra_data=pointData)
+mgrid_file = "mgrid.w7x.nc"
+bs.to_mgrid(
+    mgrid_file,
+    nr=64,
+    nz=65,
+    nphi=nphi,
+    rmin=4.5,
+    rmax=6.3,
+    zmin=-1.0,
+    zmax=1.0,
+    nfp=nfp,
+)
+
+# Create a VMEC object from an input file:
+TEST_DIR = (Path(__file__).parent / ".." / ".." / "tests" / "test_files").resolve()
+input_file = str(TEST_DIR / "input.W7-X_standard_configuration")
+
+mpi = MpiPartition(1)
+vmec = Vmec(input_file, mpi=mpi)
+
+# That input file was for fixed-boundary. We need to change some of 
+# the vmec input parameters for a free-boundary calculation:
+vmec.indata.lfreeb = True
+vmec.indata.mgrid_file = mgrid_file
+vmec.indata.nzeta = nphi
+# All the coils are written into a single "current group", so we only need to
+# set a single entry in vmec's "extcur" array:
+vmec.indata.extcur[0] = 1.0
+
+# Lower the resolution, so the example runs faster:
+vmec.indata.mpol = 6
+vmec.indata.ntor = 6
+vmec.indata.ns_array[2] = 0
+ftol = 1e-10
+vmec.indata.ftol_array[1] = ftol
+
+vmec.run()
+
+assert vmec.wout.fsql < ftol
+assert vmec.wout.fsqr < ftol
+assert vmec.wout.fsqz < ftol
+assert vmec.wout.ier_flag == 0
+np.testing.assert_allclose(vmec.wout.volume_p, 28.6017247168422, rtol=0.01)
+np.testing.assert_allclose(vmec.wout.rmnc[0, 0], 5.561878306096512, rtol=0.01)
+np.testing.assert_allclose(vmec.wout.bmnc[0, 1], 2.78074392223658, rtol=0.01)
