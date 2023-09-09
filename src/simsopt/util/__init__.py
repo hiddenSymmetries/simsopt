30c733373e26293dd3e1e5dd23bba647a5c6415a
Elizabeth <ejpaul@umd.edu>
2023-09-09 14:32:08 -0400

    Including in_github_actions.

--- src/simsopt/util/__init__.py
+++ src/simsopt/util/__init__.py
@@ -1,4 +1,13 @@
+import os
+
 from .mpi import *
 from .logger import *
 
-__all__ = (mpi.__all__ + logger.__all__)
+"""Boolean indicating if we are in the GitHub actions CI"""
+in_github_actions = "CI" in os.environ and os.environ['CI'].lower() in ['1', 'true']
+
+__all__ = (
+    mpi.__all__ 
+    + logger.__all__ 
+    + ['in_github_actions']
+)
