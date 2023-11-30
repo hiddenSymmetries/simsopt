5051c450859a94d61b903928dcb9d8fd4ae58e3a
Kenneth Hammond <khammond@pppl.gov>
2023-11-30 09:19:12 -0500

    Introduce ToroidalWireframe class
    This commit provides a basic implementation of the ToroidalWireframe
    class. The class includes a constructor that defines node points,
    maps wire segments to the nodes, and builds a constraint matrix
    to enforce current continuity at each node. It also includes a basic
    plotting method to visualize the wireframe.

---
+++ src/simsopt/geo/wireframe.py
@@ -0,0 +1,186 @@
+"""
+wireframe.py
+
+Definitions for the ToroidalWireframe class
+"""
+
+import numpy as np
+import warnings
+from simsopt.geo.surface import Surface
+from simsopt.geo.surfacerzfourier import SurfaceRZFourier
+
+class ToroidalWireframe(object):
+    """
+    ``ToroidalWireframe`` is a wireframe grid whose nodes are placed on a
+    toroidal surface as a 2D grid with regular spacing in the poloidal and
+    toroidal dimensions.
+
+    Currently only supports surfaces that exhibit stellarator symmetry.
+
+    Parameters
+    ----------
+        surface: SurfaceRZFourier class instance
+            Toroidal surface on which on which the nodes will be placed.
+        nPhi: integer
+            Number of wireframe nodes per half-period in the toroidal dimension.
+            Must be even; if an odd number is provided, it will be incremented
+            by one.
+        nTheta: integer
+            Number of wireframe nodes in the poloidal dimension. Must be even;
+            if an odd number is provided, it will be incremented by one.
+    """
+
+    def __init__(self, surface, nPhi, nTheta):
+
+        if not isinstance(surface, SurfaceRZFourier):
+            raise ValueError('Surface must be a SurfaceRZFourier object')
+
+        if not surface.stellsym:
+            raise ValueError('Surfaces without stellarator symmetry are not ' \
+                             + 'currently supported in the ToroidalWireframe ' \
+                             + 'class')
+
+        if not isinstance(nTheta, int) or not isinstance(nPhi, int):
+            raise ValueError('nTheta and nPhi must be integers')
+
+        if nTheta % 2:
+            nTheta = nTheta + 1
+            warnings.warn('nTheta must be even. Increasing to %d' % (nTheta))
+
+        if nPhi % 2:
+            nPhi = nPhi + 1
+            warnings.warn('nPhi must be even. Increasing to %d' % (nPhi))
+
+        # Make copy of surface with quadrature points according to nTheta, nPhi
+        qpoints_phi = list(np.linspace(0, 0.5/surface.nfp, nPhi+1))
+        qpoints_theta = list(np.linspace(0, 1., nTheta, endpoint=False))
+        self.surface = SurfaceRZFourier(nfp=surface.nfp, stellsym=True, \
+                                        mpol=surface.mpol, ntor=surface.ntor, \
+                                        quadpoints_phi=qpoints_phi, \
+                                        quadpoints_theta=qpoints_theta, \
+                                        dofs=surface.dofs)
+
+        # Determine the locations of the node points
+        nodes = self.surface.gamma()
+        self.nodes_x = np.ascontiguousarray(nodes[:, :, 0].reshape((-1)))
+        self.nodes_y = np.ascontiguousarray(nodes[:, :, 1].reshape((-1)))
+        self.nodes_z = np.ascontiguousarray(nodes[:, :, 2].reshape((-1)))
+        self.nNodes = len(self.nodes_x)
+        node_inds = np.arange(self.nNodes).reshape(nodes.shape[:2])
+
+
+        # Define the segments according to the pairs of nodes connecting them
+        self.nTorSegments = nTheta*nPhi
+        self.nPolSegments = nTheta*nPhi
+        self.nSegments = self.nTorSegments + self.nPolSegments
+
+        # Toroidal segments 
+        segments_tor = np.zeros((self.nTorSegments, 2))
+        segments_tor[:,0] = node_inds[:-1, :].reshape((self.nTorSegments))
+        segments_tor[:,1] = node_inds[1:,  :].reshape((self.nTorSegments))
+
+        # Map nodes to index in the segment array of segment originating 
+        # from the respective node
+        self.torSegmentKey = -np.ones(nodes.shape[:2]).astype(np.int64)
+        self.torSegmentKey[:-1,:] = \
+            np.arange(self.nTorSegments).reshape((nPhi, nTheta))
+
+        # Poloidal segments (on symmetry planes, only include segments for z>0)
+        segments_pol = np.zeros((self.nPolSegments, 2))
+        self.polSegmentKey = -np.ones(nodes.shape[:2]).astype(np.int64)
+        HalfNTheta = int(nTheta/2)
+
+        segments_pol[:HalfNTheta, 0] = node_inds[0, :HalfNTheta]
+        segments_pol[:HalfNTheta, 1] = node_inds[0, 1:HalfNTheta+1]
+        self.polSegmentKey[0, :HalfNTheta] = np.arange(HalfNTheta) + self.nTorSegments
+        for i in range(1, nPhi):
+            polInd0 = HalfNTheta + (i-1)*nTheta
+            polInd1 = polInd0 + nTheta
+            segments_pol[polInd0:polInd1, 0] = node_inds[i, :]
+            segments_pol[polInd0:polInd1-1, 1] = node_inds[i, 1:]
+            segments_pol[polInd1-1, 1] = node_inds[i, 0]
+            self.polSegmentKey[i, :] = np.arange(polInd0, polInd1) + self.nTorSegments
+
+        segments_pol[-HalfNTheta:, 0] = node_inds[-1, :HalfNTheta]
+        segments_pol[-HalfNTheta:, 1] = node_inds[-1, 1:HalfNTheta+1]
+        self.polSegmentKey[-1, :HalfNTheta] = \
+            np.arange(self.nPolSegments-HalfNTheta, self.nPolSegments) + self.nTorSegments
+
+        # Join the toroidal and poloidal segments into a single array
+        self.segments = \
+            np.ascontiguousarray(np.zeros((self.nSegments, 2)).astype(np.int64))
+        self.segments[:self.nTorSegments, :] = segments_tor[:, :]
+        self.segments[self.nTorSegments:, :] = segments_pol[:, :]
+
+        # Define constraints to enforce current continuity
+        # Constraint equations have the form B*x = d, where:
+        #   x is the array of currents in each segment
+        #   B is a matrix of coefficients of the currents in each equation
+        #   d is an array of constant terms on the RHS of each equation
+
+        self.nConstraints = self.nTorSegments - 2
+        self.constraints_B = \
+            np.ascontiguousarray(np.zeros((self.nConstraints, self.nSegments)))
+        self.constraints_d = np.ascontiguousarray(np.zeros((self.nConstraints)))
+
+        # Populate B matrix with coefficients to enforce continuity at each node
+        count = 0
+        for i in range(nPhi+1):
+            for j in range(nTheta):
+
+                if i == 0:
+                    if j == 0 or j >= nTheta/2:
+                        continue
+                    ind_tor_in  = self.torSegmentKey[i, nTheta-j]
+                    ind_tor_out = self.torSegmentKey[i, j]
+                    ind_pol_in  = self.polSegmentKey[i, j-1]
+                    ind_pol_out = self.polSegmentKey[i, j]
+
+                elif i > 0 and i < nPhi:
+                    ind_tor_in  = self.torSegmentKey[i-1, j]
+                    ind_tor_out = self.torSegmentKey[i, j]
+                    if j == 0:
+                        ind_pol_in  = self.polSegmentKey[i, nTheta-1]
+                    else:
+                        ind_pol_in  = self.polSegmentKey[i, j-1]
+                    ind_pol_out = self.polSegmentKey[i, j]
+
+                else:
+                    if j == 0 or j >= nTheta/2:
+                        continue
+                    ind_tor_in  = self.torSegmentKey[i-1, j]
+                    ind_tor_out = self.torSegmentKey[i-1, nTheta-j]
+                    ind_pol_in  = self.polSegmentKey[i, j-1]
+                    ind_pol_out = self.polSegmentKey[i, j]
+
+                self.constraints_B[count, [ind_tor_in,  ind_pol_in ]] = -1
+                self.constraints_B[count, [ind_tor_out, ind_pol_out]] = 1
+                count = count + 1
+
+    def make_plot(self):
+        """
+        Make a plot of the wireframe grid, including nodes and segments.
+        """
+
+        import matplotlib.pylab as pl
+        
+        fig = pl.figure()
+        ax = fig.add_subplot(projection='3d')
+        ax.plot(self.nodes_x, self.nodes_y, self.nodes_z, '.', \
+                color=(0,0,0), markersize=6)
+        for i in range(self.nTorSegments):
+            ax.plot(self.nodes_x[self.segments[i,:]],
+                    self.nodes_y[self.segments[i,:]],
+                    self.nodes_z[self.segments[i,:]],
+                    '-', color=(0.8,0,0), linewidth=1)
+        for i in range(self.nPolSegments):
+            ax.plot(self.nodes_x[self.segments[i+self.nTorSegments,:]],
+                    self.nodes_y[self.segments[i+self.nTorSegments,:]],
+                    self.nodes_z[self.segments[i+self.nTorSegments,:]],
+                    '-', color=(0,0,0.8), linewidth=1)
+        ax.set_aspect('equal')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        pl.show()
+
