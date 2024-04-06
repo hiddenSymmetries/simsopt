9a162dd68060306a2d437264fa90cd262e1540d3
Kenneth Hammond <khammond@pppl.gov>
2024-04-06 17:54:07 -0400

    Add unit tests for ToroidalWireframe class
    A set of unit tests have been added to the new file
    tests/field/test_wireframefield.py. Each one verifies the accuracy
    of various functionalities of the WireframeField class.
    
    Tests include:
        - Calculation of the field from a square current loop formed on a
          wireframe
        - Verifying that evenly distributed poloidal currents on a
          wireframe will create a field that converges to an ideal toroidal
          field as the poloidal resolution increases
        - Checking that a current distribution that satisfies the
          continuity constraints creates a curl-free field, whereas a
          distribution that violates the constraints creates a field with
          nonzero curl
        - Verifying that the fields from a wireframe with net toroidal and
          poloidal currents produce the correct line integrals around
          Amperian loops
        - Comparing the squared flux calculated from the inductance matrix
          with the same quantity calculated by the SquaredFlux objective
          function

--- src/simsopt/geo/__init__.py
+++ src/simsopt/geo/__init__.py
@@ -23,6 +23,7 @@ from .surfacerzfourier import *
 from .surfacexyzfourier import *
 from .surfacexyztensorfourier import *
 from .strain_optimization import *
+from .wireframe import *
 
 from .permanent_magnet_grid import *
 
@@ -37,4 +38,4 @@ __all__ = (curve.__all__ + curvehelical.__all__ +
            surfacerzfourier.__all__ + surfacexyzfourier.__all__ +
            surfacexyztensorfourier.__all__ + surfaceobjectives.__all__ +
            strain_optimization.__all__ + framedcurve.__all__ + 
-           permanent_magnet_grid.__all__)
+           wireframe.__all__ + permanent_magnet_grid.__all__)
---
+++ tests/field/test_wireframefield.py
@@ -0,0 +1,233 @@
+import unittest
+from pathlib import Path
+import numpy as np
+from simsopt.geo import CurveXYZFourier, SurfaceRZFourier, ToroidalWireframe
+from simsopt.field import WireframeField, enclosed_current
+from simsopt.objectives import SquaredFlux
+
+TEST_DIR = (Path(__file__).parent / ".." / "test_files").resolve()
+
+def surf_torus_221():
+    surf = SurfaceRZFourier(nfp=2, mpol=1, ntor=0)
+    surf.set_rc(0, 0, 2)
+    surf.set_rc(1, 0, 1)
+    surf.set_zs(1, 0, 1)
+    return surf
+
+class Testing(unittest.TestCase):
+
+    def test_toroidal_wireframe_field_from_square_loop(self):
+        '''
+        Compares field calculated at the center of a current-carrying square 
+        loop with the analytical result. The square lies in the XY plane with 
+        its vertices at (1,0), (0,1), (-1,0), and (0,-1).
+        '''
+
+        curr = 1e6
+
+        surf_wf = SurfaceRZFourier(nfp=1, mpol=1, ntor=0)
+        surf_wf.set_rc(0, 0, 2)
+        surf_wf.set_rc(1, 0, 1)
+        surf_wf.set_zs(1, 0, 1)
+
+        test_wf = ToroidalWireframe(surf_wf, 2, 4)
+        test_wf.currents[[2,6]] = curr
+        self.assertTrue(test_wf.check_constraints())
+
+        pts_test = np.zeros((1,3))
+        B_analytic = np.array([0, 0, 8*1e-7*curr])
+        dBdX_analytic = np.zeros((3,3))
+
+        field_wf = WireframeField(test_wf)
+        field_wf.set_points(pts_test)
+        assert np.allclose(field_wf.B(), B_analytic)
+        assert np.allclose(field_wf.dB_by_dX(), dBdX_analytic)
+ 
+    def test_toroidal_wireframe_toroidal_field_convergence(self):
+        '''
+        Verifies that the field inside a ToroidalWireframe with poloidal 
+        currents approaches an ideal toroidal field as the toroidal wireframe 
+        resolution increases.
+        '''
+
+        curr = 1e6
+
+        # Test points inside the wireframe where field is evaluated
+        n_test_pts = 10
+        rtest = np.linspace(1.7, 2.3, n_test_pts)
+        ztest = np.linspace(-0.3, 0.3, n_test_pts)
+        ptest = np.linspace(0, 0.5*np.pi, n_test_pts)
+        xtest = rtest*np.cos(ptest)
+        ytest = rtest*np.sin(ptest)
+
+        test_pts = np.zeros((n_test_pts, 3))
+        test_pts[:,0] = xtest[:]
+        test_pts[:,1] = ytest[:]
+        test_pts[:,2] = ztest[:]
+
+        # Analytic field at each test point
+        modB = -2*1e-7*curr/rtest
+        B_analytic = np.zeros((n_test_pts, 3))
+        B_analytic[:,0] = -modB*np.sin(ptest)
+        B_analytic[:,1] =  modB*np.cos(ptest)
+ 
+        # Analytic field Jacobian at each test point
+        dBdX_analytic = np.zeros((n_test_pts, 3, 3))
+        dBdX_analytic[:,0,0] =  modB/rtest * np.sin(2*ptest)
+        dBdX_analytic[:,0,1] =  modB/rtest * (np.sin(ptest)**2-np.cos(ptest)**2)
+        dBdX_analytic[:,1,0] =  modB/rtest * (np.sin(ptest)**2-np.cos(ptest)**2)
+        dBdX_analytic[:,1,1] = -modB/rtest * np.sin(2*ptest)
+
+        err_B = np.Inf
+        err_dBdX = np.Inf
+        for i in range(10,15): 
+
+            # Set up a wireframe on an ideal torus
+            nPhi = 2*i
+            test_wf = ToroidalWireframe(surf_torus_221(), nPhi, 4)
+            test_wf.currents[-test_wf.nPolSegments:] = curr/(2*nPhi*test_wf.nfp)
+            self.assertTrue(test_wf.check_constraints())
+
+            field_wf = WireframeField(test_wf)
+            field_wf.set_points(test_pts)
+            new_err_B = np.max(np.abs(field_wf.B() - B_analytic))
+            new_err_dBdX = np.max(np.abs(field_wf.dB_by_dX() - dBdX_analytic))
+            #print('%.8e  %.8e  %.8e  %.8e' % (new_err_B, new_err_dBdX, \
+            #          new_err_B/err_B, new_err_dBdX/err_dBdX))
+            assert new_err_B < 0.5*err_B
+            assert new_err_dBdX < 0.5*err_dBdX
+            err_B = new_err_B
+            err_dBdX = new_err_dBdX
+
+    def test_toroidal_wireframe_curlB(self):
+        '''
+        Check that the curl of the magnetic field produced by a wireframe is
+        zero if current continuity is satisfied and nonzero otherwise.
+        '''
+
+        cur_pol = 1e6
+        cur_tor = 2e6
+
+        # Test points inside the wireframe where field is evaluated
+        n_test_pts = 10
+        rtest = np.linspace(1.7, 2.3, n_test_pts)
+        ztest = np.linspace(-0.3, 0.3, n_test_pts)
+        ptest = np.linspace(0, 0.5*np.pi, n_test_pts)
+        xtest = rtest*np.cos(ptest)
+        ytest = rtest*np.sin(ptest)
+
+        test_pts = np.zeros((n_test_pts, 3))
+        test_pts[:,0] = xtest[:]
+        test_pts[:,1] = ytest[:]
+        test_pts[:,2] = ztest[:]
+
+        # Set up the wireframe
+        nPhi = 4
+        nTheta = 6
+        test_wf = ToroidalWireframe(surf_torus_221(), nPhi, nTheta)
+
+        # Currents satisfying the constraints
+        test_wf.currents[-test_wf.nPolSegments:] = -cur_pol/(2*nPhi*test_wf.nfp)
+        test_wf.currents[:test_wf.nTorSegments] = -cur_tor/nTheta
+        self.assertTrue(test_wf.check_constraints())
+
+        # Check that curlB is zero at each test point
+        field_wf = WireframeField(test_wf)
+        field_wf.set_points(test_pts)
+        dBdX = field_wf.dB_by_dX()
+        for i in range(n_test_pts):
+            assert np.allclose(dBdX[i,:,:], dBdX[i,:,:].T)
+
+        # Replace currents with values that do NOT satisfy the constraints
+        test_wf.currents[:] = 0
+        test_wf.currents[-int(0.5*test_wf.nPolSegments):] = -cur_pol
+        test_wf.currents[:int(0.5*test_wf.nTorSegments)] = -cur_tor
+        self.assertFalse(test_wf.check_constraints())
+
+        # Check that curlB is not zero at each test point
+        field_wf = WireframeField(test_wf)
+        field_wf.set_points(test_pts)
+        dBdX = field_wf.dB_by_dX()
+        for i in range(n_test_pts):
+            assert not np.allclose(dBdX[i,:,:], dBdX[i,:,:].T)
+
+    def test_toroidal_wireframe_amperian_loops(self):
+        '''
+        Verifies that the prescribed total toroidal and poloidal currents in 
+        a wireframe agree with calculations of the field along Amperian loops
+        '''
+
+        cur_pol = 1e6
+        cur_tor = 2e6
+
+        # Surface on which wireframe is constructed
+        surf = surf_torus_221()
+
+        # Set up the wireframe
+        nPhi = 4
+        nTheta = 6
+        test_wf = ToroidalWireframe(surf, nPhi, nTheta)
+        test_wf.currents[-test_wf.nPolSegments:] = -cur_pol/(2*nPhi*test_wf.nfp)
+        test_wf.currents[:test_wf.nTorSegments] = -cur_tor/nTheta
+        self.assertTrue(test_wf.check_constraints())
+
+        # Wireframe field class instance
+        field_wf = WireframeField(test_wf)
+
+        # Amperian loop through which poloidal current flows
+        amploop = CurveXYZFourier(10, 1)
+        amploop.set('xc(1)', surf.get_rc(0,0))
+        amploop.set('ys(1)', surf.get_rc(0,0))
+        amploop_curr = enclosed_current(amploop, field_wf, 200)
+        #print('    Enclosed poloidal current: %.8e' % (amploop_curr))
+        #print('                    (expected: %.8e)' % (cur_pol))
+        assert np.allclose(amploop_curr, cur_pol)
+     
+        # Amperian loop through which toroidal current flows
+        amploop = CurveXYZFourier(10, 1)
+        amploop.set('xc(0)', surf.get_rc(0,0))
+        amploop.set('xc(1)', 2*surf.get_rc(1,0))
+        amploop.set('zs(1)', 2*surf.get_zs(1,0))
+        amploop_curr = enclosed_current(amploop, field_wf, 200)
+        #print('    Enclosed toroidal current: %.8e' % (amploop_curr))
+        #print('                    (expected: %.8e)' % (cur_tor))
+        assert np.allclose(amploop_curr, cur_tor)
+ 
+    def test_toroidal_wireframe_squared_flux(self):
+        '''
+        Verifies that the squared flux on a plasma boundary calculated through
+        the inductance matrix matches the calculation of the SquaredFlux class
+        '''
+
+        # Net poloidal and toroidal current (just needs to be nontrivial)
+        cur_pol = 1e6
+        cur_tor = 2e6
+
+        # Use the rotating ellipse as the plasma boundary
+        plas_fname = TEST_DIR / 'input.rotating_ellipse'
+        surf_plas = SurfaceRZFourier.from_vmec_input(plas_fname)
+
+        # Set up the wireframe
+        nPhi = 2
+        nTheta = 4
+        surf_wf = SurfaceRZFourier.from_vmec_input(plas_fname)
+        surf_wf.extend_via_normal(1.0)
+        test_wf = ToroidalWireframe(surf_wf, nPhi, nTheta, \
+                                    constraint_tol = cur_pol*1e-12)
+        test_wf.currents[-test_wf.nPolSegments:] = -cur_pol/(2*nPhi*test_wf.nfp)
+        test_wf.currents[:test_wf.nTorSegments] = -cur_tor/nTheta
+        self.assertTrue(test_wf.check_constraints())
+
+        # Get the inductance matrix
+        field_wf = WireframeField(test_wf)
+        field_wf.set_points(surf_plas.gamma().reshape((-1,3)))
+        Amat = field_wf.dBnormal_by_dsegmentcurrents_matrix(\
+                   surf_plas, area_weighted=True)
+
+        # Squared flux integral calculated in two ways
+        SqFlux_mat = 0.5*np.sum(\
+                         np.matmul(Amat, test_wf.currents.reshape((-1,1)))**2)
+        SqFlux_ref = SquaredFlux(surf_plas, field_wf).J()
+
+        assert np.allclose(SqFlux_mat, SqFlux_ref)
+
