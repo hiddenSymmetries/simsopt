Looks like derivative of L with respect to orientations is now working. Had to fix alphas and deltas to be between -pi and pi to play nice with arctan2 function in numpy and consistently avoid changing the angles when I manipulate them. Started writing up explicit function to compute dB_dkappa which is needed to compute derivatives of A and psi part of objective.

src/simsopt/geo/psc_grid.py
@@ -291,8 +291,8 @@ class PSCgrid:
         # Randomly initialize the coil orientations
         random_initialization = kwargs.pop("random_initialization", False)
         if random_initialization:
-            psc_grid.alphas = np.random.rand(psc_grid.num_psc) * 2 * np.pi
-            psc_grid.deltas = np.random.rand(psc_grid.num_psc) * 2 * np.pi
+            psc_grid.alphas = (np.random.rand(psc_grid.num_psc) - 0.5) * 2 * np.pi
+            psc_grid.deltas = (np.random.rand(psc_grid.num_psc) - 0.5) * 2 * np.pi
             psc_grid.coil_normals = np.array(
                 [np.cos(psc_grid.alphas) * np.sin(psc_grid.deltas),
                   -np.sin(psc_grid.alphas),
@@ -311,8 +311,9 @@ class PSCgrid:
                 np.logical_and(np.isclose(psc_grid.coil_normals, 0.0), 
                                np.copysign(1.0, psc_grid.coil_normals) < 0)
                 ] *= -1.0
-            deltas = np.arctan2(psc_grid.coil_normals[:, 0], psc_grid.coil_normals[:, 2])
-            alphas = -np.arcsin(psc_grid.coil_normals[:, 1])
+            deltas = np.arctan2(psc_grid.coil_normals[:, 0], psc_grid.coil_normals[:, 2])  #+ np.pi
+            alphas = -np.arctan2(psc_grid.coil_normals[:, 1] * np.cos(deltas), psc_grid.coil_normals[:, 2])
+            # alphas = -np.arcsin(psc_grid.coil_normals[:, 1])
             
         psc_grid.setup_full_grid()
         t2 = time.time()
@@ -683,7 +684,7 @@ class PSCgrid:
         t1 = time.time()
         grad_A2 = -self.A_matrix @ (np.tensordot(
             np.tensordot(
-            self.L_inv, self.L_deriv(), axes=([-1], [1])
+            self.L_deriv(), self.L_inv, axes=([1], [0])
             ), self.L_inv, axes=([-1], [0])
             ) @ self.psi_total)[:self.num_psc, :]
         Nsym_psc = grad_A2.shape[-1]
@@ -764,11 +765,34 @@ class PSCgrid:
                 #     ) @ self.plasma_points[j, :]
                 #     ) @ self.dRi_ddeltak[i, :, :].T
                 #     ) @ self.plasma_unitnormals[j, :] / I_i
-        print('A = ', term0.T, self.A_matrix)
+        # print('A = ', term0.T, self.A_matrix)
         # exit()
         # print(term1[0, :].T, term2[0, :].T)
         # exit()
         return (term1 + term2) # .T
+    
+    def dB_dkappa(self):
+        """
+        Should return gradient of the magnetic field at each point
+        Returns
+        -------
+            dB_dkappa: 3D numpy array, shape (2 * num_psc, num_psc, num_plasma_points) 
+                The gradient of the L matrix with respect to the PSC angles
+                alpha_i and delta_i. 
+        """
+        contig = np.ascontiguousarray
+        dB_dkappa = sopp.dB_dkappa(
+            contig(self.grid_xyz_all), 
+            contig(self.plasma_points),
+            contig(self.alphas_total), 
+            contig(self.deltas_total),
+            contig(self.I_all),
+            contig(self.phi),
+            self.R,
+        ) * self.dphi  # rescale because did a phi integral
+        
+        # symmetrize it?
+        return dB_dkappa
     
     def L_deriv(self):
         """
@@ -893,6 +917,8 @@ class PSCgrid:
                 The gradient of the Psi vector with respect to the PSC angles
                 alpha_i and delta_i. 
         """
+        
+        # Need to check is alphas and deltas are in [-pi, pi] and remap if not
         self.alphas = alphas
         self.deltas = deltas
         contig = np.ascontiguousarray
@@ -985,8 +1011,10 @@ class PSCgrid:
                 self.coil_normals_all[nn * q: nn * (q + 1), 1] = self.coil_normals[:, 0] * np.sin(phi0) * stell + self.coil_normals[:, 1] * np.cos(phi0) 
                 self.coil_normals_all[nn * q: nn * (q + 1), 2] = self.coil_normals[:, 2]
                 normals = self.coil_normals_all[q * nn: (q + 1) * nn, :]
-                deltas = np.arctan2(normals[:, 0], normals[:, 2])
-                alphas = -np.arcsin(normals[:, 1])
+                deltas = np.arctan2(normals[:, 0], normals[:, 2]) # + np.pi
+                alphas = np.arctan2(-normals[:, 1] * np.cos(deltas), normals[:, 2])
+                # alphas = -np.arcsin(normals[:, 1])
+                print(alphas, deltas)
                 self.alphas_total[nn * q: nn * (q + 1)] = alphas
                 self.deltas_total[nn * q: nn * (q + 1)] = deltas
                 q = q + 1
@@ -1010,8 +1038,9 @@ class PSCgrid:
                 self.coil_normals_all[nn * q: nn * (q + 1), 1] = self.coil_normals[:, 0] * np.sin(phi0) * stell + self.coil_normals[:, 1] * np.cos(phi0) 
                 self.coil_normals_all[nn * q: nn * (q + 1), 2] = self.coil_normals[:, 2]
                 normals = self.coil_normals_all[q * nn: (q + 1) * nn, :]
-                deltas = np.arctan2(normals[:, 0], normals[:, 2])
-                alphas = -np.arcsin(normals[:, 1])
+                deltas = np.arctan2(normals[:, 0], normals[:, 2]) # + np.pi
+                alphas = -np.arctan2(normals[:, 1] * np.cos(deltas), normals[:, 2])
+                # alphas = -np.arcsin(normals[:, 1])
                 self.alphas_total[nn * q: nn * (q + 1)] = alphas
                 self.deltas_total[nn * q: nn * (q + 1)] = deltas
                 q = q + 1

tests/geo/test_psc_grid.py
@@ -10,69 +10,128 @@ import simsoptpp as sopp
 
 class Testing(unittest.TestCase):
     
-    def test_analytic_derivatives(self):
+    def test_L_analytic_derivatives(self):
         """
-        Tests the analytic calculations against finite differences for tiny 
-        changes to the angles, to see if the analytic calculations are correct.
+        Tests the analytic calculations of dL/dalpha and dL/ddelta
+        against finite differences for tiny changes to the angles, 
+        to see if the analytic calculations are correct.
         """
-        # initialize two randomly placed coils
-        np.random.seed(1)
+        
+        # Test partial derivative calculation if we change delta by epsilon
+        ncoils = 3
+        np.random.seed(ncoils)
         R0 = 1
         R = 1 
         a = 1e-5
-        points = (np.random.rand(1, 3) - 0.5) * 5
-        print(points)
-        alphas = np.random.rand(1) * 2 * np.pi
-        deltas = np.zeros(1)
+        points = (np.random.rand(ncoils, 3) - 0.5) * 5
+        
+        # Need alphas and deltas in [-pi, pi] range
+        deltas = (np.random.rand(ncoils) - 0.5) * 2 * np.pi
+        alphas = (np.random.rand(ncoils) - 0.5) * 2 * np.pi
+        psc_array = PSCgrid.geo_setup_manual(
+            points, R=R, a=a, alphas=alphas, deltas=deltas
+        )
+        L = psc_array.L
+        Linv = psc_array.L_inv
+        L_deriv = psc_array.L_deriv()
+        epsilon = 1e-6
+        deltas_new = np.copy(deltas)
+        deltas_new[0] += epsilon
+        alphas_new = alphas  # + epsilon
+        psc_array_new = PSCgrid.geo_setup_manual(
+            points, R=R, a=a, alphas=alphas_new, deltas=deltas_new
+        )
+        L_new = psc_array_new.L
+        Linv_new = psc_array_new.L_inv
+        dL_ddelta = (L_new - L) / epsilon
+        dLinv_ddelta = (Linv_new - Linv) / epsilon
+        dL_ddelta_analytic = L_deriv
+        dLinv_ddelta_analytic = -np.tensordot(
+            np.tensordot(
+            L_deriv, Linv, axes=([1], [0])
+            ), Linv, axes=([-1], [0])
+        )
+        # Symmetrizing helps with numerical error accumulation?
+        for i in range(ncoils * 2):
+            dLinv_ddelta_analytic[i, :, :] = (dLinv_ddelta_analytic[i, :, :] + dLinv_ddelta_analytic[i, :, :].T) / 2.0
+            
+        # Linv calculations looks much more incorrect that the L derivatives,
+        # maybe because of numerical error accumulation? 
+        print(dL_ddelta, dL_ddelta_analytic[ncoils, :, :])
+        print(dLinv_ddelta, dLinv_ddelta_analytic[ncoils, :, :])
+        assert(np.allclose(dL_ddelta, dL_ddelta_analytic[ncoils, :, :]))
+        assert(np.allclose(dLinv_ddelta, dLinv_ddelta_analytic[ncoils, :, :], rtol=10))
+
+        # Test partial derivative calculation if we change alpha by epsilon
+        ncoils = 3
+        points = (np.random.rand(ncoils, 3) - 0.5) * 5
+        alphas = (np.random.rand(ncoils) - 0.5) * 2 * np.pi
+        deltas = (np.random.rand(ncoils) - 0.5) * 2 * np.pi
+        psc_array = PSCgrid.geo_setup_manual(
+            points, R=R, a=a, alphas=alphas, deltas=deltas
+        )
+        L = psc_array.L
+        Linv = psc_array.L_inv
+        L_deriv = psc_array.L_deriv()
+        epsilon = 1e-6
+        alphas_new = np.copy(alphas)
+        alphas_new[0] += epsilon
+        deltas_new = deltas
+        psc_array_new = PSCgrid.geo_setup_manual(
+            points, R=R, a=a, alphas=alphas_new, deltas=deltas_new
+        )
+        L_new = psc_array_new.L
+        Linv_new = psc_array_new.L_inv
+        dL_dalpha = (L_new - L) / epsilon
+        dLinv_dalpha = (Linv_new - Linv) / epsilon
+        dL_dalpha_analytic = L_deriv
+        dLinv_dalpha_analytic = -np.tensordot(
+            np.tensordot(
+            L_deriv, Linv, axes=([1], [0])
+            ), Linv, axes=([-1], [0])
+        )
+        # Symmetrizing helps with numerical error accumulation?
+        for i in range(ncoils * 2):
+            dLinv_dalpha_analytic[i, :, :] = (dLinv_dalpha_analytic[i, :, :] + dLinv_dalpha_analytic[i, :, :].T) / 2.0
+        assert(np.allclose(dL_dalpha, dL_dalpha_analytic[0, :, :], rtol=1e-3))
+        assert(np.allclose(dLinv_dalpha, dLinv_dalpha_analytic[0, :, :], rtol=1))
+
+        
+    def test_dB_analytic_derivatives(self):
+        """
+        Tests the analytic calculations of dB/dalpha and dB/ddelta
+        against finite differences for tiny changes to the angles, 
+        to see if the analytic calculations are correct.
+        """
+        ncoils = 2
+        np.random.seed(ncoils)
+        R0 = 1
+        R = 1 
+        a = 1e-5
+        points = (np.random.rand(ncoils, 3) - 0.5) * 5
+        deltas = (np.random.rand(ncoils) - 0.5) * 2 * np.pi
+        alphas = np.zeros(ncoils)
         psc_array = PSCgrid.geo_setup_manual(
             points, R=R, a=a, alphas=alphas, deltas=deltas
         )
         B = psc_array.B_PSC
-        L = psc_array.L
-        A = psc_array.A_matrix
-        psi = psc_array.psi
-        L_deriv = psc_array.L_deriv()
-        L_deriv = np.tensordot(
-            np.tensordot(
-            psc_array.L_inv, L_deriv, axes=([-1], [1])
-            ), psc_array.L_inv, axes=([-1], [0])
-            )
-        A_deriv = psc_array.A_deriv()
-        print(psc_array.I)
-        psi_deriv = psc_array.psi_deriv()
-        epsilon = 1e-4
+        epsilon = 1e-6
+        deltas_new = deltas
+        deltas_new[0] += epsilon
         alphas_new = alphas
-        alphas_new[0] += epsilon
-        deltas_new = np.zeros(1)
         psc_array_new = PSCgrid.geo_setup_manual(
             points, R=R, a=a, alphas=alphas_new, deltas=deltas_new
         )
         B_new = psc_array_new.B_PSC
-        L_new = psc_array_new.L
-        A_new = psc_array_new.A_matrix
-        # A_deriv = psc_array_new.A_deriv()
-        psi_new = psc_array_new.psi
-        dL_dalpha = (L_new - L) / epsilon
-        dA_dalpha = (A_new - A) / epsilon
-        # print(A, A_new)
-        # exit()
         B.set_points(psc_array.plasma_points)
         B_new.set_points(psc_array.plasma_points)
-
-        dB_dalpha = (B_new.B() - B.B()) / epsilon
-        dpsi_dalpha = (psi_new - psi) / epsilon
-        dL_dalpha_analytic = L_deriv
-        dA_dalpha_analytic = A_deriv
-        dpsi_dalpha_analytic = psi_deriv
-        # print(dB_dalpha.shape)
-        # print(np.sum(dB_dalpha * psc_array.plasma_unitnormals, axis=-1), 
-        #       dA_dalpha @ psc_array.I)
-        print('dA, dA_analytic = ', dA_dalpha, dA_dalpha_analytic[1, :].T)
-              #dA_dalpha_analytic[0, :, :].T, dA_dalpha_analytic[1, :, :].T, 
-              #dA_dalpha_analytic[2, :, :].T, dA_dalpha_analytic[3, :, :].T)
-        # print(dL_dalpha, dL_dalpha_analytic[:, 1, :])
-        # print(dpsi_dalpha, dpsi_dalpha_analytic[1, :])
+        dB_ddelta = (B_new.B() - B.B()) / epsilon
+        dB_ddelta_analytic = psc_array.dB_dkappa()
+        print(dB_ddelta.shape, psc_array.dB_dkappa().shape)
         exit()
+        print(dB_ddelta, psc_array.dB_dkappa())
+        exit()
+        assert(np.allclose(dB_ddelta, dB_ddelta_analytic[ncoils, :, :]))
 
     def test_L(self):
         """