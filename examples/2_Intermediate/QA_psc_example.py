Spent a lot of time debugging the Lderiv, Aderiv, and psideriv calculations for the passive coils. Turns out the jacobian calculations for dpsi and dL were not correct so added unit tests and appears to be passing them for the QA symmetries now. Code needs a big clean up and need to speed up the derivative calculations because can only barely optimize 100 PSCs or so right now with the Jacobian method.

examples/2_Intermediate/QA_psc_example.py
@@ -216,7 +216,7 @@ make_Bnormal_plots(bs, s_plot, out_dir, "biot_savart_initial")
 # fix all the coil shapes so only the currents are optimized
 # for i in range(ncoils):
 #     base_curves[i].fix_all()
-bs = coil_optimization_QA(s, bs, base_curves, curves, out_dir)
+# bs = coil_optimization_QA(s, bs, base_curves, curves, out_dir)
 curves_to_vtk(curves, out_dir / "TF_coils", close=True)
 bs.set_points(s.gamma().reshape((-1, 3)))
 Bnormal = np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
@@ -227,7 +227,7 @@ B_axis = calculate_on_axis_B(bs, s)
 make_Bnormal_plots(bs, s_plot, out_dir, "biot_savart_TF_optimized", B_axis)
 
 # Finally, initialize the psc class
-kwargs_geo = {"Nx": 4, "out_dir": out_str, 
+kwargs_geo = {"Nx": 8, "out_dir": out_str, 
               "random_initialization": True, "poff": poff,}
               # "interpolated_field": True} 
 psc_array = PSCgrid.geo_setup_between_toroidal_surfaces(
@@ -266,7 +266,7 @@ print('fB with both (minus sign), before opt = ', fB / (B_axis ** 2 * s.area()))
 # Actually do the minimization now
 print('beginning optimization: ')
 opt_bounds = tuple([(0, 2 * np.pi) for i in range(psc_array.num_psc * 2)])
-options = {"disp": True, "maxiter": 100}
+options = {"disp": True, "maxiter": 100, "iprint": 101}
 # print(opt_bounds)
 # x0 = np.random.rand(2 * psc_array.num_psc) * 2 * np.pi
 verbose = True
@@ -277,41 +277,53 @@ kwargs_manual = {
                  "plasma_boundary" : s,
                  "coils_TF" : coils
                  }
-I_threshold = 0.0
-STLSQ_max_iters = 10
-for k in range(STLSQ_max_iters):
-    x0 = np.ravel(np.array([psc_array.alphas, psc_array.deltas]))
-    print('Number of PSCs = ', len(x0) // 2, ' in iteration ', k)
-    x_opt = minimize(psc_array.least_squares, x0, args=(verbose,),
-                     method='L-BFGS-B',
-                     # bounds=opt_bounds,
-                     jac=psc_array.least_squares_jacobian, 
-                     options=options,
-                     tol=1e-10,
-                     )
-    from matplotlib import pyplot as plt
-    plt.figure()
-    plt.semilogy(psc_array.BdotN2_list)
-    plt.show()
-    I = psc_array.I
-    small_I_inds = np.ravel(np.where(np.abs(I) < I_threshold))
-    grid_xyz = psc_array.grid_xyz
-    alphas = psc_array.alphas
-    deltas = psc_array.deltas
-    if len(small_I_inds) > 0:
-        grid_xyz = grid_xyz[small_I_inds, :]
-        alphas = alphas[small_I_inds]
-        deltas = deltas[small_I_inds]
-    else:
-        print('STLSQ converged, breaking out of loop')
-        break
-    kwargs_manual["alphas"] = alphas
-    kwargs_manual["deltas"] = deltas
-    # Initialize new PSC array with coils only at the remaining locations
-    # with initial orientations from the solve using BFGS
-    psc_array = PSCgrid.geo_setup_manual(
-        grid_xyz, psc_array.R, **kwargs_manual
-    )
+
+from scipy.optimize import approx_fprime, check_grad
+# from scipy.optimize import lbfgsb
+def callback(x):
+    print('exact: ', psc_array.least_squares(x))
+    print('approx: ', approx_fprime(x, psc_array.least_squares, 1E-3))
+    print('-----')
+    print(check_grad(psc_array.least_squares, psc_array.least_squares_jacobian, x) / np.linalg.norm(psc_array.least_squares_jacobian(x)))
+    
+
+# I_threshold = 0.0
+# STLSQ_max_iters = 10
+# for k in range(STLSQ_max_iters):
+    # x0 = np.ravel(np.array([psc_array.alphas, psc_array.deltas]))
+    # print('Number of PSCs = ', len(x0) // 2, ' in iteration ', k)
+x_opt = minimize(psc_array.least_squares, x0, args=(verbose,),
+                 method='L-BFGS-B',
+                 # bounds=opt_bounds,
+                 # jac=None,
+                    # jac=psc_array.least_squares_jacobian, 
+                 options=options,
+                 tol=1e-10,
+                 # callback=callback
+                 )
+from matplotlib import pyplot as plt
+plt.figure()
+plt.semilogy(psc_array.BdotN2_list)
+plt.show()
+    # I = psc_array.I
+    # small_I_inds = np.ravel(np.where(np.abs(I) < I_threshold))
+    # grid_xyz = psc_array.grid_xyz
+    # alphas = psc_array.alphas
+    # deltas = psc_array.deltas
+    # if len(small_I_inds) > 0:
+    #     grid_xyz = grid_xyz[small_I_inds, :]
+    #     alphas = alphas[small_I_inds]
+    #     deltas = deltas[small_I_inds]
+    # else:
+    #     print('STLSQ converged, breaking out of loop')
+    #     break
+    # kwargs_manual["alphas"] = alphas
+    # kwargs_manual["deltas"] = deltas
+    # # Initialize new PSC array with coils only at the remaining locations
+    # # with initial orientations from the solve using BFGS
+    # psc_array = PSCgrid.geo_setup_manual(
+    #     grid_xyz, psc_array.R, **kwargs_manual
+    # )
     
 # psc_array.setup_orientations(x_opt.x[:len(x_opt) // 2], x_opt.x[len(x_opt) // 2:])
 psc_array.setup_curves()

examples/2_Intermediate/QA_wp_example.py
@@ -246,7 +246,7 @@ make_Bnormal_plots(bs, s_plot, out_dir, "biot_savart_initial")
 # fix all the coil shapes so only the currents are optimized
 # for i in range(ncoils):
 #     base_curves[i].fix_all()
-bs = coil_optimization_QA(s, bs, base_curves, curves, out_dir)
+# bs = coil_optimization_QA(s, bs, base_curves, curves, out_dir)
 curves_to_vtk(curves, out_dir / "TF_coils", close=True)
 bs.set_points(s.gamma().reshape((-1, 3)))
 Bnormal = np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
@@ -257,7 +257,7 @@ B_axis = calculate_on_axis_B(bs, s)
 make_Bnormal_plots(bs, s_plot, out_dir, "biot_savart_TF_optimized", B_axis)
 
 # Finally, initialize the wp class
-kwargs_geo = {"Nx": 10, "out_dir": out_str, 
+kwargs_geo = {"Nx": 4, "out_dir": out_str, 
               "random_initialization": True, "poff": poff,}
               # "interpolated_field": True} 
 wp_array = WPgrid.geo_setup_between_toroidal_surfaces(
@@ -310,8 +310,17 @@ kwargs_manual = {
                  "plasma_boundary" : s,
                  "coils_TF" : coils
                  }
+
+from scipy.optimize import approx_fprime, check_grad
+# from scipy.optimize import lbfgsb
+def callback(x):
+    print('exact: ', wp_array.least_squares(x))
+    print('approx: ', approx_fprime(x, wp_array.least_squares, 1E-8))
+    print('-----')
+    print(check_grad(wp_array.least_squares, wp_array.least_squares_jacobian, x) / np.linalg.norm(wp_array.least_squares_jacobian(x)))
+    
 t1 = time.time()
-I_threshold = 5e5
+I_threshold = 5e6
 STLSQ_max_iters = 10
 BdotN2_list = []
 num_wps = []
@@ -322,7 +331,8 @@ for k in range(STLSQ_max_iters):
     x_opt = minimize(wp_array.least_squares, x0, args=(verbose,),
                      method='L-BFGS-B',  
                      # bounds=opt_bounds,
-                     jac=wp_array.least_squares_jacobian,
+                       jac=wp_array.least_squares_jacobian,
+                      callback=callback,
                      options=options,
                      tol=1e-20,
                      )

examples/2_Intermediate/permanent_magnet_MUSE.py
@@ -44,11 +44,11 @@ if in_github_actions:
     max_nMagnets = 20
     downsample = 100  # downsample the FAMUS grid of magnets by this factor
 else:
-    nphi = 16  # >= 64 for high-resolution runs
+    nphi = 8  # >= 64 for high-resolution runs
     nIter_max = 10000
     nBacktracking = 200
     max_nMagnets = 1000
-    downsample = 1
+    downsample = 20
 
 ntheta = nphi  # same as above
 dr = 0.01  # Radial extent in meters of the cylindrical permanent magnet bricks
@@ -143,7 +143,7 @@ print('Number of available dipoles = ', pm_opt.ndipoles)
 # Set some hyperparameters for the optimization
 algorithm = 'ArbVec_backtracking'  # Algorithm to use
 nAdjacent = 1  # How many magnets to consider "adjacent" to one another
-nHistory = 20  # How often to save the algorithm progress
+nHistory = 1  # How often to save the algorithm progress
 thresh_angle = np.pi  # The angle between two "adjacent" dipoles such that they should be removed
 kwargs = initialize_default_kwargs('GPMO')
 kwargs['K'] = nIter_max  # Maximum number of GPMO iterations to run
@@ -176,6 +176,8 @@ plt.savefig(out_dir / 'GPMO_MSE_history.png')
 # Set final m to the minimum achieved during the optimization
 min_ind = np.argmin(R2_history)
 pm_opt.m = np.ravel(m_history[:, :, min_ind])
+# m_history = m_history.reshape(pm_opt.ndipoles * 3, -1)
+# m_history = np.where(np.allclose(m_history[]))
 
 # Print effective permanent magnet volume
 B_max = 1.465
@@ -185,12 +187,12 @@ dipoles = pm_opt.m.reshape(pm_opt.ndipoles, 3)
 print('Volume of permanent magnets is = ', np.sum(np.sqrt(np.sum(dipoles ** 2, axis=-1))) / M_max)
 print('sum(|m_i|)', np.sum(np.sqrt(np.sum(dipoles ** 2, axis=-1))))
 
-save_plots = False
+save_plots = True
 if save_plots:
     # Save the MSE history and history of the m vectors
     np.savetxt(
         out_dir / f"mhistory_K{kwargs['K']}_nphi{nphi}_ntheta{ntheta}.txt", 
-        m_history.reshape(pm_opt.ndipoles * 3, kwargs['nhistory'] + 1)
+        m_history.reshape(pm_opt.ndipoles * 3, -1)
     )
     np.savetxt(
         out_dir / f"R2history_K{kwargs['K']}_nphi{nphi}_ntheta{ntheta}.txt",
@@ -202,7 +204,7 @@ if save_plots:
     make_Bnormal_plots(bs, s_plot, out_dir, "biot_savart_optimized")
 
     # Look through the solutions as function of K and make plots
-    for k in range(0, kwargs["nhistory"] + 1, 50):
+    for k in range(0, m_history.reshape(pm_opt.ndipoles * 3, -1).shape[-1]):
         mk = m_history[:, :, k].reshape(pm_opt.ndipoles * 3)
         b_dipole = DipoleField(
             pm_opt.dipole_grid_xyz,
@@ -219,9 +221,9 @@ if save_plots:
         Bnormal_total = Bnormal + Bnormal_dipoles
 
         # For plotting Bn on the full torus surface at the end with just the dipole fields
-        make_Bnormal_plots(b_dipole, s_plot, out_dir, "only_m_optimized_K{K_save}_nphi{nphi}_ntheta{ntheta}")
+        make_Bnormal_plots(b_dipole, s_plot, out_dir, f"only_m_optimized_K{K_save}_nphi{nphi}_ntheta{ntheta}")
         pointData = {"B_N": Bnormal_total[:, :, None]}
-        s_plot.to_vtk(out_dir / "m_optimized_K{K_save}_nphi{nphi}_ntheta{ntheta}", extra_data=pointData)
+        s_plot.to_vtk(out_dir / f"m_optimized_K{K_save}_nphi{nphi}_ntheta{ntheta}", extra_data=pointData)
 
     # write solution to FAMUS-type file
     pm_opt.write_to_famus(out_dir)

src/simsopt/geo/wp_grid.py
@@ -11,7 +11,7 @@ import time
 from scipy.linalg import inv
 
 __all__ = ['WPgrid']
-
+contig = np.ascontiguousarray
 
 class WPgrid:
     r"""
@@ -68,7 +68,7 @@ class WPgrid:
         
         # This is not a guarantee that coils will not touch but inductance
         # matrix blows up if they do so it is easy to tell when they do
-        self.R = min(Nmin / 2.0, self.poff / 4.0)
+        self.R = min(Nmin / 2.0, self.poff / 2.5)
         self.a = self.R / 10.0  # Hard-coded aspect ratio of 100 right now
         print('Major radius of the coils is R = ', self.R)
         print('Coils are spaced so that every coil of radius R '
@@ -120,7 +120,6 @@ class WPgrid:
             self.xyz_uniform = self.xyz_uniform[good_inds, :]
 
         # Save uniform grid before we start chopping off parts.
-        contig = np.ascontiguousarray
         pointsToVTK('uniform_grid', contig(self.xyz_uniform[:, 0]),
                     contig(self.xyz_uniform[:, 1]), contig(self.xyz_uniform[:, 2]))
 
@@ -253,7 +252,6 @@ class WPgrid:
         
         t1 = time.time()
         # Use the geometric info to initialize a grid of WPs
-        contig = np.ascontiguousarray
         normal_inner = inner_toroidal_surface.unitnormal().reshape(-1, 3)   
         normal_outer = outer_toroidal_surface.unitnormal().reshape(-1, 3)   
         wp_grid._setup_uniform_grid()
@@ -606,8 +604,6 @@ class WPgrid:
         the WP coils touching/intersecting, in which case it will be
         VERY ill-conditioned! 
         """
-        
-        contig = np.ascontiguousarray
         # A_matrix has shape (num_plasma_points, num_coils)
         # B_WP = np.zeros((self.nphi * self.ntheta, 3))
         A_matrix = np.zeros((self.nphi * self.ntheta, self.num_wp))
@@ -615,6 +611,7 @@ class WPgrid:
         # Need to rotate and flip it
         nn = self.num_wp
         q = 0
+        # t1 = time.time()
         for fp in range(self.nfp):
             for stell in self.stell_list:
                 # A_matrix2 += sopp.A_matrix_direct(
@@ -630,7 +627,6 @@ class WPgrid:
                 #     self.grid_xyz_all[q * nn: (q + 1) * nn, :], 
                 #     self.alphas_total[q * nn: (q + 1) * nn],
                 #     self.deltas_total[q * nn: (q + 1) * nn])
-                # t1 = time.time()
                 A_matrix += sopp.A_matrix_simd(
                     contig(self.grid_xyz_all[q * nn: (q + 1) * nn, :]),
                     contig(self.plasma_points),
@@ -642,7 +638,7 @@ class WPgrid:
                 # t2 = time.time()
                 # print('Time1 = ',t2 - t1)
                 # t1 = time.time()
-                # A_matrix += 2 * sopp.A_matrix_simd(
+                # A_matrix += sopp.A_matrix_simd(
                 #     contig(xyz),
                 #     contig(self.plasma_points),
                 #     contig(self.alphas_total[q * nn: (q + 1) * nn]),
@@ -650,13 +646,14 @@ class WPgrid:
                 #     contig(self.plasma_unitnormals),
                 #     self.R,
                 # )  # 
-                # t2 = time.time()
-                # print('Time2 = ',t2 - t1)
                 # print(A_matrix2, A_matrix)
                 # assert np.allclose(A_matrix2, A_matrix)
                 q = q + 1
+        # t2 = time.time()
+        # print('A_matrix time2 = ',t2 - t1)
         self.A_matrix = 2.0 * A_matrix
         self.Bn_WP = (self.A_matrix @ self.I * 1e6).reshape(-1) # missing factor of fac
+        self.Ax_b = (self.Bn_WP + self.b_opt) * self.grid_normalization
     
     def setup_curves(self):
         """ 
@@ -712,7 +709,6 @@ class WPgrid:
         from simsopt.field import InterpolatedField, Current, coils_via_symmetries
         
         curves_to_vtk(self.curves, self.out_dir + "wp_curves", close=True, scalar_data=self.I)
-        contig = np.ascontiguousarray
         if isinstance(self.B_TF, InterpolatedField):
             self.B_TF.set_points(self.grid_xyz)
             B = self.B_TF.B()
@@ -746,7 +742,6 @@ class WPgrid:
                 self.I_all[q * self.num_wp:(q + 1) * self.num_wp] = self.I * stell
                 q = q + 1
         curves_to_vtk(self.all_curves, self.out_dir + filename + "all_wp_curves", close=True, scalar_data=self.I_all)
-        contig = np.ascontiguousarray
         q = 0
         nn = self.num_wp
         self.coil_normals_all = np.zeros((nn * self.symmetry, 3))
@@ -819,15 +814,13 @@ class WPgrid:
 
         """
         t1 = time.time()
-        ind3 = len(kappa_I) // 3
-        ind3_2 = 2 * ind3
-        self.I = kappa_I[ind3_2:]
-        self.setup_orientations(kappa_I[:ind3], kappa_I[ind3:ind3_2])
-        Ax_b = (self.Bn_WP + self.b_opt) * self.grid_normalization 
-        BdotN2 = 0.5 * Ax_b.T @ Ax_b * self.fac2_norm
+        self.I = kappa_I[-self.num_wp:]
+        self.setup_orientations(kappa_I[:self.num_wp], kappa_I[self.num_wp:-self.num_wp])
+        self.setup_currents_and_fields()
+        BdotN2 = 0.5 * self.Ax_b.T @ self.Ax_b * self.fac2_norm
         self.BdotN2_list.append(BdotN2)
         t2 = time.time()
-        print('obj time = ', t2 - t1)
+        # print('obj time = ', t2 - t1)
         return BdotN2
     
     def python_A_matrix(self, points, alphas, deltas):
@@ -918,24 +911,19 @@ class WPgrid:
 
         """
         t1 = time.time()
-        ind3 = len(kappa_I) // 3
-        ind3_2 = 2 * ind3
-        self.I = kappa_I[ind3_2:]
-        self.setup_orientations(kappa_I[:ind3], kappa_I[ind3:ind3_2])
-        Ax_b = (self.Bn_WP + self.b_opt) * self.grid_normalization
-        t1_grad = time.time()
-        grad_alpha_delta = self.A_deriv() * np.hstack((self.I, self.I))
-        t2_grad = time.time()
-        print('Aderiv time = ', t2_grad - t1_grad)
-        grad_I = self.A_matrix
-        # Should be shape (num_plasma_points, 3 * num_wp)
-        # grad_kappa = np.hstack((grad_alpha1, grad_delta1))
-        grad = np.hstack((grad_alpha_delta, grad_I)) * 1.0e6
-        grad = self.grid_normalization[:, None] * grad
-        jac = Ax_b.T @ grad
+        # ind3 = len(kappa_I) // 3
+        # ind3_2 = 2 * ind3
+        self.I = kappa_I[-self.num_wp:]
+        self.setup_orientations(kappa_I[:self.num_wp], kappa_I[self.num_wp:-self.num_wp])
+        self.setup_currents_and_fields()
+        grad = self.grid_normalization[:, None] * np.hstack(
+            (self.A_deriv() * np.hstack((self.I, self.I)), self.A_matrix)
+        ) * 1.0e6
+        # jac = self.Ax_b.T @ grad
         t2 = time.time()
-        print('jac time = ', t2 - t1)
-        return jac * self.fac2_norm
+        # print('jac time = ', t2 - t1)
+        print('exact jac = ', (self.Ax_b.T @ grad) * self.fac2_norm)
+        return (self.Ax_b.T @ grad) * self.fac2_norm
     
     def A_deriv(self):
         """
@@ -948,7 +936,6 @@ class WPgrid:
                 The gradient of the A matrix evaluated on all the plasma 
                 points, with respect to the WP angles alpha_i and delta_i. 
         """
-        contig = np.ascontiguousarray
         nn = self.num_wp
         dA_dkappa = np.zeros((len(self.plasma_points), 2 * self.num_wp))
         q = 0
@@ -993,7 +980,6 @@ class WPgrid:
         self.alphas = alphas
         self.deltas = deltas
         
-        # contig = np.ascontiguousarray
         # self.coil_normals = np.array(
         #     [np.cos(alphas) * np.sin(deltas),
         #       -np.sin(alphas),
@@ -1010,7 +996,7 @@ class WPgrid:
         
         # Calculate the WP B fields
         # t1 = time.time()
-        self.setup_currents_and_fields()
+        # self.setup_currents_and_fields()
         # t2 = time.time()
         # print('Setup fields time = ', t2 - t1)
         