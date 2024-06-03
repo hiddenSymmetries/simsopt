Believe I have gone through and fixed the jacobians, which required more careful thought since both delta_prime and alpha_prime can change when a perturbation to delta is made. Had some more weird bugs during unit testing but it looks like the unit tests are only failing when a coil is accidentally placed so that it intersects with one of the symmetry planes. Going to add a warning about this and check more carefully tomorrow.

src/simsopt/geo/psc_grid.py
@@ -547,6 +547,15 @@ class PSCgrid:
         psc_grid.Bn_plasma = Bn_plasma
         psc_grid.plasma_boundary_full = kwargs.pop("plasma_boundary_full", psc_grid.plasma_boundary)
         
+        # Check if the grid intersects a symmetry plane -- oops!
+        phi0 = 2 * np.pi / psc_grid.nfp * np.arange(psc_grid.nfp)
+        phi_grid = np.arctan2(psc_grid.grid_xyz[:, 1], psc_grid.grid_xyz[:, 0])
+        phi_dev = np.arctan2(psc_grid.R, psc_grid.grid_xyz[:, 0] ** 2  + psc_grid.grid_xyz[:, 1] ** 2)
+        inds = []
+        for i in range(psc_grid.nfp):
+            inds.append(np.ravel(np.where(np.abs(phi_grid - phi0[i]) < phi_dev)))
+        print('inds = ', inds)
+        
         # generate planar TF coils
         ncoils = 4
         R0 = 1.4
@@ -576,7 +585,7 @@ class PSCgrid:
         psc_grid.dl_TF = np.array([coil.curve.gammadash() for coil in coils_TF])
         psc_grid.gamma_TF = np.array([coil.curve.gamma() for coil in coils_TF])
         TF_curves = np.array([coil.curve for coil in coils_TF])
-        curves_to_vtk(TF_curves, psc_grid.out_dir + "coils_TF" + str(psc_grid.plasma_boundary), close=True, scalar_data=psc_grid.I_TF)
+        curves_to_vtk(TF_curves, psc_grid.out_dir + "coils_TF", close=True, scalar_data=psc_grid.I_TF)
         
         # Setup the B field from the TF coils
         # Many big calls to B_TF.B() so can make an interpolated object
@@ -673,6 +682,9 @@ class PSCgrid:
         self.I_all = -self.L_inv @ self.psi_total / self.fac
         # print('psi = ', self.psi_total)
         # print('I = ', self.I_all)
+        
+        # Agrees with self.I
+        # I_test = -self.L_inv[:, :self.num_psc] @ self.psi / self.fac
         self.I = self.I_all[:self.num_psc]
         
         self.I_all_with_sign_flips = np.zeros(self.I_all.shape)
@@ -1035,8 +1047,6 @@ class PSCgrid:
                 dA = sopp.dA_dkappa_simd(
                     contig(self.grid_xyz_all[q * nn: (q + 1) * nn, :]),
                     self.plasma_points,
-                    # contig(self.alphas),
-                    # contig(self.deltas),
                     contig(self.alphas_total[q * nn: (q + 1) * nn]),
                     contig(self.deltas_total[q * nn: (q + 1) * nn]),
                     self.plasma_unitnormals,
@@ -1044,28 +1054,9 @@ class PSCgrid:
                     self.quad_weights,
                     self.R,
                 )
-                dA_dkappa[:, :self.num_psc] += dA[:, :self.num_psc] * self.aaprime_aa[q * nn: (q + 1) * nn]
-                # Factor of stell too for the currents flipping???
-                dA_dkappa[:, self.num_psc:] += dA[:, self.num_psc:] * self.ddprime_dd[q * nn: (q + 1) * nn]
+                dA_dkappa[:, :nn] += dA[:, :nn] * self.aaprime_aa[q * nn: (q + 1) * nn] + dA[:, nn:] * self.ddprime_aa[q * nn: (q + 1) * nn]
+                dA_dkappa[:, nn:] += dA[:, nn:] * self.ddprime_dd[q * nn: (q + 1) * nn] + dA[:, :nn] * self.aaprime_dd[q * nn: (q + 1) * nn]
                 q = q + 1
-        # dA = sopp.dA_dkappa_simd(
-        #     contig(self.grid_xyz_all),
-        #     self.plasma_points,
-        #     contig(self.alphas_total),
-        #     contig(self.deltas_total),
-        #     self.plasma_unitnormals,
-        #     self.quad_points_phi,
-        #     self.quad_weights,
-        #     self.R,
-        # )
-        # dA_sym = np.zeros(dA_dkappa.shape)
-        # q = 0
-        # for fp in range(self.nfp):
-        #     for stell in self.stell_list:
-        #         dA_sym += dA[:, q * nn: (q + 1) * nn] 
-        #         q += 1
-        # print('dA = ', dA[0, :])
-        # print(dA_dkappa.shape)
         return dA_dkappa * np.pi # dA_dkappa * np.pi # rescale by pi for gauss-leg quadrature
     
     def L_deriv(self):
@@ -1091,31 +1082,49 @@ class PSCgrid:
             self.quad_points_phi,
             self.quad_weights,
             self.num_psc,
-            self.stellsym,
+            self.stellsym,  # These  last few params are defunct in the function
             self.nfp
-        )  # because L_deriv_simd adds the transpose already
+        )  
         t2 = time.time()
         # print('L_deriv call = ', t2 - t1)
         # t1 = time.time()
         nsym = self.symmetry
-        # # L_deriv_copy = np.zeros(L_deriv.shape)
+        L_deriv_copy = np.zeros(L_deriv.shape)
         
         # Extra work required to impose the symmetries correctly -- only works for nfp == 2
+        nn = self.num_psc
+        ncoils_sym = nn * nsym
+        # print(L_deriv.shape, ncoils_sym)
+        # dL_dkappa = np.zeros((2 * nn, ncoils_sym, ncoils_sym))
         if nsym > 1:
-            ncoils_sym = self.num_psc * nsym
-            # for i in range(self.num_psc):
-            q = 1
+            q = 0
             for fp in range(self.nfp):
                 for stell in self.stell_list:
-                    if (fp > 0) or stell != 1:
-                        L_deriv[:self.num_psc, :, :] += L_deriv[q * self.num_psc:(q + 1) * self.num_psc, :, :] * (-1) ** fp
-                        L_deriv[ncoils_sym:ncoils_sym + self.num_psc, :, :] += L_deriv[ncoils_sym + q * self.num_psc:(q + 1) * self.num_psc + ncoils_sym, :, :] * (-1) ** fp * stell
-                        q = q + 1
-        # else:
-        #     L_deriv_copy = L_deriv
+                    # print(q, fp, stell, L_deriv[ncoils_sym:ncoils_sym + nn, :, :] )
+                    # dL = sopp.L_deriv_simd(
+                    #     self.grid_xyz_all_normalized[q * nn: (q + 1) * nn, :], 
+                    #     self.alphas_total[q * nn: (q + 1) * nn],
+                    #     self.deltas_total[q * nn: (q + 1) * nn],
+                    #     self.quad_points_phi,
+                    #     self.quad_weights,
+                    #     self.num_psc,
+                    #     self.stellsym, 
+                    #     self.nfp
+                    # )  
+                    # print(q, fp, stell, self.aaprime_aa[q * nn: (q + 1) * nn], self.ddprime_aa[q * nn: (q + 1) * nn], 
+                          # self.aaprime_dd[q * nn: (q + 1) * nn], self.ddprime_dd[q * nn: (q + 1) * nn])
+                    L_deriv_copy[:nn, :, :] += L_deriv[q * nn:(q + 1) * nn, :, :] * self.aaprime_aa[q * nn: (q + 1) * nn, None, None] + L_deriv[ncoils_sym + q * nn:ncoils_sym + (q + 1) * nn, :, :] * self.ddprime_aa[q * nn: (q + 1) * nn, None, None]
+                    L_deriv_copy[ncoils_sym:ncoils_sym + nn, :, :] += L_deriv[q * nn:(q + 1) * nn, :, :] * self.aaprime_dd[q * nn: (q + 1) * nn, None, None] + L_deriv[ncoils_sym + q * nn:ncoils_sym + (q + 1) * nn, :, :] * self.ddprime_dd[q * nn: (q + 1) * nn, None, None]
+                    # print(q, fp, stell, L_deriv_copy[:nn, :, :], L_deriv_copy[ncoils_sym:ncoils_sym + nn, :, :])
+                    q = q + 1
+        else:
+            L_deriv_copy = L_deriv
             
         # symmetrize it
-        L_deriv = (L_deriv + np.transpose(L_deriv, axes=[0, 2, 1]))
+        # print(L_deriv_copy[ncoils_sym:ncoils_sym + nn, :, :], )
+        # print(L_deriv_copy[:nn, :, :])
+        L_deriv = (L_deriv_copy + np.transpose(L_deriv_copy, axes=[0, 2, 1]))
+
         # t2 = time.time()
         # print('L_deriv reshape = ', t2 - t1)
         # if keeping track of the number of coil turns, need factor
@@ -1186,53 +1195,57 @@ class PSCgrid:
         # q = 0
         # # for fp in range(self.nfp):
         # #     for stell in self.stell_list:
-        psi_deriv = sopp.dpsi_dkappa(
-            contig(self.I_TF),
-            contig(self.dl_TF),
-            contig(self.gamma_TF),
-            contig(self.grid_xyz_all),  #_all[q * nn: (q + 1) * nn, :]),
-            contig(self.alphas_total),  # _total[q * nn: (q + 1) * nn]),
-            contig(self.deltas_total),  #_total[q * nn: (q + 1) * nn]),
-            contig(self.coil_normals_all),  # _all[q * nn: (q + 1) * nn, :]),
-            contig(self.quad_points_rho),
-            contig(self.quad_points_phi),
-            self.quad_weights,
-            self.R,
-        )
+        # psi_deriv = sopp.dpsi_dkappa(
+        #     contig(self.I_TF),
+        #     contig(self.dl_TF),
+        #     contig(self.gamma_TF),
+        #     contig(self.grid_xyz_all),  #_all[q * nn: (q + 1) * nn, :]),
+        #     contig(self.alphas_total),  # _total[q * nn: (q + 1) * nn]),
+        #     contig(self.deltas_total),  #_total[q * nn: (q + 1) * nn]),
+        #     contig(self.coil_normals_all),  # _all[q * nn: (q + 1) * nn, :]),
+        #     contig(self.quad_points_rho),
+        #     contig(self.quad_points_phi),
+        #     self.quad_weights,
+        #     self.R,
+        # )
         
-        # dpsi/dkappa depends implicity on how kappa got changed! 
-        nsym = self.symmetry
-        if nsym > 1:
-            q = 0
-            ncoils_sym = self.num_psc * nsym
-            for fp in range(self.nfp):
-                # phi0 = (2 * np.pi / self.nfp) * fp
-                for stell in self.stell_list:
-                    psi_deriv[q * self.num_psc:(q + 1) * self.num_psc] *= self.aaprime_aa[q * self.num_psc:(q + 1) * self.num_psc]
-                    psi_deriv[q * self.num_psc + ncoils_sym:ncoils_sym + (q + 1) * self.num_psc] *= self.ddprime_dd[q * self.num_psc:(q + 1) * self.num_psc]
-                    q = q + 1
+        # # dpsi/dkappa depends implicity on how kappa got changed! 
+        # dpsi_dkappa = np.zeros(psi_deriv.shape)
+        # nsym = self.symmetry
+        # if nsym > 1:
+        #     q = 0
+        #     ncoils_sym = nn * nsym
+        #     for fp in range(self.nfp):
+        #         # phi0 = (2 * np.pi / self.nfp) * fp
+        #         for stell in self.stell_list:
+        #             dpsi_dkappa[q * nn:(q + 1) * nn] += psi_deriv[q * nn: (q + 1) * nn] * self.aaprime_aa[q * nn:(q + 1) * nn
+        #                                                         ] + psi_deriv[q * nn + ncoils_sym:ncoils_sym + (q + 1) * nn] * self.ddprime_aa[q * nn:(q + 1) * nn
+        #             dpsi_dkappa[q * nn + ncoils_sym:ncoils_sym + (q + 1) * nn] += psi_deriv[q * nn: (q + 1) * nn] * self.aaprime_dd[q * nn:(q + 1) * nn
+        #                                                         ] + psi_deriv[q * nn + ncoils_sym:ncoils_sym + (q + 1) * nn] * self.ddprime_dd[q * nn:(q + 1) * nn]
+        #             q = q + 1
         # print('psi_deriv = ', psi_deriv)
         
-        # psi_deriv = np.zeros(2 * self.num_psc)
-        # q = 0
-        # for fp in range(self.nfp):
-        #     for stell in self.stell_list:
-        #         psi_deriv += sopp.dpsi_dkappa(
-        #             contig(self.I_TF),
-        #             contig(self.dl_TF),
-        #             contig(self.gamma_TF),
-        #             contig(self.grid_xyz_all[q * nn: (q + 1) * nn, :]),
-        #             contig(self.alphas_total[q * nn: (q + 1) * nn]),
-        #             contig(self.deltas_total[q * nn: (q + 1) * nn]),
-        #             contig(self.coil_normals_all[q * nn: (q + 1) * nn, :]),
-        #             contig(self.quad_points_rho),
-        #             contig(self.quad_points_phi),
-        #             self.quad_weights,
-        #             self.R,
-        #         ) * np.array([self.aaprime_aa, self.ddprime_dd])
-        #         q += 1
-    
-        return psi_deriv * (1.0 / self.gamma_TF.shape[1])
+        psi_deriv = np.zeros(2 * self.num_psc)
+        q = 0
+        for fp in range(self.nfp):
+            for stell in self.stell_list:
+                dpsi = sopp.dpsi_dkappa(
+                    contig(self.I_TF),
+                    contig(self.dl_TF),
+                    contig(self.gamma_TF),
+                    contig(self.grid_xyz_all[q * nn: (q + 1) * nn, :]),
+                    contig(self.alphas_total[q * nn: (q + 1) * nn]),
+                    contig(self.deltas_total[q * nn: (q + 1) * nn]),
+                    contig(self.coil_normals_all[q * nn: (q + 1) * nn, :]),
+                    contig(self.quad_points_rho),
+                    contig(self.quad_points_phi),
+                    self.quad_weights,
+                    self.R,
+                ) 
+                psi_deriv[:nn] += dpsi[:nn] * self.aaprime_aa[q * nn:(q + 1) * nn] + dpsi[nn:] * self.ddprime_aa[q * nn:(q + 1) * nn]
+                psi_deriv[nn:] += dpsi[:nn] * self.aaprime_dd[q * nn:(q + 1) * nn] + dpsi[nn:] * self.ddprime_dd[q * nn:(q + 1) * nn]
+                q += 1
+        return psi_deriv * (1.0 / self.gamma_TF.shape[1]) / self.nfp / (self.stellsym + 1.0)  # Factors because TF fields get overcounted
     
     def setup_orientations(self, alphas, deltas):
         """
@@ -1384,6 +1397,8 @@ class PSCgrid:
         self.deltas_total = np.zeros(nn * self.symmetry)
         self.ddprime_dd = np.zeros(nn * self.symmetry)
         self.aaprime_aa = np.zeros(nn * self.symmetry)
+        self.aaprime_dd = np.zeros(nn * self.symmetry)
+        self.ddprime_aa = np.zeros(nn * self.symmetry)
         self.coil_normals_all = np.zeros((nn * self.symmetry, 3))
         q = 0
         for fp in range(self.nfp):
@@ -1393,22 +1408,22 @@ class PSCgrid:
                 self.coil_normals_all[nn * q: nn * (q + 1), 0] = self.coil_normals[:, 0] * np.cos(phi0) * stell - self.coil_normals[:, 1] * np.sin(phi0) 
                 self.coil_normals_all[nn * q: nn * (q + 1), 1] = self.coil_normals[:, 0] * np.sin(phi0) * stell + self.coil_normals[:, 1] * np.cos(phi0) 
                 self.coil_normals_all[nn * q: nn * (q + 1), 2] = self.coil_normals[:, 2]
-                normals = self.coil_normals_all[q * nn: (q + 1) * nn, :]
+                # normals = self.coil_normals_all[q * nn: (q + 1) * nn, :]
                 # normals = self.coil_normals
                 # get new deltas by flipping sign of deltas, then rotation alpha by phi0
-                deltas = np.arctan2(normals[:, 0], normals[:, 2]) # + np.pi
+                # deltas = np.arctan2(normals[:, 0], normals[:, 2]) # + np.pi
                 
                 ###### Add back in lines once done debugging on QH ##########################
-                deltas[abs(deltas) == np.pi] = 0.0
-                if fp == 0 and stell == 1:
-                    shift_deltas = self.deltas - deltas  # +- pi
-                deltas += shift_deltas
-                alphas = -np.arctan2(normals[:, 1] * np.cos(deltas), normals[:, 2])
-                alphas[abs(alphas) == np.pi] = 0.0
-                if fp == 0 and stell == 1:
-                    shift_alphas = self.alphas - alphas  # +- pi
-                alphas += shift_alphas
-                print(q, fp, stell, alphas, deltas)
+                # deltas[abs(deltas) == np.pi] = 0.0
+                # if fp == 0 and stell == 1:
+                #     shift_deltas = self.deltas - deltas  # +- pi
+                # deltas += shift_deltas
+                # alphas = -np.arctan2(normals[:, 1] * np.cos(deltas), normals[:, 2])
+                # alphas[abs(alphas) == np.pi] = 0.0
+                # if fp == 0 and stell == 1:
+                #     shift_alphas = self.alphas - alphas  # +- pi
+                # alphas += shift_alphas
+                # print(q, fp, stell, alphas, deltas)
                 # self.alphas_total[nn * q: nn * (q + 1)] = alphas
                 # self.deltas_total[nn * q: nn * (q + 1)] = deltas
                 # alphas2 = -np.arcsin(np.sin(phi0) * stell * np.cos(
@@ -1416,31 +1431,49 @@ class PSCgrid:
                 deltas = np.arctan2(np.cos(phi0) * stell * np.cos(
                     self.alphas) * np.sin(self.deltas) + np.sin(phi0) * np.sin(self.alphas),
                     np.cos(self.alphas) * np.cos(self.deltas))
+                
+                # Fix the orientation
                 deltas[abs(deltas) == np.pi] = 0.0
                 if fp == 0 and stell == 1:
-                    shift_deltas = self.deltas - deltas  # +- pi
+                    shift_deltas = self.deltas - deltas
                 deltas += shift_deltas
+                # alphas = -np.arcsin(np.sin(phi0) * stell * np.cos(
+                #       self.alphas) * np.sin(self.deltas) - np.cos(phi0) * np.sin(self.alphas))
                 alphas = -np.arctan2(np.cos(deltas) * (np.sin(phi0) * stell * np.cos(
                     self.alphas) * np.sin(self.deltas) - np.cos(phi0) * np.sin(self.alphas)),
                     np.cos(self.alphas) * np.cos(self.deltas))
+                # Fix the orientation
                 alphas[abs(alphas) == np.pi] = 0.0
                 if fp == 0 and stell == 1:
-                    shift_alphas = self.alphas - alphas  # +- pi
+                    shift_alphas = self.alphas - alphas
                 alphas += shift_alphas
-                print(q, fp, stell, alphas, deltas)
+                # print(q, fp, stell, alphas, deltas)
+                
                 self.alphas_total[nn * q: nn * (q + 1)] = alphas
                 self.deltas_total[nn * q: nn * (q + 1)] = deltas
+                # print(q, fp, stell, self.alphas_total[nn * q: nn * (q + 1)])
+
                 x = self.alphas_total[:nn]
                 y = self.deltas_total[:nn]
                 z = phi0
                 secy = 1.0 / np.cos(y)
                 ddprime_dd = (secy * np.sin(z) * np.tan(x) * np.tan(y) + stell * np.cos(z) * (
                     1.0 + np.tan(y) ** 2)) / (1.0 + (secy * np.sin(z) * np.tan(x) + stell * np.cos(z) * np.tan(y)) ** 2)
+                ddprime_aa = (secy * np.sin(z) * (1 + np.tan(x) ** 2)
+                              ) / (1 + secy ** 2  * np.sin(z) ** 2 * np.tan(x) ** 2 + stell * 2.0 * np.cos(z) * secy * np.sin(z) * np.tan(x) * np.tan(y) + np.cos(z) ** 2 * np.tan(y) ** 2)
                 self.ddprime_dd[nn * q: nn * (q + 1)] = ddprime_dd
-                aaprime_aa = (np.sin(x) * np.sin(y) * np.sin(z) - stell * np.cos(x) * np.cos(z) 
-                              )/np.sqrt(1 - (np.sin(x) * np.cos(z) + stell * np.cos(x) * np.sin(y) * np.sin(z)) ** 2)
+                self.ddprime_aa[nn * q: nn * (q + 1)] = ddprime_aa
+                
+                # Need to choose the branch with arcsin in -pi/2 < arcsin(x) < pi / 2 or -1 < x < 1
+                xx = (np.sin(x) * np.cos(z) - stell * np.cos(x) * np.sin(y) * np.sin(z)) ** 2
+                aaprime_aa = (np.sin(x) * np.sin(y) * np.sin(z) * stell + np.cos(x) * np.cos(z) 
+                              ) / np.sqrt(1 - xx)  #* np.sign(alphas)
+                # print(q, fp, stell, alphas, self.alphas)
+                aaprime_dd = -stell * (np.cos(x) * np.cos(y) * np.sin(z)) / np.sqrt(1.0 - (np.cos(z) * np.sin(x) - stell * np.cos(x) * np.sin(y) * np.sin(z)) ** 2)
                 self.aaprime_aa[nn * q: nn * (q + 1)] = aaprime_aa
+                self.aaprime_dd[nn * q: nn * (q + 1)] = aaprime_dd
                 # print(q, fp, stell, self.ddprime_dd[nn * q: nn * (q + 1)])
+                # print(q, fp, stell, ddprime_dd, ddprime_aa, aaprime_aa, aaprime_dd)
                 q = q + 1
         self.alphas_total = contig(self.alphas_total)
         self.deltas_total = contig(self.deltas_total)

tests/geo/test_psc_grid.py
@@ -32,12 +32,12 @@ surf4 = SurfaceRZFourier.from_wout(
     surface_filename, range='half period', nphi=16, ntheta=16
 )
 surfs = [surf1, surf2, surf3, surf4]
-ncoils = 2
-np.random.seed(10)
-R = 0.1
+ncoils = 13
+np.random.seed(24)
+R = 0.05
 a = 1e-5
 points = (np.random.rand(ncoils, 3) - 0.5) * 3
-points[:, -1] = 0.5
+points[:, -1] = 0.4
 alphas = (np.random.rand(ncoils) - 0.5) * 2 * np.pi
 deltas = (np.random.rand(ncoils) - 0.5) * 2 * np.pi
 epsilon = 1e-4  # smaller epsilon and numerical accumulation starts to be an issue
@@ -132,11 +132,11 @@ class Testing(unittest.TestCase):
                 points, R=R, a=a, alphas=alphas, deltas=deltas, **kwargs_manual
             )
             A = psc_array.A_matrix
-            A_full = psc_array.A_matrix_full
-            psi = psc_array.psi_total / psc_array.fac
+            psi = psc_array.psi / psc_array.fac
             b = psc_array.b_opt # * psc_array.fac
-            Linv = psc_array.L_inv # / psc_array.fac
-            Bn_objective = 0.5 * ((A_full @ (Linv @ psi))[:len(b)] + b).T @ ((A_full @ (Linv @ psi))[:len(b)] + b)
+            Linv = psc_array.L_inv[:psc_array.num_psc, :psc_array.num_psc] # / psc_array.fac
+            I = (-Linv @ psi)
+            Bn_objective = 0.5 * (A @ I + b).T @ (A @ I + b)
             # Bn_objective = 0.5 * (A @ (Linv @ psi)[:psc_array.num_psc] + b).T @ (A @ (Linv @ psi)[:psc_array.num_psc] + b)
             # print('Lpsi = ', Linv @ psi)
             # print('A_matrix = ', A[0, :])
@@ -148,25 +148,25 @@ class Testing(unittest.TestCase):
             psc_array_new = PSCgrid.geo_setup_manual(
                 points, R=R, a=a, alphas=alphas_new, deltas=deltas_new, **kwargs_manual
             )
-            psi_new = psc_array_new.psi_total / psc_array.fac
+            psi_new = psc_array_new.psi / psc_array.fac
+            I_new = (-Linv @ psi_new)
             # print(psi)
             # print(psi - psi_new)
             # print(psi, psi_new)
             # Bn_objective_new = 0.5 * (A @ (Linv @ psi_new)[:psc_array.num_psc] + b).T @ (A @ (Linv @ psi_new)[:psc_array.num_psc] + b)
-            Bn_objective_new = 0.5 * ((A_full @ (Linv @ psi_new))[:len(b)] + b).T @ ((A_full @ (Linv @ psi_new))[:len(b)] + b)
+            Bn_objective_new = 0.5 * (A @ I_new + b).T @ (A @ I_new + b)
             # print(Bn_objective, Bn_objective_new)
             dBn_objective = (Bn_objective_new - Bn_objective) / epsilon
             # print(A @ Linv @ psi_new, A, Linv, psi, psi_new)
             # print(b)
-            dpsi_ddelta = (psi_new - psi) / epsilon
+            dpsi_ddelta = (psc_array_new.psi - psc_array.psi) / epsilon  / psc_array.fac
             psi_deriv = psc_array.psi_deriv()  # * 1e-7
             print('dpsi = ', dpsi_ddelta[0], psi_deriv[ncoils])
             # print(psi_deriv)
-            # assert(np.allclose(dpsi_ddelta[0], psi_deriv[ncoils], rtol=1e-3))
-            ncoils_sym = psc_array.num_psc * psc_array.symmetry
-            dBn_analytic = (A @ (Linv @ psi)[:psc_array.num_psc] + b).T @ (A @ (Linv * psi_deriv[ncoils_sym:])[:psc_array.num_psc, :])
+            assert(np.allclose(dpsi_ddelta[0], psi_deriv[ncoils], rtol=1e-3))
+            dBn_analytic = (A @ I + b).T @ (-A @ (Linv * psi_deriv[ncoils:]))
             print(dBn_objective, dBn_analytic[0])
-            # assert np.isclose(dBn_objective, dBn_analytic[0], rtol=1e-2)
+            assert np.isclose(dBn_objective, dBn_analytic[0], rtol=1e-2)
             
             # Repeat but change coil 3
             # psc_array = PSCgrid.geo_setup_manual(
@@ -218,30 +218,33 @@ class Testing(unittest.TestCase):
             # print(dBn_objective, dBn_analytic[0])
             # assert np.isclose(dBn_objective, dBn_analytic[0], rtol=1e-1)
             
-            # # Repeat for changing coil 7
+            # Repeat for changing coil 7
             # psc_array = PSCgrid.geo_setup_manual(
             #     points, R=R, a=a, alphas=alphas, deltas=deltas, **kwargs_manual
             # )
             # A = psc_array.A_matrix
-            # psi = psc_array.psi
+            # psi = psc_array.psi / psc_array.fac
             # Linv = psc_array.L_inv[:psc_array.num_psc, :psc_array.num_psc]
-            # Bn_objective = 0.5 * (A @ Linv @ psi + b).T @ (A @ Linv @ psi + b)
+            # I = (-Linv @ psi)
+            # Bn_objective = 0.5 * (A @ I + b).T @ (A @ I + b)
             # alphas_new = np.copy(alphas)
-            # alphas_new[6] += epsilon
+            # alphas_new[3] += epsilon
             # deltas_new = np.copy(deltas)
             # psc_array_new = PSCgrid.geo_setup_manual(
             #     points, R=R, a=a, alphas=alphas_new, deltas=deltas_new, **kwargs_manual
             # )
-            # psi_new = psc_array_new.psi
-            # Bn_objective_new = 0.5 * (A @ Linv @ psi_new + b).T @ (A @ Linv @ psi_new + b)
+            # psi_new = psc_array_new.psi / psc_array.fac
+            # I_new = (-Linv @ psi_new)
+            # Bn_objective_new = 0.5 * (A @ I_new + b).T @ (A @ I_new + b)
             # dBn_objective = (Bn_objective_new - Bn_objective) / epsilon
-            # dpsi_dalpha = (psi_new - psi) / epsilon
-            # psi_deriv = psc_array.psi_deriv() * 1e-7
-            # print(dpsi_dalpha[6], psi_deriv[6])
-            # assert(np.allclose(dpsi_dalpha[6], psi_deriv[6], rtol=1e-3))
-            # dBn_analytic = (A @ Linv @ psi + b).T @ (A @ (Linv * psi_deriv[:ncoils]))
-            # print(dBn_objective, dBn_analytic[6])
-            # assert np.isclose(dBn_objective, dBn_analytic[6], rtol=1e-2)
+            # dpsi_dalpha = (psi_new - psi) / epsilon 
+
+            # psi_deriv = psc_array.psi_deriv() # * 1e-7
+            # print(dpsi_dalpha[3], psi_deriv[3])
+            # # assert(np.allclose(dpsi_dalpha[6], psi_deriv[6], rtol=1e-3))
+            # dBn_analytic = (A @ I + b).T @ (-A @ (Linv * psi_deriv[:ncoils]))
+            # print(dBn_objective, dBn_analytic[3])
+            # assert np.isclose(dBn_objective, dBn_analytic[3], rtol=1e-2)
     
     def test_L_analytic_derivatives(self):
         """
@@ -256,11 +259,11 @@ class Testing(unittest.TestCase):
                 points, R=R, a=a, alphas=alphas, deltas=deltas, **kwargs_manual
             )
             A = psc_array.A_matrix
-            psi = psc_array.psi_total
+            psi = psc_array.psi_total 
             b = psc_array.b_opt * psc_array.fac
             L = psc_array.L
             Linv = psc_array.L_inv
-            I = (Linv @ psi)[:psc_array.num_psc]
+            I = (-Linv @ psi)[:psc_array.num_psc]
             Bn_objective = 0.5 * (A @ I + b).T @ (A @ I + b)
             L_deriv = psc_array.L_deriv()
             deltas_new = np.copy(deltas)
@@ -271,14 +274,15 @@ class Testing(unittest.TestCase):
             )
             L_new = psc_array_new.L
             Linv_new = psc_array_new.L_inv
-            I_new = (Linv_new @ psi)[:psc_array.num_psc]
+            I_new = (-Linv_new @ psi)[:psc_array.num_psc]
             Bn_objective_new = 0.5 * (A @ I_new + b).T @ (A @ I_new + b)
             dBn_objective = (Bn_objective_new - Bn_objective) / epsilon
             dL_ddelta = (L_new - L) / epsilon
             dLinv_ddelta = (Linv_new - Linv) / epsilon
             ncoils_sym = L_deriv.shape[0] // 2
-            print(np.array_str(dL_ddelta, precision=4))
-            print(np.array_str(L_deriv[ncoils_sym, :, :], precision=4))
+            # print(np.array_str(dL_ddelta*1e7, precision=4, suppress_small=True))
+            # print(np.array_str(L_deriv[ncoils_sym, :, :]*1e7, precision=4, suppress_small=True))
+            # print(np.array_str(L_deriv[ncoils_sym, :, :], precision=4))
             # print(np.array_str(dL_ddelta / L_deriv[ncoils_sym, :, :], precision=4, suppress_small=True))
 
             dL_ddelta_analytic = L_deriv
@@ -289,10 +293,10 @@ class Testing(unittest.TestCase):
 
             # Linv calculations looks much more incorrect that the L derivatives,
             # maybe because of numerical error accumulation? 
-            # print(np.array_str(dL_ddelta/ dL_ddelta_analytic[ncoils_sym, :, :], precision=4, suppress_small=True))
+            print(np.max(np.abs(dL_ddelta - dL_ddelta_analytic[ncoils_sym, :, :])))
             assert(np.allclose(dL_ddelta, dL_ddelta_analytic[ncoils_sym, :, :], rtol=1e-1))
             assert(np.allclose(dLinv_ddelta, dLinv_ddelta_analytic[ncoils_sym, :, :], rtol=10))
-            dBn_analytic = (A @ I + b).T @ (A @ (dLinv_ddelta_analytic @ psi)[:, :psc_array.num_psc].T)
+            dBn_analytic = (A @ I + b).T @ (-A @ (dLinv_ddelta_analytic @ psi)[:, :psc_array.num_psc].T)
             print(dBn_objective, dBn_analytic[ncoils_sym])
             assert np.isclose(dBn_objective, dBn_analytic[ncoils_sym], rtol=1e-1)
             
@@ -365,18 +369,18 @@ class Testing(unittest.TestCase):
             # )
             # L = psc_array.L
             # Linv = psc_array.L_inv
-            # I = (Linv @ psi)[:psc_array.num_psc]
+            # I = (-Linv @ psi)[:psc_array.num_psc]
             # Bn_objective_new = 0.5 * (A @ I + b).T @ (A @ I + b)
             # L_deriv = psc_array.L_deriv()
             # alphas_new = np.copy(alphas)
-            # alphas_new[2] += epsilon
+            # alphas_new[1] += epsilon
             # deltas_new = np.copy(deltas)
             # psc_array_new = PSCgrid.geo_setup_manual(
             #     points, R=R, a=a, alphas=alphas_new, deltas=deltas_new, **kwargs_manual
             # )
             # L_new = psc_array_new.L
             # Linv_new = psc_array_new.L_inv
-            # I_new = (Linv_new @ psi)[:psc_array.num_psc]
+            # I_new = (-Linv @ psi)[:psc_array.num_psc]
             # Bn_objective_new = 0.5 * (A @ I_new + b).T @ (A @ I_new + b)
             # dBn_objective = (Bn_objective_new - Bn_objective) / epsilon
             # dL_ddelta = (L_new - L) / epsilon
@@ -384,13 +388,14 @@ class Testing(unittest.TestCase):
             # dL_ddelta_analytic = L_deriv
             # dLinv_ddelta_analytic = -L_deriv @ Linv @ Linv
                 
-            # # print(np.array_str(dL_ddelta, precision=3, suppress_small=True))
-            # # print(np.array_str(L_deriv[2, :, :], precision=3, suppress_small=True))
-            # assert(np.allclose(dL_ddelta, dL_ddelta_analytic[2, :, :], rtol=1e-2))
+            # print(np.array_str(dL_ddelta*1e7, precision=3, suppress_small=True))
+            # print(np.array_str(L_deriv[1, :, :]*1e7, precision=3, suppress_small=True))
+            # # print(np.max(np.abs(dL_ddelta - dL_ddelta_analytic[ncoils_sym, :, :])))
+            # assert(np.allclose(dL_ddelta, dL_ddelta_analytic[1, :, :], rtol=1e-2))
             # # assert(np.allclose(dLinv_ddelta, dLinv_ddelta_analytic[2, :, :], rtol=10))
-            # dBn_analytic = (A @ I + b).T @ (A @ (dLinv_ddelta_analytic @ psi)[:, :psc_array.num_psc].T)
-            # print(dBn_objective, dBn_analytic[2])
-            # assert np.isclose(dBn_objective, dBn_analytic[2], rtol=1e-2)
+            # dBn_analytic = (A @ I + b).T @ (-A @ (dLinv_ddelta_analytic @ psi)[:, :psc_array.num_psc].T)
+            # print(dBn_objective, dBn_analytic[1])
+            # assert np.isclose(dBn_objective, dBn_analytic[1], rtol=1e-2)
         
         
     def test_symmetrized_quantities(self):
@@ -472,6 +477,7 @@ class Testing(unittest.TestCase):
                 points, R=R, a=a, alphas=alphas_new, deltas=deltas_new, **kwargs_manual
             )
             A_new = psc_array_new.A_matrix
+            
             # A_new_full = psc_array_new.A_matrix_full
             Bn_objective_new = 0.5 * (A_new @ I + b).T @ (A_new @ I + b)
             # print('Bns = ', Bn_objective, Bn_objective_new)
@@ -479,23 +485,23 @@ class Testing(unittest.TestCase):
             A_deriv = psc_array.A_deriv()
             dBn_analytic = (A @ I + b).T @ (A_deriv[:, ncoils:] * I)
             print(dBn_objective, dBn_analytic[0])
-            # assert np.isclose(dBn_objective, dBn_analytic[0], rtol=1e-2)
+            assert np.isclose(dBn_objective, dBn_analytic[0], rtol=1e-1)
             dA_dalpha = (A_new - A) / epsilon
             dA_dkappa_analytic = A_deriv
-            # print(dA_dalpha[:, 0], dA_dkappa_analytic[:, 0])
-            # print('dA0 = ', dA_dalpha[0, :], dA_dkappa_analytic[0, :])
-            print('dA0 = ', dA_dalpha[-1, 0], dA_dkappa_analytic[-1, ncoils])
-            # assert np.allclose(dA_dalpha[:, 0], dA_dkappa_analytic[:, 0], rtol=1)
+            # print(dA_dalpha[:, 0], dA_dkappa_analytic[:, ncoils])
+            print('dA0 = ', np.max(np.abs(dA_dalpha[:, 0] / dA_dkappa_analytic[:, ncoils])))
+            # print('dA0 = ', dA_dalpha[-1, 0], dA_dkappa_analytic[-1, ncoils])
+            assert np.allclose(dA_dalpha[:, 0], dA_dkappa_analytic[:, ncoils], rtol=1)
             
             # Repeat but change coil 3
             # psc_array = PSCgrid.geo_setup_manual(
             #     points, R=R, a=a, alphas=alphas, deltas=deltas, **kwargs_manual
             # )
             # A = psc_array.A_matrix
-            # Linv = psc_array.L_inv[:ncoils, :ncoils]
-            # psi = psc_array.psi
-            # Linv_psi = Linv @ psi
-            # Bn_objective = 0.5 * (A @ Linv_psi + b).T @ (A @ Linv_psi + b)
+            # Linv = psc_array.L_inv
+            # psi = psc_array.psi_total
+            # I = (-Linv @ psi)[:psc_array.num_psc]
+            # Bn_objective = 0.5 * (A @ I + b).T @ (A @ I + b)
             # alphas_new = np.copy(alphas)
             # alphas_new[4] += epsilon
             # deltas_new = np.copy(deltas)
@@ -503,16 +509,27 @@ class Testing(unittest.TestCase):
             #     points, R=R, a=a, alphas=alphas_new, deltas=deltas_new, **kwargs_manual
             # )
             # A_new = psc_array_new.A_matrix
-            # Bn_objective_new = 0.5 * (A_new @ Linv_psi + b).T @ (A_new @ Linv_psi + b)
+            # dd = psc_array_new.deltas - psc_array.deltas
+            # ddprime = psc_array_new.deltas_total - psc_array.deltas_total
+            # aaprime = psc_array_new.alphas_total - psc_array.alphas_total
+            # # print('dd, ddprime = ', dd, ddprime, ddprime / epsilon)
+            # # print('a = ', psc_array.alphas_total)
+            # # print('a_new = ', psc_array_new.alphas_total)
+            # # print('aaprime = ', aaprime / epsilon, psc_array.aaprime_aa)
+            # # print('aaprime_dd = ', psc_array.aaprime_dd)
+            # # print('ddprime_aa = ', psc_array.ddprime_aa)
+            # # print('aaprime_dd = ', psc_array.aaprime_dd)
+            # # print('aaprime_dd = ', psc_array.aaprime_dd)
+            # Bn_objective_new = 0.5 * (A_new @ I + b).T @ (A_new @ I + b)
             # dBn_objective = (Bn_objective_new - Bn_objective) / epsilon
             # A_deriv = psc_array.A_deriv()
-            # dBn_analytic = (A @ Linv_psi + b).T @ (A_deriv[:, :ncoils] * Linv_psi)
+            # dBn_analytic = (A @ I + b).T @ (A_deriv[:, :ncoils] * I)
             # print(dBn_objective, dBn_analytic[4])
-            # assert np.isclose(dBn_objective, dBn_analytic[4], rtol=1)
+            # assert np.isclose(dBn_objective, dBn_analytic[4], rtol=1e-2)
             # dA_dalpha = (A_new - A) / epsilon
             # dA_dkappa_analytic = A_deriv
-            # print(dA_dalpha[-1, 4] / dA_dkappa_analytic[-1, 4])
-            # assert np.allclose(dA_dalpha[:, 4], dA_dkappa_analytic[:, 4], rtol=1e-1)
+            # # print(dA_dalpha[:, 4] / dA_dkappa_analytic[:, 4])
+            # assert np.allclose(dA_dalpha[:, 4], dA_dkappa_analytic[:, 4], rtol=1)
             
             # # Repeat but changing delta instead of alpha
             # psc_array = PSCgrid.geo_setup_manual(