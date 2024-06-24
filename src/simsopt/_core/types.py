98d9f20636ab1e28256aba858e81fc445f10eed4
Bharat Medasani <mbkumar@gmail.com>
2024-06-24 10:02:13 -0400

    replace nptyping with numpy.typing

--- src/simsopt/_core/types.py
+++ src/simsopt/_core/types.py
@@ -9,11 +9,14 @@ This module contains small utility functions and classes.
 from typing import Union, Sequence, Any
 from numbers import Integral, Real
 
-from nptyping import NDArray, Float, Int, Bool
+# from nptyping import NDArray, Float, Int, Bool
+import numpy as np
+from numpy.typing import NDArray
 
-RealArray = Union[Sequence[Real], NDArray[Any, Float]]
-IntArray = Union[Sequence[Integral], NDArray[Any, Int]]
+# RealArray is a type alias for numpy double array or RealArray
+RealArray = Union[Sequence[Real], NDArray[np.double]]
+IntArray = Union[Sequence[Integral], NDArray[np.int_]]
 StrArray = Sequence[str]
-BoolArray = Union[Sequence[bool], NDArray[Any, Bool]]
+BoolArray = Union[Sequence[bool], NDArray[np.bool]]
 Key = Union[Integral, str]
 
