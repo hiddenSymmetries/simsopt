Believe I fixed the dI calculation in the direct coil calculation to correctly compute the rotated coils contribution. Unit tests confusingly pass a basic test with ncoils = 8, but fail with a different number of coils. Similar confusing behavior when I change epsilon value of the finite differences. Seems like curve and current variables are getting changed accidentally somewhere. Will debug the basis test with different number of coils first.

examples/2_Intermediate/QA_psc.py
@@ -169,6 +169,10 @@ def coil_optimization_PSC(s, bs, bpsc):
     print(Btot.dof_names)
     # print([bpsc._coils[j]._current.get_value() for j in range(len(bpsc._coils))])
 
+    pscs = bpsc._coils[0]._curve._psc_array
+    # pscs.update_psi()
+    # pscs.setup_currents_and_fields()
+    # pscs.psi_deriv()
     Jf = SquaredFlux(s, Btot)
     B_axis = calculate_on_axis_B(bs, s, print_out=False)
     axis_norm = (B_axis * s.area())
@@ -179,14 +183,17 @@ def coil_optimization_PSC(s, bs, bpsc):
     print('Jf initial = ', Jf.J() / (B_axis ** 2 * s.area()))
     # Don't need other terms until let the coils move around
     JF = Jf 
-    pscs = bpsc._coils[0]._curve._psc_array
+    print(bpsc._coils[0]._curve.gamma()[-1, :], bpsc._coils[0]._current.get_value())
 
     def fun(dofs):
         """ Function for coil optimization grabbed from stage_two_optimization.py """
         JF.x = dofs
+        # print(dofs)
+        # exit()
         # print('dofs = ', dofs)
         # print(JF.dof_names)
         # Get the dofs for the PSCs, convert to alphas and deltas, and update the coils
+        # print(dofs, dofs.shape)
         dofs = dofs.reshape(-1, 4)
         dofs = dofs / np.sqrt(np.sum(dofs ** 2, axis=-1))[:, None]  # normalize the quaternion
         # print(dofs, np.sqrt(np.sum(dofs ** 2, axis=-1)), dofs.shape)
@@ -196,35 +203,24 @@ def coil_optimization_PSC(s, bs, bpsc):
             np.sqrt(1.0 + 2 * (dofs[:, 0] * dofs[:, 2] - dofs[:, 1] * dofs[:, 3])), 
             np.sqrt(1.0 - 2 * (dofs[:, 0] * dofs[:, 2] - dofs[:, 1] * dofs[:, 3])))
         # print('a, d = ', np.max(np.abs(alphas)), np.max(np.abs(deltas)))
+        print(bpsc._coils[1]._curve.gamma()[-1, :], bpsc._coils[1]._current.get_value())
+        pscs = bpsc._coils[0]._curve._psc_array
         pscs.setup_orientations(alphas, deltas)
-        #setup_curves
-        # pscs.update_curves()
         pscs.update_psi()
         pscs.setup_currents_and_fields()
         pscs.psi_deriv()
-        # pscs.psi_deriv_full()
-        # JF.field.B_vjp(dJdB)
-        # print(bpsc._coils[-1].curve.dgamma_by_dcoeff_vjp(v_gamma)(JF),
-        #     + bpsc._coils[-1].curve.dgammadash_by_dcoeff_vjp(v_gammadash)(JF),
-        #     + bpsc._coils[-1].psc_current_contribution_vjp(bpsc._coils[-1].dkappa_dcoef_vjp(v_current))(JF))
-        # print('a1, a2 = ', pscs.alphas, 
-        #       bpsc._coils[1]._current.get_value(), 
-        #       bpsc._coils[1].curve._psc_array.I[1],
-        #       bpsc._coils[1].curve._psc_array.all_currents[1].get_value(),
-        #       bpsc._coils[1].curve.gamma()[-1, :])
-        # exit()
-        
+        pscs.setup_curves()
         J = JF.J()
         grad = JF.dJ()
-        # print(bpsc._coils[0]._curve.gamma()[-1, :], bpsc._coils[0]._current.get_value())
+        print(bpsc._coils[1]._curve.gamma()[-1, :], bpsc._coils[1]._current.get_value())
         # print('grad = ', grad, grad.shape)
-        BdotN = np.mean(np.abs(np.sum(Btot.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)))
-        bs.set_points(s.gamma().reshape(-1, 3))
-        jf = Jf.J() / axis_norm2
-        grad_normed = grad / axis_norm2
-        outstr = f"J={J:.3e}, Jf={jf:.3e}, ⟨B·n⟩={BdotN:.3e}"
-        Bnnormed = BdotN / axis_norm
-        outstr += f", ║∇J║={np.linalg.norm(grad_normed):.3e}"
+        # BdotN = np.mean(np.abs(np.sum(Btot.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)))
+        # bs.set_points(s.gamma().reshape(-1, 3))
+        jf = J  #/ axis_norm2
+        grad_normed = grad  #/ axis_norm2
+        outstr = f"J={J:.3e}, Jf={jf:.3e}"  #, ⟨B·n⟩={BdotN:.3e}"
+        # Bnnormed = BdotN / axis_norm
+        outstr += f", ║∇J║={np.linalg.norm(grad):.3e}"
         print(outstr)
         return J, grad
 
@@ -236,6 +232,19 @@ def coil_optimization_PSC(s, bs, bpsc):
     f = fun
     dofs = JF.x
     h = np.random.uniform(size=dofs.shape)
+    # now try normalized in a general direction
+    # dofs_reshape = dofs.reshape(-1, 4)
+    # normalization = np.sqrt(np.sum(dofs_reshape ** 2, axis=-1))
+    # dofs_normed = dofs_reshape / normalization[:, None]
+    # alphas1 = np.arctan2(2 * (dofs_normed[:, 0] * dofs_normed[:, 1] + dofs_normed[:, 2] * dofs_normed[:, 3]), 
+    #                     1 - 2.0 * (dofs_normed[:, 1] ** 2 + dofs_normed[:, 2] ** 2))
+    # deltas1 = -np.pi / 2.0 + 2.0 * np.arctan2(
+    #     np.sqrt(1.0 + 2 * (dofs_normed[:, 0] * dofs_normed[:, 2] - dofs_normed[:, 1] * dofs_normed[:, 3])), 
+    #     np.sqrt(1.0 - 2 * (dofs_normed[:, 0] * dofs_normed[:, 2] - dofs_normed[:, 1] * dofs_normed[:, 3])))
+    # pscs.setup_orientations(alphas1, deltas1)
+    # pscs.update_psi()
+    # pscs.setup_currents_and_fields()
+    # pscs.psi_deriv()
 
     J0, dJ0 = f(dofs)
     dJh = sum(dJ0 * h)
@@ -367,7 +376,7 @@ B_axis = calculate_on_axis_B(bs, s)
 make_Bnormal_plots(bs, s_plot, out_dir, "BTF_0", B_axis)
 
 # Finally, initialize the psc class
-kwargs_geo = {"Nx": 5, "out_dir": out_str,
+kwargs_geo = {"Nx": 6, "out_dir": out_str,
                 "initialization": "plasma", 
               "poff": poff,}
 

src/simsopt/field/biotsavart.py
@@ -91,48 +91,6 @@ class BiotSavart(sopp.BiotSavart, MagneticField):
         )
 
         return res
-
-    def B_vjp(self, v):
-        r"""
-        Assume the field was evaluated at points :math:`\mathbf{x}_i, i\in \{1, \ldots, n\}` and denote the value of the field at those points by
-        :math:`\{\mathbf{B}_i\}_{i=1}^n`.
-        These values depend on the shape of the coils, i.e. on the dofs :math:`\mathbf{c}_k` of each coil.
-        This function returns the vector Jacobian product of this dependency, i.e.
-
-        .. math::
-
-            \{ \sum_{i=1}^{n} \mathbf{v}_i \cdot \partial_{\mathbf{c}_k} \mathbf{B}_i \}_k.
-
-        """
-        # from simsopt.geo.curveplanarfourier import PSCCurve
-
-        coils = self._coils
-        gammas = [coil.curve.gamma() for coil in coils]
-        gammadashs = [coil.curve.gammadash() for coil in coils]
-        currents = [coil.current.get_value() for coil in coils]
-        res_gamma = [np.zeros_like(gamma) for gamma in gammas]
-        res_gammadash = [np.zeros_like(gammadash) for gammadash in gammadashs]
-
-        points = self.get_points_cart_ref()
-        sopp.biot_savart_vjp_graph(points, gammas, gammadashs, currents, v,
-                                   res_gamma, res_gammadash, [], [], [])
-        dB_by_dcoilcurrents = self.dB_by_dcoilcurrents()
-        res_current = [np.sum(v * dB_by_dcoilcurrents[i]) for i in range(len(dB_by_dcoilcurrents))]
-        # print(res_current)
-        # print(np.shape(dB_by_dcoilcurrents), np.shape(dB_by_dcoilcurrents[0]), np.shape(res_current))
-        # print('check = ', [coils[i].vjp(res_gamma[i], res_gammadash[i], np.asarray([res_current[i]])).data for i in range(len(coils))])
-        # print('check2 = ', sum([coils[i].vjp(res_gamma[i], res_gammadash[i], np.asarray([res_current[i]])) for i in range(len(coils))]).data)
-        # print(np.shape(sum([coils[i].vjp(res_gamma[i], res_gammadash[i], np.asarray([res_current[i]])) for i in range(len(coils))])))
-        # exit()
-        # curves = [coil.curve for coil in coils]
-        # if np.any(isinstance(curves, PSCCurve)):
-        #     dI = np.zeros(len(curves))
-        #     for i in range(len(coils)): 
-        #         dI[i] = (coils[i].curve.psc_current_contribution_vjp([res_current[i]])
-        #     dI = -coils[i].curve._psc_array.L_inv @ dI
-        #     return dB
-        # else:
-        return sum([coils[i].vjp(res_gamma[i], res_gammadash[i], np.asarray([res_current[i]])) for i in range(len(coils))])
     
     def B_vjp(self, v):
         r"""
@@ -173,16 +131,49 @@ class BiotSavart(sopp.BiotSavart, MagneticField):
         if np.any(curve_flags):
             order = coils[0].curve.order 
             ndofs = 2 * order + 8
-            dI = np.zeros((len(coils), ndofs))
+            # dI = np.zeros((coils[0].curve.npsc, ndofs))
+            # print('res_current = ', res_current)
             # dI_deriv = []
-            for i in range(coils[0].curve.npsc):  #len(coils)):   # Not using the rotated ones directly
+            # for i in range(coils[0].curve.npsc):   # Not using the rotated ones directly
                 # print(i, coils[i].curve._index)
-                dI[i, :] = coils[i].curve.dkappa_dcoef_vjp([res_current[i]], coils[i].curve._index)
-                print(i, dI[i, :])
+                # for q in range(coils[0].curve._psc_array.symmetry):
+                # for fp in range(coils[0].curve._psc_array.nfp):
+                #     for stell in coils[0].curve._psc_array.stell_list:
+                # dI[i, :] = coils[i].curve.dkappa_dcoef_vjp([res_current[i]], i)
+            dI = np.zeros((len(coils), ndofs))
+            # for i in range(len(coils)):   # Not using the rotated ones directly
+                # print(i, coils[i].curve._index)
+                # for q in range(coils[0].curve._psc_array.symmetry):
+            q = 0
+            for fp in range(coils[0].curve._psc_array.nfp):
+                for stell in coils[0].curve._psc_array.stell_list:
+                    for i in range(coils[0].curve.npsc):   # Not using the rotated ones directly
+                        # dI[i, :] += coils[i + q * coils[0].curve.npsc].curve.dkappa_dcoef_vjp(
+                        #     [res_current[i + q * coils[0].curve.npsc]], i + q * coils[0].curve.npsc)
+                        dI[i, :] += coils[i + q * coils[0].curve.npsc].curve.dkappa_dcoef_vjp(
+                            [res_current[i + q * coils[0].curve.npsc]], i) * stell
+                    q += 1
+                # dI[i % coils[0].curve.npsc, :] += coils[i].curve.dkappa_dcoef_vjp([res_current[i]], i)   
+                # coils[i].curve.plot()
+                # print(i, dI[i, :])
             Linv = coils[0].curve._psc_array.L_inv
-            Linv[coils[0].curve.npsc:, :] = 0.0
+            # Linv[coils[0].curve.npsc:, :] = 0.0
+            # q = 0
+            # dI_all = np.zeros((len(coils), ndofs))
+            # for fp in range(coils[0].curve._psc_array.nfp):
+            #     for stell in coils[0].curve._psc_array.stell_list:
+            #         dI_all[q * coils[0].curve.npsc:(q + 1) * coils[0].curve.npsc, :] = dI[:coils[0].curve.npsc, :] * stell
+            #         q += 1
+            # dI = - Linv @ dI_all
             dI = - Linv @ dI
-            print(dI.shape)
+            self.dI_psc = dI[:, 2 * order + 1: 2 * order + 5]
+            # print('dI_ mid = ', self.dI_psc)
+            # dI = np.zeros(dI.shape)
+            ### Issue is that the disrete symmetries not getting imposed right here. Have
+            # dJ / dB * dB / dI * dI / ddofs and fairly sure dI / ddofs looks correct when dB/dI is just set to one. 
+            # Clearly dB / dI should be diffferent for the coils at discrete symmetries. 
+
+            # print(dI.shape)
             # for q in range(1, 4):
             #     dI[:coils[0].curve.npsc, :] += dI[coils[0].curve.npsc * q:coils[0].curve.npsc * (q + 1), :]
             # dI = np.zeros(dI.shape)
@@ -191,7 +182,9 @@ class BiotSavart(sopp.BiotSavart, MagneticField):
             #     dI_deriv.append(np.asarray(Derivative{coils[i].curve: dI[i]}))
             # dB = 
             # print([coils[i].vjp(res_gamma[i], res_gammadash[i], np.asarray([Derivative({coils[i].curve: dI[i, :]})])) for i in range(len(coils))])
-            return sum([coils[i].vjp(res_gamma[i], res_gammadash[i]) + Derivative({coils[i].curve: dI[i, :]}) for i in range(len(coils))])
+            # print(res_gamma, res_gammadash, res_current)
+            # exit()
+            return sum([coils[i].vjp(res_gamma[i], res_gammadash[i], dI[i, :]) for i in range(len(coils))])
         else:
             return sum([coils[i].vjp(res_gamma[i], res_gammadash[i], np.asarray([res_current[i]])) for i in range(len(coils))])
 

src/simsopt/geo/psc_grid.py
@@ -851,6 +851,9 @@ class PSCgrid:
                     self.R,
                 ) # accounts for sign change of the currents
                 q = q + 1
+        # if hasattr(self, 'A_matrix'):
+        #     'do nothing'
+        # else:
         self.A_matrix = 2 * A_matrix  
     
     def setup_curves(self):
@@ -1378,21 +1381,31 @@ class PSCgrid:
         """
         
         # Need to check is alphas and deltas are in [-pi, pi] and remap if not
+        self.coil_normals = np.array(
+            [np.cos(alphas) * np.sin(deltas),
+              -np.sin(alphas),
+              np.cos(alphas) * np.cos(deltas)]
+        ).T
+        alphas = np.arctan2(
+                -self.coil_normals[:, 1], 
+                np.sqrt(self.coil_normals[:, 0] ** 2 + self.coil_normals[:, 2] ** 2))
+        deltas = np.arcsin(self.coil_normals[:, 0] / \
+                                    np.sqrt(self.coil_normals[:, 0] ** 2 + self.coil_normals[:, 2] ** 2))
         self.alphas = alphas
         self.deltas = deltas
         kappas = np.ravel(np.array([self.alphas, self.deltas]))
         self.kappas = kappas
         
         self.coil_normals = np.array(
-            [np.cos(alphas) * np.sin(deltas),
-              -np.sin(alphas),
-              np.cos(alphas) * np.cos(deltas)]
+            [np.cos(self.alphas) * np.sin(self.deltas),
+              -np.sin(self.alphas),
+              np.cos(self.alphas) * np.cos(self.deltas)]
         ).T
         # deal with -0 terms in the normals, which screw up the arctan2 calculations
-        self.coil_normals[
-            np.logical_and(np.isclose(self.coil_normals, 0.0), 
-                           np.copysign(1.0, self.coil_normals) < 0)
-            ] *= -1.0
+        # self.coil_normals[
+        #     np.logical_and(np.isclose(self.coil_normals, 0.0), 
+        #                    np.copysign(1.0, self.coil_normals) < 0)
+        #     ] *= -1.0
                 
         # Apply discrete symmetries to the alphas and deltas and coordinates
         self.update_alphas_deltas()