3546bc98c9782d543168e26ae06f75fd5aef7469
WillhHoffman <112882136+WillhHoffman@users.noreply.github.com>
2024-08-07 15:47:29 -0400

    Removed m_maxima kwarg from grid

--- examples/2_Intermediate/permanent_magnet_PM4Stell.py
+++ examples/2_Intermediate/permanent_magnet_PM4Stell.py
@@ -39,10 +39,10 @@ if in_github_actions:
     max_nMagnets = 20
     downsample = 100  # drastically downsample the grid if running CI
 else:
-    N = 32  # >= 64 for high-resolution runs
-    nIter_max = 10000
+    N = 16  # >= 64 for high-resolution runs
+    nIter_max = 100000
     max_nMagnets = 10000
-    downsample = 10
+    downsample = 1
 
 nphi = N
 ntheta = N
@@ -50,9 +50,9 @@ algorithm = 'ArbVec_backtracking'
 nBacktracking = 200 
 nAdjacent = 10
 thresh_angle = np.pi  # / np.sqrt(2)
-nHistory = 10
+nHistory = 100
 angle = int(thresh_angle * 180 / np.pi)
-out_dir = Path("PM4Stell") 
+out_dir = Path("PM4Stell_angle{angle}_nb{nBacktracking)_na{nAdjacent}") 
 out_dir.mkdir(parents=True, exist_ok=True)
 print('out directory = ', out_dir)
 
@@ -143,10 +143,7 @@ pol_vectors[:, :, 1] = mag_data.pol_y
 pol_vectors[:, :, 2] = mag_data.pol_z
 
 # Using m_maxima functionality to try out unrealistically strong magnets
-B_max = 5  # 5 Tesla!!!!
-mu0 = 4 * np.pi * 1e-7
-m_maxima = B_max / mu0
-kwargs_geo = {"pol_vectors": pol_vectors, "m_maxima": m_maxima, "downsample": downsample}
+kwargs_geo = {"pol_vectors": pol_vectors, "downsample": downsample}
 
 # Initialize the permanent magnet grid from the PM4Stell arrangement
 pm_ncsx = PermanentMagnetGrid.geo_setup_from_famus(
@@ -200,7 +197,7 @@ if True:
     nmags = m_history.shape[0]
     nhist = m_history.shape[2]
     m_history_2d = m_history.reshape((nmags*m_history.shape[1], nhist))
-    np.savetxt(out_dir / 'm_history.txt', m_history_2d)
+    np.savetxt(out_dir / 'm_history_nmags={nmags}_nhist={nhist}.txt', m_history_2d)
 t_end = time.time()  
 print('Script took in total t = ', t_end - t_start, ' s')
 
