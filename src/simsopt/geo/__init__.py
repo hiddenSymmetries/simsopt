76a2fa10268a5513ad06ec702d205d1b188a385d
Elizabeth <ejpaul@umd.edu>
2024-08-12 15:52:13 -0400

    Merge in master.

--- src/simsopt/geo/__init__.py
+++ src/simsopt/geo/__init__.py
@@ -8,9 +8,6 @@ from .curverzfourier import *
 from .curvexyzfourier import *
 from .curveperturbed import *
 from .curveobjectives import *
-from .curveplanarfourier import *
-from .framedcurve import *
-from .finitebuild import *
 from .plotting import *
 
 from .boozersurface import *
@@ -22,19 +19,14 @@ from .surfaceobjectives import *
 from .surfacerzfourier import *
 from .surfacexyzfourier import *
 from .surfacexyztensorfourier import *
-from .strain_optimization import *
 
-from .permanent_magnet_grid import *
 
 __all__ = (curve.__all__ + curvehelical.__all__ +
            curverzfourier.__all__ + curvexyzfourier.__all__ +
            curveperturbed.__all__ + curveobjectives.__all__ +
-           curveplanarfourier.__all__ +
-           finitebuild.__all__ + plotting.__all__ +
+           plotting.__all__ +
            boozersurface.__all__ + qfmsurface.__all__ +
            surface.__all__ +
            surfacegarabedian.__all__ + surfacehenneberg.__all__ +
            surfacerzfourier.__all__ + surfacexyzfourier.__all__ +
-           surfacexyztensorfourier.__all__ + surfaceobjectives.__all__ +
-           strain_optimization.__all__ + framedcurve.__all__ + 
-           permanent_magnet_grid.__all__)
+           surfacexyztensorfourier.__all__ + surfaceobjectives.__all__)
--- src/simsopt/util/__init__.py
+++ src/simsopt/util/__init__.py
@@ -2,9 +2,6 @@ import os
 
 from .mpi import *
 from .logger import *
-from .famus_helpers import *
-from .polarization_project import *
-from .permanent_magnet_helper_functions import *
 
 """Boolean indicating if we are in the GitHub actions CI"""
 in_github_actions = "CI" in os.environ and os.environ['CI'].lower() in ['1', 'true']
@@ -12,8 +9,5 @@ in_github_actions = "CI" in os.environ and os.environ['CI'].lower() in ['1', 'tr
 __all__ = (
     mpi.__all__ 
     + logger.__all__ 
-    + famus_helpers.__all__ 
-    + polarization_project.__all__ 
-    + permanent_magnet_helper_functions.__all__
     + ['in_github_actions']
 )
