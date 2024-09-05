6664167f1a1a5480635c2bcabbd22ab07f27f915
WillhHoffman <112882136+WillhHoffman@users.noreply.github.com>
2024-09-05 18:04:15 -0400

    added more time comparisons for c++ vs for loop python vs vectorized python

--- tests/exact/timeTest.py
+++ tests/exact/timeTest.py
@@ -6,8 +6,8 @@ import Bcube_nonVec as floop
 import simsopt.field as vec
 import time
 
-N = 1000
-D = 10000
+N = 100
+D = 1000
 
 pos_points = np.random.uniform(150,1500, size = (N,3))
 signs = np.random.choice([-1,1], size = (N,3))
@@ -19,10 +19,10 @@ phiThetas = np.random.uniform(0,2*np.pi, size = (D,3))
 norms = np.random.uniform(-1,1, size = (N,3))
 dims = np.array([1,1,1])
 
-# t41 = time.time()
-# B4 = floop.B_direct(points, magPos, M, dims, phiThetas)
-# t42 = time.time()
-# print('for loop B_direct took t = ', t42 - t41,' s')
+t41 = time.time()
+B4 = floop.B_direct(points, magPos, M, dims, phiThetas)
+t42 = time.time()
+print('for loop B_direct took t = ', t42 - t41,' s')
 
 tv1 = time.time()
 Bv = vec.B_direct(points, magPos, M, dims, phiThetas)
@@ -33,3 +33,18 @@ tc1 = time.time()
 Bc = sopp.B_direct(points, magPos, M, dims, phiThetas)
 tc2 = time.time()
 print('c++ B_direct took t = ', tc2 - tc1,' s')
+
+t41 = time.time()
+B4 = floop.Acube(points, magPos, norms, dims, phiThetas)
+t42 = time.time()
+print('for loop A matrix took t = ', t42 - t41,' s')
+
+tv1 = time.time()
+Bv = vec.Acube(points, magPos, norms, dims, phiThetas)
+tv2 = time.time()
+print('vectorized A matrix took t = ', tv2 - tv1,' s')
+
+tc1 = time.time()
+Bc = sopp.Acube(points, magPos, norms, dims, phiThetas)
+tc2 = time.time()
+print('c++ A matrix took t = ', tc2 - tc1,' s')
