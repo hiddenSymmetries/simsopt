fb84c6bef156f2076c1d59348aa7b119dbd81967
Elizabeth <ejpaul@umd.edu>
2024-09-24 21:27:43 -0400

    Update to defaults in particle_guiding_center_boozer_tracing.

--- src/simsoptpp/tracing.h
+++ src/simsoptpp/tracing.h
@@ -119,5 +119,5 @@ tuple<vector<array<double, 5>>, vector<array<double, 6>>>
 particle_guiding_center_boozer_tracing(
         shared_ptr<BoozerMagneticField<T>> field, array<double, 3> stz_init,
         double m, double q, double vtotal, double vtang, double tmax, double dt, double abstol, double reltol, double roottol,
-        bool vacuum, bool noK, bool solveSympl, vector<double> zetas, vector<double> omegas,
-        vector<shared_ptr<StoppingCriterion>> stopping_criteria, double dt_save, vector<double> vpars, bool zetas_stop=false, bool vpars_stop=false, bool forget_exact_path=false, int axis=0, bool predictor_step=true);
+        bool vacuum, bool noK, bool solveSympl, vector<double> zetas={}, vector<double> omegas={},
+        vector<shared_ptr<StoppingCriterion>> stopping_criteria={}, double dt_save=1e-6, vector<double> vpars={}, bool zetas_stop=false, bool vpars_stop=false, bool forget_exact_path=false, int axis=0, bool predictor_step=true);
