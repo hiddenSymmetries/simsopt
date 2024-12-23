c3016296d50c2ef2e671dee5c29adfbece7d60f6
Alex Knyazev <aknyazev@ucsd.edu>
2024-12-23 11:26:37 -0500

    Add ShearAlfvenHarmonic class
    Introduced ShearAlfvenHarmonic class to represent
    a single harmonic Shear Alfvén Wave.
    
    Includes computation of scalar potential `Phi`,
    vector potential parameter `alpha`, and their derivatives.

--- src/simsoptpp/boozermagneticfield.h
+++ src/simsoptpp/boozermagneticfield.h
@@ -728,3 +728,253 @@ public:
            (s_values[i_right] - s_values[i_left]);
   }
 };
+
+/**
+* @brief Class representing a single harmonic Shear Alfvén Wave.
+* See Paul. et al, JPP (2023;89(5):905890515. doi:10.1017/S0022377823001095)
+*
+* Initializes the Shear Alfvén Wave with the scalar potential of the form
+* \f$ \Phi=\hat{\Phi}(s)\sin(m\theta - n\zeta + \omega t + \text{phase}) \f$
+* and vector potential alpha determined by the ideal
+* Ohm's law (i.e., zero electric field along the field line).
+*
+* @tparam T Template for tensor type. It should be a template class
+* compatible with xtensor-like syntax.
+*/
+template <template <class, std::size_t, xt::layout_type> class T>
+class ShearAlfvenHarmonic : public ShearAlfvenWave<T> {
+public:
+  using Tensor2 = T<double, 2, xt::layout_type::row_major>;
+  Phihat phihat;
+  int Phim;       // Poloidal mode number.
+  int Phin;       // Toroidal mode number.
+  double omega;   // Frequency of the wave.
+  double phase;   // Phase offset of the wave.
+
+protected:
+
+  /**
+  * @brief Computes te scalar potential `Phi` at each point.
+  *
+  * This method calculates the scalar potential \f$ \Phi \f$
+  * based on the given harmonic parameters and profile \f$ \hat{\Phi}(s) \f$.
+  *
+  * @param Phi Tensor to be filled with the computed scalar potential values.
+  */
+  void _Phi_impl(Tensor2 &Phi) override {
+    const Tensor2 &points = this->get_points_ref();
+    const auto &ss = xt::view(points, xt::all(), 0);
+    const auto &thetas = xt::view(points, xt::all(), 1);
+    const auto &zetas = xt::view(points, xt::all(), 2);
+    const auto &times = xt::view(points, xt::all(), 3);
+    for (std::size_t i = 0; i < ss.size(); ++i) {
+      xt::view(Phi, i, 0) = this->phihat(ss(i))
+                          * sin(this->Phim * thetas(i) - this->Phin * zetas(i) + this->omega * times(i) + this->phase);
+    }
+  }
+
+  /**
+  * @brief Computes the partial derivative of `Phi` with respect
+  * to toroidal flux coordinate `psi`.
+  *
+  * This method computes the partial derivative
+  * \f$ \frac{\partial \Phi}{\partial \psi} \f$ using the derivative
+  * of \f$ \hat{\Phi}(s) \f$ and the harmonic parameters.
+  *
+  * @param dPhidpsi Tensor to be filled with the computed values.
+  */
+  void _dPhidpsi_impl(Tensor2 &dPhidpsi) override {
+    const Tensor2 &points = this->get_points_ref();
+    const auto &ss = xt::view(points, xt::all(), 0);
+    const auto &thetas = xt::view(points, xt::all(), 1);
+    const auto &zetas = xt::view(points, xt::all(), 2);
+    const auto &times = xt::view(points, xt::all(), 3);
+    for (std::size_t i = 0; i < ss.size(); ++i) {
+      xt::view(dPhidpsi, i, 0) =
+        this->phihat.derivative(ss(i)) / (this->B0->psi0)
+          * sin(this->Phim * thetas(i) - this->Phin * zetas(i)
+            + this->omega * times(i) + this->phase);
+    }
+  }
+
+  /**
+  * @brief Computes the partial derivative of `Phi`
+  * with respect to poloidal angle `theta`.
+  *
+  * @param dPhidtheta Tensor to be filled with the computed values.
+  */
+  void _dPhidtheta_impl(Tensor2 &dPhidtheta) override {
+    const auto Phidot_values = xt::squeeze(this->Phidot_ref(), 1);
+
+    xt::view(dPhidtheta, xt::all(), 0) =
+      Phidot_values * (this->Phim / this->omega);
+  }
+
+  /**
+  * @brief Computes the partial derivative of `Phi`
+  * with respect to toroidal angle `zeta`.
+  *
+  * @param dPhidzeta Tensor to be filled with the computed values.
+  */
+  void _dPhidzeta_impl(Tensor2 &dPhidzeta) override {
+    const auto Phidot_values = xt::squeeze(this->Phidot_ref(), 1);
+
+    xt::view(dPhidzeta, xt::all(), 0) =
+      -Phidot_values * (this->Phin / this->omega);
+  }
+
+  /**
+  * @brief Computes the time derivative of `Phi`.
+  *
+  * This method computes the time derivative
+  * \f$ \frac{\partial \Phi}{\partial t} \f$.
+  *
+  * @param Phidot Tensor to be filled with the computed time derivative values.
+  */
+  void _Phidot_impl(Tensor2 &Phidot) override {
+    const Tensor2 &points = this->get_points_ref();
+    const auto &ss = xt::view(points, xt::all(), 0);
+    const auto thetas = xt::view(points, xt::all(), 1);
+    const auto zetas = xt::view(points, xt::all(), 2);
+    const auto times = xt::view(points, xt::all(), 3);
+
+    for (std::size_t i = 0; i < ss.size(); ++i) {
+      xt::view(Phidot, i, 0) = this->phihat(ss(i)) * this->omega
+        * cos(this->Phim * thetas(i) - this->Phin * zetas(i)
+          + this->omega * times(i) + this->phase);
+    }
+  }
+
+  /**
+  * @brief Computes the vector potential parameter `alpha`.
+  *
+  * @param alpha Tensor to be filled with the computed
+  * vector potential parameter values.
+  */
+  void _alpha_impl(Tensor2 &alpha) override {
+    const auto phi = xt::squeeze(this->Phi_ref(), 1);
+    const auto iota_ref = xt::squeeze(this->B0->iota_ref(), 1);
+    const auto G_ref = xt::squeeze(this->B0->G_ref(), 1);
+    const auto I_ref = xt::squeeze(this->B0->I_ref(), 1);
+
+    xt::view(alpha, xt::all(), 0) =
+        - phi * ((iota_ref * this->Phim - this->Phin)
+                  / (this->omega * (G_ref + iota_ref * I_ref)));
+  }
+
+  /**
+  * @brief Computes the time derivative of `alpha`.
+  *
+  * This method computes the time derivative
+  * \f$ \frac{\partial \alpha}{\partial t} \f$.
+  *
+  * @param alphadot Tensor to be filled with the computed
+  * time derivative values.
+  */
+  void _alphadot_impl(Tensor2 &alphadot) override {
+    const auto phidot = xt::squeeze(this->Phidot_ref(), 1);
+    const auto iota_ref = xt::squeeze(this->B0->iota_ref(), 1);
+    const auto G_ref = xt::squeeze(this->B0->G_ref(), 1);
+    const auto I_ref = xt::squeeze(this->B0->I_ref(), 1);
+
+    xt::view(alphadot, xt::all(), 0) =
+      -phidot * ((iota_ref * this->Phim - this->Phin)
+        / (this->omega * (G_ref + iota_ref * I_ref)));
+  }
+
+  /**
+  * @brief Computes the partial derivative of `alpha`
+  * with respect to toroidal flux coordinate `psi`.
+  *
+  * This method computes the partial derivative
+  * \f$ \frac{\partial \alpha}{\partial \psi} \f$ using the derivative
+  * of \f$ \hat{\Phi}(s) \f$ and the harmonic parameters.
+  *
+  * @param dalphadpsi Tensor to be filled with the computed values.
+  */
+  void _dalphadpsi_impl(Tensor2 &dalphadpsi) override {
+    const auto diotadpsi_values = xt::squeeze(this->B0->diotads_ref(), 1)
+                            / this->B0->psi0;
+    const auto dGdpsi_values = xt::squeeze(this->B0->dGds_ref(), 1)
+                         / this->B0->psi0;
+    const auto dIdpsi_values = xt::squeeze(this->B0->dIds_ref(), 1)
+                         / this->B0->psi0;
+    const auto iota_values = xt::squeeze(this->B0->iota_ref(), 1);
+    const auto G_values = xt::squeeze(this->B0->G_ref(), 1);
+    const auto I_values = xt::squeeze(this->B0->I_ref(), 1);
+    const auto dPhidpsi_values = xt::squeeze(this->dPhidpsi_ref(), 1);
+    const auto Phi_values = xt::squeeze(this->Phi_ref(), 1);
+
+    xt::view(dalphadpsi, xt::all(), 0) =
+      - dPhidpsi_values * (iota_values * this->Phim - this->Phin)
+                        / (this->omega * (G_values + iota_values * I_values))
+      - (Phi_values / this->omega)
+      * ( diotadpsi_values * this->Phim / (G_values + iota_values * I_values)
+          - (iota_values * this->Phim - this->Phin)
+            / (
+                (G_values + iota_values * I_values)
+                * (G_values + iota_values * I_values)
+              )
+            * (
+                dGdpsi_values + diotadpsi_values * I_values
+                + iota_values * dIdpsi_values
+              )
+        );
+  }
+
+  /**
+  * @brief Computes the partial derivative of `alpha`
+  * with respect to poloidal angle `theta`.
+  *
+  * @param dalphadtheta Tensor to be filled with the computed values.
+  */
+  void _dalphadtheta_impl(Tensor2 &dalphadtheta) override {
+    const auto iota_values = xt::squeeze(this->B0->iota_ref(), 1);
+    const auto G_values = xt::squeeze(this->B0->G_ref(), 1);
+    const auto I_values = xt::squeeze(this->B0->I_ref(), 1);
+    const auto dPhidtheta_values = xt::squeeze(this->dPhidtheta_ref(), 1);
+
+    xt::view(dalphadtheta, xt::all(), 0) =
+      - dPhidtheta_values * (iota_values * this->Phim - this->Phin)
+        / (this->omega * (G_values + iota_values * I_values));
+  }
+
+  /**
+  * @brief Computes the partial derivative of `alpha`
+  * with respect to toroidal angle `zeta`.
+  *
+  * @param dalphadzeta Tensor to be filled with the computed values.
+  */
+  void _dalphadzeta_impl(Tensor2 &dalphadzeta) override {
+    const auto iota_values = xt::squeeze(this->B0->iota_ref(), 1);
+    const auto G_values = xt::squeeze(this->B0->G_ref(), 1);
+    const auto I_values = xt::squeeze(this->B0->I_ref(), 1);
+    const auto dPhidzeta_values = xt::squeeze(this->dPhidzeta_ref(), 1);
+
+    xt::view(dalphadzeta, xt::all(), 0) =
+        - dPhidzeta_values * (iota_values * this->Phim - this->Phin)
+          / (this->omega * (G_values + iota_values * I_values));
+  }
+
+public:
+
+  /**
+  * @brief Constructor for the ShearAlfvenHarmonic class.
+  *
+  * Initializes the Shear Alfvén Harmonic with a given profile `phihat`,
+  * mode numbers `m` and `n`,
+  * wave frequency `omega`, phase `phase`, and equilibrium magnetic field `B0`.
+  *
+  * @param phihat_in Profile of the scalar potential.
+  * @param Phim Poloidal mode number.
+  * @param Phin Toroidal mode number.
+  * @param omega Frequency of the wave.
+  * @param phase Phase offset of the wave.
+  * @param B0field Shared pointer to the equilibrium Boozer magnetic field.
+  */
+  ShearAlfvenHarmonic(const Phihat &phihat_in, int Phim, int Phin, double omega,
+                      double phase,
+                      shared_ptr<BoozerMagneticField<T>> B0field)
+      : ShearAlfvenWave<T>(B0field),
+        phihat(phihat_in), Phim(Phim), Phin(Phin), omega(omega), phase(phase) {}
+};
