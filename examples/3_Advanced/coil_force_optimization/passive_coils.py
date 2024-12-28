Fixed a bug in the A() calculation where I wasnt dividing by the number of quadpoints during the biotsavart integral.

examples/3_Advanced/coil_force_optimization/passive_coils.py
@@ -138,9 +138,9 @@ for i in range(len(base_curves)):
     for j in range(2 * order + 1):
         base_curves[i].fix('x' + str(j))
     # # Fix center points of each coil FOR NOW
-    base_curves[i].fix('x' + str(2 * order + 5))
-    base_curves[i].fix('x' + str(2 * order + 6))
-    base_curves[i].fix('x' + str(2 * order + 7))
+    # base_curves[i].fix('x' + str(2 * order + 5))
+    # base_curves[i].fix('x' + str(2 * order + 6))
+    # base_curves[i].fix('x' + str(2 * order + 7))
     # base_curves[i].fix_all()
 ncoils = len(base_curves)
 a_list = np.ones(len(base_curves)) * a

src/simsopt/field/force.py
@@ -2116,7 +2116,7 @@ def net_fluxes_pure(gammas, gammadashs, gammas2, gammadashs2, currents2, downsam
     gammadashs2 = gammadashs2[:, ::downsample, :]
     rij_norm = jnp.linalg.norm(gammas[:, :, None, None, :] - gammas2[None, None, :, :, :], axis=-1)
     # sum over the currents, and sum over the biot savart integral
-    A_ext = jnp.sum(currents2[None, None, :, None] * jnp.sum(gammadashs2[None, None, :, :, :] / rij_norm[:, :, :, :, None], axis=-2), axis=-2)
+    A_ext = jnp.sum(currents2[None, None, :, None] * jnp.sum(gammadashs2[None, None, :, :, :] / rij_norm[:, :, :, :, None], axis=-2), axis=-2) / jnp.shape(gammadashs2)[1]
     # print(A_ext.shape, gammadashs.shape, gammadashs2.shape, rij_norm.shape, currents2.shape)
     # Now sum over the PSC coil loops 
     return 1e-7 * jnp.sum(jnp.sum(A_ext * gammadashs, axis=-1), axis=-1) / jnp.shape(gammadashs)[1]