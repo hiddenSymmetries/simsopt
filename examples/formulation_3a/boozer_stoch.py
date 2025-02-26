60078b4bc882e49486c0439db01dafe0dbc635b0
Andrew Giuliani <agiuliani@flatironinstitute.org>
2025-02-26 14:41:25 -0500

    adding runfile [skip ci]

--- examples/formulation_3a/boozer_stoch.py
+++ examples/formulation_3a/boozer_stoch.py
@@ -50,27 +50,6 @@ too complex.  The BFGS optimizer is used, and quasisymmetry is improved substant
 More details on this work can be found at doi:10.1017/S0022377822000563 or arxiv:2203.03753.
 """
 
-#class MPIGradient(Optimizable):
-#
-#    def __init__(self, objective, comm):
-#        Optimizable.__init__(self, x0=np.asarray([]), depends_on=[objective])
-#        self.objective = objective
-#        self.comm = comm
-#        self.unique_lineage = [obj for obj in objective.unique_dof_lineage if obj.dofs._x.size > 0 and obj.x.size > 0]
-#        r_unique_lineage = [[obj]*(obj.x.size) for obj in self.unique_lineage]
-#        self.r_unique_lineage = [item for sublist in r_unique_lineage for item in sublist]
-#        import ipdb;ipdb.set_trace()
-#
-#    def J(self):
-#        return self.objective.J()
-#
-#    @derivative_dec
-#    def dJ(self):
-#        return 0.
-
-
-
-
 # Directory for output
 OUT_DIR      = f"./Nsamples={comm.size}/"
 os.makedirs(OUT_DIR, exist_ok=True)
@@ -139,13 +118,188 @@ rg = np.random.Generator(PCG64(seed, inc=0))
 L = 0.4*np.pi
 sqrtC = GaussianSampler(base_curves[0].quadpoints, SIGMA, L, n_derivs=1).L
 
-surface = s_orig
-boozer_surface = boozer_surface_orig
+surfaces = [s_orig]
+boozer_surfaces = [boozer_surface_orig]
 J_nonQSRatio = NonQuasiSymmetricRatio(boozer_surface_orig, BiotSavart(coils), sqrtC=sqrtC)
 
-x = J_nonQSRatio.x.copy()
-x[10] += 1e-200*1j
-J_nonQSRatio.x = x
-print(J_nonQSRatio.J())
-tr = J_nonQSRatio.GN_trace()
-print(tr.real, tr.imag/1e-200)
+#x = J_nonQSRatio.x.copy()
+#x[10] += 1e-200*1j
+#J_nonQSRatio.x = x
+#print(J_nonQSRatio.J())
+#tr = J_nonQSRatio.GN_trace()
+#print(tr.real, tr.imag/1e-200)
+
+## SET UP THE OPTIMIZATION PROBLEM AS A SUM OF OPTIMIZABLES ##
+MIN_DIST_THRESHOLD = 0.15
+KAPPA_THRESHOLD = 15.
+MSC_THRESHOLD = 15.
+IOTAS_TARGET = -0.415
+MR_TARGET = 1.467434
+
+LENGTH_WEIGHT = 1.
+MR_WEIGHT = 1.
+MIN_DIST_WEIGHT = 1e2
+KAPPA_WEIGHT = 1.
+MSC_WEIGHT = 1.
+IOTAS_WEIGHT = 1e2
+ARCLENGTH_WEIGHT = 1e-2
+
+ls = [CurveLength(c) for c in base_curves]
+mrs = [MajorRadius(boozer_surface) for boozer_surface in boozer_surfaces]
+iotas = [Iotas(boozer_surface) for boozer_surface in boozer_surfaces]
+
+J_major_radius = QuadraticPenalty(mrs[0], MR_TARGET, 'identity')
+J_iotas = QuadraticPenalty(iotas[0], IOTAS_TARGET, 'identity')
+Jls = QuadraticPenalty(sum(ls), sum(ls).J(), 'max')
+Jccdist = CurveCurveDistance(curves, MIN_DIST_THRESHOLD, num_basecurves=len(base_curves))
+Jcs = sum([LpCurveCurvature(c, 2, KAPPA_THRESHOLD) for c in base_curves])
+msc_list = [MeanSquaredCurvature(c) for c in base_curves]
+Jmsc = sum(QuadraticPenalty(J, MSC_THRESHOLD, "max") for J in msc_list)
+Jals = sum([ArclengthVariation(c) for c in base_curves])
+
+# sum the objectives together
+#JF = J_nonQSRatio + IOTAS_WEIGHT*J_iotas + MR_WEIGHT*J_major_radius + LENGTH_WEIGHT*Jls\
+#        + MIN_DIST_WEIGHT * Jccdist + KAPPA_WEIGHT*Jcs + MSC_WEIGHT*Jmsc + ARCLENGTH_WEIGHT*Jals
+JF = IOTAS_WEIGHT*J_iotas + MR_WEIGHT*J_major_radius + LENGTH_WEIGHT*Jls + ARCLENGTH_WEIGHT*Jals
+
+
+#curves_to_vtk(curves, OUT_DIR + "curves_init")
+#boozer_surface.surface.to_vtk(OUT_DIR + "surf_init")
+
+
+eps = 1e-200
+# dictionary used to save the last accepted surface dofs in the line search, in case Newton's method fails
+prevs = {'sdofs': [surface.x.copy() for surface in surfaces], 'iota': [boozer_surface.res['iota'] for boozer_surface in boozer_surfaces],
+         'G': [boozer_surface.res['G'] for boozer_surface in boozer_surfaces], 'J': JF.J(), 'it': 0}
+def fun(dofs):
+    # initialize to last accepted surface values
+    for idx, surface in enumerate(surfaces):
+        surface.x = prevs['sdofs'][idx]
+    for idx, boozer_surface in enumerate(boozer_surfaces):
+        boozer_surface.res['iota'] = prevs['iota'][idx]
+        boozer_surface.res['G'] = prevs['G'][idx]
+    
+    # this check makes sure that all ranks have exactly the same dofs
+    if comm is not None:
+        alldofs = comm.allgather(dofs)
+        assert np.all(np.all(alldofs[0]-d == 0) for d in alldofs)
+    
+    dofs_complex = dofs.copy().astype(complex)
+    dofs_complex[rank] = dofs[rank] + eps*1j
+    JF.x = dofs_complex
+    J = JF.J() + J_nonQSRatio.J() + J_nonQSRatio.GN_trace() * 0.01**2
+    grad = np.array(comm.allgather(J)).imag/eps
+    
+    # check to make sure that all the surface solves succeeded
+    success1 = np.all([boozer_surface.res['success'] for boozer_surface in boozer_surfaces])
+    # check to make sure that the surfaces are not self-intersecting
+    success2 = np.all([not boozer_surface.res['is_self_intersecting'] for boozer_surface in boozer_surfaces])
+    if not (success1 and success2):
+        J = prevs['J']
+        #grad = -prevs['dJ']
+        for idx, boozer_surface in enumerate(boozer_surfaces):
+            boozer_surface.surface.x = prevs['sdofs'][idx]
+            boozer_surface.res['iota'] = prevs['iota'][idx]
+            boozer_surface.res['G'] = prevs['G'][idx]
+    return J, grad
+
+def callback(x):
+    x = x.astype(complex)
+    # since this set of coil dofs was accepted, set the backup surface dofs
+    # to accepted ones in case future Newton solves fail.
+    for idx, surface in enumerate(surfaces):
+        prevs['sdofs'][idx] = surface.x.copy()
+    for idx, boozer_surface in enumerate(boozer_surfaces):
+        prevs['iota'][idx] = boozer_surface.res['iota']
+        prevs['G'][idx] = boozer_surface.res['G']
+    prevs['J'] = JF.J() + J_nonQSRatio.J() + J_nonQSRatio.GN_trace() * 0.01**2
+
+    
+    mr_list    = [boozer_surface.res["minor_radius"] for boozer_surface in boozer_surfaces]
+    Mr_list    = [boozer_surface.res["major_radius"] for boozer_surface in boozer_surfaces]
+    AR_list    = [boozer_surface.res["aspect_ratio"] for boozer_surface in boozer_surfaces]
+    iota_list  = [boozer_surface.res['iota'] for boozer_surface in boozer_surfaces]
+    width = 35
+
+#JF = J_nonQSRatio + IOTAS_WEIGHT*J_iotas + MR_WEIGHT*J_major_radius + LENGTH_WEIGHT*Jls\
+#        + MIN_DIST_WEIGHT * Jccdist + KAPPA_WEIGHT*Jcs + MSC_WEIGHT*Jmsc + ARCLENGTH_WEIGHT*Jals
+
+
+    outstr = f"\nIteration {prevs['it']}\n"
+    outstr += f"{'J':{width}} {prevs['J'].real:.6e} \n"
+    outstr += f"{'ι on surfaces':{width}} ({IOTAS_WEIGHT*J_iotas.J().real:.6e}):  " + ", ".join([f"{iot.real:.6f}" for iot in [np.min(iota_list), np.mean(iota_list), np.max(iota_list)]]) + "\n"
+    outstr += f"{'major radius on surfaces':{width}} ({MR_WEIGHT*J_major_radius.J().real:.6e}):  " + ', '.join([f'{Mr.real:.6f}' for Mr in [np.min(Mr_list), np.mean(Mr_list), np.max(Mr_list)]]) + "\n"
+    #outstr += f"{'shortest coil to coil distance':{width}} ({MIN_DIST_WEIGHT * Jccdist.J():.6e}):  {Jccdist.shortest_distance():.3f} \n"
+    outstr += f"{'coil length sum':{width}} ({LENGTH_WEIGHT*Jls.J().real:.6e}):  {sum(J.J().real for J in ls):.3f} \n"
+    outstr += f"{'max κ':{width}} ({KAPPA_WEIGHT*Jcs.J().real:.6e}):  " + ', '.join([f'{np.max(c.kappa().real):.6f}' for c in base_curves]) + "\n"
+    outstr += f"{'∫ κ^2 dl / ∫ dl':{width}} ({MSC_WEIGHT*Jmsc.J().real:.6e}):  " + ', '.join([f'{Jmsc.J().real:.6f}' for Jmsc in msc_list]) + "\n"
+    outstr += f"{'Arclength':{width}} ({ARCLENGTH_WEIGHT*Jals.J().real:.6e}):  " + ', '.join([f'{np.var(np.linalg.norm(c.gammadash().real, axis=-1)):.6f}' for c in base_curves]) + "\n"
+    outstr += f"{'aspect ratio radius on surfaces':{width}}" + ', '.join([f'{AR.real:.6f}' for AR in [np.min(AR_list), np.mean(AR_list), np.max(AR_list)]]) + "\n"
+    outstr += f"{'minor radius on surfaces':{width}}" + ', '.join([f'{mr.real:.6f}' for mr in [np.min(mr_list), np.mean(mr_list), np.max(mr_list)]]) + "\n"
+    outstr += f"{'coil lengths':{width}}" + ', '.join([f'{J.J().real:5.6f}' for J in ls]) + "\n"
+    outstr += "\n\n"
+    
+    proc0_print(outstr, flush=True)
+    prevs['it'] += 1
+
+f = fun
+dofs = JF.x.copy().real
+callback(dofs)
+
+#fval = [prevs['J']]
+#epsval = np.linspace(0, 1e-2, 100)[:15]
+#h = JF.dJ()
+#for eps in epsval[1:]:
+#    ff, _ = f(dofs - eps*h)
+#    if ff == prevs['J']:
+#        break
+#    fval.append(ff)
+#    proc0_print(ff)
+#
+#if rank == 0:
+#    fval = np.array(fval)
+#    np.savetxt(f'fval{comm.size}.txt', np.concatenate((epsval[:fval.size, None], fval[:, None]), axis=1))
+#quit()
+
+
+#print("""
+#################################################################################
+#### Perform a Taylor test ######################################################
+#################################################################################
+#""")
+#np.random.seed(1)
+#h = np.random.uniform(size=dofs.shape)
+#J0, dJ0 = f(dofs)
+#dJh = sum(dJ0 * h)
+#for eps in [1e-6, 1e-7, 1e-8, 1e-9]:
+#    J1, _ = f(dofs + 2*eps*h)
+#    J2, _ = f(dofs + eps*h)
+#    J3, _ = f(dofs - eps*h)
+#    J4, _ = f(dofs - 2*eps*h)
+#    proc0_print("err", ((J1*(-1/12) + J2*(8/12) + J3*(-8/12) + J4*(1/12))/eps - dJh)/np.linalg.norm(dJh))
+#
+#quit()
+
+proc0_print("""
+################################################################################
+### Run the optimization #######################################################
+################################################################################
+""", flush=True)
+# Number of iterations to perform:
+MAXITER = 5e3
+restart = 1
+
+for j in range(restart):
+    res = minimize(fun, dofs, jac=True, method='BFGS', options={'maxiter': MAXITER}, tol=1e-15, callback=callback)
+    proc0_print("RESTARTING ", j)
+    
+    iota_list = [boozer_surface.res['iota'] for boozer_surface in boozer_surfaces]
+    G_list = [boozer_surface.res['G'] for boozer_surface in boozer_surfaces]
+    if rank == 0:
+        save([coils, surfaces, iota_list, G_list], OUT_DIR + f'stoch.json')
+    #curves_to_vtk(curves, OUT_DIR + "curves_opt")
+    #boozer_surface_orig.surface.to_vtk(OUT_DIR + "surf_opt")
+    dofs = res['x'].copy()
+
+proc0_print("End of 2_Intermediate/boozerQA.py", flush=True)
+proc0_print("================================", flush=True)
