7076d220ce6a1c9a7fb19fce91e75fd2aa63d0f9
Matt Landreman <mattland@umd.edu>
2025-03-13 13:07:25 -0400

    Trying to fix test_extend_via_normal in CI

--- src/simsopt/geo/surfacerzfourier.py
+++ src/simsopt/geo/surfacerzfourier.py
@@ -832,6 +832,10 @@ class SurfaceRZFourier(sopp.SurfaceRZFourier, Surface):
             Z_basis_funcs = R_basis_funcs
 
         # Fit the mode amplitudes to the new points:
+        if not np.all(np.isfinite(R)):
+            print("extend_via_normal: R has non-finite values. isnan: ", np.any(np.isnan(R)), " isinf: ", np.any(np.isinf(R)))
+        if not np.all(np.isfinite(R_basis_funcs)):
+            print("extend_via_normal: R_basis_funcs has non-finite values. isnan: ", np.any(np.isnan(R_basis_funcs)), " isinf: ", np.any(np.isinf(R_basis_funcs)))
         R_dofs = np.linalg.lstsq(R_basis_funcs, R, rcond=None)[0]
         Z_dofs = np.linalg.lstsq(Z_basis_funcs, Z, rcond=None)[0]
 
