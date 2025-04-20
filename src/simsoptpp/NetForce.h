48a29fb71c0b9fecccbf98fcf260916c423bd35a
mp5650 <71712126+mp5650@users.noreply.github.com>
2025-04-20 01:11:44 -0400

    Made net forces calc super fast

--- src/simsoptpp/NetForce.h
+++ src/simsoptpp/NetForce.h
@@ -5,6 +5,4 @@
 #include <cstddef> // for size_t
 #include "xtensor-python/pyarray.hpp"     // Numpy bindings
 typedef xt::pyarray<double> Array;
-
-Array dipole_force_matrix(Array& magnetMoments, Array& magnetPositions);
 Array net_force_matrix(Array& magnetMoments, Array& magnetPositions);
