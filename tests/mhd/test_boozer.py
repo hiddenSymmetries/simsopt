df0da84c42870b4f0961e8c6371caea8fc289304
Stefan Buller <sbuller@umd.edu>
2025-05-07 13:17:08 -0400

    Reverting files to master version

--- tests/mhd/test_boozer.py
+++ tests/mhd/test_boozer.py
@@ -8,12 +8,12 @@ from scipy.io import netcdf_file
 try:
     import booz_xform
 except ImportError:
-    booz_xform = None
+    booz_xform = None 
 
 try:
     import vmec
 except ImportError:
-    vmec = None
+    vmec = None 
 
 try:
     from mpi4py import MPI
@@ -27,6 +27,8 @@ if MPI is not None:
 from . import TEST_DIR
 
 logger = logging.getLogger(__name__)
+#logging.basicConfig(level=logging.DEBUG)
+
 
 class MockBoozXform():
     """
@@ -52,7 +54,11 @@ class MockBoozXform():
         arr2 = arr1 + 1
         arr2[0] = 100
         self.bmnc_b = np.stack((arr1, arr2)).transpose()
-        
+        print('bmnc_b:')
+        print(self.bmnc_b)
+        # print('booz_xform_found:', booz_xform_found)
+
+
 class MockBoozer(Optimizable):
     """
     This class exists only for testing the Quasisymmetry class.  It
@@ -86,32 +92,26 @@ class QuasisymmetryTests(unittest.TestCase):
         # bmnc:  [100 21 31 41 51 61 71 81 91 101 111 121 131 141 151 161 171 181]]
 
         # QA
-        s = 0
-        q = Quasisymmetry(b, s, 1, 0, "B00", "even")
+        s = 0; q = Quasisymmetry(b, s, 1, 0, "B00", "even")
         np.testing.assert_allclose(q.J(), [2, 3, 4, 5, 7, 8, 9, 10, 12, 13, 14, 15, 17, 18])
-        s = 1
-        q = Quasisymmetry(b, s, 1, 0, "B00", "even")
+        s = 1; q = Quasisymmetry(b, s, 1, 0, "B00", "even")
         np.testing.assert_allclose(q.J(), [.21, .31, .41, .51, .71, .81, .91, 1.01, 1.21, 1.31, 1.41, 1.51, 1.71, 1.81])
-        s = (0, 1)
-        q = Quasisymmetry(b, s, 1, 0, "B00", "even")
+        s = (0, 1); q = Quasisymmetry(b, s, 1, 0, "B00", "even")
         np.testing.assert_allclose(q.J(), [2, 3, 4, 5, 7, 8, 9, 10, 12, 13, 14, 15, 17, 18,
                                            .21, .31, .41, .51, .71, .81, .91, 1.01, 1.21, 1.31, 1.41, 1.51, 1.71, 1.81])
 
         # QP
         s = 0
         q = Quasisymmetry(b, s, 0, 1, "B00", "even")
-        np.testing.assert_allclose(
-            q.J(), [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
+        np.testing.assert_allclose(q.J(), [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
 
         # QH
         q = Quasisymmetry(b, s, 1, 1, "B00", "even")
-        np.testing.assert_allclose(
-            q.J(), [2, 3, 4, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18])
+        np.testing.assert_allclose(q.J(), [2, 3, 4, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18])
 
         # QH, opposite "chirality"
         q = Quasisymmetry(b, s, 1, -1, "B00", "even")
-        np.testing.assert_allclose(
-            q.J(), [2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18])
+        np.testing.assert_allclose(q.J(), [2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18])
 
     @unittest.skipIf(booz_xform is None, "booz_xform python package not found")
     def test_boozer_register(self):
@@ -222,30 +222,6 @@ class QuasisymmetryTests(unittest.TestCase):
                                    atol=atol, rtol=rtol)
         np.testing.assert_allclose(bmnc[:, 1], bmnc_ref[:, -1],
                                    atol=atol, rtol=rtol)
-
-    def test_boozer_basic_non_stellsym(self):
-        """Check that we are close to precalculated quasisymmetry errors.
-        For a stellarator assymmetric configuration with less-used normalization flags to increase code coverage.
-        Also registers surfaces with a float instead of an iterable for code-coverage.
-        In practical code, this is redundant since the Quasisymmetry objective would register the needed surface automatically.
-        This is run for an unrealistically low number of iterations for the sake of speed."""
-        with ScratchDir("."):
-            v = Vmec(os.path.join(TEST_DIR, "input.basic_non_stellsym"))
-            v.indata.niter_array[:2] = [100, 0]  # Low number of iterations
-            b = Boozer(v, mpol=32, ntor=16)
-            # code coverage for the case where we register a float and not something iterable.
-            b.register(0.5)
-            J = Quasisymmetry(b, s=0.5, helicity_m=1, helicity_n=0,
-                              normalization="symmetric", weight="stellopt").J()
-
-            np.testing.assert_allclose(
-                J[0], -3.2704931879062826, err_msg='Weight "stellopt" for normalization "symmetric" does not match precalculated value for a non-stellarator symmetric configuration.')
-            J = Quasisymmetry(b, s=0.5, helicity_m=1, helicity_n=0,
-                              normalization="B00", weight="stellopt_ornl").J()
-            np.testing.assert_allclose(
-                J[0], 0.8089189591823078, err_msg='Weight "stellopt_ornl" does not match precalculated value.')
-            surfs = b.bx.compute_surfs
-            self.assertEqual(surfs[0], 49, msg='Surface index is not the expected value. Wrong surface is being used in the calculation.')
 
 
 if __name__ == "__main__":
