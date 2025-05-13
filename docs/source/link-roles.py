af2574d2816bd6368bdb287d166a6c32354fac36
Bharat Medasani <mbkumar@gmail.com>
2025-05-13 19:07:27 -0400

    Fix bug in link-roles

--- docs/source/link-roles.py
+++ docs/source/link-roles.py
@@ -9,8 +9,8 @@ def run_cmd_get_output(cmd):
 
 
 def get_github_rev():
-    path = run_cmd_get_output('git rev-parse --short HEAD')
-    tag = run_cmd_get_output('git describe --exact-match')
+    path = run_cmd_get_output(['git', 'rev-parse', '--short', 'HEAD'])
+    tag = run_cmd_get_output(['git', 'describe', '--exact-match'])
     print('Git commit ID: ', path)
     if len(tag):
         print('Git tag: ', tag)
