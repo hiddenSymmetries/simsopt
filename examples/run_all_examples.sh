612a2acebe1e83597968f733e728a40d3e2edb84
ejpaul <ejp2170@columbia.edu>
2025-06-24 07:15:22 -0700

    Added run all examples slurm script.

---
+++ examples/run_all_examples.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=0:30:00
+#SBATCH --constraint=cpu
+#SBATCH --qos=debug 
+#SBATCH --account=m4680 # Change to your account number
+
+module load python cray-hdf5/1.14.3.1 cray-netcdf/4.9.0.13
+conda activate firm3d # Change to the name of your environment
+srun -n 128 -c 1 --chdir=fusion_distribution python -u fusion_distribution.py
+srun -n 128 -c 1 --chdir=passing_frequencies python -u passing_frequencies.py
+srun -n 128 -c 1 --chdir=passing_map_unperturbed python -u passing_map.py
+srun -n 128 -c 1 --chdir=plot_trajectory python -u plot_trajectory.py
+srun -n 128 -c 1 --chdir=trapped_frequencies python -u trapped_frequencies.py
+srun -n 128 -c 1 --chdir=trapped_map python -u trapped_map.py
+srun -n 128 -c 1 --chdir=trapped_map_QI python -u trapped_map_QI.py
+srun -n 128 -c 1 --chdir=uniform_surf_distribution python -u uniform_surf_distribution.py
+srun -n 128 -c 1 --chdir=uniform_vol_distribution python -u uniform_vol_distribution.py
