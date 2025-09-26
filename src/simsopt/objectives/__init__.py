e381c279816b7205539a17831c24509efe8a104e
Lanke Fu <lf2869@nyu.edu>
2025-09-26 15:56:07 -0400

    Removing quadcoil prototype codes because quadcoil is now a separate code.

--- src/simsopt/objectives/__init__.py
+++ src/simsopt/objectives/__init__.py
@@ -2,12 +2,10 @@ from .constrained import *
 from .fluxobjective import *
 from .least_squares import *
 from .utilities import *
-from .quadcoil_objectives import *
 
 __all__ = (
     fluxobjective.__all__ +
     least_squares.__all__ +
     utilities.__all__ +
-    constrained.__all__ +
-    quadcoil_objectives.__all__
+    constrained.__all__
 )
