b8fda8dab99884fc8921c9da98ad2600d80f1069
Kenneth Hammond <khammond@pppl.gov>
2025-11-20 22:34:38 -0800

    Restore axis limit print statements; keep axis limits unset

--- src/simsopt/geo/wireframe_toroidal.py
+++ src/simsopt/geo/wireframe_toroidal.py
@@ -1639,11 +1639,11 @@ class ToroidalWireframe(object):
         # Call limit getters to avoid errors in certain systems
         #_ = ax.get_xlim()
         #_ = ax.get_ylim()
-        #print('DEBUG: query the plot limits and upstream quantities')
-        #print('    Output of get_xlim(): ', ax.get_xlim())
-        #print('    Output of get_ylim(): ', ax.get_ylim())
-        #print('    delta_x: ', delta_x)
-        #print('    delta_y: ', delta_y)
+        print('DEBUG: query the plot limits and upstream quantities')
+        print('    Output of get_xlim(): ', ax.get_xlim())
+        print('    Output of get_ylim(): ', ax.get_ylim())
+        print('    delta_x: ', delta_x)
+        print('    delta_y: ', delta_y)
  
         ax.set_xlabel(label_x)
         ax.set_ylabel(label_y)
