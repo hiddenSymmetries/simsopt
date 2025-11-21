8bf9b2b335ddabd83c05b92e2066b2990024ec5d
Kenneth Hammond <khammond@pppl.gov>
2025-11-20 21:27:51 -0800

    Restore print statements involving axis limits

--- src/simsopt/geo/wireframe_toroidal.py
+++ src/simsopt/geo/wireframe_toroidal.py
@@ -1640,9 +1640,14 @@ class ToroidalWireframe(object):
         ax.set_ylim((-delta_y, delta_y*(self.n_theta + 1)))
 
         # Call limit getters to avoid errors in certain systems
-        _ = ax.get_xlim()
-        _ = ax.get_ylim()
-
+        #_ = ax.get_xlim()
+        #_ = ax.get_ylim()
+        print('DEBUG: query the plot limits and upstream quantities')
+        print('    Output of get_xlim(): ', ax.get_xlim())
+        print('    Output of get_ylim(): ', ax.get_ylim())
+        print('    delta_x: ', delta_x)
+        print('    delta_y: ', delta_y)
+ 
         ax.set_xlabel(label_x)
         ax.set_ylabel(label_y)
         if add_colorbar:
