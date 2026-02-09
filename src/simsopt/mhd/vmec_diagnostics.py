83143aa971907fc10f4eaa13ca8a0aaffa84f00d
Jake Halpern <jhalpern@purdue.edu>
2026-02-09 13:22:13 -0500

    Updating docstrings after checking them against the built html with Misha's help

--- src/simsopt/mhd/vmec_diagnostics.py
+++ src/simsopt/mhd/vmec_diagnostics.py
@@ -1828,16 +1828,18 @@ def vmec_fieldlines(vs,
           after creating the plot.
 
     Returns:
-        A :class:`VmecGeometryResults` object containing all computed geometric quantities on
-        the :math:`\theta_{pest}` grid. In addition to the standard outputs documented on
-        :class:`VmecGeometryResults`, this function populates the following
+        A :class:`VmecGeometryResults` object containing all computed geometric quantities
+        evaluated along the field lines on the :math:`\theta_{pest}` grid.
+
+        In addition to the standard attributes documented for
+        :class:`VmecGeometryResults`, this function adds the following
         field-line-specific attributes:
-            nalpha: Number of field line labels.
-            nl: Number of grid points along each field line.
-            alpha: 1D array of field line labels used to construct the field lines.
-            theta1d: 1D array of :math:`\theta_{pest}` grid points
-              (only when ``theta1d`` is provided).
-            phi1d: 1D array of :math:`\phi` grid points (only when ``phi1d`` is provided).
+
+        - ``nalpha``: Number of field line labels.
+        - ``nl``: Number of grid points along each field line.
+        - ``alpha``: 1D array of field line labels used to construct the field lines.
+        - ``theta1d``: 1D array of :math:`\theta_{pest}` grid points (only if ``theta1d`` is provided).
+        - ``phi1d``: 1D array of :math:`\phi` grid points (only if ``phi1d`` is provided).
     """
     # If given a Vmec object, convert it to vmec_splines:
     if isinstance(vs, Vmec):
